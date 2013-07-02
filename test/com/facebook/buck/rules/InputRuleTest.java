facebook/buck

commit b592dc1c1c01ffba5b72a3ecae202f7f2f10511c

Add BuildRuleParams#getPathRelativizer().
Summary:
This makes it possible to relativize paths in `AbstractBuildRuleBuilder#getInputs()`.
This will help with implementing new integration tests, as we have places where
we have failed to use a ProjectFilesystem to resolve relative paths. Because our
integration tests run outside of the root directory, this causes problems that today
are observed only in integration tests.

Test Plan: Sandcastle builds.

src/com/facebook/buck/rules/AbstractCachingBuildRule.java
@@ -27,10 +27,12 @@ import com.facebook.buck.util.ProjectFilesystem;
 import com.google.common.annotations.Beta;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Charsets;
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 import com.google.common.util.concurrent.FutureCallback;
@@ -82,6 +84,8 @@ public abstract class AbstractCachingBuildRule extends AbstractBuildRule impleme
    */
   private final SettableFuture<BuildRuleSuccess> buildRuleResult;
 
+  private final Function<String, String> pathRelativizer;
+
   /** @see #getInputsToCompareToOutput(BuildContext) */
   private Iterable<InputRule> inputsToCompareToOutputs;
 
@@ -89,6 +93,7 @@ public abstract class AbstractCachingBuildRule extends AbstractBuildRule impleme
       super(buildRuleParams);
     this.hasBuildStarted = new AtomicBoolean(false);
     this.buildRuleResult = SettableFuture.create();
+    this.pathRelativizer = buildRuleParams.getPathRelativizer();
   }
 
   /**
@@ -124,7 +129,8 @@ public abstract class AbstractCachingBuildRule extends AbstractBuildRule impleme
   @Override
   public Iterable<InputRule> getInputs() {
     if (inputsToCompareToOutputs == null) {
-      inputsToCompareToOutputs = InputRule.inputPathsAsInputRules(getInputsToCompareToOutput());
+      inputsToCompareToOutputs = InputRule.inputPathsAsInputRules(
+          Iterables.transform(getInputsToCompareToOutput(), pathRelativizer));
     }
     return inputsToCompareToOutputs;
   }

commit 451181373fcc697b1b2860f2721564a795145197

Fix a regression in `buck audit owner` by relativizing file paths properly.
Summary:
This regression was introduced in b592dc1c1c01ffba5b72a3ecae202f7f2f10511c.

That diff introduced a change where paths were relativized before being
passed to the constructor of an `InputRule` in `AbstractCachingBuildRule.getInputs()`.
Unfortunately, `AuditOwnerCommand` was still using the `InputRule(String)` constructor
for an `InputRule`, which created an `InputRule` using `new File(String)`, which
means the `File` created for an `InputRule` in `AuditOwnerCommand` did not go through
the same path relativization as those created by an
`AbstractCachingBuildRule.getInputs()`. This mismatch caused the regression in
`buck audit owner`.

This diff eliminates the `InputRule(String)` constructor and forces `InputRule`s to
be created via a static method that requires a `pathRelativizer` as a parameter.
This also introduces `InputRuleTest` that ensures that the `BuildTarget` for an
`InputRule` is composed of only the relative path.

Test Plan: Sandcastle builds.

src/com/facebook/buck/cli/AuditOwnerCommand.java
@@ -24,7 +24,9 @@ import com.facebook.buck.rules.DependencyGraph;
 import com.facebook.buck.rules.InputRule;
 import com.facebook.buck.util.Ansi;
 import com.facebook.buck.util.BuckConstant;
+import com.facebook.buck.util.ProjectFilesystem;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Function;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
@@ -96,23 +98,25 @@ public class AuditOwnerCommand extends AbstractCommandRunner<AuditOwnerOptions> 
   @VisibleForTesting
   OwnersReport generateOwnersReport(DependencyGraph graph, AuditOwnerOptions options) {
 
-    // Process arguments assuming they are all relative file paths
+    // Process arguments assuming they are all relative file paths.
     Set<InputRule> inputs = Sets.newHashSet();
     Set<String> nonExistentInputs = Sets.newHashSet();
     Set<String> nonFileInputs = Sets.newHashSet();
 
+    ProjectFilesystem projectFilesystem = getProjectFilesystem();
+    Function<String, String> pathRelativizer = projectFilesystem.getPathRelativizer();
     for (String filePath : options.getArguments()) {
-      File file = getProjectFilesystem().getFileForRelativePath(filePath);
+      File file = projectFilesystem.getFileForRelativePath(filePath);
       if (!file.exists()) {
         nonExistentInputs.add(filePath);
       } else if (!file.isFile()) {
         nonFileInputs.add(filePath);
       } else {
-        inputs.add(new InputRule(filePath));
+        inputs.add(InputRule.inputPathAsInputRule(filePath, pathRelativizer));
       }
     }
 
-    // Try to find owners for each valid and existing file
+    // Try to find owners for each valid and existing file.
     Set<InputRule> inputsWithNoOwners = Sets.newHashSet(inputs);
     SetMultimap<BuildRule, InputRule> owners = createOwnersMap();
     for (BuildRule rule : graph.getNodes()) {
@@ -124,7 +128,7 @@ public class AuditOwnerCommand extends AbstractCommandRunner<AuditOwnerOptions> 
       }
     }
 
-    // Try to guess owners for nonexistent files
+    // Try to guess owners for nonexistent files.
     if (options.isGuessForDeletedEnabled()) {
       guessOwnersForNonExistentFiles(graph, owners, nonExistentInputs);
     }
@@ -140,8 +144,10 @@ public class AuditOwnerCommand extends AbstractCommandRunner<AuditOwnerOptions> 
   private void guessOwnersForNonExistentFiles(DependencyGraph graph,
       SetMultimap<BuildRule, InputRule> owners, Set<String> nonExistentFiles) {
 
+    ProjectFilesystem projectFilesystem = getProjectFilesystem();
+    Function<String, String> pathRelativizer = projectFilesystem.getPathRelativizer();
     for (String nonExistentFile : nonExistentFiles) {
-      File file = getProjectFilesystem().getFileForRelativePath(nonExistentFile);
+      File file = projectFilesystem.getFileForRelativePath(nonExistentFile);
       File buck = findBuckFileFor(file);
       for (BuildRule rule : graph.getNodes()) {
         if (rule.getType() == BuildRuleType.PROJECT_CONFIG) {
@@ -150,7 +156,7 @@ public class AuditOwnerCommand extends AbstractCommandRunner<AuditOwnerOptions> 
         File ruleBuck = rule.getBuildTarget().getBuildFile();
         try {
           if (buck.getCanonicalFile().equals(ruleBuck.getCanonicalFile())) {
-            owners.put(rule, new InputRule(nonExistentFile));
+            owners.put(rule, InputRule.inputPathAsInputRule(nonExistentFile, pathRelativizer));
           }
         } catch (IOException ex) {
           throw Throwables.propagate(ex);

src/com/facebook/buck/cli/TargetsCommand.java
@@ -28,10 +28,10 @@ import com.facebook.buck.rules.InputRule;
 import com.facebook.buck.util.HumanReadableException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -104,6 +104,7 @@ public class TargetsCommand extends AbstractCommandRunner<TargetsCommandOptions>
         graph.getDependencyGraph(),
         new TargetsCommandPredicate(
             graph,
+            getProjectFilesystem().getPathRelativizer(),
             buildRuleTypesBuilder.build(),
             options.getReferencedFiles(getProjectFilesystem().getProjectRoot()),
             matchingBuildTargets));
@@ -304,6 +305,7 @@ public class TargetsCommand extends AbstractCommandRunner<TargetsCommandOptions>
 
     public TargetsCommandPredicate(
         PartialGraph partialGraph,
+        Function<String, String> pathRelativizer,
         ImmutableSet<BuildRuleType> buildRuleTypes,
         ImmutableSet<String> referencedFiles,
         ImmutableSet<BuildTarget> matchingBuildRules) {
@@ -313,8 +315,7 @@ public class TargetsCommand extends AbstractCommandRunner<TargetsCommandOptions>
 
       Preconditions.checkNotNull(referencedFiles);
       if (!referencedFiles.isEmpty()) {
-        this.referencedInputs = InputRule.inputPathsAsInputRules(
-            ImmutableSortedSet.copyOf(referencedFiles));
+        this.referencedInputs = InputRule.inputPathsAsInputRules(referencedFiles, pathRelativizer);
         BuildFileTree tree = new BuildFileTree(partialGraph.getTargets());
         basePathOfTargets = Sets.newHashSet();
         dependentTargets = Sets.newHashSet();

src/com/facebook/buck/model/BuildTarget.java
@@ -74,12 +74,21 @@ public final class BuildTarget implements Comparable<BuildTarget> {
   /**
    * For use by InputRule, which is synthetic, and therefore has no buildFile.
    * @param inputFile Input file.
+   * @param relativePath
    */
-  public BuildTarget(File inputFile) {
-    this.buildFile = null;
+  private BuildTarget(File inputFile, String relativePath) {
     Preconditions.checkNotNull(inputFile);
-    this.baseName = String.format("//%s", inputFile.getPath());
+    Preconditions.checkNotNull(relativePath);
+    this.buildFile = null;
+    this.baseName = String.format("//%s", relativePath);
     this.shortName = inputFile.getName();
+  }
+
+  /**
+   * For exclusive use by {@link com.facebook.buck.rules.InputRule#InputRule(File, String)}.
+   */
+  public static BuildTarget createBuildTargetForInputFile(File inputFile, String relativePath) {
+    return new BuildTarget(inputFile, relativePath);
   }
 
   /**

src/com/facebook/buck/rules/AbstractCachingBuildRule.java
@@ -32,7 +32,6 @@ import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.eventbus.EventBus;
 import com.google.common.io.Files;
@@ -127,7 +126,7 @@ public abstract class AbstractCachingBuildRule extends AbstractBuildRule impleme
   public Iterable<InputRule> getInputs() {
     if (inputsToCompareToOutputs == null) {
       inputsToCompareToOutputs = InputRule.inputPathsAsInputRules(
-          Iterables.transform(getInputsToCompareToOutput(), pathRelativizer));
+          getInputsToCompareToOutput(), pathRelativizer);
     }
     return inputsToCompareToOutputs;
   }

src/com/facebook/buck/rules/InputRule.java
@@ -19,10 +19,13 @@ package com.facebook.buck.rules;
 import com.facebook.buck.model.BuildTarget;
 import com.facebook.buck.model.BuildTargetPattern;
 import com.facebook.buck.util.ProjectFilesystem;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Iterables;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
@@ -39,37 +42,47 @@ public class InputRule implements BuildRule {
   private static final BuildRuleSuccess.Type SUCCESS_TYPE = BuildRuleSuccess.Type.BY_DEFINITION;
 
   private final File inputFile;
+  private final String relativePath;
   private final BuildTarget buildTarget;
+  private final ListenableFuture<BuildRuleSuccess> buildOutput;
   @Nullable private OutputKey outputKey;
   @Nullable private RuleKey ruleKey;
-  private final ListenableFuture<BuildRuleSuccess> buildOutput;
+
+  /**
+   * It is imperative that {@code inputFile} be properly relativized, so use
+   * {@link #inputPathAsInputRule(String, Function)} to create an {@link InputRule}. The only reason
+   * this method is {@code protected} rather than {@code private} is so that fakes can be created
+   * for testing.
+   */
+  @VisibleForTesting
+  protected InputRule(File inputFile, String relativePath) {
+    this.inputFile = Preconditions.checkNotNull(inputFile);
+    this.relativePath = Preconditions.checkNotNull(relativePath);
+    this.buildTarget = BuildTarget.createBuildTargetForInputFile(inputFile, relativePath);
+    this.buildOutput = Futures.immediateFuture(new BuildRuleSuccess(this, SUCCESS_TYPE));
+  }
+
+  public static InputRule inputPathAsInputRule(String relativePath,
+      Function<String, String> pathRelativizer) {
+    return Iterables.getOnlyElement(
+        inputPathsAsInputRules(ImmutableList.of(relativePath), pathRelativizer));
+  }
 
   /**
    * Convert a set of input file paths to InputRules.
    */
-  public static ImmutableSortedSet<InputRule> inputPathsAsInputRules(Iterable<String> paths) {
+  public static ImmutableSortedSet<InputRule> inputPathsAsInputRules(Iterable<String> paths,
+      Function<String, String> pathRelativizer) {
     ImmutableSortedSet.Builder<InputRule> builder = ImmutableSortedSet.naturalOrder();
     for (String path : paths) {
-      builder.add(new InputRule(path));
+      builder.add(new InputRule(new File(pathRelativizer.apply(path)), path));
     }
     return builder.build();
-  }
-
-  public InputRule(File input) {
-    inputFile = Preconditions.checkNotNull(input);
-    buildTarget = new BuildTarget(input);
-
-    BuildRuleSuccess buildRuleSuccess = new BuildRuleSuccess(this, SUCCESS_TYPE);
-    this.buildOutput = Futures.immediateFuture(buildRuleSuccess);
   }
 
   @Override
   public BuildRuleSuccess.Type getBuildResultType() {
     return SUCCESS_TYPE;
-  }
-
-  public InputRule(String input) {
-    this(new File(input));
   }
 
   @Override
@@ -104,7 +117,7 @@ public class InputRule implements BuildRule {
 
   @Override
   public Iterable<InputRule> getInputs() {
-    return Lists.newArrayList();
+    return ImmutableList.of();
   }
 
   @Override
@@ -134,7 +147,7 @@ public class InputRule implements BuildRule {
 
   @Override
   public String getPathToOutputFile() {
-    return inputFile.getPath();
+    return relativePath;
   }
 
   @Override

test/com/facebook/buck/cli/AuditOwnerCommandTest.java
@@ -341,7 +341,8 @@ public class AuditOwnerCommandTest {
         "com/test/subtest/random.java"
     };
     ImmutableSortedSet<InputRule> inputs
-        = InputRule.inputPathsAsInputRules(ImmutableSortedSet.copyOf(args));
+        = InputRule.inputPathsAsInputRules(ImmutableSortedSet.copyOf(args),
+            filesystem.getPathRelativizer());
 
     // Create options
     AuditOwnerOptions options = getOptions(args);
@@ -378,7 +379,8 @@ public class AuditOwnerCommandTest {
         "com/test/subtest/random.java"
     };
     ImmutableSortedSet<InputRule> inputs
-        = InputRule.inputPathsAsInputRules(ImmutableSortedSet.copyOf(args));
+        = InputRule.inputPathsAsInputRules(ImmutableSortedSet.copyOf(args),
+            filesystem.getPathRelativizer());
 
     // Build rule that owns all inputs
     BuildTarget target = BuildTargetFactory.newInstance("//base/name", "name");
@@ -428,11 +430,12 @@ public class AuditOwnerCommandTest {
         "com/test/subtest/random.java"
     };
     ImmutableSortedSet<InputRule> inputs
-        = InputRule.inputPathsAsInputRules(ImmutableSortedSet.copyOf(args));
+        = InputRule.inputPathsAsInputRules(ImmutableSortedSet.copyOf(args),
+            filesystem.getPathRelativizer());
 
     // Build rule that owns all inputs
-    BuildTarget target1 = BuildTargetFactory.newInstance("//base/name1", "name1");
-    BuildTarget target2 = BuildTargetFactory.newInstance("//base/name2", "name2");
+    BuildTarget target1 = BuildTargetFactory.newInstance("//base/name1:name1");
+    BuildTarget target2 = BuildTargetFactory.newInstance("//base/name2:name2");
     BuildRule owner1Rule = new StubBuildRule(target1, inputs);
     BuildRule owner2Rule = new StubBuildRule(target2, inputs);
 

test/com/facebook/buck/cli/TargetsCommandTest.java
@@ -53,6 +53,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.base.Function;
+import com.google.common.base.Functions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
@@ -75,6 +76,7 @@ import java.util.SortedMap;
 
 public class TargetsCommandTest {
 
+  private static final Function<String, String> PATH_RELATIVIZER = Functions.identity();
   private final String projectRootPath = ".";
   private final File projectRoot = new File(projectRootPath);
   private TestConsole console;
@@ -305,7 +307,7 @@ public class TargetsCommandTest {
     SortedMap<String, BuildRule> matchingBuildRules =
         targetsCommand.getMatchingBuildRules(
             graph.getDependencyGraph(),
-            new TargetsCommandPredicate(graph, buildRuleTypes, referencedFiles, targetBuildRules));
+            new TargetsCommandPredicate(graph, PATH_RELATIVIZER, buildRuleTypes, referencedFiles, targetBuildRules));
     assertTrue(matchingBuildRules.isEmpty());
 
     // Only test-android-library target depends on the referenced file.
@@ -313,7 +315,7 @@ public class TargetsCommandTest {
     matchingBuildRules =
         targetsCommand.getMatchingBuildRules(
             graph.getDependencyGraph(),
-            new TargetsCommandPredicate(graph, buildRuleTypes, referencedFiles, targetBuildRules));
+            new TargetsCommandPredicate(graph, PATH_RELATIVIZER, buildRuleTypes, referencedFiles, targetBuildRules));
     assertEquals(
         ImmutableSet.of("//javatest:test-java-library"),
         matchingBuildRules.keySet());
@@ -324,7 +326,7 @@ public class TargetsCommandTest {
     matchingBuildRules =
         targetsCommand.getMatchingBuildRules(
             graph.getDependencyGraph(),
-            new TargetsCommandPredicate(graph, buildRuleTypes, referencedFiles, targetBuildRules));
+            new TargetsCommandPredicate(graph, PATH_RELATIVIZER, buildRuleTypes, referencedFiles, targetBuildRules));
     assertEquals(
         ImmutableSet.of("//javatest:test-java-library", "//javasrc:java-library"),
         matchingBuildRules.keySet());
@@ -335,7 +337,7 @@ public class TargetsCommandTest {
     matchingBuildRules =
         targetsCommand.getMatchingBuildRules(
             graph.getDependencyGraph(),
-            new TargetsCommandPredicate(graph, buildRuleTypes, referencedFiles, targetBuildRules));
+            new TargetsCommandPredicate(graph, PATH_RELATIVIZER, buildRuleTypes, referencedFiles, targetBuildRules));
     assertEquals(
         ImmutableSet.of("//javatest:test-java-library"),
         matchingBuildRules.keySet());
@@ -346,6 +348,7 @@ public class TargetsCommandTest {
         targetsCommand.getMatchingBuildRules(
             graph.getDependencyGraph(),
             new TargetsCommandPredicate(graph,
+                PATH_RELATIVIZER,
                 buildRuleTypes,
                 ImmutableSet.<String>of(),
                 targetBuildRules));
@@ -362,6 +365,7 @@ public class TargetsCommandTest {
             graph.getDependencyGraph(),
             new TargetsCommandPredicate(
                 graph,
+                PATH_RELATIVIZER,
                 ImmutableSet.of(BuildRuleType.JAVA_TEST, BuildRuleType.JAVA_LIBRARY),
                 ImmutableSet.<String>of(),
                 targetBuildRules));
@@ -378,6 +382,7 @@ public class TargetsCommandTest {
             graph.getDependencyGraph(),
             new TargetsCommandPredicate(
                 graph,
+                PATH_RELATIVIZER,
                 ImmutableSet.of(BuildRuleType.JAVA_TEST, BuildRuleType.JAVA_LIBRARY),
                 ImmutableSet.<String>of(),
                 ImmutableSet.of(BuildTargetFactory.newInstance("//javasrc:java-library"))));
@@ -390,6 +395,7 @@ public class TargetsCommandTest {
             graph.getDependencyGraph(),
             new TargetsCommandPredicate(
                 graph,
+                PATH_RELATIVIZER,
                 ImmutableSet.<BuildRuleType>of(),
                 ImmutableSet.<String>of(),
                 ImmutableSet.of(BuildTargetFactory.newInstance("//javasrc:java-library"))));
@@ -403,6 +409,7 @@ public class TargetsCommandTest {
             graph.getDependencyGraph(),
             new TargetsCommandPredicate(
                 graph,
+                PATH_RELATIVIZER,
                 ImmutableSet.<BuildRuleType>of(),
                 ImmutableSet.of("javatest/TestJavaLibrary.java"),
                 ImmutableSet.of(BuildTargetFactory.newInstance("//javasrc:java-library"))));

test/com/facebook/buck/rules/AbstractCachingBuildRuleTest.java
@@ -101,12 +101,8 @@ public class AbstractCachingBuildRuleTest extends EasyMockSupport {
     AbstractCachingBuildRule cachingRule = createRule(
         ImmutableSet.of(dep),
         visibilityPatterns,
-        ImmutableList.<InputRule>of(new InputRule("/dev/null") {
-          @Override
-          public RuleKey getRuleKey() {
-            return new RuleKey("ae8c0f860a0ecad94ecede79b69460434eddbfbc");
-          }
-        }),
+        ImmutableList.<InputRule>of(FakeInputRule.createWithRuleKey("/dev/null",
+            new RuleKey("ae8c0f860a0ecad94ecede79b69460434eddbfbc"))),
         buildSteps,
         /* ruleKeyOnDisk */ Optional.<RuleKey>absent(),
         output);

test/com/facebook/buck/rules/BUCK
@@ -3,6 +3,7 @@ java_library(
   srcs = [
     'FakeAbstractBuildRuleBuilderParams.java',
     'FakeBuildRule.java',
+    'FakeInputRule.java',
     'FakeTestRule.java',
     'RetainOrderComparator.java',
   ],

test/com/facebook/buck/rules/DirArtifactCacheTest.java
@@ -48,7 +48,7 @@ public class DirArtifactCacheTest {
     DirArtifactCache dirArtifactCache = new DirArtifactCache(cacheDir);
 
     Files.write("x", fileX, Charsets.UTF_8);
-    InputRule inputRuleX = new InputRule(fileX);
+    InputRule inputRuleX = new InputRuleForTest(fileX);
     RuleKey ruleKeyX = RuleKey.builder(inputRuleX).build();
 
     assertFalse(dirArtifactCache.fetch(ruleKeyX, fileX));
@@ -62,19 +62,19 @@ public class DirArtifactCacheTest {
     DirArtifactCache dirArtifactCache = new DirArtifactCache(cacheDir);
 
     Files.write("x", fileX, Charsets.UTF_8);
-    InputRule inputRuleX = new InputRule(fileX);
+    InputRule inputRuleX = new InputRuleForTest(fileX);
     RuleKey ruleKeyX = RuleKey.builder(inputRuleX).build();
 
     dirArtifactCache.store(ruleKeyX, fileX);
 
     // Test that artifact overwrite works.
     assertTrue(dirArtifactCache.fetch(ruleKeyX, fileX));
-    assertEquals(inputRuleX, new InputRule(fileX));
+    assertEquals(inputRuleX, new InputRuleForTest(fileX));
 
     // Test that artifact creation works.
     assertTrue(fileX.delete());
     assertTrue(dirArtifactCache.fetch(ruleKeyX, fileX));
-    assertEquals(inputRuleX, new InputRule(fileX));
+    assertEquals(inputRuleX, new InputRuleForTest(fileX));
   }
 
   @Test
@@ -85,14 +85,14 @@ public class DirArtifactCacheTest {
     DirArtifactCache dirArtifactCache = new DirArtifactCache(cacheDir);
 
     Files.write("x", fileX, Charsets.UTF_8);
-    InputRule inputRuleX = new InputRule(fileX);
+    InputRule inputRuleX = new InputRuleForTest(fileX);
     RuleKey ruleKeyX = RuleKey.builder(inputRuleX).build();
 
     dirArtifactCache.store(ruleKeyX, fileX);
     dirArtifactCache.store(ruleKeyX, fileX); // Overwrite.
 
     assertTrue(dirArtifactCache.fetch(ruleKeyX, fileX));
-    assertEquals(inputRuleX, new InputRule(fileX));
+    assertEquals(inputRuleX, new InputRuleForTest(fileX));
   }
 
   @Test
@@ -108,9 +108,9 @@ public class DirArtifactCacheTest {
     Files.write("y", fileY, Charsets.UTF_8);
     Files.write("z", fileZ, Charsets.UTF_8);
 
-    InputRule inputRuleX = new InputRule(fileX);
-    InputRule inputRuleY = new InputRule(fileY);
-    InputRule inputRuleZ = new InputRule(fileZ);
+    InputRule inputRuleX = new InputRuleForTest(fileX);
+    InputRule inputRuleY = new InputRuleForTest(fileY);
+    InputRule inputRuleZ = new InputRuleForTest(fileZ);
     assertFalse(inputRuleX.equals(inputRuleY));
     assertFalse(inputRuleX.equals(inputRuleZ));
     assertFalse(inputRuleY.equals(inputRuleZ));
@@ -135,8 +135,14 @@ public class DirArtifactCacheTest {
     assertTrue(dirArtifactCache.fetch(ruleKeyY, fileY));
     assertTrue(dirArtifactCache.fetch(ruleKeyZ, fileZ));
 
-    assertEquals(inputRuleX, new InputRule(fileX));
-    assertEquals(inputRuleY, new InputRule(fileY));
-    assertEquals(inputRuleZ, new InputRule(fileZ));
+    assertEquals(inputRuleX, new InputRuleForTest(fileX));
+    assertEquals(inputRuleY, new InputRuleForTest(fileY));
+    assertEquals(inputRuleZ, new InputRuleForTest(fileZ));
+  }
+
+  private static class InputRuleForTest extends InputRule {
+    private InputRuleForTest(File file) {
+      super(file, file.getPath());
+    }
   }
 }

test/com/facebook/buck/rules/FakeInputRule.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright 2013-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.rules;
+
+import com.google.common.base.Preconditions;
+
+import java.io.File;
+
+/**
+ * An {@link InputRule} that uses the {@link RuleKey} passed to its constructor rather than
+ * computing the value from the underlying file contents.
+ */
+public class FakeInputRule extends InputRule {
+
+  private final RuleKey customRuleKey;
+
+  private FakeInputRule(File inputFile, RuleKey customRuleKey) {
+    super(inputFile, inputFile.getPath());
+    this.customRuleKey = Preconditions.checkNotNull(customRuleKey);
+  }
+
+  @Override
+  public RuleKey getRuleKey() {
+    return customRuleKey;
+  }
+
+  public static InputRule createWithRuleKey(String absolutePath, RuleKey customRuleKey) {
+    return new FakeInputRule(new File(absolutePath), Preconditions.checkNotNull(customRuleKey));
+  }
+}

test/com/facebook/buck/rules/InputRuleTest.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2012-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.rules;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.facebook.buck.util.ProjectFilesystem;
+import com.google.common.collect.Iterables;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+public class InputRuleTest {
+
+  private InputRule inputRule;
+
+  @Before
+  public void setUp() {
+    ProjectFilesystem projectFilesystem = new ProjectFilesystem(new File("."));
+    inputRule = InputRule.inputPathAsInputRule("src/com/facebook/buck/cli/Main.java",
+        projectFilesystem.getPathRelativizer());
+  }
+
+  @Test
+  public void testBuildTargetForInputRule() {
+    assertEquals("//src/com/facebook/buck/cli/Main.java:Main.java",
+        inputRule.getBuildTarget().getFullyQualifiedName());
+  }
+
+  @Test
+  public void testGetPathToOutputFileIsRelative() {
+    assertEquals("src/com/facebook/buck/cli/Main.java", inputRule.getPathToOutputFile());
+  }
+
+  @Test
+  public void testGetInputsIsEmpty() {
+    assertTrue("Even though inputRule is itself an InputRule, its getInputs() should be empty.",
+        Iterables.isEmpty(inputRule.getInputs()));
+  }
+
+  @Test
+  public void testGetBuildResultTypeIsByDefinition() {
+    assertEquals(BuildRuleSuccess.Type.BY_DEFINITION, inputRule.getBuildResultType());
+  }
+
+  @Test
+  public void testToStringIsFullyQualifiedName() {
+    assertEquals(inputRule.getFullyQualifiedName(), inputRule.toString());
+  }
+}

test/com/facebook/buck/rules/MultiArtifactCacheTest.java
@@ -29,7 +29,8 @@ import java.io.File;
 import javax.annotation.Nullable;
 
 public class MultiArtifactCacheTest {
-  private static final RuleKey dummyRuleKey = RuleKey.builder(new InputRule(new File(""))).build();
+  private static final RuleKey dummyRuleKey =
+      new RuleKey("76b1c1beae69428db2d1befb31cf743ac8ce90df");
   private static final File dummyFile = new File("dummy");
 
   class DummyArtifactCache implements ArtifactCache {