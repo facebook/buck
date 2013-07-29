76e0e57e5ab2a756a14d7caa25178d4940c97ec2
Michael Bolin <mbolin@fb.com>
2013-07-29 16:57:10 -0700

    Fix a bug in Genrule: order of srcs matters.
    Summary:
    The order of `this.srcs` matters because it affects
    the creation of the `$SRCS` environment variable when
    executing the genrule. Users should be able to depend
    on this order.
    
    Test Plan: Sandcastle builds.

--- src/com/facebook/buck/shell/Genrule.java
+++ src/com/facebook/buck/shell/Genrule.java
@@ -111,7 +111,7 @@ public class Genrule extends AbstractCachingBuildRule {
   /**
    * The order in which elements are specified in the {@code srcs} attribute of a genrule matters.
    */
-  protected final ImmutableSortedSet<String> srcs;
+  protected final ImmutableList<String> srcs;
 
   protected final String cmd;
 
@@ -132,7 +132,7 @@ public class Genrule extends AbstractCachingBuildRule {
       String out,
       Function<String, String> relativeToAbsolutePathFunction) {
     super(buildRuleParams);
-    this.srcs = ImmutableSortedSet.<String>naturalOrder().addAll(srcs).build();
+    this.srcs = ImmutableList.copyOf(srcs);
     this.cmd = Preconditions.checkNotNull(cmd);
     this.srcsToAbsolutePaths = Maps.toMap(srcs, relativeToAbsolutePathFunction);
 
@@ -171,7 +171,7 @@ public class Genrule extends AbstractCachingBuildRule {
 
   @Override
   protected ImmutableSortedSet<String> getInputsToCompareToOutput() {
-    return srcs;
+    return ImmutableSortedSet.copyOf(srcs);
   }
 
   @Override
