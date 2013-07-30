4bed69f8ee5fc40cd0d20a560800b5ad744166cc
Roy Williams <royw@fb.com>
2013-07-29 17:52:54 -0700

    Synchronize all printing to the DirtyPrintStreamDecorator to prevent interlacing of output.
    Test Plan: Unit tests

--- src/com/facebook/buck/util/DirtyPrintStreamDecorator.java
+++ src/com/facebook/buck/util/DirtyPrintStreamDecorator.java
@@ -19,7 +19,6 @@ package com.facebook.buck.util;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Locale;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Decorator of PrintStreams that tracks whether or not that stream has been written to.  This is
@@ -28,193 +27,193 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class DirtyPrintStreamDecorator extends PrintStream {
   private final PrintStream delegate;
-  private final AtomicBoolean dirty;
+  private volatile boolean dirty;
 
   public DirtyPrintStreamDecorator(PrintStream delegate) {
     super(delegate);
     this.delegate = delegate;
-    this.dirty = new AtomicBoolean(false);
+    this.dirty = false;
   }
 
-  public PrintStream getRawStream() {
+  public synchronized PrintStream getRawStream() {
     return delegate;
   }
 
-  public boolean isDirty() {
-    return dirty.get();
+  public synchronized boolean isDirty() {
+    return dirty;
   }
 
   @Override
-  public void write(int b) {
-    dirty.set(true);
+  public synchronized void write(int b) {
+    dirty = true;
     super.write(b);
   }
 
   @Override
-  public void write(byte[] buf, int off, int len) {
-    dirty.set(true);
+  public synchronized void write(byte[] buf, int off, int len) {
+    dirty = true;
     super.write(buf, off, len);
   }
 
   @Override
-  public void print(boolean b) {
-    dirty.set(true);
+  public synchronized void print(boolean b) {
+    dirty = true;
     super.print(b);
   }
 
   @Override
-  public void print(char c) {
-    dirty.set(true);
+  public synchronized void print(char c) {
+    dirty = true;
     super.print(c);
   }
 
   @Override
-  public void print(int i) {
-    dirty.set(true);
+  public synchronized void print(int i) {
+    dirty = true;
     super.print(i);
   }
 
   @Override
-  public void print(long l) {
-    dirty.set(true);
+  public synchronized void print(long l) {
+    dirty = true;
     super.print(l);
   }
 
   @Override
-  public void print(float f) {
-    dirty.set(true);
+  public synchronized void print(float f) {
+    dirty = true;
     super.print(f);
   }
 
   @Override
-  public void print(double d) {
-    dirty.set(true);
+  public synchronized void print(double d) {
+    dirty = true;
     super.print(d);
   }
 
   @Override
-  public void print(char[] s) {
-    dirty.set(true);
+  public synchronized void print(char[] s) {
+    dirty = true;
     super.print(s);
   }
 
   @Override
-  public void print(String s) {
-    dirty.set(true);
+  public synchronized void print(String s) {
+    dirty = true;
     super.print(s);
   }
 
   @Override
-  public void print(Object obj) {
-    dirty.set(true);
+  public synchronized void print(Object obj) {
+    dirty = true;
     super.print(obj);
   }
 
   @Override
-  public void println() {
-    dirty.set(true);
+  public synchronized void println() {
+    dirty = true;
     super.println();
   }
 
   @Override
-  public void println(boolean x) {
-    dirty.set(true);
+  public synchronized void println(boolean x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(char x) {
-    dirty.set(true);
+  public synchronized void println(char x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(int x) {
-    dirty.set(true);
+  public synchronized void println(int x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(long x) {
-    dirty.set(true);
+  public synchronized void println(long x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(float x) {
-    dirty.set(true);
+  public synchronized void println(float x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(double x) {
-    dirty.set(true);
+  public synchronized void println(double x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(char[] x) {
-    dirty.set(true);
+  public synchronized void println(char[] x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(String x) {
-    dirty.set(true);
+  public synchronized void println(String x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public void println(Object x) {
-    dirty.set(true);
+  public synchronized void println(Object x) {
+    dirty = true;
     super.println(x);
   }
 
   @Override
-  public PrintStream printf(String format, Object... args) {
-    dirty.set(true);
+  public synchronized PrintStream printf(String format, Object... args) {
+    dirty = true;
     return super.printf(format, args);
   }
 
   @Override
-  public PrintStream printf(Locale l, String format, Object... args) {
-    dirty.set(true);
+  public synchronized PrintStream printf(Locale l, String format, Object... args) {
+    dirty = true;
     return super.printf(l, format, args);
   }
 
   @Override
-  public PrintStream format(String format, Object... args) {
-    dirty.set(true);
+  public synchronized PrintStream format(String format, Object... args) {
+    dirty = true;
     return super.format(format, args);
   }
 
   @Override
-  public PrintStream format(Locale l, String format, Object... args) {
-    dirty.set(true);
+  public synchronized PrintStream format(Locale l, String format, Object... args) {
+    dirty = true;
     return super.format(l, format, args);
   }
 
   @Override
-  public PrintStream append(CharSequence csq) {
-    dirty.set(true);
+  public synchronized PrintStream append(CharSequence csq) {
+    dirty = true;
     return super.append(csq);
   }
 
   @Override
-  public PrintStream append(CharSequence csq, int start, int end) {
-    dirty.set(true);
+  public synchronized PrintStream append(CharSequence csq, int start, int end) {
+    dirty = true;
     return super.append(csq, start, end);
   }
 
   @Override
-  public PrintStream append(char c) {
-    dirty.set(true);
+  public synchronized PrintStream append(char c) {
+    dirty = true;
     return super.append(c);
   }
 
   @Override
-  public void write(byte[] b) throws IOException {
-    dirty.set(true);
+  public synchronized void write(byte[] b) throws IOException {
+    dirty = true;
     super.write(b);
   }
 }
