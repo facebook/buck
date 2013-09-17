ff4723894d96eb458f072fe3850262c83a25c5f8
Michael Bolin <mbolin@fb.com>
2013-09-16 20:27:06 -0700

    Introduce SymlinkFilesIntoDirectoryStep.
    Summary: This is needed for upcoming work in `android_binary`.
    
    Test Plan: Sandcastle builds.

---
+++ src/com/facebook/buck/shell/SymlinkFilesIntoDirectoryStep.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2013-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.shell;
+
+import com.facebook.buck.event.ThrowableLogEvent;
+import com.facebook.buck.step.AbstractExecutionStep;
+import com.facebook.buck.step.ExecutionContext;
+import com.facebook.buck.step.Step;
+import com.facebook.buck.util.ProjectFilesystem;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSet;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+
+/**
+ * {@link Step} that takes a collection of entries in a directory and creates a set of read-only
+ * symlinks (with the same structure) to the original entries in a new directory.
+ */
+public class SymlinkFilesIntoDirectoryStep extends AbstractExecutionStep {
+
+  private final Path srcDir;
+  private final ImmutableSet<String> entries;
+  private final Path outDir;
+
+  /**
+   * @param srcDir relative to the project root that contains the {@code entries}.
+   * @param entries that exist in {@code srcDir}.
+   * @param outDir relative to the project root where the symlinks will be created.
+   */
+  public SymlinkFilesIntoDirectoryStep(Path srcDir, Set<String> entries, Path outDir) {
+    super("symlinking files into " + outDir);
+    this.srcDir = Preconditions.checkNotNull(srcDir);
+    this.entries = ImmutableSet.copyOf(entries);
+    this.outDir = Preconditions.checkNotNull(outDir);
+  }
+
+  @Override
+  public int execute(ExecutionContext context) {
+    // Note that because these paths are resolved to absolute paths, the symlinks will be absolute
+    // paths, as well.
+    ProjectFilesystem projectFilesystem = context.getProjectFilesystem();
+    Path outDir = projectFilesystem.resolve(this.outDir);
+    Path srcDir = projectFilesystem.resolve(this.srcDir);
+
+    for (String entry : entries) {
+      Path link = outDir.resolve(entry);
+      Path target = srcDir.resolve(entry);
+      try {
+        Files.createDirectories(link.getParent());
+        Files.createSymbolicLink(link, target);
+      } catch (IOException e) {
+        context.getBuckEventBus().post(ThrowableLogEvent.create(e,
+            "Failed to create symlink from %s to %s.",
+            link,
+            target));
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+}
---
+++ test/com/facebook/buck/shell/SymlinkFilesIntoDirectoryStepIntegrationTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.shell;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.facebook.buck.step.ExecutionContext;
+import com.facebook.buck.step.TestExecutionContext;
+import com.facebook.buck.testutil.integration.DebuggableTemporaryFolder;
+import com.facebook.buck.testutil.integration.ProjectWorkspace;
+import com.facebook.buck.testutil.integration.TestDataHelper;
+import com.facebook.buck.util.ProjectFilesystem;
+import com.google.common.collect.ImmutableSet;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SymlinkFilesIntoDirectoryStepIntegrationTest {
+
+  @Rule
+  public DebuggableTemporaryFolder tmp = new DebuggableTemporaryFolder();
+
+  /**
+   * Verifies that {@link SymlinkFilesIntoDirectoryStep} works correctly by symlinking files at
+   * various depth levels into an empty output directory.
+   */
+  @Test
+  public void testSymlinkFilesIntoDirectory() throws IOException {
+    ProjectWorkspace workspace = TestDataHelper.createProjectWorkspaceForScenario(
+        this, "symlink_files_into_directory", tmp);
+    workspace.setUp();
+
+    File outputFolder = tmp.newFolder("output");
+
+    ProjectFilesystem projectFilesystem = new ProjectFilesystem(tmp.getRoot());
+    ExecutionContext executionContext = TestExecutionContext
+        .newBuilder()
+        .setProjectFilesystem(projectFilesystem)
+        .build();
+    SymlinkFilesIntoDirectoryStep symlinkStep = new SymlinkFilesIntoDirectoryStep(
+        tmp.getRoot().toPath(),
+        ImmutableSet.of("a.txt", "foo/b.txt", "foo/bar/c.txt"),
+        outputFolder.toPath());
+    int exitCode = symlinkStep.execute(executionContext);
+    assertEquals(0, exitCode);
+
+    Path symlinkToADotTxt = new File(tmp.getRoot(), "output/a.txt").toPath();
+    assertTrue(Files.isSymbolicLink(symlinkToADotTxt));
+    assertEquals(projectFilesystem.resolve(Paths.get("a.txt")),
+        Files.readSymbolicLink(symlinkToADotTxt));
+
+    Path symlinkToBDotTxt = new File(tmp.getRoot(), "output/foo/b.txt").toPath();
+    assertTrue(Files.isSymbolicLink(symlinkToBDotTxt));
+    assertEquals(projectFilesystem.resolve(Paths.get("foo/b.txt")),
+        Files.readSymbolicLink(symlinkToBDotTxt));
+
+    Path symlinkToCDotTxt = new File(tmp.getRoot(), "output/foo/bar/c.txt").toPath();
+    assertTrue(Files.isSymbolicLink(symlinkToCDotTxt));
+    assertEquals(projectFilesystem.resolve(Paths.get("foo/bar/c.txt")),
+        Files.readSymbolicLink(symlinkToCDotTxt));
+  }
+}
