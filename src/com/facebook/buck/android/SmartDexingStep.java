6324ea911d7696ba33f8ad5bf0811b755ac2620e
Michael Bolin <mbolin@fb.com>
2013-12-05 18:40:05 -0800

    Fix a suffix check bug.

--- src/com/facebook/buck/android/SmartDexingStep.java
+++ src/com/facebook/buck/android/SmartDexingStep.java
@@ -474,7 +474,8 @@ public class SmartDexingStep implements Step {
       steps.add(new RmStep(tempDexJarOutput, true));
       steps.add(new XzStep(repackedJar));
       return new CompositeStep(steps);
-    } else if (outputPath.endsWith(DexStore.JAR.getExtension())) {
+    } else if (outputPath.endsWith(DexStore.JAR.getExtension()) ||
+        outputPath.endsWith("classes.dex")) {
       return new DxStep(outputPath, filesToDex, dxOptions);
     } else {
       throw new IllegalArgumentException(String.format(
