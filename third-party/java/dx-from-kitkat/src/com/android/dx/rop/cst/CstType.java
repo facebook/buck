2924fc30cb930d387f941c43c26c02cf5254248b
David Reiss <dreiss@fb.com>
2014-05-21 11:53:13 -0700

    Use descriptor instead of Type for DX's CstType intern map
    Summary:
    This should be safe because Type already uses its descriptor for its
    .equals() method, so the mapping should be identical.  (And,
    semantically, a Type is just a parsed descriptor.)
    
    This is useful because it means this table no longer holds a hard
    reference to any Type in its key, so an upcoming diff can make the map
    use weak values without worrying about weak keys.
    
    Test Plan: ant dx-tests

--- third-party/java/dx-from-kitkat/src/com/android/dx/rop/cst/CstType.java
+++ third-party/java/dx-from-kitkat/src/com/android/dx/rop/cst/CstType.java
@@ -24,8 +24,8 @@ import java.util.HashMap;
  */
 public final class CstType extends TypedConstant {
     /** {@code non-null;} map of interned types */
-    private static final HashMap<Type, CstType> interns =
-        new HashMap<Type, CstType>(100);
+    private static final HashMap<String, CstType> interns =
+        new HashMap<String, CstType>(100);
 
     /** {@code non-null;} instance corresponding to the class {@code Object} */
     public static final CstType OBJECT = intern(Type.OBJECT);
@@ -123,11 +123,11 @@ public final class CstType extends TypedConstant {
      */
     public static CstType intern(Type type) {
         synchronized (interns) {
-            CstType cst = interns.get(type);
+            CstType cst = interns.get(type.getDescriptor());
 
             if (cst == null) {
                 cst = new CstType(type);
-                interns.put(type, cst);
+                interns.put(type.getDescriptor(), cst);
             }
 
             return cst;
