ad626264d8b4efb829675f787acdc947a01d3171
Edward Speyer <edward@fb.com>
2014-05-22 06:22:57 -0700

    Test that exacerbates the NoTestsRemainException bug
    Summary: A long standing issue with test filtering is that if you are
    using a filter in JUnit and you encounter a class without any tests, an
    exception is raised where one wouldn't normally be raised if filtering
    was turned off.  This unit test will catch any regressions.
    
    Test Plan: Run it!

---
+++ test/com/facebook/buck/junit/testdata/test_selectors_testless_classes/test/ClassWithTestsA.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+
+public class ClassWithTestsA {
+
+  @Test
+  public void testOne() {
+    // empty
+  }
+
+  @Test
+  public void testTwo() {
+    // empty
+  }
+}
---
+++ test/com/facebook/buck/junit/testdata/test_selectors_testless_classes/test/ClassWithTestsB.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+
+public class ClassWithTestsB {
+
+  @Test
+  public void testOne() {
+    // empty
+  }
+
+  @Test
+  public void testTwo() {
+    // empty
+  }
+}
