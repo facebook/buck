acbb190171fa52bda89002b2ed494c3cb25f9ddb
dominik <dominik@fb.com>
2014-06-03 08:54:00 -0700

    Using fake javac in external javac with zip test
    Summary:
    instead of using the systems javac in the unit test we use a fake javac
    that fails when it gets a zip file passed.
    
    Test Plan: ##buck test --all##

--- test/com/facebook/buck/java/testdata/external_javac_src_zip/javac.sh
+++ test/com/facebook/buck/java/testdata/external_javac_src_zip/javac.sh
@@ -1,3 +1,30 @@
 #!/bin/sh -x
 
-javac "$@"
+for i in "$@"; do
+    case $i in
+        -d)
+            CLASS_OUT="$2"
+            shift
+            ;;
+        -Abuck*)
+            ABI_OUT="${1#*=}"
+            shift
+            ;;
+        -version)
+            echo "fakeJavac" 1>&2
+            exit 0
+            ;;
+        *)
+            if [[ $i == *.zip ]]; then
+                exit 1
+            fi
+            shift
+            ;;
+    esac
+done
+
+echo $CLASS_OUT >> log
+echo $ABI_OUT >> log
+
+echo "fakeClass" > "$CLASS_OUT/Example.class"
+echo "0000000000000000000000000000000000000000" > $ABI_OUT
