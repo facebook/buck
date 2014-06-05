a189717f135c5bc6324508fd82436635d452ec0c
Natthu Bharambe <natthu@fb.com>
2014-06-05 16:33:31 -0700

    Look for aapt in platform specific directories.
    Summary:
    The buckconfig value should point to a directory that contains the aapt
    binaries in platform specific sub-directories (linux/mac/windows).
    
    Test Plan:
    In fbandroid:
    $ mkdir -p aapt/linux
    $ ln -s /home/natthu/devtools/aosp/out/host/linux-x86/bin/aapt aapt/linux/aapt
    
    Edit .buckconfig tools section
    ```
    [tools]
        proguard = proguard/proguard-fork.jar
        aapt = aapt
    ```
    
    $ buck build instagram
    
    Inspect chrome trace and verify overridden aapt is used.

--- src/com/facebook/buck/cli/BuckConfig.java
+++ src/com/facebook/buck/cli/BuckConfig.java
@@ -793,15 +793,28 @@ public class BuckConfig {
   }
 
   /**
-   * Returns the path to the aapt executable that is overridden by the current project. If not
-   * specified, the Android platform aapt will be used.
+   * Returns the path to the platform specific aapt executable that is overridden by the current
+   * project. If not specified, the Android platform aapt will be used.
    */
   public Optional<Path> getAaptOverride() {
     Optional<String> pathString = getValue("tools", "aapt");
-    if (pathString.isPresent()) {
-      return checkPathExists(pathString.get(), "Overridden aapt path not found: ");
+    if (!pathString.isPresent()) {
+      return Optional.absent();
     }
-    return Optional.absent();
+
+    String platformDir;
+    if (platform == Platform.LINUX) {
+      platformDir = "linux";
+    } else if (platform == Platform.MACOS) {
+      platformDir = "mac";
+    } else if (platform == Platform.WINDOWS) {
+      platformDir = "windows";
+    } else {
+      return Optional.absent();
+    }
+
+    Path pathToAapt = Paths.get(pathString.get(), platformDir, "aapt");
+    return checkPathExists(pathToAapt.toString(), "Overridden aapt path not found: ");
   }
 
   public Optional<Path> checkPathExists(String pathString, String errorMsg) {
