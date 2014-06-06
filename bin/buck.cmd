29d8f1b064057899ba00887fd406acea5d3a4913
Andrew Gallagher <agallagher@fb.com>
2014-06-05 17:02:31 -0700

    Add Apache's commons-compress library to third-party
    Summary:
    This provides a zip archive extractor implementation which supports
    some features that the built in one does not, namely support for
    external attributes so we can preserve execute permissions in the
    artifact cache.
    
    (Note: this will and should *not* be used to replace our internal
    implemenation for creating the zip archive).
    
    Test Plan:
    Ran builds with subsequent diffs that use this implemenatation in
    `Unzip`.

--- bin/buck.cmd
+++ bin/buck.cmd
@@ -125,7 +125,8 @@ set BUCK_JAVA_CLASSPATH=^
 %BUCK_DIRECTORY%\third-party\java\gson\gson-2.2.4.jar;^
 %BUCK_DIRECTORY%\third-party\java\jetty\jetty-all-9.0.4.v20130625.jar;^
 %BUCK_DIRECTORY%\third-party\java\jetty\servlet-api.jar;^
-%BUCK_DIRECTORY%\third-party\java\xz-java-1.3\xz-1.3.jar
+%BUCK_DIRECTORY%\third-party\java\xz-java-1.3\xz-1.3.jar;^
+%BUCK_DIRECTORY%\third-party\java\commons-compress\commons-compress-1.8.1.jar
 
 "%JAVA%" ^
   -classpath %BUCK_JAVA_CLASSPATH% ^
