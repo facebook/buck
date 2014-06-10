fd76c8e833f14566472d51eca4383aa7aa90b504
Natthu Bharambe <natthu@fb.com>
2014-06-10 14:31:03 -0700

    Fix checkstyle errors.
    Test Plan: ant lint

--- src/com/facebook/buck/python/PythonPackageComponents.java
+++ src/com/facebook/buck/python/PythonPackageComponents.java
@@ -70,18 +70,26 @@ public class PythonPackageComponents {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
 
     PythonPackageComponents that = (PythonPackageComponents) o;
 
-    if (modules != null ? !modules.equals(that.modules) : that.modules != null) return false;
+    if (modules != null ? !modules.equals(that.modules) : that.modules != null) {
+      return false;
+    }
     if (nativeLibraries != null ?
         !nativeLibraries.equals(that.nativeLibraries) :
-        that.nativeLibraries != null)
+        that.nativeLibraries != null) {
       return false;
-    if (resources != null ? !resources.equals(that.resources) : that.resources != null)
+    }
+    if (resources != null ? !resources.equals(that.resources) : that.resources != null) {
       return false;
+    }
 
     return true;
   }
