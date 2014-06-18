b3b1bfd88a5886e165c9123e7a5f7a0843bfe5cc
Ben Gertzfield <beng@fb.com>
2014-06-18 10:17:30 -0700

    Refactor (file extension -> Apple UTI) map and add inverse
    Summary:
    To parse .xcspec files, I need to re-use the mapping from
    (file extension -> Apple UTI).
    
    I actually need the inverse mapping, so this refactors the data into
    a new class com.facebook.buck.apple.FileTypes and adds the inverse.
    
    Test Plan: Unit tests included.

--- src/com/facebook/buck/apple/xcode/xcodeproj/PBXFileReference.java
+++ src/com/facebook/buck/apple/xcode/xcodeproj/PBXFileReference.java
@@ -16,85 +16,22 @@
 
 package com.facebook.buck.apple.xcode.xcodeproj;
 
+import com.facebook.buck.apple.FileTypes;
 import com.facebook.buck.apple.xcode.XcodeprojSerializer;
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 
 /**
  * Reference to a concrete file.
  */
 public class PBXFileReference extends PBXReference {
-  private static final ImmutableMap<String, String> FILE_TYPE_TO_FILE_TYPE_IDENTIFIERS =
-      ImmutableMap.<String, String>builder()
-          .put("a", "archive.ar")
-          .put("app", "wrapper.application")
-          .put("bdic", "file")
-          .put("bin", "archive.macbinary")
-          .put("bmp", "image.bmp")
-          .put("bundle", "wrapper.cfbundle")
-          .put("c", "sourcecode.c.c")
-          .put("cc", "sourcecode.cpp.cpp")
-          .put("cpp", "sourcecode.cpp.cpp")
-          .put("css", "text.css")
-          .put("cxx", "sourcecode.cpp.cpp")
-          .put("dart", "sourcecode")
-          .put("dylib", "compiled.mach-o.dylib")
-          .put("exp", "sourcecode.exports")
-          .put("framework", "wrapper.framework")
-          .put("fsh", "sourcecode.glsl")
-          .put("gyp", "sourcecode")
-          .put("gypi", "text")
-          .put("h", "sourcecode.c.h")
-          .put("hxx", "sourcecode.cpp.h")
-          .put("icns", "image.icns")
-          .put("java", "sourcecode.java")
-          .put("jar", "archive.jar")
-          .put("jpeg", "image.jpeg")
-          .put("jpg", "image.jpeg")
-          .put("js", "sourcecode.javascript")
-          .put("json", "text.json")
-          .put("m", "sourcecode.c.objc")
-          .put("mm", "sourcecode.cpp.objcpp")
-          .put("nib", "wrapper.nib")
-          .put("o", "compiled.mach-o.objfile")
-          .put("octest", "wrapper.cfbundle")
-          .put("pdf", "image.pdf")
-          .put("pl", "text.script.perl")
-          .put("plist", "text.plist.xml")
-          .put("pm", "text.script.perl")
-          .put("png", "image.png")
-          .put("proto", "text")
-          .put("py", "text.script.python")
-          .put("r", "sourcecode.rez")
-          .put("rez", "sourcecode.rez")
-          .put("rtf", "text.rtf")
-          .put("s", "sourcecode.asm")
-          .put("storyboard", "file.storyboard")
-          .put("strings", "text.plist.strings")
-          .put("tif", "image.tiff")
-          .put("tiff", "image.tiff")
-          .put("tcc", "sourcecode.cpp.cpp")
-          .put("ttf", "file")
-          .put("vsh", "sourcecode.glsl")
-          .put("xcassets", "folder.assetcatalog")
-          .put("xcconfig", "text.xcconfig")
-          .put("xcodeproj", "wrapper.pb-project")
-          .put("xcdatamodel", "wrapper.xcdatamodel")
-          .put("xcdatamodeld", "wrapper.xcdatamodeld")
-          .put("xctest", "wrapper.cfbundle")
-          .put("xib", "file.xib")
-          .put("y", "sourcecode.yacc")
-          .put("zip", "archive.zip")
-          .build();
-
   private Optional<String> explicitFileType;
 
   public PBXFileReference(String name, String path, SourceTree sourceTree) {
     super(name, path, sourceTree);
 
     // this is necessary to prevent O(n^2) behavior in xcode project loading
-    String fileType = FILE_TYPE_TO_FILE_TYPE_IDENTIFIERS.get(Files.getFileExtension(name));
+    String fileType = FileTypes.FILE_EXTENSION_TO_UTI.get(Files.getFileExtension(name));
     explicitFileType = Optional.fromNullable(fileType);
   }
 
---
+++ test/com/facebook/buck/apple/FileTypesTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import com.google.common.collect.ImmutableList;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link FileTypes}.
+ */
+public class FileTypesTest {
+  @Test
+  public void testFileExtensionsContainNoDots() {
+    for (String fileExtension : FileTypes.FILE_EXTENSION_TO_UTI.keySet()) {
+      assertFalse(fileExtension.contains("."));
+    }
+  }
+
+  @Test
+  public void testFileExtensionMapsToUTI() {
+    assertEquals(FileTypes.FILE_EXTENSION_TO_UTI.get("cpp"), "sourcecode.cpp.cpp");
+  }
+
+  @Test
+  public void testUTIMapsToSingleFileExtension() {
+    assertEquals(FileTypes.UTI_TO_FILE_EXTENSIONS.get("image.pdf"), ImmutableList.of("pdf"));
+  }
+
+  @Test
+  public void testUTIMapsToMultipleSingleFileExtensions() {
+    assertEquals(
+        FileTypes.UTI_TO_FILE_EXTENSIONS.get("sourcecode.cpp.cpp"),
+        ImmutableList.of("cc", "cpp", "cxx", "tcc"));
+  }
+}
