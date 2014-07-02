687c45ee7da6c0b74b7974fc6e9626d9a59818b3
Natthu Bharambe <natthu@fb.com>
2014-07-02 12:17:09 -0700

    Fix log spew from UnixFileSystemProvider in DirArtifactCacheTest.
    Summary:
    Logs would contain the following warning without this change:
    ```
    [junit] Jul 02, 2014 12:07:52 PM com.facebook.buck.log.Logger warn
    [junit] WARNING: Artifact store(d82cb7d741d952d9e187239c85a32c2dbdad44b4, /tmp/junit1512953101460783752/x) error
    [junit] java.nio.file.FileAlreadyExistsException: /tmp/junit1512953101460783752/junit3761567524009369532/d82cb7d741d952d9e187239c85a32c2dbdad44b4
    [junit]   at sun.nio.fs.UnixCopyFile.move(UnixCopyFile.java:422)
    [junit]   at sun.nio.fs.UnixFileSystemProvider.move(UnixFileSystemProvider.java:262)
    [junit]   at java.nio.file.Files.move(Files.java:1345)
    [junit]   at com.facebook.buck.rules.DirArtifactCache.store(DirArtifactCache.java:86)
    [junit]   at com.facebook.buck.rules.DirArtifactCacheTest.testCacheStoreOverwrite(DirArtifactCacheTest.java:117)
    [junit]   at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
    [junit]   at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:57)
    [junit]   at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
    [junit]   at java.lang.reflect.Method.invoke(Method.java:606)
    [junit]   at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:47)
    [junit]   at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
    [junit]   at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:44)
    [junit]   at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
    [junit]   at org.junit.rules.ExternalResource$1.evaluate(ExternalResource.java:48)
    [junit]   at org.junit.rules.RunRules.evaluate(RunRules.java:20)
    [junit]   at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:271)
    [junit]   at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:70)
    [junit]   at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:50)
    [junit]   at org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)
    [junit]   at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)
    [junit]   at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)
    [junit]   at org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)
    [junit]   at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)
    [junit]   at org.junit.runners.ParentRunner.run(ParentRunner.java:309)
    [junit]   at junit.framework.JUnit4TestAdapter.run(JUnit4TestAdapter.java:38)
    [junit]   at org.apache.tools.ant.taskdefs.optional.junit.JUnitTestRunner.run(JUnitTestRunner.java:518)
    [junit]   at org.apache.tools.ant.taskdefs.optional.junit.JUnitTestRunner.launch(JUnitTestRunner.java:1052)
    [junit]   at org.apache.tools.ant.taskdefs.optional.junit.JUnitTestRunner.main(JUnitTestRunner.java:879)
    ```
    
    Test Plan: run DirArtifactCacheTest and notice no more log spew.

--- src/com/facebook/buck/rules/DirArtifactCache.java
+++ src/com/facebook/buck/rules/DirArtifactCache.java
@@ -83,7 +83,7 @@ public class DirArtifactCache implements ArtifactCache {
       // as valid artifacts during subsequent buck runs.
       tmpCacheEntry = File.createTempFile(ruleKey.toString(), ".tmp", cacheDir).toPath();
       Files.copy(output.toPath(), tmpCacheEntry, REPLACE_EXISTING);
-      Files.move(tmpCacheEntry, cacheEntry.toPath());
+      Files.move(tmpCacheEntry, cacheEntry.toPath(), REPLACE_EXISTING);
     } catch (IOException e) {
       logger.warn(e, "Artifact store(%s, %s) error",
           ruleKey,
