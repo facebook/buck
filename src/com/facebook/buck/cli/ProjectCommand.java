ced741009d549592bded0dd195dfeaa75ed7bdb0
Ben Gertzfield <beng@fb.com>
2014-07-14 16:01:14 -0700

    Make 'buck project --workspace-and-projects' regenerate all workspaces/projects
    Summary:
    This builds on D1434510 to make 'buck project
    --workspace-and-projects' generate all workspaces and projects if
    called with no arguments.
    
    Depends On D1434510
    
    Test Plan:
    buck project --workspace-and-projects with 0
    arguments. Confirmed all workspaces and projects were generated as
    expected.

--- src/com/facebook/buck/cli/ProjectCommand.java
+++ src/com/facebook/buck/cli/ProjectCommand.java
@@ -17,6 +17,7 @@
 package com.facebook.buck.cli;
 
 import com.facebook.buck.apple.XcodeProjectConfigDescription;
+import com.facebook.buck.apple.XcodeWorkspaceConfigDescription;
 import com.facebook.buck.apple.xcode.ProjectGenerator;
 import com.facebook.buck.apple.xcode.SeparatedProjectsGenerator;
 import com.facebook.buck.apple.xcode.WorkspaceAndProjectGenerator;
@@ -250,11 +251,19 @@ public class ProjectCommand extends AbstractCommandRunner<ProjectCommandOptions>
           optionsBuilder.addAll(ProjectGenerator.COMBINED_PROJECT_OPTIONS).build());
       projectGenerator.createXcodeProjects();
     } else if (options.getWorkspaceAndProjects()) {
+      ImmutableSet<BuildTarget> targets;
+      if (passedInTargetsSet.isEmpty()) {
+        targets = getAllTargetsOfType(
+            partialGraph.getActionGraph().getNodes(),
+            XcodeWorkspaceConfigDescription.TYPE);
+      } else {
+        targets = passedInTargetsSet;
+      }
       WorkspaceAndProjectGenerator generator = new WorkspaceAndProjectGenerator(
           getProjectFilesystem(),
           partialGraph,
           executionContext,
-          passedInTargetsSet,
+          targets,
           optionsBuilder.build());
       generator.generateWorkspacesAndDependentProjects();
     } else {
@@ -263,13 +272,9 @@ public class ProjectCommand extends AbstractCommandRunner<ProjectCommandOptions>
 
       ImmutableSet<BuildTarget> targets;
       if (passedInTargetsSet.isEmpty()) {
-        ImmutableSet.Builder<BuildTarget> targetsBuilder = ImmutableSet.builder();
-        for (BuildRule node : partialGraph.getActionGraph().getNodes()) {
-          if (node.getType() == XcodeProjectConfigDescription.TYPE) {
-            targetsBuilder.add(node.getBuildTarget());
-          }
-        }
-        targets = targetsBuilder.build();
+        targets = getAllTargetsOfType(
+            partialGraph.getActionGraph().getNodes(),
+            XcodeProjectConfigDescription.TYPE);
       } else {
         targets = passedInTargetsSet;
       }
@@ -287,6 +292,18 @@ public class ProjectCommand extends AbstractCommandRunner<ProjectCommandOptions>
     }
 
     return 0;
+  }
+
+  private static final ImmutableSet<BuildTarget> getAllTargetsOfType(
+      Iterable<BuildRule> nodes,
+      BuildRuleType type) {
+    ImmutableSet.Builder<BuildTarget> targetsBuilder = ImmutableSet.builder();
+    for (BuildRule node : nodes) {
+      if (node.getType() == type) {
+        targetsBuilder.add(node.getBuildTarget());
+      }
+    }
+    return targetsBuilder.build();
   }
 
   /**
