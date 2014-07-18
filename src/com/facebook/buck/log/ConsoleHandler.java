93d66441b7c93f3089b40930e9a81dfc7266c3a7
Michael Bolin <mbolin@fb.com>
2014-07-18 04:46:55 -0700

    Fix malformed Javadoc.
    Test Plan: Yellow warning icon goes away in Eclipse.

--- src/com/facebook/buck/log/ConsoleHandler.java
+++ src/com/facebook/buck/log/ConsoleHandler.java
@@ -16,27 +16,26 @@
 
 package com.facebook.buck.log;
 
-import java.io.IOException;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+
 import java.io.IOError;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.logging.Handler;
-
 import java.util.logging.Formatter;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
-
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
 
 /**
  * Implementation of Handler which writes to System.err.
  *
  * Unlike {@link java.util.logging.ConsoleHandler}, this
  * implementation allows changing its OutputStream via
- * {@link setOutputStream()} without closing the old one first.
+ * {@link #setOutputStream(OutputStream)} without closing the old one first.
  *
  * (Closing System.err is generally not a side effect we want our logging
  * infrastructure to have.)
