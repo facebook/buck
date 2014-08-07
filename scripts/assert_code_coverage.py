c63ba7f789e2243d3d9ec069cb39b701ccc2f72f
Michael Bolin <mbolin@fb.com>
2014-08-07 07:41:26 -0700

    Verify against INSTRUCTION coverage. Now assert_code_coverage.py shows all metrics.
    Summary:
    This updates assert_code_coverage.py so that all types of
    coverage measured by Jacoco (INSTRUCTION, LINE, BRANCH,
    METHOD, CLASS, COMPLEXITY) are printed to the console.
    
    Insofar as this script functions as a test, it now verifies
    INSTRUCTION coverage against the target percentage rather
    than LINE coverage. The current INSTRUCTION coverage for Buck
    is 78.76% whereas the LINE coverage is at 81.52%.
    
    Test Plan:
    Ran `python scripts/assert_code_coverage.py` and observed
    that the expected values were printed to the console.
    
    Also tested with `CODE_COVERAGE_GOAL` set to 80 and verified
    that the expected failure message was printed.

--- scripts/assert_code_coverage.py
+++ scripts/assert_code_coverage.py
@@ -12,7 +12,7 @@ PATH_TO_CODE_COVERAGE_XML = 'buck-out/gen/jacoco/code-coverage/coverage.xml'
 # fail the build. This is designed to far enough below our current
 # standards (80% coverage) that this should be possible to sustain
 # given the inevitable ebb and flow of the code coverage level.
-CODE_COVERAGE_BY_LINE_GOAL = 78
+CODE_COVERAGE_GOAL = 78
 
 
 def is_covered_package_name(package_name):
@@ -30,11 +30,35 @@ def is_covered_package_name(package_name):
 def calculate_code_coverage():
     root = ElementTree.parse(PATH_TO_CODE_COVERAGE_XML)
 
-    line_coverage = []
+    # The length of the longest Java package included in the report.
+    # Used for display purposes.
     max_package_name = 0
 
-    total_line_covered = 0
-    total_line = 0
+    # Coverage types measured by Jacoco and present in the report.
+    # DO NOT ALPHA-SORT. List determines column display order in final report.
+    TYPES = [
+        'INSTRUCTION',
+        'LINE',
+        'BRANCH',
+        'METHOD',
+        'CLASS',
+        'COMPLEXITY',
+    ]
+
+    # Column by which rows will be sorted in the final report.
+    SORT_TYPE = TYPES[0]
+
+    # Keys are values from TYPES; values are integers.
+    total_covered_by_type = {}
+    total_missed_plus_covered_type = {}
+    for coverage_type in TYPES:
+        total_covered_by_type[coverage_type] = 0
+        total_missed_plus_covered_type[coverage_type] = 0
+
+    # Values are dicts. Will have key 'package_name' as well as all values
+    # from TYPES as keys. For entries from TYPES, values are the corresponding
+    # coverage percentage for that type.
+    coverage_by_package = []
 
     for element in root.findall('.//package'):
         package_name = element.attrib['name']
@@ -42,20 +66,22 @@ def calculate_code_coverage():
             continue
 
         max_package_name = max(max_package_name, len(package_name))
+        coverage = {}
+        coverage['package_name'] = package_name
+        coverage_by_package.append(coverage)
         for counter in element.findall('./counter'):
             counter_type = counter.attrib.get('type')
             missed = int(counter.attrib.get('missed'))
             covered = int(counter.attrib.get('covered'))
 
             percentage = round(100 * covered / float(missed + covered), 2)
-            if counter_type == 'LINE':
-                total_line_covered += covered
-                total_line += missed + covered
-                line_coverage.append({'package_name': package_name, 'percentage': percentage})
+            total_covered_by_type[counter_type] += covered
+            total_missed_plus_covered_type[counter_type] += missed + covered
+            coverage[counter_type] = percentage
 
     def pair_compare(p1, p2):
         # High percentage should be listed first.
-        diff1 = cmp(p2['percentage'], p1['percentage'])
+        diff1 = cmp(p2[SORT_TYPE], p1[SORT_TYPE])
         if diff1:
             return diff1
         # Ties are broken by lexicographic comparison.
@@ -65,38 +91,59 @@ def calculate_code_coverage():
         return label + ' ' * (max_package_name - len(label)) + ' '
 
     def print_separator():
-        print '-' * (max_package_name + 7)
+        print '-' * (max_package_name + 1 + len(TYPES) * 8)
+
+    def get_color_for_percentage(percentage):
+        # \033[92m is OKGREEN.
+        # \033[93m is WARNING.
+        return '\033[92m' if percentage >= CODE_COVERAGE_GOAL else '\033[93m'
 
     # Print header.
-    print label_with_padding('PACKAGE') + 'LINE'
+    # Type column headers are right-justified and truncated to 7 characters.
+    column_names = map(lambda x: x[0:7].rjust(7), TYPES)
+    print label_with_padding('PACKAGE') + ' ' + ' '.join(column_names)
     print_separator()
+
+    # Create the format string to use for each row.
+    format_string = '%(color)s%(label)s'
+    for coverage_type in TYPES:
+        format_string += '%(' + coverage_type + ')7.2f%%'
+    format_string += '\033[0m'
 
     # Print rows sorted by line coverage then package name.
-    line_coverage.sort(cmp=pair_compare)
-    for item in line_coverage:
-        package_name = item['package_name']
-        padding = ' ' * (max_package_name - len(package_name))
-        percentage = item['percentage']
-        color = '\033[92m' if percentage >= CODE_COVERAGE_BY_LINE_GOAL else '\033[93m'
-        print '%s%s%s%%\033[0m' % (color, label_with_padding(package_name), percentage)
+    coverage_by_package.sort(cmp=pair_compare)
+    for item in coverage_by_package:
+        info = item.copy()
+        info['color'] = get_color_for_percentage(item[SORT_TYPE])
+        info['label'] = label_with_padding(item['package_name'])
+        print format_string % info
 
     # Print aggregate numbers.
-    total_line_percentage = round(100 * total_line_covered / float(total_line), 2)
-    color = '\033[92m' if total_line_percentage >= CODE_COVERAGE_BY_LINE_GOAL else '\033[93m'
+    overall_percentages = {}
+    for coverage_type in TYPES:
+        numerator = total_covered_by_type[coverage_type]
+        denominator = total_missed_plus_covered_type[coverage_type]
+        percentage = 100.0 * numerator / denominator
+        overall_percentages[coverage_type] = percentage
+
+    observed_percentage = overall_percentages[SORT_TYPE]
+    overall_percentages['color'] = get_color_for_percentage(observed_percentage)
+    overall_percentages['label'] = label_with_padding('TOTAL')
     print_separator()
-    print '%s%s%s%%\033[0m' % (color, label_with_padding('TOTAL'), total_line_percentage)
+    print format_string % overall_percentages
 
-    return total_line_percentage
-
-
-def is_code_coverage_met():
-    coverage = calculate_code_coverage()
-    return coverage >= CODE_COVERAGE_BY_LINE_GOAL
+    return observed_percentage
 
 
 def main():
     """Exits with 0 or 1 depending on whether the code coverage goal is met."""
-    if not is_code_coverage_met():
+    coverage = calculate_code_coverage()
+    if coverage < CODE_COVERAGE_GOAL:
+        data = {
+            'expected': CODE_COVERAGE_GOAL,
+            'observed': coverage,
+        }
+        print '\033[91mFAIL: %(observed).2f%% does not meet goal of %(expected).2f%%\033[0m' % data
         sys.exit(1)
 
 
