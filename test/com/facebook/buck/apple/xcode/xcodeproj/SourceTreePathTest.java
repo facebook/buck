5edce2b7216cba2a9a7f4ffd5751ee8ebfd757cc
Ben Gertzfield <beng@fb.com>
2014-08-13 14:26:49 -0700

    Fix flipped Boolean logic in SourceTreePath.equals()
    Summary:
    I got a report that ProjectGenerator was missing some files.
    With the logging in D1495765, I noticed PBXFileReference objects
    were being re-used for two SourceTreePaths.
    
    I tracked this down to SourceTreePath.equals() having a flipped
    Boolean operator. I wrote a test to confirm it was broken, and
    fixing the operator fixed the test as well as the missing files
    from ProjectGenerator.
    
    Test Plan:
    Unit test included. Ran 'buck project' to generate Xcode
    project, confirmed missing files were generated.

---
+++ test/com/facebook/buck/apple/xcode/xcodeproj/SourceTreePathTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple.xcode.xcodeproj;
+
+import org.junit.Test;
+import static org.junit.Assert.assertNotEquals;
+
+import java.nio.file.Paths;
+
+/**
+ * Tests for {@link SourceTreePath}.
+ */
+public class SourceTreePathTest {
+  @Test
+  public void sourceTreePathsWithDifferentPathsAreDifferent() {
+    SourceTreePath path1 = new SourceTreePath(
+        PBXReference.SourceTree.SOURCE_ROOT,
+        Paths.get("foo/bar.c"));
+    SourceTreePath path2 = new SourceTreePath(
+        PBXReference.SourceTree.SOURCE_ROOT,
+        Paths.get("foo/baz.c"));
+    assertNotEquals(path1, path2);
+  }
+}
