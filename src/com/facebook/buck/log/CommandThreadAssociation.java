7146a34a1908b5bfd547bdb3533d99d965b20a45
Ben Gertzfield <beng@fb.com>
2014-08-26 09:50:37 -0700

    New class CommandThreadAssociation factored out of LogFormatter
    Summary:
    Currently, the LogFormatter class contains a helper
    class used to manipulate the <thread ID: command ID> map.
    
    Since I need to factor that map out to fix #4985049, it made
    sense to factor out the static classes as well; they really
    don't belong there any more.
    
    This factors out the CommandThreadAssociation class from LogFormatter
    into a new class. A subsequent diff will make use of it.
    
    I removed the Closeable implementation as well, since @natthu and
    I discovered that using a try-with-resources block for this is
    inappropriate, as we need to log inside a catch block, which depends
    on the <thread ID: commmand ID> map.
    
    Depends On D1519662
    
    Test Plan: Tested in subsequent diff.

---
+++ src/com/facebook/buck/log/CommandThreadAssociation.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.log;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.Collections;
+
+/**
+ * An association between the current thread and a given command.
+ */
+public class CommandThreadAssociation {
+
+  private final String commandId;
+  private final ConcurrentMap<Long, String> threadIdToCommandId;
+
+  public CommandThreadAssociation(String commandId) {
+    this(commandId, GlobalState.THREAD_ID_TO_COMMAND_ID);
+  }
+
+  @VisibleForTesting
+  CommandThreadAssociation(
+      String commandId,
+      ConcurrentMap<Long, String> threadIdToCommandId) {
+    this.commandId = Preconditions.checkNotNull(commandId);
+    this.threadIdToCommandId = Preconditions.checkNotNull(threadIdToCommandId);
+    String oldCommandId = threadIdToCommandId.put(Thread.currentThread().getId(), commandId);
+    // We better not have overwritten an old mapping.
+    Preconditions.checkState(oldCommandId == null);
+  }
+
+  public void stop() {
+    // remove(commandId) would only remove the first association.
+    boolean removed = threadIdToCommandId.values().removeAll(
+        Collections.singleton(commandId));
+    // We better have removed something, or we messed up.
+    Preconditions.checkState(removed);
+  }
+}
