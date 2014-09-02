8a0e9a12e56402de5535f367d1203ae38c444123
Ben Gertzfield <beng@fb.com>
2014-09-02 16:07:29 -0700

    Add Chrome tracing to buck launcher
    Summary:
    This extends Chrome tracing out into the Python buck launcher.
    To use the tracer, just write:
    
      from tracing import Tracing
    
      with Tracing('eventname'):
        pass
    
    Then, at the end of your run:
    
      Tracing.write_to_dir(buck_log_dir)
    
    We currently write to buck-out/log/traces/launch.[uuid].trace and make
    a symlink from launch.trace to that file.
    
    Test Plan:
    Added tracing to buck's main() method, ran 'buck project',
    confirmed trace was written to buck-out/log/traces/launch.[uuid].trace
    and symlink existed. Opened in chrome://tracing in Chrome, confirmed
    timestamps made sense and matched build.trace.

---
+++ bin/tracing.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import errno
+import json
+import os
+import time
+from timing import monotonic_time_nanos
+from uuid import uuid4
+
+
+class _TraceEventPhases(object):
+    BEGIN = 'B'
+    END = 'E'
+    IMMEDIATE = 'I'
+    COUNTER = 'C'
+    ASYNC_START = 'S'
+    ASYNC_FINISH = 'F'
+    OBJECT_SNAPSHOT = 'O'
+    OBJECT_NEW = 'N'
+    OBJECT_DELETE = 'D'
+    METADATA = 'M'
+
+
+class Tracing(object):
+    _trace_events = []
+
+    def __init__(self, name, args={}):
+        self.name = name
+        self.args = args
+        self.pid = os.getpid()
+
+    def __enter__(self):
+        now_us = monotonic_time_nanos() / 1000
+        self._add_trace_event(
+            'buck-launcher',
+            self.name,
+            _TraceEventPhases.BEGIN,
+            self.pid,
+            1,
+            now_us,
+            self.args)
+
+    def __exit__(self, x_type, x_value, x_traceback):
+        now_us = monotonic_time_nanos() / 1000
+        self._add_trace_event(
+            'buck-launcher',
+            self.name,
+            _TraceEventPhases.END,
+            self.pid,
+            1,
+            now_us,
+            self.args)
+
+    @staticmethod
+    def _add_trace_event(
+            category,
+            name,
+            phase,
+            pid,
+            tid,
+            ts,
+            args):
+        Tracing._trace_events.append({
+            'cat': category,
+            'name': name,
+            'ph': phase,
+            'pid': pid,
+            'tid': tid,
+            'ts': ts,
+            'args': args})
+
+    @staticmethod
+    def write_to_dir(buck_log_dir):
+        filename_time = time.strftime('%Y-%m-%d.%H-%M-%S')
+        trace_filename = os.path.join(
+            buck_log_dir, 'launch.{0}.{1}.trace'.format(filename_time, uuid4()))
+        trace_filename_link = os.path.join(buck_log_dir, 'launch.trace')
+        json.dump(Tracing._trace_events, file(trace_filename, 'w'))
+        try:
+            os.symlink(trace_filename, trace_filename_link)
+        except OSError, e:
+            if e.errno == errno.EEXIST:
+                os.remove(trace_filename_link)
+                os.symlink(trace_filename, trace_filename_link)
