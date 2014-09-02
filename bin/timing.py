de33b1d7cc8be6a7089c0add595fd2a2b6697798
Ben Gertzfield <beng@fb.com>
2014-09-02 14:16:25 -0700

    New Python function timing.monotonic_time_nanos()
    Summary:
    I'm planning on extending the Chrome tracing subsystem out to
    the Python launcher, so we can include timing for things like launching
    watchmand and buckd itself.
    
    To do this, we need two things:
    
    1) A monotonically-increasing timestamp to write events from Python, and
    2) A consistent timestamp epoch between Python and Java
    
    This diff gives us 1) by adding a cross-platform function to get a
    monotonically-increasing time value in nanoseconds. I implemented
    it for Linux, OS X (Darwin), and Windows.
    
    Test Plan:
    import timing; print timing.monotonic_time_nanos() on
    OS X 10.9.4 and Linux 3.2.51 (CentOS 6.3).

---
+++ bin/timing.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import ctypes
+import os
+import platform
+
+
+def monotonic_time_nanos():
+    '''Returns a monotonically-increasing timestamp value in nanoseconds.
+
+    The epoch of the return value is undefined. To use this, you must call
+    it more than once and calculate the delta between two calls.
+    '''
+    # This function should be overwritten below on supported platforms.
+    raise Exception('Unsupported platform: ' + platform.system())
+
+NSEC_PER_SEC = 1000000000
+
+if platform.system() == 'Linux':
+    # From <linux/time.h>, available since 2.6.28 (released 24-Dec-2008).
+    CLOCK_MONOTONIC_RAW = 4
+    librt = ctypes.CDLL('librt.so.1', use_errno=True)
+    clock_gettime = librt.clock_gettime
+
+    class struct_timespec(ctypes.Structure):
+        _fields_ = [('tv_sec', ctypes.c_long), ('tv_nsec', ctypes.c_long)]
+    clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(struct_timespec)]
+
+    def _monotonic_time_nanos_linux():
+        t = struct_timespec()
+        clock_gettime(CLOCK_MONOTONIC_RAW, ctypes.byref(t))
+        return t.tv_sec * NSEC_PER_SEC + t.tv_nsec
+    monotonic_time_nanos = _monotonic_time_nanos_linux
+elif platform.system() == 'Darwin':
+    # From <mach/mach_time.h>
+    KERN_SUCCESS = 0
+    libSystem = ctypes.CDLL('libSystem.dylib', use_errno=True)
+    mach_timebase_info = libSystem.mach_timebase_info
+
+    class struct_mach_timebase_info(ctypes.Structure):
+        _fields_ = [('numer', ctypes.c_uint32), ('denom', ctypes.c_uint32)]
+    mach_timebase_info.argtypes = [ctypes.POINTER(struct_mach_timebase_info)]
+    mach_ti = struct_mach_timebase_info()
+    ret = mach_timebase_info(ctypes.byref(mach_ti))
+    if ret != KERN_SUCCESS:
+        raise Exception('Could not get mach_timebase_info, error: ' + str(ret))
+    mach_absolute_time = libSystem.mach_absolute_time
+    mach_absolute_time.restype = ctypes.c_uint64
+
+    def _monotonic_time_nanos_darwin():
+        return (mach_absolute_time() * mach_ti.numer) / mach_ti.denom
+    monotonic_time_nanos = _monotonic_time_nanos_darwin
+elif platform.system() == 'Windows':
+    # From <Winbase.h>
+    perf_frequency = ctypes.c_uint64()
+    ctypes.windll.kernel32.QueryPerformanceFrequency(ctypes.byref(perf_frequency))
+
+    def _monotonic_time_nanos_windows():
+        perf_counter = ctypes.c_uint64()
+        ctypes.windll.kernel32.QueryPerformanceCounter(ctypes.byref(perf_counter))
+        return perf_counter * NSEC_PER_SEC / perf_frequency
+    monotonic_time_nanos = _monotonic_time_nanos_windows
