c0849146a3e304992e8d2394b352b64ffba5e899
Michael Bolin <mbolin@fb.com>
2014-09-17 10:26:40 -0700

    Regression test for https://github.com/facebook/buck/issues/187.
    Summary:
    Introduce `MultipleBuildConfigIntegrationTest` to verify the issue
    with multiple `android_build_config()` rules explained in #187.
    This will be used as a regression test (go TDD!).
    
    This is annotated with `@Ignore` so that it does not break the build.
    
    Test Plan:
    Removed the `@Ignore` and ran the test from Eclipse where it failed with the following error:
    
    ```
    java.lang.AssertionError:
    Expected: a string containing "com/example/config2/BuildConfig.smali"
         but: was "/private/var/folders/w1/_2mc6m050qn232npsf9z3hfsh58_jh/T/junit147277075926323298/buck-out/gen/java/com/buildconfigs/extract-classes-dex__tmp/com/example/config1/BuildConfig.smali
    /private/var/folders/w1/_2mc6m050qn232npsf9z3hfsh58_jh/T/junit147277075926323298/buck-out/gen/java/com/buildconfigs/extract-classes-dex__tmp/com/example/ConfigConsumer.smali
    /private/var/folders/w1/_2mc6m050qn232npsf9z3hfsh58_jh/T/junit147277075926323298/buck-out/gen/java/com/buildconfigs/extract-classes-dex__tmp/com/sample/R$drawable.smali
    /private/var/folders/w1/_2mc6m050qn232npsf9z3hfsh58_jh/T/junit147277075926323298/buck-out/gen/java/com/buildconfigs/extract-classes-dex__tmp/com/sample/R$string.smali
    /private/var/folders/w1/_2mc6m050qn232npsf9z3hfsh58_jh/T/junit147277075926323298/buck-out/gen/java/com/buildconfigs/extract-classes-dex__tmp/com/sample/R.smali
    "
    	at org.hamcrest.MatcherAssert.assertThat(MatcherAssert.java:20)
    	at org.junit.Assert.assertThat(Assert.java:865)
    	at org.junit.Assert.assertThat(Assert.java:832)
    	at com.facebook.buck.android.MultipleBuildConfigIntegrationTest.testAndroidBinarySupportsMultipleBuildConfigs(MultipleBuildConfigIntegrationTest.java:49)
    	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
    	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:57)
    	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
    	at java.lang.reflect.Method.invoke(Method.java:606)
    	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:47)
    	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
    	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:44)
    	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
    	at org.junit.rules.ExternalResource$1.evaluate(ExternalResource.java:48)
    	at org.junit.rules.RunRules.evaluate(RunRules.java:20)
    	at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:271)
    	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:70)
    	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:50)
    	at org.junit.runners.ParentRunner$3.run(ParentRunner.java:238)
    	at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:63)
    	at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:236)
    	at org.junit.runners.ParentRunner.access$000(ParentRunner.java:53)
    	at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:229)
    	at org.junit.runners.ParentRunner.run(ParentRunner.java:309)
    	at org.eclipse.jdt.internal.junit4.runner.JUnit4TestReference.run(JUnit4TestReference.java:50)
    	at org.eclipse.jdt.internal.junit.runner.TestExecution.run(TestExecution.java:38)
    	at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:467)
    	at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:683)
    	at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.run(RemoteTestRunner.java:390)
    	at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.main(RemoteTestRunner.java:197)
    ```

---
+++ test/com/facebook/buck/android/MultipleBuildConfigIntegrationTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.android;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import com.facebook.buck.testutil.integration.DebuggableTemporaryFolder;
+import com.facebook.buck.testutil.integration.ProjectWorkspace;
+import com.facebook.buck.testutil.integration.TestDataHelper;
+
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class MultipleBuildConfigIntegrationTest {
+
+  @Rule
+  public DebuggableTemporaryFolder tmp = new DebuggableTemporaryFolder();
+
+  /**
+   * Regression test for https://github.com/facebook/buck/issues/187.
+   */
+  @Test
+  @Ignore
+  public void testAndroidBinarySupportsMultipleBuildConfigs() throws IOException {
+    ProjectWorkspace workspace = TestDataHelper.createProjectWorkspaceForScenario(
+        this, "android_project", tmp);
+    workspace.setUp();
+    workspace.runBuckCommand("build", "//java/com/buildconfigs:extract-classes-dex")
+        .assertSuccess();
+
+    String smali = workspace.getFileContents("buck-out/gen/java/com/buildconfigs/smali-files.txt");
+    assertThat(smali, containsString("com/example/config1/BuildConfig.smali"));
+    assertThat(smali, containsString("com/example/config2/BuildConfig.smali"));
+  }
+}
