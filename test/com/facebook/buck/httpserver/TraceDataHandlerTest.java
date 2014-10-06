080238a95162ad4a304889e7124f3263714e7c6c
Ben Gertzfield <beng@fb.com>
2014-10-06 15:04:15 -0700

    Simplify TraceDataHandlerTest
    Summary:
    I'm modifying TracesHelper and found TraceDataHandlerTest's use of
    mocks to make the test very fragile.
    
    This change simply mocks out TracesHelper instead of mocking all of
    its dependencies.
    
    Test Plan:
    ant java-test -Dtest.class=TraceDataHandlerTest

--- test/com/facebook/buck/httpserver/TraceDataHandlerTest.java
+++ test/com/facebook/buck/httpserver/TraceDataHandlerTest.java
@@ -21,9 +21,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import com.facebook.buck.util.BuckConstant;
-import com.facebook.buck.util.ProjectFilesystem;
-
 import com.google.common.base.Charsets;
 
 import org.easymock.EasyMockSupport;
@@ -31,13 +28,10 @@ import org.eclipse.jetty.server.Request;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 
@@ -79,20 +73,12 @@ public class TraceDataHandlerTest extends EasyMockSupport {
     expect(response.getWriter()).andReturn(printWriter);
     response.flushBuffer();
 
-    ProjectFilesystem projectFilesystem = createMock(ProjectFilesystem.class);
-    File traceFile = createMock(File.class);
-    String name = "build.abcdef.trace";
-    expect(traceFile.getName()).andStubReturn(name);
-    Path pathToTraceFile = BuckConstant.BUCK_TRACE_DIR.resolve(name);
-    expect(traceFile.toPath()).andReturn(pathToTraceFile);
-    expect(projectFilesystem.listFiles(BuckConstant.BUCK_TRACE_DIR)).andStubReturn(
-        new File[] {traceFile});
-    expect(
-        projectFilesystem.getInputStreamForRelativePath(
-            Paths.get("buck-out/log/traces/build.abcdef.trace")))
-        .andReturn(new ByteArrayInputStream("{\"foo\":\"bar\"}".getBytes()));
-    TraceDataHandler traceDataHandler = new TraceDataHandler(
-        new TracesHelper(projectFilesystem));
+    TracesHelper tracesHelper = createMock(TracesHelper.class);
+    Iterable<InputStream> traces = Arrays.<InputStream>asList(
+        new ByteArrayInputStream("{\"foo\":\"bar\"}".getBytes(Charsets.UTF_8))
+    );
+    expect(tracesHelper.getInputsForTraces("abcdef")).andReturn(traces);
+    TraceDataHandler traceDataHandler = new TraceDataHandler(tracesHelper);
 
     replayAll();
     traceDataHandler.handle("/trace/abcdef",
@@ -158,19 +144,12 @@ public class TraceDataHandlerTest extends EasyMockSupport {
     expect(response.getWriter()).andReturn(printWriter);
     response.flushBuffer();
 
-    ProjectFilesystem projectFilesystem = createMock(ProjectFilesystem.class);
-    File traceFile = createMock(File.class);
-    String name = "build.abcdef.trace";
-    expect(traceFile.getName()).andStubReturn(name);
-    Path pathToTraceFile = BuckConstant.BUCK_TRACE_DIR.resolve(name);
-    expect(traceFile.toPath()).andReturn(pathToTraceFile);
-    expect(projectFilesystem.listFiles(BuckConstant.BUCK_TRACE_DIR)).andStubReturn(
-        new File[] {traceFile});
-    expect(
-        projectFilesystem.getInputStreamForRelativePath(pathToTraceFile))
-        .andReturn(new ByteArrayInputStream("{\"foo\":\"bar\"}".getBytes()));
-    TraceDataHandler traceDataHandler = new TraceDataHandler(
-        new TracesHelper(projectFilesystem));
+    TracesHelper tracesHelper = createMock(TracesHelper.class);
+    Iterable<InputStream> traces = Arrays.<InputStream>asList(
+        new ByteArrayInputStream("{\"foo\":\"bar\"}".getBytes(Charsets.UTF_8))
+    );
+    expect(tracesHelper.getInputsForTraces("abcdef")).andReturn(traces);
+    TraceDataHandler traceDataHandler = new TraceDataHandler(tracesHelper);
 
     replayAll();
     traceDataHandler.handle("/trace/abcdef?callback=my.callback",
