55f418051bfaa63b119edffc6a71bc9caf13f18f
Ben Gertzfield <beng@fb.com>
2014-10-20 20:57:59 -0700

    New interface ProcessManager and implementation PkillProcessManager
    Summary:
    I'm implementing a warning if the IDE is open when running
    'buck project'.
    
    This diff adds an interface `ProcessManager` to check for and kill
    running processes as well as an implementation `PkillProcessManager`
    which we use on non-Windows systems.
    
    Test Plan:
    Unit tests included. Ran
    `ant test -Dtest.class=PkillProcessManagerTest` and
    `buck test -f PkillProcessManagerTest //test/com/facebook/buck/util:util`.

---
+++ src/com/facebook/buck/util/PkillProcessManager.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util;
+
+import com.facebook.buck.log.Logger;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+import java.io.IOException;
+
+/**
+ * Checks for and kills processes by name.
+ *
+ * Only supported on non-Windows platforms (depends on {@code pkill} binary).
+ */
+public class PkillProcessManager implements ProcessManager {
+  private static final Logger LOG = Logger.get(PkillProcessManager.class);
+  private static final String PKILL_BINARY = "pkill";
+
+  private final ProcessExecutor processExecutor;
+
+  public PkillProcessManager(ProcessExecutor processExecutor) {
+    this.processExecutor = Preconditions.checkNotNull(processExecutor);
+  }
+
+  @Override
+  public boolean isProcessRunning(String processName) throws InterruptedException, IOException {
+    LOG.debug("Checking if %s is running...", processName);
+    ImmutableList<String> processAndArgs = ImmutableList.of(PKILL_BINARY, "-0", processName);
+    ProcessExecutor.Result result = runProcess(processAndArgs);
+    if (result.getExitCode() == 0) {
+      LOG.debug("Process %s is running", processName);
+      return true;
+    } else if (result.getExitCode() == 1) {
+      LOG.debug("Process %s is not running", processName);
+      return false;
+    } else {
+      throw new HumanReadableException(
+          "Unexpected error running %s: exit code %d, stdout %s, stderr %s",
+          Joiner.on(" ").join(processAndArgs),
+          result.getExitCode(),
+          result.getStdout(),
+          result.getStderr());
+    }
+  }
+
+  @Override
+  public void killProcess(String processName) throws InterruptedException, IOException {
+    LOG.debug("Killing process %s...", processName);
+    ImmutableList<String> processAndArgs = ImmutableList.of(PKILL_BINARY, processName);
+    ProcessExecutor.Result result = runProcess(processAndArgs);
+    if (result.getExitCode() == 0) {
+      LOG.debug("Killed process %s.", processName);
+    } else if (result.getExitCode() == 1) {
+      LOG.debug("Could not kill process %s (not running)", processName);
+      // We don't treat this as an error, because the user could have killed
+      // the process themselves.
+    } else {
+      throw new HumanReadableException(
+          "Unexpected error running %s: exit code %d, stdout %s, stderr %s",
+          Joiner.on(" ").join(processAndArgs),
+          result.getExitCode(),
+          result.getStdout(),
+          result.getStderr());
+    }
+  }
+
+  private ProcessExecutor.Result runProcess(ImmutableList<String> processAndArgs)
+      throws InterruptedException, IOException {
+    ProcessExecutorParams params = ProcessExecutorParams.builder()
+        .setCommand(processAndArgs)
+        .build();
+    return processExecutor.launchAndExecute(params);
+  }
+}
---
+++ test/com/facebook/buck/util/PkillProcessManagerTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PkillProcessManagerTest {
+
+  @Test
+  public void processIsRunningIfPkill0Succeeds() throws IOException, InterruptedException {
+    ProcessExecutorParams pkill0Params =
+        ProcessExecutorParams.builder()
+            .setCommand(ImmutableList.of("pkill", "-0", "processName"))
+            .build();
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.<ProcessExecutorParams, FakeProcess>of(
+            pkill0Params,
+            new FakeProcess(0)));
+    PkillProcessManager processManager = new PkillProcessManager(
+        processExecutor);
+    assertTrue(processManager.isProcessRunning("processName"));
+    assertTrue(processExecutor.isProcessLaunched(pkill0Params));
+  }
+
+  @Test
+  public void processIsNotRunningIfPkill0Fails() throws IOException, InterruptedException {
+    ProcessExecutorParams pkill0Params =
+        ProcessExecutorParams.builder()
+            .setCommand(ImmutableList.of("pkill", "-0", "processName"))
+            .build();
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.<ProcessExecutorParams, FakeProcess>of(
+            pkill0Params,
+            new FakeProcess(1)));
+    PkillProcessManager processManager = new PkillProcessManager(
+        processExecutor);
+    assertFalse(processManager.isProcessRunning("processName"));
+    assertTrue(processExecutor.isProcessLaunched(pkill0Params));
+  }
+
+  @Test(expected = HumanReadableException.class)
+  public void isProcessRunningThrowsOnUnexpectedExitCode()
+      throws IOException, InterruptedException {
+    ProcessExecutorParams pkill0Params =
+        ProcessExecutorParams.builder()
+            .setCommand(ImmutableList.of("pkill", "-0", "processName"))
+            .build();
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.<ProcessExecutorParams, FakeProcess>of(
+            pkill0Params,
+            new FakeProcess(49152)));
+    PkillProcessManager processManager = new PkillProcessManager(processExecutor);
+    processManager.isProcessRunning("processName");
+  }
+
+  @Test
+  public void killProcessLaunchesProcessWithSuccess() throws IOException, InterruptedException {
+    ProcessExecutorParams pkillParams =
+        ProcessExecutorParams.builder()
+            .setCommand(ImmutableList.of("pkill", "processName"))
+            .build();
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.<ProcessExecutorParams, FakeProcess>of(
+            pkillParams,
+            new FakeProcess(0)));
+    PkillProcessManager processManager = new PkillProcessManager(processExecutor);
+    processManager.killProcess("processName");
+    assertTrue(processExecutor.isProcessLaunched(pkillParams));
+  }
+
+  @Test
+  public void killProcessLaunchesProcessWithFailure() throws IOException, InterruptedException {
+    ProcessExecutorParams pkillParams =
+        ProcessExecutorParams.builder()
+            .setCommand(ImmutableList.of("pkill", "processName"))
+            .build();
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.<ProcessExecutorParams, FakeProcess>of(
+            pkillParams,
+            new FakeProcess(1)));
+    PkillProcessManager processManager = new PkillProcessManager(processExecutor);
+    processManager.killProcess("processName");
+    assertTrue(processExecutor.isProcessLaunched(pkillParams));
+  }
+
+  @Test(expected = HumanReadableException.class)
+  public void killProcessThrowsWithUnexpectedExitCode()
+      throws IOException, InterruptedException {
+    ProcessExecutorParams pkillParams =
+        ProcessExecutorParams.builder()
+            .setCommand(ImmutableList.of("pkill", "processName"))
+            .build();
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.<ProcessExecutorParams, FakeProcess>of(
+            pkillParams,
+            new FakeProcess(64738)));
+    PkillProcessManager processManager = new PkillProcessManager(processExecutor);
+    processManager.killProcess("processName");
+  }
+}
