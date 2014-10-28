bc9a39d371a8c158a0f0d1faf8fc41bbe0100c84
Ben Gertzfield <beng@fb.com>
2014-10-28 15:59:56 -0700

    New utility class AppleSdkDiscovery
    Summary:
    To build the `AppleCxxPlatform`, we need to discover the
    location of the components of the Apple SDK.
    
    This introduces a new utility class `AppleSdkDiscovery` which takes
    the path to the Xcode installation (which I will get from @mbolin's
    D1630292) and discovers the Mac OS X, iPhone OS, and iPhone Simulator
    SDKs inside.
    
    It also reads the `SDKSettings.plist` file inside each SDK so we can
    refer to them by the same name as shown by `xcodebuild -showsdks`:
    
      % xcodebuild -showsdks
      OS X SDKs:
         OS X 10.9                            -sdk macosx10.9
    
      iOS SDKs:
          iOS 8.0                             -sdk iphoneos8.0
    
      iOS Simulator SDKs:
          Simulator - iOS 8.0                 -sdk iphonesimulator8.0
    
    Test Plan: Unit test included.

---
+++ src/com/facebook/buck/apple/AppleSdkDiscovery.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple;
+
+import com.dd.plist.NSDictionary;
+import com.dd.plist.NSString;
+import com.dd.plist.PropertyListParser;
+
+import com.facebook.buck.log.Logger;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Utility class to discover the location of SDKs contained inside an Xcode
+ * installation.
+ */
+public class AppleSdkDiscovery {
+
+  private static final Logger LOG = Logger.get(AppleSdkDiscovery.class);
+
+  // Utility class; do not instantiate.
+  private AppleSdkDiscovery() { }
+
+  /**
+   * Given a path to an Xcode developer directory, walks through the
+   * platforms and builds a map of (sdk-name: {@link AppleSdkPaths}) objects
+   * describing the paths to the SDKs inside.
+   *
+   * The sdk-name strings match the ones displayed by {@code xcodebuild -showsdks}
+   * and look like {@code macosx10.9}, {@code iphoneos8.0}, {@code iphonesimulator8.0},
+   * etc.
+   */
+  public static ImmutableMap<String, AppleSdkPaths> discoverAppleSdkPaths(Path xcodeDir)
+      throws IOException {
+    LOG.debug("Searching for Xcode platforms under %s", xcodeDir);
+
+    ImmutableMap.Builder<String, AppleSdkPaths> appleSdkPathsBuilder = ImmutableMap.builder();
+    try (DirectoryStream<Path> platformStream = Files.newDirectoryStream(
+             xcodeDir.resolve("Platforms"),
+             "*.platform")) {
+      for (Path platformDir : platformStream) {
+        LOG.debug("Searching for Xcode SDKs under %s", platformDir);
+        try (DirectoryStream<Path> sdkStream = Files.newDirectoryStream(
+                 platformDir.resolve("Developer/SDKs"),
+                 "*.sdk")) {
+          for (Path sdkDir : sdkStream) {
+            LOG.debug("Fetching SDK name for %s", sdkDir);
+            if (Files.isSymbolicLink(sdkDir)) {
+              continue;
+            }
+
+            Optional<String> sdkName = getSdkNameForPath(sdkDir);
+            if (sdkName.isPresent()) {
+              LOG.debug("Found SDK name %s", sdkName.get());
+              ImmutableAppleSdkPaths xcodePaths = ImmutableAppleSdkPaths.builder()
+                  .toolchainPath(xcodeDir.resolve("Toolchains/XcodeDefault.xctoolchain"))
+                  .platformDeveloperPath(platformDir.resolve("Developer"))
+                  .sdkPath(sdkDir)
+                  .build();
+              appleSdkPathsBuilder.put(sdkName.get(), xcodePaths);
+            }
+          }
+        }
+      }
+    }
+    return appleSdkPathsBuilder.build();
+  }
+
+  private static Optional<String> getSdkNameForPath(Path sdkDir) throws IOException {
+    try (InputStream sdkSettingsPlist = Files.newInputStream(sdkDir.resolve("SDKSettings.plist"));
+         BufferedInputStream bufferedSdkSettingsPlist = new BufferedInputStream(sdkSettingsPlist)) {
+      NSDictionary sdkSettings;
+      try {
+        sdkSettings = (NSDictionary) PropertyListParser.parse(bufferedSdkSettingsPlist);
+      } catch (Exception e) {
+        throw new IOException(e);
+      }
+      NSString canonicalName = (NSString) sdkSettings.objectForKey("CanonicalName");
+      return Optional.of(canonicalName.toString());
+    } catch (FileNotFoundException e) {
+      LOG.error(e, "No SDKSettings.plist found under SDK path %s", sdkDir);
+      return Optional.absent();
+    }
+  }
+}
---
+++ test/com/facebook/buck/apple/AppleSdkDiscoveryTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link AppleSdkDiscovery}.
+ */
+public class AppleSdkDiscoveryTest {
+  @Test
+  public void appleSdkPathsBuiltFromDirectory() throws Exception {
+    Path root = Paths.get("test/com/facebook/buck/apple/testdata/sdk-discovery");
+    ImmutableMap<String, ImmutableAppleSdkPaths> expected = ImmutableMap.of(
+        "macosx10.9",
+        ImmutableAppleSdkPaths.builder()
+            .toolchainPath(root.resolve("Toolchains/XcodeDefault.xctoolchain"))
+            .platformDeveloperPath(root.resolve("Platforms/MacOSX.platform/Developer"))
+            .sdkPath(root.resolve("Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"))
+            .build(),
+        "iphoneos8.0",
+        ImmutableAppleSdkPaths.builder()
+            .toolchainPath(root.resolve("Toolchains/XcodeDefault.xctoolchain"))
+            .platformDeveloperPath(root.resolve("Platforms/iPhoneOS.platform/Developer"))
+            .sdkPath(root.resolve("Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"))
+            .build(),
+        "iphonesimulator8.0",
+        ImmutableAppleSdkPaths.builder()
+            .toolchainPath(root.resolve("Toolchains/XcodeDefault.xctoolchain"))
+            .platformDeveloperPath(root.resolve("Platforms/iPhoneSimulator.platform/Developer"))
+            .sdkPath(
+                root.resolve(
+                    "Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk"))
+            .build());
+    assertEquals(expected, AppleSdkDiscovery.discoverAppleSdkPaths(root));
+  }
+}
