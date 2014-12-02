5d4c966cb275014c5a3d0df26f326429d636e52f
Ben Gertzfield <beng@fb.com>
2014-12-02 12:10:31 -0800

    New Hamcrest matcher hasConsecutiveItems()
    Summary:
    Hamcrest includes `Matcher` implementations which:
    
    1) Check if a collection includes a single item anywhere (`hasItem`)
    2) Check if a collection includes multiple items anywhere (`hasItems`)
    3) Check if a collection exactly matches another collection in order (`contains`)
    4) Check if a collection exactly matches another collection in any order (`containsInAnyOrder`)
    
    Bizarrely, Hamcrest even recently added a `Matcher` which searches for
    the items of a collection in relative order anywhere inside the target
    collection (`containsInRelativeOrder`). (Note that this searches anywhere
    inside the container, unlike the other `contains` `Matcher`s!)
    
    https://github.com/hamcrest/JavaHamcrest/pull/74
    
    We need a `Matcher` which finds a sub-collection inside a larger
    collection in order (i.e., finding `[2, 3]` inside `[1, 2, 3]` but not
    inside `[1, 2, 42, 3]`).
    
    This diff adds that matcher, `hasConsecutiveItems()`, along with tests.
    
    Test Plan: Unit tests includes.

---
+++ test/com/facebook/buck/testutil/HasConsecutiveItemsMatcher.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.testutil;
+
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
+
+/**
+ * Hamcrest matcher which checks if a collection contains another collection's items in order
+ * inside.
+ *
+ * Unlike isIn(), contains(), and containsInAnyOrder(), this matcher matches if the
+ * items to match are found in order with no gaps anywhere inside the collection.
+ */
+public class HasConsecutiveItemsMatcher<T>
+    extends TypeSafeDiagnosingMatcher<Collection<? extends T>> {
+  private final List<Matcher<? super T>> matchers;
+
+  public HasConsecutiveItemsMatcher(List<Matcher<? super T>> matchers) {
+    this.matchers = matchers;
+  }
+
+  @Override
+  public boolean matchesSafely(
+      Collection<? extends T> itemsToMatch,
+      Description mismatchDescription) {
+    List<? extends T> itemsToMatchList = new ArrayList<>(itemsToMatch);
+
+    for (int i = 0; i <= itemsToMatchList.size() - matchers.size(); i++) {
+      boolean allMatchersMatched = true;
+      for (int j = 0; j < matchers.size(); j++) {
+        Matcher<? super T> matcher = matchers.get(j);
+        if (!matcher.matches(itemsToMatchList.get(i + j))) {
+          allMatchersMatched = false;
+          break;
+        }
+      }
+      if (allMatchersMatched) {
+        return true;
+      }
+    }
+
+    mismatchDescription
+        .appendText("could not find items inside collection ")
+        .appendValueList("[", ", ", "]", itemsToMatch);
+    return false;
+  }
+
+  @Override
+  public void describeTo(Description description) {
+    description
+        .appendText("collection contains consecutive items matching ")
+        .appendList("[", ", ", "]", matchers);
+  }
+
+  @Factory
+  public static <T> Matcher<Collection<? extends T>> hasConsecutiveItems(
+      List<Matcher<? super T>> matchers) {
+    return new HasConsecutiveItemsMatcher<T>(matchers);
+  }
+
+  @Factory
+  @SafeVarargs
+  public static <T> Matcher<Collection<? extends T>> hasConsecutiveItems(
+      Matcher<? super T>... matchers) {
+    return hasConsecutiveItems(Arrays.asList(matchers));
+  }
+
+  @Factory
+  public static <T> Matcher<Collection<? extends T>> hasConsecutiveItems(
+      Iterable<? extends T> items) {
+    List<Matcher<? super T>> matchers = new ArrayList<>();
+    for (Object item : items) {
+      matchers.add(equalTo(item));
+    }
+    return new HasConsecutiveItemsMatcher<T>(matchers);
+  }
+
+  @Factory
+  @SafeVarargs
+  public static <T> Matcher<Collection<? extends T>> hasConsecutiveItems(T... elements) {
+    return hasConsecutiveItems(Arrays.asList(elements));
+  }
+}
---
+++ test/com/facebook/buck/testutil/HasConsecutiveItemsMatcherTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.testutil;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.IsNot.not;
+
+import static com.facebook.buck.testutil.HasConsecutiveItemsMatcher.hasConsecutiveItems;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Unit tests for {@link HasConsecutiveItemsMatcher}.
+ */
+public class HasConsecutiveItemsMatcherTest {
+
+  @Test
+  public void emptyIncludesEmpty() {
+    assertThat(
+        ImmutableList.of(),
+        hasConsecutiveItems());
+  }
+
+  @Test
+  public void nonEmptyIncludesEmpty() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems());
+  }
+
+  @Test
+  public void includesSingleItem() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems("a"));
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems("b"));
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems("c"));
+  }
+
+  @Test
+  public void includesSublist() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems("b", "c"));
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems("a", "b"));
+  }
+
+  @Test
+  public void includesEntireList() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems("a", "b", "c"));
+  }
+
+  @Test
+  public void doesNotIncludeExtraItem() {
+    assertThat(
+        ImmutableList.of("a", "b", "fnord", "c"),
+        not(hasConsecutiveItems("a", "b", "c")));
+  }
+
+  @Test
+  public void doesNotIncludeMissingItem() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        not(hasConsecutiveItems("d")));
+  }
+
+  @Test
+  public void doesNotIncludeListTooLong() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        not(hasConsecutiveItems("a", "b", "c", "d")));
+  }
+
+  @Test
+  public void includesCollection() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        hasConsecutiveItems(ImmutableList.of("b", "c")));
+  }
+
+  @Test
+  public void doesNotIncludeCollectionWithMissingItem() {
+    assertThat(
+        ImmutableList.of("a", "b", "c"),
+        not(hasConsecutiveItems(ImmutableList.of("d"))));
+  }
+}
