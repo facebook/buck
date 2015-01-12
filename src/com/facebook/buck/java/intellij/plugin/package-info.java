9ad0a6b76982c6b36e2fd9ca232d74b0726a427a
Damien Allison <damien@sitemorph.net>
2015-01-12 14:23:49 +0000

    Aded readme and package info as well as package plugin place holder.

--- src/com/facebook/buck/java/intellij/plugin/package-info.java
+++ src/com/facebook/buck/java/intellij/plugin/package-info.java
@@ -1,4 +1,10 @@
 /**
- * Created by dak on 12/01/15.
+ * IntelliJ Plugin to support primary developer actions for Buck based build and development using
+ * the community version of IntelliJ 1.4*. The plugin supports multiple functions around integration
+ * with core IntelliJ interactions and work flows. This plugin makes some initial assumptions around
+ * project layout and structure that are specific to standard practice at Facebook but it is
+ * intended that these be minimal.
+ *
+ * @author code@damienallison.com
  */
 package com.facebook.buck.java.intellij.plugin;
