4ba2edab6b5b9039255a32a0898837ad8c6946e4
Simon Stewart <simons@fb.com>
2015-01-15 09:59:52 -0800

    Extract an httpd for testing.
    Summary: It's generally useful to have an httpd that can be used for testing.
    
    Test Plan: buck test --all

--- test/com/facebook/buck/file/HttpDownloaderIntegrationTest.java
+++ test/com/facebook/buck/file/HttpDownloaderIntegrationTest.java
@@ -20,19 +20,11 @@ import static java.nio.charset.StandardCharsets.UTF_16;
 import static org.junit.Assert.assertEquals;
 
 import com.facebook.buck.event.BuckEventBusFactory;
+import com.facebook.buck.testutil.integration.HttpdForTests;
 import com.google.common.base.Optional;
 import com.google.common.io.Files;
 
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.handler.DefaultHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.MovedContextHandler;
-import org.eclipse.jetty.util.log.JavaUtilLog;
-import org.eclipse.jetty.util.log.Log;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -46,49 +38,26 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 public class HttpDownloaderIntegrationTest {
 
   @Rule public TemporaryFolder tmp = new TemporaryFolder();
 
-  private static Server server;
-  private static int port;
   private Downloader downloader;
   private Path outputDir;
+  private static HttpdForTests httpd;
 
   @BeforeClass
   public static void startHttpd() throws Exception {
-    // Configured using a singleton. *furrfu*
-    Log.setLog(new JavaUtilLog());
-    server = new Server();
+    httpd = new HttpdForTests();
+    httpd.addHandler(new MovedContextHandler(null, "/redirect", "/out"));
+    httpd.addStaticContent("cheese");
 
-    ServerConnector connector = new ServerConnector(server);
-    connector.addConnectionFactory(new HttpConnectionFactory());
-    // Choose a port randomly upon listening for socket connections.
-    connector.setPort(0);
-    server.addConnector(connector);
-
-    HandlerList handlers = new HandlerList();
-    handlers.addHandler(new MovedContextHandler(null, "/redirect", "/out"));
-    handlers.addHandler(new StaticContent("cheese"));
-    handlers.addHandler(new DefaultHandler());
-
-    server.setHandler(handlers);
-    server.start();
-
-    // Since we called setPort(0), an unused port number is chosen
-    // upon calling start().  This returns the actual port number to
-    // which the socket is locally bound.
-    port = connector.getLocalPort();
+    httpd.start();
   }
 
   @AfterClass
   public static void shutdownHttpd() throws Exception {
-    server.stop();
-    server.join();
+    httpd.close();
   }
 
   @Before
@@ -99,7 +68,7 @@ public class HttpDownloaderIntegrationTest {
 
   @Test
   public void canDownloadFromAUrlDirectly() throws IOException, URISyntaxException {
-    URI uri = new URI(String.format("http://localhost:%d/example", port));
+    URI uri = httpd.getUri("/example");
 
     Path output = outputDir.resolve("cheese");
     downloader.fetch(BuckEventBusFactory.newInstance(), uri, output);
@@ -109,33 +78,11 @@ public class HttpDownloaderIntegrationTest {
 
   @Test
   public void canDownloadFromAUrlWithARedirect() throws IOException, URISyntaxException {
-    URI uri = new URI(String.format("http://localhost:%d/redirect", port));
+    URI uri = httpd.getUri("/redirect");
 
     Path output = outputDir.resolve("cheese");
     downloader.fetch(BuckEventBusFactory.newInstance(), uri, output);
 
     assertEquals("cheese", Files.toString(output.toFile(), UTF_16));
   }
-
-
-  private static class StaticContent extends AbstractHandler {
-
-    private final String content;
-
-    public StaticContent(String content) {
-      this.content = content;
-    }
-
-    @Override
-    public void handle(
-        String target,
-        Request request,
-        HttpServletRequest httpServletRequest,
-        HttpServletResponse httpServletResponse) throws IOException, ServletException {
-      // Use an unusual charset.
-      httpServletResponse.getOutputStream().write(content.getBytes(UTF_16));
-      request.setHandled(true);
-    }
-  }
-
 }
---
+++ test/com/facebook/buck/testutil/integration/HttpdForTests.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.testutil.integration;
+
+import static java.nio.charset.StandardCharsets.UTF_16;
+import static org.junit.Assert.assertFalse;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.util.log.JavaUtilLog;
+import org.eclipse.jetty.util.log.Log;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* Lightweight wrapper around an httpd to make testing using an httpd nicer.
+*/
+public class HttpdForTests implements AutoCloseable {
+
+  private final HandlerList handlerList;
+  private Server server;
+  private boolean isRunning;
+
+  public HttpdForTests() {
+    // Configure the logging for jetty. Which uses a singleton. Ho hum.
+    Log.setLog(new JavaUtilLog());
+    server = new Server();
+
+    ServerConnector connector = new ServerConnector(server);
+    connector.addConnectionFactory(new HttpConnectionFactory());
+    // Choose a port randomly upon listening for socket connections.
+    connector.setPort(0);
+    server.addConnector(connector);
+
+    handlerList = new HandlerList();
+  }
+
+  public void addHandler(Handler handler) {
+    assertFalse(isRunning);
+
+    handlerList.addHandler(handler);
+  }
+
+  public void addStaticContent(String contentToReturn) {
+    addHandler(new StaticContent(contentToReturn));
+  }
+
+  public void start() throws Exception {
+    assertFalse(isRunning);
+
+    handlerList.addHandler(new DefaultHandler());
+    server.setHandler(handlerList);
+    server.start();
+  }
+
+  @Override
+  public void close() throws Exception {
+    server.stop();
+    server.join();
+    isRunning = false;
+  }
+
+  public URI getUri(String path) throws URISyntaxException {
+    URI baseUri = server.getURI();
+    return new URI(
+        baseUri.getScheme(), /* user info */
+        null,
+        baseUri.getHost(),
+        baseUri.getPort(),
+        path,
+        null,
+        null);
+  }
+
+  private static class StaticContent extends AbstractHandler {
+
+    private final String content;
+
+    public StaticContent(String content) {
+      this.content = content;
+    }
+
+    @Override
+    public void handle(
+        String target,
+        Request request,
+        HttpServletRequest httpServletRequest,
+        HttpServletResponse httpServletResponse) throws IOException, ServletException {
+      // Use an unusual charset.
+      httpServletResponse.getOutputStream().write(content.getBytes(UTF_16));
+      request.setHandled(true);
+    }
+  }
+}
