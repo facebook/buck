35102ea3ad8dd22cdabdf8ce710c4198c1f249c3
Ben Hamilton <beng@fb.com>
2015-01-23 11:13:48 -0800

    Skip testAppleLibraryBuildsSomethingUsingAppleCxxPlatform if macosx platform unavailable
    Summary:
    I ran `ant java-test` on an OS X host with no Xcode installed.
    The test `AppleLibraryIntegrationTest.testAppleLibraryBuildsSomethingUsingAppleCxxPlatform`
    failed:
    
        [junit] Testcase: testAppleLibraryBuildsSomethingUsingAppleCxxPlatform took 0.178 sec
        [junit] 	Caused an ERROR
        [junit] Unrecognized flavor in target //Libraries/TestLibrary:TestLibrary#macosx-x86_64,static while parsing //Libraries/TestLibrary/BUCK.
        [junit] com.facebook.buck.util.HumanReadableException: Unrecognized flavor in target //Libraries/TestLibrary:TestLibrary#macosx-x86_64,static while parsing //Libraries/TestLibrary/BUCK.
        [junit] 	at com.facebook.buck.parser.Parser$CachedState.get(Parser.java:1114)
        [junit] 	at com.facebook.buck.parser.Parser.getTargetNode(Parser.java:431)
        [junit] 	at com.facebook.buck.parser.Parser$2.findChildren(Parser.java:465)
        [junit] 	at com.facebook.buck.parser.Parser$2.findChildren(Parser.java:1)
        [junit] 	at com.facebook.buck.graph.AbstractAcyclicDepthFirstPostOrderTraversal$Explorable.<init>(AbstractAcyclicDepthFirstPostOrderTraversal.java:113)
        [junit] 	at com.facebook.buck.graph.AbstractAcyclicDepthFirstPostOrderTraversal.traverse(AbstractAcyclicDepthFirstPostOrderTraversal.java:53)
        [junit] 	at com.facebook.buck.parser.Parser.buildTargetGraph(Parser.java:524)
        [junit] 	at com.facebook.buck.parser.Parser.buildTargetGraphForTargetNodeSpecs(Parser.java:392)
        [junit] 	at com.facebook.buck.parser.Parser.buildTargetGraphForBuildTargets(Parser.java:419)
        [junit] 	at com.facebook.buck.cli.BuildCommand.runCommandWithOptionsInternal(BuildCommand.java:103)
        [junit] 	at com.facebook.buck.cli.BuildCommand.runCommandWithOptionsInternal(BuildCommand.java:1)
        [junit] 	at com.facebook.buck.cli.AbstractCommandRunner.runCommandWithOptions(AbstractCommandRunner.java:139)
        [junit] 	at com.facebook.buck.cli.AbstractCommandRunner.runCommand(AbstractCommandRunner.java:111)
        [junit] 	at com.facebook.buck.cli.Command.execute(Command.java:126)
        [junit] 	at com.facebook.buck.cli.Main.executeCommand(Main.java:656)
        [junit] 	at com.facebook.buck.cli.Main.runMainWithExitCode(Main.java:464)
        [junit] 	at com.facebook.buck.testutil.integration.ProjectWorkspace.runBuckCommandWithEnvironmentAndContext(ProjectWorkspace.java:288)
        [junit] 	at com.facebook.buck.testutil.integration.ProjectWorkspace.runBuckCommand(ProjectWorkspace.java:221)
        [junit] 	at com.facebook.buck.apple.AppleLibraryIntegrationTest.testAppleLibraryBuildsSomethingUsingAppleCxxPlatform(AppleLibraryIntegrationTest.java:61)
    
    This is because there are no SDKs installed on this host, so detection provides no matching SDK.
    
    This diff skips the test if there are no OS X SDKs installed locally instead of failing it.
    
    Test Plan:
    ant java-test -Dtest.class=AppleLibraryIntegrationTest
    
    After fix, before installing Xcode:
    
        [junit] Testsuite: com.facebook.buck.apple.AppleLibraryIntegrationTest
        [junit] Tests run: 2, Failures: 0, Errors: 0, Skipped: 1, Time elapsed: 2.39 sec
        [junit]
        [junit] Testcase: testAppleLibraryBuildsSomethingUsingAppleCxxPlatform took 0.256 sec
        [junit] 	SKIPPED: got: <false>, expected: is <true>
        [junit] Testcase: testAppleLibraryBuildsSomethingUsingAppleCxxPlatform took 0.256 sec
        [junit] Testcase: testAppleLibraryBuildsSomething took 2.114 sec
    
    After installing Xcode:
    
        [junit] Testsuite: com.facebook.buck.apple.AppleLibraryIntegrationTest
        [junit] Tests run: 2, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 2.306 sec
        [junit]
        [junit] Testcase: testAppleLibraryBuildsSomethingUsingAppleCxxPlatform took 1.834 sec
        [junit] Testcase: testAppleLibraryBuildsSomething took 0.455 sec

--- test/com/facebook/buck/apple/AppleLibraryIntegrationTest.java
+++ test/com/facebook/buck/apple/AppleLibraryIntegrationTest.java
@@ -19,11 +19,19 @@ package com.facebook.buck.apple;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
+import com.facebook.buck.cli.BuckConfig;
+import com.facebook.buck.cli.FakeBuckConfig;
+import com.facebook.buck.testutil.TestConsole;
 import com.facebook.buck.testutil.integration.DebuggableTemporaryFolder;
 import com.facebook.buck.testutil.integration.ProjectWorkspace;
 import com.facebook.buck.testutil.integration.TestDataHelper;
 import com.facebook.buck.util.BuckConstant;
+import com.facebook.buck.util.Console;
+import com.facebook.buck.util.ProcessExecutor;
 import com.facebook.buck.util.environment.Platform;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -54,6 +62,8 @@ public class AppleLibraryIntegrationTest {
   @Test
   public void testAppleLibraryBuildsSomethingUsingAppleCxxPlatform() throws IOException {
     assumeTrue(Platform.detect() == Platform.MACOS);
+    assumeTrue(isApplePlatformAvailable(ApplePlatform.MACOSX));
+
     ProjectWorkspace workspace = TestDataHelper.createProjectWorkspaceForScenario(
         this, "apple_library_builds_something", tmp);
     workspace.setUp();
@@ -66,4 +76,18 @@ public class AppleLibraryIntegrationTest {
     assertTrue(Files.exists(tmp.getRootPath().resolve(BuckConstant.GEN_DIR)));
   }
 
+  private static boolean isApplePlatformAvailable(final ApplePlatform platform) {
+    BuckConfig buckConfig = new FakeBuckConfig();
+    AppleConfig appleConfig = new AppleConfig(buckConfig);
+    Console console = new TestConsole();
+    ProcessExecutor executor = new ProcessExecutor(console);
+    return Iterables.any(
+        appleConfig.getAppleSdkPaths(executor).keySet(),
+        new Predicate<AppleSdk>() {
+            @Override
+            public boolean apply(AppleSdk sdk) {
+              return sdk.getApplePlatform() == platform;
+            }
+        });
+  }
 }
