203739570da1b3ba66a35f845d854494cd80ac1c
Daniel Colascione <dancol@fb.com>
2015-01-25 20:10:33 -0800

    Add a ClassLoaderCache facility we'll use later
    Test Plan: Later diffs

---
+++ src/com/facebook/buck/util/ClassLoaderCache.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.HashMap;
+import javax.annotation.Nullable;
+
+/**
+ * Maintain a cache mapping class paths to class loaders that load from these class paths.  The
+ * class loaders remain active until ClassLoaderCache itself is unloaded.
+ */
+public final class ClassLoaderCache implements AutoCloseable {
+
+  private static final Function<Path, URL> PATH_TO_URL = new Function<Path, URL>() {
+      @Override
+      public URL apply(Path p) {
+        try {
+          return p.toUri().toURL();
+        } catch (MalformedURLException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    };
+
+  private final Map<ClassLoader, Map<ImmutableList<Path>, ClassLoader>> cache = new HashMap<>();
+
+  private int referenceCount = 1;
+
+  public synchronized ClassLoader getClassLoaderForClassPath(
+      @Nullable ClassLoader parentClassLoader,
+      ImmutableList<Path> classPath) {
+    Map<ImmutableList<Path>, ClassLoader> cacheForParent =
+        cache.get(parentClassLoader);
+
+    if (cacheForParent == null) {
+      cacheForParent = new HashMap<>();
+      cache.put(parentClassLoader, cacheForParent);
+    }
+
+    ClassLoader classLoader = cacheForParent.get(classPath);
+    if (classLoader == null) {
+      URL[] urls = FluentIterable.from(classPath)
+          .transform(PATH_TO_URL)
+          .toArray(URL.class);
+      classLoader = new URLClassLoader(urls, parentClassLoader);
+      cacheForParent.put(classPath, classLoader);
+    }
+
+    return classLoader;
+  }
+
+  public synchronized ClassLoaderCache addRef() {
+    referenceCount += 1;
+    return this;
+  }
+
+  @Override
+  public synchronized void close() throws IOException {
+    if (referenceCount > 1) {
+      referenceCount -= 1;
+      return;
+    }
+
+    Optional<IOException> caughtEx = Optional.absent();
+
+    for (Map<ImmutableList<Path>, ClassLoader> cacheForParent : cache.values()) {
+      for (ClassLoader cl : cacheForParent.values()) {
+        try {
+          if (cl instanceof URLClassLoader) {
+            ((URLClassLoader) cl).close();
+          }
+        } catch (IOException ex) {
+          if (caughtEx.isPresent()) {
+            caughtEx.get().addSuppressed(ex);
+          } else {
+            caughtEx = Optional.of(ex);
+          }
+        }
+      }
+    }
+
+    if (caughtEx.isPresent()) {
+      throw caughtEx.get();
+    }
+  }
+}
---
+++ test/com/facebook/buck/util/ClassLoaderCacheTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.FluentIterable;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class ClassLoaderCacheTest {
+  private static final String DUMMYDIR = "d7b9d9fd-1a83-4c76-8981-52deb0fa4d17";
+
+  private static final Function<Path, URL> PATH_TO_URL = new Function<Path, URL>() {
+      @Override
+      public URL apply(Path p) {
+        try {
+          return p.toUri().toURL();
+        } catch (MalformedURLException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    };
+
+  @Test
+  public void cacheLoaderReturnsSameClassLoader() {
+    ClassLoaderCache clc = new ClassLoaderCache();
+    ClassLoader dummyParent = ClassLoader.getSystemClassLoader();
+    ImmutableList<Path> dummyClassPath = ImmutableList.of(
+        Paths.get(DUMMYDIR, "foo"),
+        Paths.get(DUMMYDIR, "bar"));
+    ClassLoader cl1 = clc.getClassLoaderForClassPath(dummyParent, dummyClassPath);
+    ClassLoader cl2 = clc.getClassLoaderForClassPath(dummyParent, dummyClassPath);
+
+    // The class loader had better have been cached
+    assertSame(cl1, cl2);
+
+    // And the class loader should contain the URLs we supplied
+    URL[] dummyUrls = FluentIterable.from(dummyClassPath)
+        .transform(PATH_TO_URL)
+        .toArray(URL.class);
+
+    assertArrayEquals(
+        dummyUrls,
+        ((URLClassLoader) cl1).getURLs());
+  }
+}
