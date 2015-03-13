cff381c788db9755c9f92f56a93eab24989d8633
Jakub Zika <jakubzika@fb.com>
2015-03-13 10:22:19 -0700

    Add a heuristic check for absolute paths with @AddToRuleKey
    Summary:
      * Add a simple heuristic method that checks whether an object whose string
        value is being added to the rule key contains an absolute path. The method
        is not perfect as it does not take into account how the object's `toString`
        method works, but it should work sufficiently well with most types that we
        put into the rule key.
    
    Test Plan:
      * Run for an existing project as-is, check that no warnings are generated.
      * Modify a rule to use `@AddToRuleKey` on an object containing an absolute
        path, check that warnings are generated.
      * `buck test`.

--- src/com/facebook/buck/rules/keys/StringifyAlterRuleKey.java
+++ src/com/facebook/buck/rules/keys/StringifyAlterRuleKey.java
@@ -16,15 +16,64 @@
 
 package com.facebook.buck.rules.keys;
 
+import com.facebook.buck.log.Logger;
 import com.facebook.buck.rules.BuildRule;
+import com.facebook.buck.rules.PathSourcePath;
 import com.facebook.buck.rules.RuleKey;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 
 import java.lang.reflect.Field;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Map;
 
 class StringifyAlterRuleKey extends AbstractAlterRuleKey {
 
+  private static final Logger LOG = Logger.get(StringifyAlterRuleKey.class);
+
+  private static final Function<Object, Iterable<Path>> FIND_ABSOLUTE_PATHS =
+      new Function<Object, Iterable<Path>>() {
+        @Override
+        public Iterable<Path> apply(Object val) {
+          return findAbsolutePaths(val);
+        }
+      };
+
   public StringifyAlterRuleKey(Field field) {
     super(field);
+  }
+
+  @VisibleForTesting
+  static Iterable<Path> findAbsolutePaths(Object val) {
+    if (val instanceof Path) {
+      Path path = (Path) val;
+      if (path.isAbsolute()) {
+        return Collections.singleton(path);
+      }
+    } else if (val instanceof PathSourcePath) {
+      return findAbsolutePaths(((PathSourcePath) val).getRelativePath());
+    } else if (val instanceof Iterable) {
+      return FluentIterable.from((Iterable<?>) val)
+          .transformAndConcat(FIND_ABSOLUTE_PATHS);
+    } else if (val instanceof Map) {
+      Map<?, ?> map = (Map<?, ?>) val;
+      Iterable<?> allSubValues = Iterables.concat(map.keySet(), map.values());
+      return FluentIterable.from(allSubValues)
+          .transformAndConcat(FIND_ABSOLUTE_PATHS);
+    } else if (val instanceof Optional) {
+      Optional<?> optional = (Optional<?>) val;
+      if (optional.isPresent()) {
+        return findAbsolutePaths(optional.get());
+      }
+    }
+
+    return ImmutableList.of();
   }
 
   @Override
@@ -33,5 +82,15 @@ class StringifyAlterRuleKey extends AbstractAlterRuleKey {
     builder.setReflectively(
         field.getName(),
         val == null ? null : String.valueOf(val));
+
+    if (val != null) {
+      Iterable<Path> absolutePaths = findAbsolutePaths(val);
+      if (!Iterables.isEmpty(absolutePaths)) {
+        LOG.warn(
+            "Field %s contains absolute paths %s and it is included in a rule key.",
+            field.getName(),
+            ImmutableSet.copyOf(absolutePaths));
+      }
+    }
   }
 }
---
+++ test/com/facebook/buck/rules/keys/StringifyAlterRuleKeyTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.rules.keys;
+
+import com.facebook.buck.rules.PathSourcePath;
+import com.facebook.buck.rules.SourcePath;
+import com.facebook.buck.testutil.FakeProjectFilesystem;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class StringifyAlterRuleKeyTest {
+
+  @Test
+  public void findAbsolutePathsInAbsolutePath() {
+    Path path = Paths.get("/some/thing");
+    Assert.assertEquals(
+        ImmutableSet.of(path),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(path)));
+  }
+
+  @Test
+  public void findAbsolutePathsInRelativePath() {
+    Path path = Paths.get("some/thing");
+    Assert.assertEquals(
+        ImmutableSet.of(),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(path)));
+  }
+
+  @Test
+  public void findAbsolutePathsInListOfPaths() {
+    Path path1 = Paths.get("/some/thing");
+    Path path2 = Paths.get("some/thing");
+    List<Path> input = ImmutableList.of(path1, path2);
+    Assert.assertEquals(
+        ImmutableSet.of(path1),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(input)));
+  }
+
+  @Test
+  public void findAbsolutePathsInMapOfPaths() {
+    Path path1 = Paths.get("/some/thing");
+    Path path2 = Paths.get("some/thing");
+    Path path3 = Paths.get("other/thing");
+    Path path4 = Paths.get("/other/thing");
+    Map<Path, Path> input = ImmutableMap.of(
+        path1, path2,
+        path3, path4);
+    Assert.assertEquals(
+        ImmutableSet.of(path1, path4),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(input)));
+  }
+
+  @Test
+  public void findAbsolutePathsInAbsentOptional() {
+    Optional<Path> input = Optional.absent();
+    Assert.assertEquals(
+        ImmutableSet.<Path>of(),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(input)));
+  }
+
+  @Test
+  public void findAbsolutePathsInListOfOptionals() {
+    Path path1 = Paths.get("/some/thing");
+    Path path2 = Paths.get("some/thing");
+    List<Optional<Path>> input = ImmutableList.of(
+        Optional.<Path>absent(),
+        Optional.of(path2),
+        Optional.<Path>absent(),
+        Optional.of(path1));
+    Assert.assertEquals(
+        ImmutableSet.of(path1),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(input)));
+  }
+
+  @Test
+  public void findAbsolutePathsInListOfPathSourcePaths() {
+    FakeProjectFilesystem projectFilesystem = new FakeProjectFilesystem();
+    Path path1 = Paths.get("/some/thing");
+    Path path2 = Paths.get("some/thing");
+    List<SourcePath> input = ImmutableList.<SourcePath>of(
+        new PathSourcePath(projectFilesystem, path2),
+        new PathSourcePath(projectFilesystem, path1));
+    Assert.assertEquals(
+        ImmutableSet.of(path1),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(input)));
+  }
+
+  @Test
+  public void findAbsolutePathsInRecursiveStructure() {
+    FakeProjectFilesystem projectFilesystem = new FakeProjectFilesystem();
+    Path path1 = Paths.get("/some/thing");
+    Path path2 = Paths.get("/other/thing");
+    Path path3 = Paths.get("/yet/another/thing");
+    Object input = ImmutableList.of(
+        ImmutableMap.of(
+            Optional.absent(), path1),
+        ImmutableSet.of(Optional.of(path2)),
+        Optional.absent(),
+        Optional.of(new PathSourcePath(projectFilesystem, path3)));
+    Assert.assertEquals(
+        ImmutableSet.of(path1, path2, path3),
+        ImmutableSet.copyOf(
+            StringifyAlterRuleKey.findAbsolutePaths(input)));
+  }
+
+}
