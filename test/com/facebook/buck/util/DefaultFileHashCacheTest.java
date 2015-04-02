1c6f556ebfdca23cfc6207feab3f8f71b470ade9
Ben Hamilton <beng@fb.com>
2015-04-02 13:34:53 -0700

    Support hashing directories in DefaultFileHashCache
    Summary:
    Previously, `DefaultFileHashCache` only supported calculating
    and storing hash codes for normal files.
    
    If you tried to get a hash code for a directory, you got a lovely error:
    
      Caused by: java.io.FileNotFoundException: path/to/dir (Is a directory)
      at java.io.FileInputStream.open0(Native Method)
      at java.io.FileInputStream.open(FileInputStream.java:195)
      at java.io.FileInputStream.<init>(FileInputStream.java:138)
      at com.google.common.io.Files$FileByteSource.openStream(Files.java:125)
      at com.google.common.io.Files$FileByteSource.openStream(Files.java:115)
      at com.google.common.io.ByteSource.copyTo(ByteSource.java:219)
      at com.google.common.io.ByteSource.hash(ByteSource.java:297)
      at com.facebook.buck.util.DefaultFileHashCache.getHashCode(DefaultFileHashCache.java:64)
      at com.facebook.buck.util.DefaultFileHashCache.access$0(DefaultFileHashCache.java:60)
      at com.facebook.buck.util.DefaultFileHashCache$1.load(DefaultFileHashCache.java:55)
      at com.facebook.buck.util.DefaultFileHashCache$1.load(DefaultFileHashCache.java:1)
    
    This diff builds on my previous diff
    `Refactor path hashing out of TargetGraphHashing into new class PathHashing`
    and adds support for hashing paths to `DefaultFileHashCache`.
    
    This allows `BuildRule`s to include `Path`s which refer to directories
    in their `RuleKey`s, which I will use in `AppleBundle` rules.
    
    I was very careful to only add file I/O to check if the `Path` refers
    to a file or directory in the codepath where we were doing I/O anyway
    (i.e., calling `get(Path)` on an already-cached `Path` still doesn't do I/O).
    
    I'm not sure how we want to handle empty directories. We already ignore directory
    filesystem events in `WatchmanWatcher`; I investigating extending it to support
    directories, but I was worried there would be a lot of unintended side effects.
    
    Test Plan: Integration and unit tests added.

--- test/com/facebook/buck/util/DefaultFileHashCacheTest.java
+++ test/com/facebook/buck/util/DefaultFileHashCacheTest.java
@@ -20,6 +20,7 @@ import static com.facebook.buck.testutil.WatchEvents.createOverflowEvent;
 import static com.facebook.buck.testutil.WatchEvents.createPathEvent;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.facebook.buck.io.ProjectFilesystem;
@@ -49,7 +50,10 @@ public class DefaultFileHashCacheTest {
     DefaultFileHashCache cache =
         new DefaultFileHashCache(new FakeProjectFilesystem());
     Path path = new File("SomeClass.java").toPath();
-    cache.loadingCache.put(path, HashCode.fromInt(42));
+    HashCodeAndFileType value = HashCodeAndFileType.of(
+        HashCode.fromInt(42),
+        HashCodeAndFileType.Type.FILE);
+    cache.loadingCache.put(path, value);
     assertTrue("Cache should contain path", cache.contains(path));
   }
 
@@ -58,29 +62,35 @@ public class DefaultFileHashCacheTest {
     DefaultFileHashCache cache =
         new DefaultFileHashCache(new FakeProjectFilesystem());
     Path path = new File("SomeClass.java").toPath();
-    HashCode hash = HashCode.fromInt(42);
-    cache.loadingCache.put(path, hash);
-    assertEquals("Cache should contain hash", hash, cache.get(path));
+    HashCodeAndFileType value = HashCodeAndFileType.of(
+        HashCode.fromInt(42),
+        HashCodeAndFileType.Type.FILE);
+    cache.loadingCache.put(path, value);
+    assertEquals("Cache should contain hash", value.getHashCode(), cache.get(path));
   }
 
   @Test
   public void whenNotifiedOfOverflowEventCacheIsCleared() throws IOException {
-    DefaultFileHashCache cache =
-        new DefaultFileHashCache(new FakeProjectFilesystem());
+    DefaultFileHashCache cache = new DefaultFileHashCache(
+        new FakeProjectFilesystem());
     Path path = new File("SomeClass.java").toPath();
-    HashCode hash = HashCode.fromInt(42);
-    cache.loadingCache.put(path, hash);
+    HashCodeAndFileType value = HashCodeAndFileType.of(
+        HashCode.fromInt(42),
+        HashCodeAndFileType.Type.FILE);
+    cache.loadingCache.put(path, value);
     cache.onFileSystemChange(createOverflowEvent());
     assertFalse("Cache should not contain path", cache.contains(path));
   }
 
   @Test
   public void whenNotifiedOfCreateEventCacheEntryIsRemoved() throws IOException {
-    DefaultFileHashCache cache =
-        new DefaultFileHashCache(new FakeProjectFilesystem());
+    DefaultFileHashCache cache = new DefaultFileHashCache(
+        new FakeProjectFilesystem());
     Path path = Paths.get("SomeClass.java");
-    HashCode hash = HashCode.fromInt(42);
-    cache.loadingCache.put(path, hash);
+    HashCodeAndFileType value = HashCodeAndFileType.of(
+        HashCode.fromInt(42),
+        HashCodeAndFileType.Type.FILE);
+    cache.loadingCache.put(path, value);
     cache.onFileSystemChange(createPathEvent(path, StandardWatchEventKinds.ENTRY_CREATE));
     assertFalse("Cache should not contain path", cache.contains(path));
   }
@@ -90,8 +100,10 @@ public class DefaultFileHashCacheTest {
     DefaultFileHashCache cache =
         new DefaultFileHashCache(new FakeProjectFilesystem());
     Path path = Paths.get("SomeClass.java");
-    HashCode hash = HashCode.fromInt(42);
-    cache.loadingCache.put(path, hash);
+    HashCodeAndFileType value = HashCodeAndFileType.of(
+        HashCode.fromInt(42),
+        HashCodeAndFileType.Type.FILE);
+    cache.loadingCache.put(path, value);
     cache.onFileSystemChange(createPathEvent(path, StandardWatchEventKinds.ENTRY_MODIFY));
     assertFalse("Cache should not contain path", cache.contains(path));
   }
@@ -101,8 +113,10 @@ public class DefaultFileHashCacheTest {
     DefaultFileHashCache cache =
         new DefaultFileHashCache(new FakeProjectFilesystem());
     Path path = Paths.get("SomeClass.java");
-    HashCode hash = HashCode.fromInt(42);
-    cache.loadingCache.put(path, hash);
+    HashCodeAndFileType value = HashCodeAndFileType.of(
+        HashCode.fromInt(42),
+        HashCodeAndFileType.Type.FILE);
+    cache.loadingCache.put(path, value);
     cache.onFileSystemChange(createPathEvent(path, StandardWatchEventKinds.ENTRY_DELETE));
     assertFalse("Cache should not contain path", cache.contains(path));
   }
@@ -120,5 +134,42 @@ public class DefaultFileHashCacheTest {
     Files.write("class SomeClass {}".getBytes(Charsets.US_ASCII), inputFile);
     cache.get(Paths.get(ignoredFile));
     assertFalse("Cache should not contain path.", cache.contains(inputFile.toPath()));
+  }
+
+  @Test
+  public void directoryHashChangesWhenFileInsideDirectoryChanges() throws IOException {
+    DefaultFileHashCache cache =
+        new DefaultFileHashCache(
+            new ProjectFilesystem(
+                tmp.getRoot().toPath()));
+    tmp.newFolder("foo", "bar");
+    File inputFile = tmp.newFile("foo/bar/baz");
+    Files.write("Hello world".getBytes(Charsets.UTF_8), inputFile);
+
+    Path dir = Paths.get("foo/bar");
+    HashCode dirHash = cache.get(dir);
+    Files.write("Goodbye world".getBytes(Charsets.UTF_8), inputFile);
+    cache.onFileSystemChange(
+        createPathEvent(
+            dir.resolve("baz"),
+            StandardWatchEventKinds.ENTRY_MODIFY));
+    HashCode dirHash2 = cache.get(dir);
+    assertNotEquals(dirHash, dirHash2);
+  }
+
+  @Test
+  public void whenNotifiedOfChangeToSubPathThenDirCacheEntryIsRemoved() throws IOException {
+    DefaultFileHashCache cache =
+        new DefaultFileHashCache(new FakeProjectFilesystem());
+    Path dir = Paths.get("foo/bar/baz");
+    HashCodeAndFileType value = HashCodeAndFileType.of(
+        HashCode.fromInt(42),
+        HashCodeAndFileType.Type.DIRECTORY);
+    cache.loadingCache.put(dir, value);
+    cache.onFileSystemChange(
+        createPathEvent(
+            dir.resolve("blech"),
+            StandardWatchEventKinds.ENTRY_CREATE));
+    assertFalse("Cache should not contain path", cache.contains(dir));
   }
 }
