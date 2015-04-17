a09bdf146289e4d6a82a8711db291c693ded6d5e
David Reiss <dreiss@fb.com>
2015-04-17 15:54:06 -0700

    Call super.on{Create,Destroy} in exopackage test
    Summary:
    The test app was crashing without this, which was making junk show up in
    the logs.
    
    Test Plan: Ran the test.  Saw a clean logcat.

--- test/com/facebook/buck/android/testdata/exopackage-device/LogActivity.java
+++ test/com/facebook/buck/android/testdata/exopackage-device/LogActivity.java
@@ -28,6 +28,8 @@ public class LogActivity extends Activity {
 
   @Override
   public void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+
     Log.i("EXOPACKAGE_TEST", "VALUE=" + Value.VALUE);
 
     ExopackageSoLoader.loadLibrary("one");
@@ -41,6 +43,8 @@ public class LogActivity extends Activity {
 
   @Override
   public void onDestroy() {
+    super.onDestroy();
+
     // Workaround for the fact that that "am force-stop" doesn't work on Gingerbread.
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
       Process.killProcess(Process.myPid());
