f31d001124ba1c319a6fa6cb31fb391c36d0c933
Ben Hamilton <beng@fb.com>
2015-05-05 10:29:30 -0700

    Apple tests: New class XctoolRunTestsStep
    Summary:
    This is split off from my larger diff
    `Natively run Apple tests [3/3]: Run Apple tests with xctool`.
    
    In this diff, I introduce a new class `XctoolRunTestsStep` which
    manages running `xctool` with the arguments needed to launch one
    or more logic or application tests.
    
    This depends on a few changes I'm currently working on in the
    xctool github repo with @nekto:
    
    https://github.com/facebook/xctool/tree/nekto/logic-and-app-tests
    
    We'll be sure to clean those up and land them in master before
    making Buck depend on them.
    
    Test Plan:
    Unit tests included.
      ant java-test -Dtest.class=XctoolRunTestsStepTest
      buck test //test/com/facebook/buck/apple:apple -f XctoolRunTestsStepTest

---
+++ src/com/facebook/buck/apple/XctoolRunTestsStep.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple;
+
+import com.facebook.buck.shell.ShellStep;
+import com.facebook.buck.util.ProcessExecutor;
+import com.facebook.buck.step.ExecutionContext;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.Collection;
+import java.util.Map;
+import java.nio.file.Path;
+
+/**
+ * {@link ShellStep} implementation which runs {@code xctool} on
+ * one or more logic tests and/or application tests (each paired with
+ * a host application).
+ *
+ * The output is written in streaming JSON format to {@code outputPath}
+ * and can be parsed by {@link XctoolOutputParsing}.
+ */
+public class XctoolRunTestsStep extends ShellStep {
+
+  private final Path xctoolPath;
+  private final String architecture;
+  private final String sdkName;
+  private final ImmutableSet<Path> logicTestBundlePaths;
+  private final ImmutableMap<Path, Path> appTestBundleToHostAppPaths;
+  private final Path outputPath;
+
+  public XctoolRunTestsStep(
+      Path xctoolPath,
+      String architecture,
+      String sdkName,
+      Collection<Path> logicTestBundlePaths,
+      Map<Path, Path> appTestBundleToHostAppPaths,
+      Path outputPath) {
+    Preconditions.checkArgument(
+        !(logicTestBundlePaths.isEmpty() &&
+          appTestBundleToHostAppPaths.isEmpty()),
+        "Either logic tests (%s) or app tests (%s) must be present",
+        logicTestBundlePaths,
+        appTestBundleToHostAppPaths);
+
+    // Each test bundle must have one of these extensions. (xctool
+    // depends on them to choose which test runner to use.)
+    Preconditions.checkArgument(
+        AppleBundleExtensions.allPathsHaveValidTestExtensions(logicTestBundlePaths),
+        "Extension of all logic tests must be one of %s (got %s)",
+        AppleBundleExtensions.VALID_XCTOOL_BUNDLE_EXTENSIONS,
+        logicTestBundlePaths);
+    Preconditions.checkArgument(
+        AppleBundleExtensions.allPathsHaveValidTestExtensions(logicTestBundlePaths),
+        "Extension of all app tests must be one of %s (got %s)",
+        AppleBundleExtensions.VALID_XCTOOL_BUNDLE_EXTENSIONS,
+        appTestBundleToHostAppPaths.keySet());
+
+    this.xctoolPath = xctoolPath;
+    this.architecture = architecture;
+    this.sdkName = sdkName;
+    this.logicTestBundlePaths = ImmutableSet.copyOf(logicTestBundlePaths);
+    this.appTestBundleToHostAppPaths = ImmutableMap.copyOf(appTestBundleToHostAppPaths);
+    this.outputPath = outputPath;
+  }
+
+  @Override
+  public String getShortName() {
+    return "xctool-run-tests";
+  }
+
+  @Override
+  protected ImmutableList<String> getShellCommandInternal(ExecutionContext context) {
+    ImmutableList.Builder<String> args = ImmutableList.builder();
+    args.add(xctoolPath.toString());
+    args.add("-reporter");
+    args.add("json-stream:" + outputPath.toString());
+    args.add("-sdk", sdkName);
+    args.add("-destination");
+    args.add("arch=" + architecture);
+    for (Path logicTestBundlePath : logicTestBundlePaths) {
+      args.add("-logicTest");
+      args.add(logicTestBundlePath.toString());
+    }
+    for (Map.Entry<Path, Path> appTestBundleAndHostApp : appTestBundleToHostAppPaths.entrySet()) {
+      args.add("-appTest");
+      args.add(appTestBundleAndHostApp.getKey() + ":" + appTestBundleAndHostApp.getValue());
+    }
+    args.add("run-tests");
+
+    return args.build();
+  }
+
+  @Override
+  protected int getExitCodeFromResult(ExecutionContext context, ProcessExecutor.Result result) {
+    int processExitCode = result.getExitCode();
+    if (processExitCode == 0 || processExitCode == 1) {
+      // Test failure is denoted by xctool returning 1. Unfortunately, there's no way
+      // to distinguish an internal xctool error from a test failure:
+      //
+      // https://github.com/facebook/xctool/issues/511
+      //
+      // We don't want to fail the step on a test failure, so return 0 on either
+      // xctool exit code.
+      return 0;
+    } else {
+      // Some unknown failure.
+      return processExitCode;
+    }
+  }
+}
---
+++ test/com/facebook/buck/apple/XctoolRunTestsStepTest.java
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple;
+
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+import com.facebook.buck.step.ExecutionContext;
+import com.facebook.buck.step.TestExecutionContext;
+import com.facebook.buck.testutil.FakeProjectFilesystem;
+import com.facebook.buck.util.FakeProcess;
+import com.facebook.buck.util.FakeProcessExecutor;
+import com.facebook.buck.util.ProcessExecutorParams;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableMap;
+
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class XctoolRunTestsStepTest {
+
+  @Test
+  public void xctoolCommandWithOnlyLogicTests() throws Exception {
+    FakeProjectFilesystem projectFilesystem = new FakeProjectFilesystem();
+    XctoolRunTestsStep step = new XctoolRunTestsStep(
+        Paths.get("/path/to/xctool"),
+        "x86_64",
+        "iphonesimulator",
+        ImmutableSet.of(Paths.get("/path/to/Foo.xctest")),
+        ImmutableMap.<Path, Path>of(),
+        Paths.get("/path/to/output.json"));
+    ProcessExecutorParams xctoolParams =
+        ProcessExecutorParams.builder()
+            .setCommand(
+                ImmutableList.of(
+                    "/path/to/xctool",
+                    "-reporter",
+                    "json-stream:/path/to/output.json",
+                    "-sdk",
+                    "iphonesimulator",
+                    "-destination",
+                    "arch=x86_64",
+                    "-logicTest",
+                    "/path/to/Foo.xctest",
+                    "run-tests"))
+            // This mimics the hard-coded behavior of ShellStep.
+            .setDirectory(projectFilesystem.getRootPath().toAbsolutePath().toFile())
+            .setEnvironment(ImmutableMap.<String, String>of())
+            .build();
+    FakeProcess fakeXctoolSuccess = new FakeProcess(0, "", "");
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.of(xctoolParams, fakeXctoolSuccess));
+    ExecutionContext executionContext = TestExecutionContext.newBuilder()
+        .setProcessExecutor(processExecutor)
+        .setProjectFilesystem(projectFilesystem)
+        .setEnvironment(ImmutableMap.<String, String>of())
+        .build();
+    assertThat(
+        step.execute(executionContext),
+        equalTo(0));
+  }
+
+  @Test
+  public void xctoolCommandWithOnlyAppTests() throws Exception {
+    FakeProjectFilesystem projectFilesystem = new FakeProjectFilesystem();
+    XctoolRunTestsStep step = new XctoolRunTestsStep(
+        Paths.get("/path/to/xctool"),
+        "x86_64",
+        "iphonesimulator",
+        ImmutableSet.<Path>of(),
+        ImmutableMap.of(
+            Paths.get("/path/to/FooAppTest.xctest"),
+            Paths.get("/path/to/Foo.app")),
+        Paths.get("/path/to/output.json"));
+    ProcessExecutorParams xctoolParams =
+        ProcessExecutorParams.builder()
+            .setCommand(
+                ImmutableList.of(
+                    "/path/to/xctool",
+                    "-reporter",
+                    "json-stream:/path/to/output.json",
+                    "-sdk",
+                    "iphonesimulator",
+                    "-destination",
+                    "arch=x86_64",
+                    "-appTest",
+                    "/path/to/FooAppTest.xctest:/path/to/Foo.app",
+                    "run-tests"))
+            // This mimics the hard-coded behavior of ShellStep.
+            .setDirectory(projectFilesystem.getRootPath().toAbsolutePath().toFile())
+            .setEnvironment(ImmutableMap.<String, String>of())
+            .build();
+    FakeProcess fakeXctoolSuccess = new FakeProcess(0, "", "");
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.of(xctoolParams, fakeXctoolSuccess));
+    ExecutionContext executionContext = TestExecutionContext.newBuilder()
+        .setProcessExecutor(processExecutor)
+        .setProjectFilesystem(projectFilesystem)
+        .setEnvironment(ImmutableMap.<String, String>of())
+        .build();
+    assertThat(
+        step.execute(executionContext),
+        equalTo(0));
+  }
+
+  @Test
+  public void xctoolCommandWithAppAndLogicTests() throws Exception {
+    FakeProjectFilesystem projectFilesystem = new FakeProjectFilesystem();
+    XctoolRunTestsStep step = new XctoolRunTestsStep(
+        Paths.get("/path/to/xctool"),
+        "x86_64",
+        "iphonesimulator",
+        ImmutableSet.of(
+            Paths.get("/path/to/FooLogicTest.xctest")),
+        ImmutableMap.of(
+            Paths.get("/path/to/FooAppTest.xctest"),
+            Paths.get("/path/to/Foo.app")),
+        Paths.get("/path/to/output.json"));
+    ProcessExecutorParams xctoolParams =
+        ProcessExecutorParams.builder()
+            .setCommand(
+                ImmutableList.of(
+                    "/path/to/xctool",
+                    "-reporter",
+                    "json-stream:/path/to/output.json",
+                    "-sdk",
+                    "iphonesimulator",
+                    "-destination",
+                    "arch=x86_64",
+                    "-logicTest",
+                    "/path/to/FooLogicTest.xctest",
+                    "-appTest",
+                    "/path/to/FooAppTest.xctest:/path/to/Foo.app",
+                    "run-tests"))
+            // This mimics the hard-coded behavior of ShellStep.
+            .setDirectory(projectFilesystem.getRootPath().toAbsolutePath().toFile())
+            .setEnvironment(ImmutableMap.<String, String>of())
+            .build();
+    FakeProcess fakeXctoolSuccess = new FakeProcess(0, "", "");
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.of(xctoolParams, fakeXctoolSuccess));
+    ExecutionContext executionContext = TestExecutionContext.newBuilder()
+        .setProcessExecutor(processExecutor)
+        .setProjectFilesystem(projectFilesystem)
+        .setEnvironment(ImmutableMap.<String, String>of())
+        .build();
+    assertThat(
+        step.execute(executionContext),
+        equalTo(0));
+  }
+
+  @Test
+  public void xctoolCommandWhichReturnsExitCode1DoesNotFailStep() throws Exception {
+    FakeProjectFilesystem projectFilesystem = new FakeProjectFilesystem();
+    XctoolRunTestsStep step = new XctoolRunTestsStep(
+        Paths.get("/path/to/xctool"),
+        "x86_64",
+        "iphonesimulator",
+        ImmutableSet.of(Paths.get("/path/to/Foo.xctest")),
+        ImmutableMap.<Path, Path>of(),
+        Paths.get("/path/to/output.json"));
+    ProcessExecutorParams xctoolParams =
+        ProcessExecutorParams.builder()
+            .setCommand(
+                ImmutableList.of(
+                    "/path/to/xctool",
+                    "-reporter",
+                    "json-stream:/path/to/output.json",
+                    "-sdk",
+                    "iphonesimulator",
+                    "-destination",
+                    "arch=x86_64",
+                    "-logicTest",
+                    "/path/to/Foo.xctest",
+                    "run-tests"))
+            // This mimics the hard-coded behavior of ShellStep.
+            .setDirectory(projectFilesystem.getRootPath().toAbsolutePath().toFile())
+            .setEnvironment(ImmutableMap.<String, String>of())
+            .build();
+    FakeProcess fakeXctoolSuccess = new FakeProcess(1, "", "");
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.of(xctoolParams, fakeXctoolSuccess));
+    ExecutionContext executionContext = TestExecutionContext.newBuilder()
+        .setProcessExecutor(processExecutor)
+        .setProjectFilesystem(projectFilesystem)
+        .setEnvironment(ImmutableMap.<String, String>of())
+        .build();
+    assertThat(
+        step.execute(executionContext),
+        equalTo(0));
+  }
+
+  @Test
+  public void xctoolCommandWhichReturnsExitCode400FailsStep() throws Exception {
+    FakeProjectFilesystem projectFilesystem = new FakeProjectFilesystem();
+    XctoolRunTestsStep step = new XctoolRunTestsStep(
+        Paths.get("/path/to/xctool"),
+        "x86_64",
+        "iphonesimulator",
+        ImmutableSet.of(Paths.get("/path/to/Foo.xctest")),
+        ImmutableMap.<Path, Path>of(),
+        Paths.get("/path/to/output.json"));
+    ProcessExecutorParams xctoolParams =
+        ProcessExecutorParams.builder()
+            .setCommand(
+                ImmutableList.of(
+                    "/path/to/xctool",
+                    "-reporter",
+                    "json-stream:/path/to/output.json",
+                    "-sdk",
+                    "iphonesimulator",
+                    "-destination",
+                    "arch=x86_64",
+                    "-logicTest",
+                    "/path/to/Foo.xctest",
+                    "run-tests"))
+            // This mimics the hard-coded behavior of ShellStep.
+            .setDirectory(projectFilesystem.getRootPath().toAbsolutePath().toFile())
+            .setEnvironment(ImmutableMap.<String, String>of())
+            .build();
+    FakeProcess fakeXctoolSuccess = new FakeProcess(400, "", "");
+    FakeProcessExecutor processExecutor = new FakeProcessExecutor(
+        ImmutableMap.of(xctoolParams, fakeXctoolSuccess));
+    ExecutionContext executionContext = TestExecutionContext.newBuilder()
+        .setProcessExecutor(processExecutor)
+        .setProjectFilesystem(projectFilesystem)
+        .setEnvironment(ImmutableMap.<String, String>of())
+        .build();
+    assertThat(
+        step.execute(executionContext),
+        not(equalTo(0)));
+  }
+}
