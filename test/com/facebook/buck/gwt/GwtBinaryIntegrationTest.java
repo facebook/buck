c25a9a8840aafc4c41d2df4625e0522c86fd0878
Simon Stewart <simons@fb.com>
2015-05-07 07:00:23 -0700

    Add integration test for existing GWT support.
    Summary:
    This is a pretty basic test, but it exercises the key area, that
    is, compiling a gwt_binary
    
    Test Plan: buck test --all

---
+++ test/com/facebook/buck/gwt/GwtBinaryIntegrationTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.gwt;
+
+import com.facebook.buck.testutil.integration.DebuggableTemporaryFolder;
+import com.facebook.buck.testutil.integration.ProjectWorkspace;
+import com.facebook.buck.testutil.integration.TestDataHelper;
+import com.facebook.buck.testutil.integration.ZipInspector;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GwtBinaryIntegrationTest {
+
+  @Rule
+  public DebuggableTemporaryFolder tmp = new DebuggableTemporaryFolder();
+
+  @Test(timeout = (2 * 60 * 1000)) // two minutes because CI times out on heavily loaded machines
+  public void shouldBeAbleToBuildAGwtBinary() throws IOException {
+    ProjectWorkspace workspace = TestDataHelper.createProjectWorkspaceForScenario(
+        this,
+        "gwt_binary",
+        tmp);
+    workspace.setUp();
+
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild("//:binary");
+
+    result.assertSuccess();
+
+    File zip = workspace.buildAndReturnOutput("//:binary");
+    ZipInspector inspector = new ZipInspector(zip);
+    inspector.assertFileExists("a/a.devmode.js");
+    inspector.assertFileExists("a/a.nocache.js");
+    inspector.assertFileExists("a/clear.cache.gif");
+    inspector.assertFileExists("a/compilation-mappings.txt");
+  }
+
+}
---
+++ test/com/facebook/buck/gwt/testdata/gwt_binary/com/example/gwt/client/A.java
@@ -0,0 +1,9 @@
+package com.example.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class A implements EntryPoint {
+
+  public void onModuleLoad() {
+  }
+}
