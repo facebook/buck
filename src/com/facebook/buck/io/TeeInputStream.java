34744141c790a15955c4d27ebd6290e90551513a
Ben Hamilton <beng@fb.com>
2015-05-27 16:44:31 -0700

    New class TeeInputStream
    Summary:
    I need to take an `InputStream` from a launched `Process`
    and simultaneously read its data as well as write that data to
    a file.
    
    This introduces a new class `TeeInputStream` modeled after the Unix
    `tee` command which takes an `InputStream` and an `OutputStream` as
    parameters. Whenever `TeeInputStream.read()` is called, it reads
    the data from the `InputStream`, writes it to the `OutputStream`,
    and returns it.
    
    Test Plan: Unit tests included. `ant java-test -Dtest.class=TeeInputStreamTest`

---
+++ src/com/facebook/buck/io/TeeInputStream.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.io;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Wraps a source {@link InputStream}, writing to a destination {@link OutputStream}
+ * any bytes read from this object.
+ *
+ * Does not close either the InputStream or the OutputStream automatically.
+ */
+public class TeeInputStream extends FilterInputStream {
+  private final OutputStream outputStream;
+
+  public TeeInputStream(InputStream inputStream, OutputStream outputStream) {
+    super(inputStream);
+    this.outputStream = outputStream;
+  }
+
+  @Override
+  public int read() throws IOException {
+    // We don't call super.read() here because it's not documented which
+    // other variant of read() it calls.
+    int val = in.read();
+    if (val != -1) {
+      outputStream.write(val);
+    }
+    return val;
+  }
+
+  @Override
+  public int read(byte[] buffer) throws IOException {
+    // Ditto on not using super.
+    int numBytesRead = in.read(buffer);
+    if (numBytesRead > 0) {
+      outputStream.write(buffer, 0, numBytesRead);
+    }
+    return numBytesRead;
+  }
+
+  @Override
+  public int read(byte[] buffer, int startIndex, int numBytesToRead) throws IOException {
+    // Ditto on not using super.
+    int numBytesRead = in.read(buffer, startIndex, numBytesToRead);
+    if (numBytesRead > 0) {
+      outputStream.write(buffer, startIndex, numBytesRead);
+    }
+    return numBytesRead;
+  }
+}
---
+++ test/com/facebook/buck/io/TeeInputStreamTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.io;
+
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Tests for {@link TeeInputStream}.
+ */
+public class TeeInputStreamTest {
+
+  private static final String UTF_8 = StandardCharsets.UTF_8.name();
+
+  @Test
+  public void readEmpty() throws IOException {
+    byte[] input = new byte[0];
+    try (ByteArrayInputStream sourceStream = new ByteArrayInputStream(input);
+         ByteArrayOutputStream destinationStream = new ByteArrayOutputStream();
+         TeeInputStream teeStream = new TeeInputStream(sourceStream, destinationStream)) {
+      assertThat(teeStream.read(), is(equalTo(-1)));
+      assertThat(destinationStream.toString(UTF_8), is(emptyString()));
+    }
+  }
+
+  @Test
+  public void readEmptyWithBuffer() throws IOException {
+    byte[] input = new byte[0];
+    try (ByteArrayInputStream sourceStream = new ByteArrayInputStream(input);
+         ByteArrayOutputStream destinationStream = new ByteArrayOutputStream();
+         TeeInputStream teeStream = new TeeInputStream(sourceStream, destinationStream)) {
+      byte[] buffer = new byte[1];
+      assertThat(teeStream.read(buffer), is(equalTo(-1)));
+      assertThat(destinationStream.toString(UTF_8), is(emptyString()));
+    }
+  }
+
+  @Test
+  public void readEmptyWithBufferAndIndexAndOffset() throws IOException {
+    byte[] input = new byte[0];
+    try (ByteArrayInputStream sourceStream = new ByteArrayInputStream(input);
+         ByteArrayOutputStream destinationStream = new ByteArrayOutputStream();
+         TeeInputStream teeStream = new TeeInputStream(sourceStream, destinationStream)) {
+      byte[] buffer = new byte[1];
+      assertThat(teeStream.read(buffer, 0, 1), is(equalTo(-1)));
+      assertThat(destinationStream.toString(UTF_8), is(emptyString()));
+    }
+  }
+
+  @Test
+  public void read() throws IOException {
+    byte[] input = new byte[] { 'X' };
+    try (ByteArrayInputStream sourceStream = new ByteArrayInputStream(input);
+         ByteArrayOutputStream destinationStream = new ByteArrayOutputStream();
+         TeeInputStream teeStream = new TeeInputStream(sourceStream, destinationStream)) {
+      assertThat(teeStream.read(), is(equalTo((int) 'X')));
+      assertThat(destinationStream.toString(UTF_8), is("X"));
+    }
+  }
+
+  @Test
+  public void readWithBuffer() throws IOException {
+    byte[] input = new byte[] { 'X' };
+    try (ByteArrayInputStream sourceStream = new ByteArrayInputStream(input);
+         ByteArrayOutputStream destinationStream = new ByteArrayOutputStream();
+         TeeInputStream teeStream = new TeeInputStream(sourceStream, destinationStream)) {
+      byte[] buffer = new byte[1];
+      assertThat(teeStream.read(buffer), is(equalTo(1)));
+      assertThat(buffer, is(equalTo(input)));
+      assertThat(destinationStream.toString(UTF_8), is("X"));
+    }
+  }
+
+  @Test
+  public void readWithBufferAndIndexAndOffset() throws IOException {
+    byte[] input = new byte[] { 'X' };
+    try (ByteArrayInputStream sourceStream = new ByteArrayInputStream(input);
+         ByteArrayOutputStream destinationStream = new ByteArrayOutputStream();
+         TeeInputStream teeStream = new TeeInputStream(sourceStream, destinationStream)) {
+      byte[] buffer = new byte[1];
+      assertThat(teeStream.read(buffer, 0, 1), is(equalTo(1)));
+      assertThat(buffer, is(equalTo(input)));
+      assertThat(destinationStream.toString(UTF_8), is("X"));
+    }
+  }
+}
