ab9b32598272579be477b693d8197eb4178da982
Ben Hamilton <beng@fb.com>
2015-06-08 08:32:03 -0700

    Allow FakeProcessExecutor to run a sequence of processes (even if they have the same params)
    Summary:
    Currently, the test utility `FakeProcessExecutor` takes as input a
    `Map` of `ProcessExecutorParam` to `FakeProcess`.
    
    Because a map can only contain one value per key, this means the class
    can't currently model the same process being invoked multiple times
    with different results.
    
    This diff updates the utility to take either a Map, an Iterable, or a Function
    to model any arbitrary sequences of processes executing in sequence.
    
    Test Plan: Used in subsequent diff.

--- test/com/facebook/buck/util/FakeProcessExecutor.java
+++ test/com/facebook/buck/util/FakeProcessExecutor.java
@@ -16,17 +16,20 @@
 
 package com.facebook.buck.util;
 
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class FakeProcessExecutor extends ProcessExecutor {
 
-  private final ImmutableMap<ProcessExecutorParams, FakeProcess> processMap;
+  private final Function<ProcessExecutorParams, FakeProcess> processFunction;
   private final Set<ProcessExecutorParams> launchedProcesses;
 
   public FakeProcessExecutor() {
@@ -38,19 +41,62 @@ public class FakeProcessExecutor extends ProcessExecutor {
   }
 
   public FakeProcessExecutor(
+      Iterable<Map.Entry<ProcessExecutorParams, FakeProcess>> processIterable) {
+    this(processIterable, new Console(Verbosity.ALL, System.out, System.err, Ansi.withoutTty()));
+  }
+
+  public FakeProcessExecutor(
+      final Iterable<Map.Entry<ProcessExecutorParams, FakeProcess>> processIterable,
+      Console console) {
+    this(
+        new Function<ProcessExecutorParams, FakeProcess>() {
+          final Iterator<Map.Entry<ProcessExecutorParams, FakeProcess>> processIterator =
+              processIterable.iterator();
+
+          @Override
+          public FakeProcess apply(ProcessExecutorParams params) {
+            Preconditions.checkState(
+                processIterator.hasNext(),
+                "Ran out of fake processes when asked to run %s",
+                params);
+            Map.Entry<ProcessExecutorParams, FakeProcess> nextProcess = processIterator.next();
+            Preconditions.checkState(
+                nextProcess.getKey().equals(params),
+                "Mismatch when asked to run process %s (expecting %s)",
+                params,
+                nextProcess.getKey());
+            return nextProcess.getValue();
+          }
+        },
+        console);
+  }
+
+  public FakeProcessExecutor(
       Map<ProcessExecutorParams, FakeProcess> processMap,
       Console console) {
+    this(Functions.forMap(processMap), console);
+  }
+
+  public FakeProcessExecutor(
+      Function<ProcessExecutorParams, FakeProcess> processFunction,
+      Console console) {
     super(console);
-    this.processMap = ImmutableMap.copyOf(processMap);
+    this.processFunction = processFunction;
     this.launchedProcesses = new HashSet<>();
   }
 
   @Override
   Process launchProcessInternal(ProcessExecutorParams params) throws IOException {
-    Preconditions.checkArgument(processMap.keySet().contains(params), params.toString());
-    FakeProcess fakeProcess = Preconditions.checkNotNull(processMap.get(params));
-    launchedProcesses.add(params);
-    return fakeProcess;
+    try {
+      FakeProcess fakeProcess = processFunction.apply(params);
+      launchedProcesses.add(params);
+      return fakeProcess;
+    } catch (IllegalArgumentException e) {
+      throw new IOException(
+          String.format(
+              "FakeProcessExecutor not configured to run process with params %s",
+              params));
+    }
   }
 
   public boolean isProcessLaunched(ProcessExecutorParams params) {
