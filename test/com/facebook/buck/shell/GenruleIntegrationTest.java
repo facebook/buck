9065ed40f7565cf009cac7fd9718a9fd2f4bc38a
Liuyang Li <liuyang.li@addepar.com>
2015-06-15 12:25:56 -0700

    Fix GenruleIntegrationTest.
    Summary:
      Making the regex matching more robust.
    
    Test Plan:
      `buck test`.

--- test/com/facebook/buck/shell/GenruleIntegrationTest.java
+++ test/com/facebook/buck/shell/GenruleIntegrationTest.java
@@ -70,8 +70,8 @@ public class GenruleIntegrationTest {
 
     // "(?s)" enables multiline matching for ".*". Parens have to be escaped.
     String outputPattern =
-        "(?s).*BUILD FAILED: //:fail failed with exit code 1:\n" +
-        "\\(cd [-_ \\d\\w/]*/buck-out/gen/fail__srcs && /bin/bash -e -c 'false; echo >&2 hi'\\).*";
+        "(?s).*BUILD FAILED: //:fail failed with exit code 1:(?s).*" +
+        "\\(cd .*/buck-out/gen/fail__srcs && /bin/bash -e -c 'false; echo >&2 hi'\\)(?s).*";
 
     assertTrue(
         "Unexpected output:\n" + quoteOutput(buildResult.getStderr()),
