f3a5294ca83f966232e15536556c1288053adbe2
Andrew Gallagher <agallagher@fb.com>
2015-06-15 23:36:57 -0700

    Move super console cache stats calculation to rule finished event
    Summary:
    With the introduction of input-based rule key caching, we're now
    potentially performing two fetches for each artifact during a build.
    So this diff moves the cache hit/error ratio calculation to the rule
    finished event handler, so that a artifact that misses on the normal
    rule key fetch still reports a hit if found on a input-based rule key
    fetch.
    
    Test Plan: `buck test --all`

--- src/com/facebook/buck/event/listener/SuperConsoleEventBusListener.java
+++ src/com/facebook/buck/event/listener/SuperConsoleEventBusListener.java
@@ -92,7 +92,6 @@ public class SuperConsoleEventBusListener extends AbstractConsoleEventBusListene
   // Counts the number of cache hits and errors, respectively.
   private final AtomicInteger cacheHits = new AtomicInteger(0);
   private final AtomicInteger cacheErrors = new AtomicInteger(0);
-  private final AtomicInteger cacheFetches = new AtomicInteger(0);
 
   private final ConcurrentLinkedQueue<ConsoleEvent> logEvents;
 
@@ -218,16 +217,16 @@ public class SuperConsoleEventBusListener extends AbstractConsoleEventBusListene
         List<String> columns = Lists.newArrayList();
         columns.add(String.format("%d/%d JOBS", numRulesCompleted.get(), ruleCount.get()));
         columns.add(String.format("%d UPDATED", updated.get()));
-        if (cacheFetches.get() > 0) {
+        if (updated.get() > 0) {
           columns.add(
               String.format(
                   "%.1f%% CACHE HITS",
-                  100 * (double) cacheHits.get() / cacheFetches.get()));
+                  100 * (double) cacheHits.get() / updated.get()));
           if (cacheErrors.get() > 0) {
             columns.add(
                 String.format(
                     "%.1f%% CACHE ERRORS",
-                    100 * (double) cacheErrors.get() / cacheFetches.get()));
+                    100 * (double) cacheErrors.get() / updated.get()));
           }
         }
         jobSummary = "(" + Joiner.on(", ").join(columns) + ")";
@@ -480,6 +479,11 @@ public class SuperConsoleEventBusListener extends AbstractConsoleEventBusListene
     CacheResult cacheResult = finished.getCacheResult();
     if (cacheResult.getType() != CacheResult.Type.LOCAL_KEY_UNCHANGED_HIT) {
       updated.incrementAndGet();
+      if (cacheResult.getType() == CacheResult.Type.HIT) {
+        cacheHits.incrementAndGet();
+      } else if (cacheResult.getType() == CacheResult.Type.ERROR) {
+        cacheErrors.incrementAndGet();
+      }
     }
   }
 
@@ -523,15 +527,6 @@ public class SuperConsoleEventBusListener extends AbstractConsoleEventBusListene
   @Subscribe
   public void artifactFinished(ArtifactCacheEvent.Finished finished) {
     threadsToRunningStep.put(finished.getThreadId(), Optional.<StepEvent>absent());
-    if (finished.getOperation() == ArtifactCacheEvent.Operation.FETCH) {
-      Preconditions.checkState(finished.getCacheResult().isPresent());
-      cacheFetches.incrementAndGet();
-      if (finished.getCacheResult().get().getType() == CacheResult.Type.HIT) {
-        cacheHits.incrementAndGet();
-      } else if (finished.getCacheResult().get().getType() == CacheResult.Type.ERROR) {
-        cacheErrors.incrementAndGet();
-      }
-    }
   }
 
   @Subscribe
