4d3e6bfc29458295f95c65b4f79afaa5dec9fa2f
Jonathan Keljo <jkeljo@fb.com>
2015-06-22 16:29:15 -0700

    Add some docs on ClassLoaderBootstrapper
    Summary: I realized some rationale explanation here would be helpful.
    
    Test Plan: Comment change only

--- src/com/facebook/buck/cli/bootstrapper/ClassLoaderBootstrapper.java
+++ src/com/facebook/buck/cli/bootstrapper/ClassLoaderBootstrapper.java
@@ -24,6 +24,20 @@ import java.net.URLClassLoader;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
+/**
+ * This class sets up a separate ClassLoader for most of Buck's implementation, leaving only the
+ * bare minimum bootstrapping classes (and a few classes for compatibility with library code that
+ * is not ClassLoader-aware) in the system ClassLoader. This is done so that annotation processors
+ * do not have their classpaths polluted with Buck's dependencies when Buck is compiling Java code
+ * in-process.
+ *
+ * Under JSR-199, when the Java compiler is run in-process it uses a ClassLoader that is a child
+ * of the system ClassLoader. In order for annotation processors to access the Compiler Tree API
+ * (which lives in tools.jar with the compiler itself), they must be loaded with a ClassLoader
+ * descended from the compiler's. If Buck used the system ClassLoader as a normal Java application
+ * would, this would result in annotation processors getting Buck's versions of Guava, Jackson,
+ * etc. instead of their own.
+ */
 public final class ClassLoaderBootstrapper {
   private static ClassLoader classLoader;
 
