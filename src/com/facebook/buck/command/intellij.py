797e42773d2702e600fdf474d3381d4b9e82f671
Liuyang Li <liuyang.li@addepar.com>
2015-06-24 03:21:45 -0700

    Intellij Buck Support Update
    Summary:
    
    Part of https://github.com/facebook/buck/pull/349
    
    Test Plan: Imported from GitHub

--- src/com/facebook/buck/command/intellij.py
+++ src/com/facebook/buck/command/intellij.py
@@ -16,7 +16,9 @@ MISC_XML = """<?xml version="1.0" encoding="UTF-8"?>
     assert-keyword="true"
     jdk-15="true"
     project-jdk-name="%(project_jdk_name)s"
-    project-jdk-type="%(project_jdk_type)s" />
+    project-jdk-type="%(project_jdk_type)s">
+    %(project_output_url)s
+  </component>
 </project>"""
 
 MODULE_XML_START = """<?xml version="1.0" encoding="UTF-8"?>
@@ -268,7 +270,7 @@ def write_modules(modules, generate_minimum_project, android_auto_generation_dis
         # <content> element for the buck-out/android/gen folder should be
         # listed before the other source folders.
         num_source_folders = len(module['sourceFolders'])
-        if num_source_folders > 1:
+        if num_source_folders > 1 and module['hasAndroidFacet']:
             xml = add_buck_android_source_folder(xml, module)
 
         # Source folders.
@@ -296,7 +298,7 @@ def write_modules(modules, generate_minimum_project, android_auto_generation_dis
         # Empirically, if there is one source folder, then the <content>
         # element for the buck-out/android/gen folder should be listed after
         # the other source folders.
-        if num_source_folders <= 1:
+        if num_source_folders <= 1 and module['hasAndroidFacet']:
             xml = add_buck_android_source_folder(xml, module)
 
         # Dependencies.
@@ -459,10 +461,14 @@ def write_all_modules(modules):
 
 def write_misc_file(java_settings):
     """Writes a misc.xml file to define some settings specific to the project."""
+    output_url = '<output url="file://$PROJECT_DIR$/' + \
+        java_settings.get('outputUrl', 'build-ij/classes') + '" />'
     xml = MISC_XML % {
         'java_language_level': java_settings.get('languageLevel', 'JDK_1_6'),
         'project_jdk_name': java_settings.get('jdkName', 'Android API 21 Platform'),
         'project_jdk_type': java_settings.get('jdkType', 'Android SDK'),
+        'project_jdk_type': java_settings.get('jdkType', 'Android SDK'),
+        'project_output_url': output_url
     }
 
     write_file_if_changed('.idea/misc.xml', xml)
