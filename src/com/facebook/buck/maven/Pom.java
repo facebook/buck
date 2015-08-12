3f220ebf778c332a80d9e60fd5bb154af16b9c5b
Eugene Susla <eugenesusla@fb.com>
2015-08-11 17:38:48 -0700

    Pom generator
    Summary:
    To publish to maven central anything that has dependencies, we have to publish a pom file alongside.
    Also, given that the pom files tend to contain a lot of information, that we cannot auto-populate, it's reasonable to assume that user may want to add data other than dependencies to the pom file, before publishing it to the central repository, so ideally, we need to also support updating existing `pom.xml` without overwriting it as much as possible.
    
    Test Plan:
      buck test   --filter MainIntegrationTest test/com/facebook/buck/cli &&
      buck test   --filter PomCommandIntegrationTest test/com/facebook/buck/cli &&
      buck test   --filter XmlDomParserTest test/com/facebook/buck/util

---
+++ src/com/facebook/buck/maven/Pom.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.maven;
+
+import com.facebook.buck.java.MavenPublishable;
+import com.facebook.buck.rules.BuildRule;
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.building.DefaultModelBuilderFactory;
+import org.apache.maven.model.building.DefaultModelBuildingRequest;
+import org.apache.maven.model.building.ModelBuilder;
+import org.apache.maven.model.building.ModelBuildingException;
+import org.apache.maven.model.building.ModelBuildingRequest;
+import org.apache.maven.model.building.ModelBuildingResult;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class Pom {
+
+  private static final MavenXpp3Writer POM_WRITER = new MavenXpp3Writer();
+  private static final DefaultModelBuilderFactory MODEL_BUILDER_FACTORY =
+      new DefaultModelBuilderFactory();
+  /**
+   * Consistent with the value used in the implementation of {@link MavenXpp3Writer#write}
+   */
+  private static final String POM_MODEL_VERSION = "4.0.0";
+
+  private final Model model;
+  private final MavenPublishable publishable;
+  private final Path path;
+
+  public Pom(Path path, MavenPublishable buildRule) throws IOException {
+    this.path = path;
+    this.publishable = buildRule;
+    this.model = constructModel();
+    applyBuildRule();
+  }
+
+  public static void generatePomFile(
+      MavenPublishable rule,
+      Path optionallyExistingPom) throws IOException {
+    new Pom(optionallyExistingPom, rule).flushToFile();
+  }
+
+  private void applyBuildRule() {
+    Optional<String> mavenCoords = getMavenCoords(publishable);
+    if (!mavenCoordsPresent(mavenCoords)) {
+      throw new IllegalArgumentException(
+          "Cannot retrieve maven coordinates for target" +
+              publishable.getBuildTarget().getFullyQualifiedName());
+    }
+    DefaultArtifact artifact = new DefaultArtifact(mavenCoords.get());
+
+    Iterable<Artifact> deps = FluentIterable
+        .from(publishable.getDeps())
+        .transform(
+            new Function<BuildRule, Optional<String>>() {
+              @Override
+              public Optional<String> apply(BuildRule input) {
+                return getMavenCoords(input);
+              }
+            })
+        .filter(
+            new Predicate<Optional<String>>() {
+              @Override
+              public boolean apply(Optional<String> input) {
+                return mavenCoordsPresent(input);
+              }
+            })
+        .transform(
+            new Function<Optional<String>, Artifact>() {
+              @Override
+              public Artifact apply(Optional<String> input) {
+                return new DefaultArtifact(input.get());
+              }
+            });
+
+    updateModel(artifact, deps);
+  }
+
+  private Model constructModel() throws IOException {
+    File file = path.toFile();
+    if (file.isFile()) {
+      ModelBuildingRequest modelBuildingRequest = new DefaultModelBuildingRequest()
+          .setPomFile(file);
+      ModelBuilder modelBuilder = MODEL_BUILDER_FACTORY.newInstance();
+      try {
+        ModelBuildingResult modelBuildingResult = modelBuilder.build(modelBuildingRequest);
+
+        // Would contain extra stuff: <build/>, <repositories/>, <pluginRepositories/>, <reporting/>
+        // model = modelBuildingResult.getEffectiveModel();
+
+        return Preconditions.checkNotNull(modelBuildingResult.getRawModel());
+      } catch (ModelBuildingException e) {
+        throw Throwables.propagate(e);
+      }
+    } else {
+      Model model = new Model();
+      model.setModelVersion(POM_MODEL_VERSION);
+      return model;
+    }
+  }
+
+  private void updateModel(Artifact mavenCoordinates, Iterable<Artifact> deps) {
+    model.setGroupId(mavenCoordinates.getGroupId());
+    model.setArtifactId(mavenCoordinates.getArtifactId());
+    model.setVersion(mavenCoordinates.getVersion());
+    if (Strings.isNullOrEmpty(model.getName())) {
+      model.setName(mavenCoordinates.getArtifactId()); // better than nothing
+    }
+
+    // Dependencies
+    ImmutableMap<DepKey, Dependency> depIndex = Maps.uniqueIndex(
+        getModel().getDependencies(), new Function<Dependency, DepKey>() {
+          @Override
+          public DepKey apply(Dependency input) {
+            return new DepKey(input);
+          }
+        });
+    for (Artifact artifactDep : deps) {
+      DepKey key = new DepKey(artifactDep);
+      Dependency dependency = depIndex.get(key);
+      if (dependency == null) {
+        dependency = key.createDependency();
+        getModel().addDependency(dependency);
+      }
+      updateDependency(dependency, artifactDep);
+    }
+  }
+
+  private static void updateDependency(Dependency dependency, Artifact providedMavenCoordinates) {
+    dependency.setVersion(providedMavenCoordinates.getVersion());
+    dependency.setClassifier(providedMavenCoordinates.getClassifier());
+  }
+
+  public void flushToFile() throws IOException {
+    getModel(); // Ensure model is initialized, reading file if necessary
+    flushTo(Files.newOutputStream(getPath()));
+  }
+
+  private void flushTo(OutputStream destination) throws IOException {
+    POM_WRITER.write(new OutputStreamWriter(destination,
+        Charset.forName(getModel().getModelEncoding())), getModel());
+  }
+
+  private static Optional<String> getMavenCoords(BuildRule buildRule) {
+    if (buildRule instanceof MavenPublishable) {
+      return ((MavenPublishable) buildRule).getMavenCoords();
+    }
+    return Optional.absent();
+  }
+
+  private static boolean mavenCoordsPresent(Optional<String> coords) {
+    return coords.isPresent() && !coords.get().isEmpty();
+  }
+
+  public Model getModel() {
+    return model;
+  }
+
+  public Path getPath() {
+    return path;
+  }
+
+  private static final class DepKey {
+    private final String groupId;
+    private final String artifactId;
+
+    public DepKey(Artifact artifact) {
+      groupId = artifact.getGroupId();
+      artifactId = artifact.getArtifactId();
+      validate();
+    }
+
+    public DepKey(Dependency dependency) {
+      groupId = dependency.getGroupId();
+      artifactId = dependency.getArtifactId();
+      validate();
+    }
+
+    private void validate() {
+      Preconditions.checkNotNull(groupId);
+      Preconditions.checkNotNull(artifactId);
+    }
+
+    public Dependency createDependency() {
+      Dependency dependency = new Dependency();
+      dependency.setGroupId(groupId);
+      dependency.setArtifactId(artifactId);
+      return dependency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof DepKey)) {
+        return false;
+      }
+
+      DepKey depKey = (DepKey) o;
+
+      return Objects.equals(groupId, depKey.groupId) &&
+          Objects.equals(artifactId, depKey.artifactId);
+    }
+
+    @Override
+    public int hashCode() {
+      int result = groupId.hashCode();
+      result = 31 * result + artifactId.hashCode();
+      return result;
+    }
+  }
+}
---
+++ test/com/facebook/buck/maven/PomIntegrationTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.maven;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.facebook.buck.java.MavenPublishable;
+import com.facebook.buck.rules.BuildRule;
+import com.facebook.buck.testutil.integration.DebuggableTemporaryFolder;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSortedSet;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.easymock.EasyMockSupport;
+import org.junit.Rule;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.transform.TransformerException;
+
+public class PomIntegrationTest extends EasyMockSupport {
+
+  private static final MavenXpp3Writer MAVEN_XPP_3_WRITER = new MavenXpp3Writer();
+  private static final MavenXpp3Reader MAVEN_XPP_3_READER = new MavenXpp3Reader();
+  private static final String URL = "http://example.com";
+
+  @Rule
+  public DebuggableTemporaryFolder tmp = new DebuggableTemporaryFolder();
+
+  @Test
+  public void testMultipleInvocation() throws Exception{
+
+    // Setup: deps: com.example:with-deps:jar:1.0 -> com.othercorp:no-deps:jar:1.0
+    MavenPublishable dep = mockMavenPublishable(
+        "com.othercorp:no-deps:1.0",
+        ImmutableSortedSet.<BuildRule>of());
+
+    MavenPublishable item = mockMavenPublishable(
+        "com.example:with-deps:1.0",
+        ImmutableSortedSet.<BuildRule>of(dep));
+
+    Path pomPath = tmp.getRootPath().resolve("pom.xml");
+    File pomFile = pomPath.toFile();
+    assertFalse(pomFile.exists());
+
+    // Basic case
+    Pom.generatePomFile(item, pomPath);
+
+    Model pomModel = parseAndVerifyPomFile(pomFile);
+
+    // Corrupt dependency data and ensure buck restores that
+    removeDependencies(pomModel, pomFile);
+
+    Pom.generatePomFile(item, pomPath);
+
+    pomModel = parseAndVerifyPomFile(pomFile);
+
+    // Add extra pom data and ensure buck preserves that
+    pomModel.setUrl(URL);
+    serializePom(pomModel, pomFile);
+
+    Pom.generatePomFile(item, pomPath);
+
+    pomModel = parseAndVerifyPomFile(pomFile);
+    assertEquals(URL, pomModel.getUrl());
+  }
+
+  private MavenPublishable mockMavenPublishable(
+      String mavenCoords,
+      ImmutableSortedSet<BuildRule> deps) {
+    MavenPublishable mavenPublishable = createNiceMock(MavenPublishable.class);
+    expect(mavenPublishable.getMavenCoords())
+        .andReturn(Optional.fromNullable(mavenCoords))
+        .anyTimes();
+    expect(mavenPublishable.getDeps()).andReturn(deps).anyTimes();
+    replay(mavenPublishable);
+    return mavenPublishable;
+  }
+
+  private static void serializePom(Model pomModel, File destination) throws IOException {
+    MAVEN_XPP_3_WRITER.write(new FileWriter(destination), pomModel);
+  }
+
+  private static void removeDependencies(Model model, File pomFile)
+      throws IOException, SAXException, TransformerException {
+    model.setDependencies(Collections.<Dependency>emptyList());
+    serializePom(model, pomFile);
+  }
+
+  /**
+   * assert deps: com.example:with-deps:jar:1.0 -> com.othercorp:no-deps:jar:1.0
+   */
+  private static Model parseAndVerifyPomFile(File pomFile) throws Exception {
+    assertTrue(pomFile.isFile());
+    Model pomModel = MAVEN_XPP_3_READER.read(new FileReader(pomFile));
+    assertEquals("com.example", pomModel.getGroupId());
+    assertEquals("with-deps", pomModel.getArtifactId());
+    assertEquals("1.0", pomModel.getVersion());
+    List<Dependency> dependencies = pomModel.getDependencies();
+    assertEquals(1, dependencies.size());
+    Dependency dependency = dependencies.get(0);
+    assertEquals("com.othercorp", dependency.getGroupId());
+    assertEquals("no-deps", dependency.getArtifactId());
+    assertEquals("1.0", dependency.getVersion());
+    return pomModel;
+  }
+}
