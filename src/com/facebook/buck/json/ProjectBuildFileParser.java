4d922574c74c2f8330ed9386e51f51fa8cfeee98
Ben Hamilton <beng@fb.com>
2015-08-19 14:01:25 -0700

    Easy: Lower log level when buck.py parser exits unexpectedly
    Summary:
    When parsing a `BUCK` file, if the python process exits
    unexpectedly (say, due to a syntax error), the `BSER` parser will
    throw `BserDeserializer.BserEofException`.
    
    In this case, we already have user-visible messaging to include
    the Python stack trace and let them know the build failed.
    
    Before this diff, we use `LOG.error()` to include the `BserEofException`
    stack trace and print it to the console. However, the Java stack
    trace isn't really useful to the user here.
    
    Since we already show the Python stack trace and let the user know
    the build failed, this diff simply lowers the log level from `LOG.error()`
    to `LOG.warn()` (which prints the Java stack trace to the log file but
    not to console).
    
    Test Plan:
    `buck audit dependencies //path/to:lib` where `BUCK` file
    contained a call to an undefined function

--- src/com/facebook/buck/json/ProjectBuildFileParser.java
+++ src/com/facebook/buck/json/ProjectBuildFileParser.java
@@ -311,7 +311,7 @@ public class ProjectBuildFileParser implements AutoCloseable {
         Preconditions.checkState(deserializedValue instanceof List<?>);
         result = (List<Map<String, Object>>) deserializedValue;
       } catch (BserDeserializer.BserEofException e) {
-        LOG.error(e, "Parser exited while decoding BSER data");
+        LOG.warn(e, "Parser exited while decoding BSER data");
         throw new IOException("Parser exited unexpectedly", e);
       }
     } else {
@@ -367,7 +367,7 @@ public class ProjectBuildFileParser implements AutoCloseable {
         LOG.debug("Waiting for process %s to exit...", buckPyProcess);
         int exitCode = processExecutor.waitForLaunchedProcess(buckPyProcess);
         if (exitCode != 0) {
-          LOG.error("Process %s exited with error code %d", buckPyProcess, exitCode);
+          LOG.warn("Process %s exited with error code %d", buckPyProcess, exitCode);
           throw BuildFileParseException.createForUnknownParseError(
               String.format("Parser did not exit cleanly (exit code: %d)", exitCode));
         }
