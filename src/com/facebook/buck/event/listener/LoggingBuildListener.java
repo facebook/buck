014b37cf51e3614836c00bf0e2d41212d5cd91d0
Martin Kosiba <mkosiba@fb.com>
2015-08-26 18:06:04 -0700

    Don't put SimplePerfEvent into the buck log files by default.
    Summary:
    The SimplePerfEvents are a bit noisy so we're only going to log
    them to the .log file if the logging level is verbose (they're
    still going to go into the .trace file)
    
    Test Plan: checked manually

--- src/com/facebook/buck/event/listener/LoggingBuildListener.java
+++ src/com/facebook/buck/event/listener/LoggingBuildListener.java
@@ -19,6 +19,7 @@ package com.facebook.buck.event.listener;
 import com.facebook.buck.event.AbstractBuckEvent;
 import com.facebook.buck.event.BuckEventListener;
 import com.facebook.buck.event.ConsoleEvent;
+import com.facebook.buck.event.SimplePerfEvent;
 import com.facebook.buck.model.BuildId;
 import com.facebook.buck.rules.BuildEvent;
 import com.facebook.buck.rules.BuildRuleEvent;
@@ -108,8 +109,12 @@ public class LoggingBuildListener implements BuckEventListener {
     if (EXPLICITLY_HANDLED_EVENT_TYPES.contains(event.getClass())) {
       return;
     }
+    Level level = Level.FINE;
+    if (event instanceof SimplePerfEvent) {
+      level = Level.FINER;
+    }
     // Use a format so we avoid paying the cost of event.toString() unless we have to.
-    LOG.log(Level.FINE, "{0}", event);
+    LOG.log(level, "{0}", event);
   }
 
   @Override
