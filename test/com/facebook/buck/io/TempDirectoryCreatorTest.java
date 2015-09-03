aa5a9edc437e8978b434763d15594b9d56045050
Ben Hamilton <beng@fb.com>
2015-09-02 18:06:24 -0700

    New class TempDirectoryCreator
    Summary:
    To enable Watchman globbing by default, we need Buck's own
    Java tests to use a temporary directory outside of `buck-out` (since
    that directory is purposefully ignored by Watchman).
    
    This diff introduces a new class, `TempDirectoryCreator`, which allow
    a `try-with-resources` style of scoped temporary directory creation
    which cleans up after itself when done.
    
    We have similar logic in `buck_project.py` already, but I can't
    re-use that here since `buck_project.py` doesn't know anything
    about per-repo Buck configuration settings.
    
    Test Plan:
    Lovely jimfs unit tests included. `ant java-test -Dtest.class=TempDirectoryCreator`
    and `buck test -f TempDirectoryCreator //test/com/facebook/buck/io:io`

---
+++ test/com/facebook/buck/io/TempDirectoryCreatorTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.io;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import com.google.common.base.Optional;
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+
+public class TempDirectoryCreatorTest {
+  @Test
+  public void whenPathAbsentThenNoExceptionsThrown() throws IOException {
+    Optional<Path> noPath = Optional.absent();
+    TempDirectoryCreator tempDirectoryCreator = new TempDirectoryCreator(noPath);
+    tempDirectoryCreator.close();
+  }
+
+  @Test
+  public void whenPathPresentThenTempDirectoryCreatedThenDeletedUponClose() throws IOException {
+    FileSystem vfs = Jimfs.newFileSystem(Configuration.unix());
+    Path fakeTmpDir = vfs.getPath("/tmp/fake-tmp-dir");
+
+    assertThat(Files.exists(fakeTmpDir), is(false));
+
+    try (TempDirectoryCreator tempDirectoryCreator =
+         new TempDirectoryCreator(Optional.of(fakeTmpDir))) {
+      assertThat(Files.exists(fakeTmpDir), is(true));
+    }
+
+    assertThat(Files.exists(fakeTmpDir), is(false));
+  }
+}
