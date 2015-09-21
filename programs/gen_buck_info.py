c973a60367c1a53c2e88e4869e74b072f1dad39c
Ben Hamilton <beng@fb.com>
2015-09-21 11:34:00 -0700

    Update gen_buck_info.py to support .buckrelease file when building outside git repo
    Summary:
    Currently, `programs/gen_buck_info.py` crashes if run from outside a git repo:
    
      fatal: Not a git repository (or any of the parent directories): .git
    
    We want to make PEX releases for Homebrew users from the github-hosted
    .tar.gz source archives of Buck releases. These archives, of course,
    do not contain a `.git` directory, so we need to make sure we can
    provide the information `gen_buck_info.py` needs from non-git source
    archives.
    
    I updated the release process to include running `scripts/create_tag.sh` to create a `.buckrelease` file
    in the root of the repo to support building Buck outside git repos.
    
    This diff updates `gen_buck_info.py` to read this file.
    
    Test Plan:
    1) `buck build buck` from git repo
    2) `cp -a path/to/buck path/to/buck2 && rm -rf path/to/buck2/.git && cd path/to/buck2 && buck build buck`
    3) `echo v2015.09.21.01 > path/to/buck2/.buckrelease && cd path/to/buck2 && buck build buck`
    
    Confirmed at each step that `buck-out/gen/programs/gen_buck_package_info/buck_package_info.json`
    was created and contained timestamp and version info.
    
    Ran generated PEX via `buck-out/gen/programs/buck.pex --help` and
    confirmed in logs that version information was correctly included:
    
      [com.facebook.buck.cli.Main] Starting up (build date 2015-09-21 11:54:54 -0700, rev v2015.09.21.01), args: [--help]

--- programs/gen_buck_info.py
+++ programs/gen_buck_info.py
@@ -1,6 +1,8 @@
+import errno
 import os
 import json
 import sys
+import time
 
 import buck_version
 
@@ -13,14 +15,29 @@ def main(argv):
     while not os.path.exists(os.path.join(path, '.buckconfig')):
         path = os.path.dirname(path)
 
-    # Attempt to create a "clean" version, but fall back to a "dirty"
-    # one if need be.
-    version = buck_version.get_clean_buck_version(path)
-    timestamp = -1
-    if version is None:
-        version = buck_version.get_dirty_buck_version(path)
+    if os.path.exists(os.path.join(path, '.git')):
+        # Attempt to create a "clean" version, but fall back to a "dirty"
+        # one if need be.
+        version = buck_version.get_clean_buck_version(path)
+        timestamp = -1
+        if version is None:
+            version = buck_version.get_dirty_buck_version(path)
+        else:
+            timestamp = buck_version.get_git_revision_timestamp(path)
     else:
-        timestamp = buck_version.get_git_revision_timestamp(path)
+        # We're building outside a git repo. Check for the special
+        # .buckrelease file created by the release process.
+        try:
+            with open(os.path.join(path, '.buckrelease')) as f:
+                timestamp = int(os.fstat(f.fileno()).st_mtime)
+                version = f.read().strip()
+        except IOError, e:
+            if e.errno == errno.ENOENT:
+                # No .buckrelease file. Do the best that we can.
+                version = '(unknown version)'
+                timestamp = int(time.time())
+            else:
+                raise e
 
     json.dump(
         {'version': version, 'timestamp': timestamp},
