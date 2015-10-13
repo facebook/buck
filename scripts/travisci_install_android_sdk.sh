bc0141890ef65a52bd6e26642e79ae60758126ff
Jakub Zika <jakubzika@fb.com>
2015-10-13 04:06:59 -0700

    Enable Travis CI builds again
    Summary:
    Travis CI has come a long way since when I disabled it earlier this
    year.  With the new container support, and some safe limiting on our own
    end, we can actually run successfully now.
    
    This change adds basic support like we had before.  It should be noted
    that this disables one Go test which fails, and will continue to fail
    until https://github.com/facebook/buck/pull/479 gets merged in.  That PR
    should probably re-enable that test.
    
    It also removes the `ant test` run.  It doesn't really give us much value
    and makes the whole Travis run take much longer.
    
    In the future, we can also add support for OS X, NDK, D, Rust, etc.
    
    Test Plan: https://travis-ci.org/sdwilsh/buck/builds/88004577

--- scripts/travisci_install_android_sdk.sh
+++ scripts/travisci_install_android_sdk.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 set -x
 
+export ANDROID_TOOL=${ANDROID_HOME}/tools/android
+
+# Because we cache ANDROID_HOME in Travis, we cannot test for the existance of
+# the directory; it always gets created before we run.  Instead, check for the
+# tool we care about, and if it doesn't exist, download the SDK.
+if [ ! -x ${ANDROID_TOOL} ]; then
+  wget http://dl.google.com/android/android-sdk_r23-linux.tgz
+  tar -zxf android-sdk_r23-linux.tgz
+  rm -rf ${ANDROID_HOME}
+  mv android-sdk-linux ${ANDROID_HOME}
+fi
+
+# We always update the SDK, even if it is cached, in case we change the
+# versions of things we care about.
 # Values from `android list sdk --extended --all`
 (while :
 do
     echo y
     sleep 2
- done) | android update sdk --force --no-ui --all --filter \
+ done) | ${ANDROID_TOOL} update sdk \
+  --force \
+  --no-ui \
+  --all \
+  --filter \
 tools,\
 platform-tools,\
-build-tools-19.1.0,\
-android-16,\
-addon-google_apis-google-16,\
-android-18,\
-addon-google_apis-google-18,\
-android-19,\
-addon-google_apis-google-19,\
+build-tools-23.0.1,\
+android-23,\
+addon-google_apis-google-23,\
 android-21,\
 addon-google_apis-google-21,\
 extra-android-support
---
+++ scripts/travisci_run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eux
+
+# We export TERM=dumb to hide Buck's SuperConsole
+# because otherwise Travis thinks our logs are too long.
+export TERM=dumb
+
+# Use Buck to run the tests first because its output is more
+# conducive to diagnosing failed tests than Ant's is.
+./bin/buck build buck || { cat buck-out/log/ant.log; exit 1; }
+# There are only two cores in the containers, but Buck thinks there are 12
+# and tries to use all of them.  As a result, we seem to have our test
+# processes killed if we do not limit our threads.
+# Until we get https://github.com/facebook/buck/pull/479, we cannot run the
+# Go tests in Travis
+./bin/buck test \
+  --num-threads=3 \
+  --test-selectors '!GoBinaryIntegrationTest'
+
+# Run all the other checks with ant.
+ant travis
