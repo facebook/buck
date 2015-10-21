209ac50783a533b27b9906b9f5c10785afb55969
Petru Marius Balau <petrumarius@fb.com>
2015-10-21 12:21:51 -0700

    [WS] build output will be represented by a tree
    Summary: Added the UI classes.
    
    Test Plan: N/A

---
+++ src/com/facebook/buck/intellij/plugin/src/com/facebook/buck/intellij/plugin/ui/tree/renderers/FileErrorNodeRenderer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.intellij.plugin.ui.tree.renderers;
+
+import com.facebook.buck.intellij.plugin.ui.tree.BuckTreeNodeDetail;
+import com.facebook.buck.intellij.plugin.ui.tree.BuckTreeNodeFileError;
+import com.intellij.icons.AllIcons;
+import com.intellij.ui.components.JBLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Component;
+
+public class FileErrorNodeRenderer implements BuildElementRenderer {
+    @Override
+    public Component render(Object value) {
+
+        JBLabel result = new JBLabel(
+                ((BuckTreeNodeFileError ) value).getFilePath(),
+                AllIcons.Ide.Warning_notifications,
+                SwingConstants.HORIZONTAL);
+
+        BuckTreeNodeFileError buckNode = (BuckTreeNodeFileError ) value;
+        for (int i = 0; i < buckNode.getChildCount(); i++) {
+            BuckTreeNodeDetail childNode = (BuckTreeNodeDetail ) buckNode.getChildAt(i);
+            if (childNode.getType() == BuckTreeNodeDetail.DetailType.ERROR) {
+                result.setIcon(AllIcons.Ide.Error);
+                result.setForeground(Color.RED);
+                break;
+            }
+        }
+        return result;
+    }
+}
