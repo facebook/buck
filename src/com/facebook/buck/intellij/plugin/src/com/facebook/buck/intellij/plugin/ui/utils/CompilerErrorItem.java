63ce542c2378dd49095f56b9ed5ec19766fc9cc4
Petru Marius Balau <petrumarius@fb.com>
2015-10-21 12:38:10 -0700

    [WS] utils to extract the error message from compiler error message
    Summary: Added utility class to extract info from error messages from java compiler.
    
    Test Plan: In IntelliJ run tests/ErrorExtractorTest.java

---
+++ src/com/facebook/buck/intellij/plugin/src/com/facebook/buck/intellij/plugin/ui/utils/CompilerErrorItem.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.intellij.plugin.ui.utils;
+
+public class CompilerErrorItem {
+    public enum Type {
+        ERROR,
+        WARNING
+    }
+    private String mError;
+    private String mFilePath;
+    private int mColumn;
+    private int mLine;
+    private Type mType;
+
+    public CompilerErrorItem(
+            String filePath,
+            int line,
+            Type type
+    ) {
+        mFilePath = filePath;
+        mColumn = -1;
+        mLine = line;
+        mType = type;
+        mError = "";
+    }
+
+    public void setError(String error) {
+        mError = error;
+    }
+    public void setColumn(int column) { mColumn = column; }
+
+    public String getError() { return mError; }
+    public String getFilePath() { return mFilePath; }
+    public int getColumn() { return mColumn; }
+    public int getLine() { return mLine; }
+    public Type getType() { return mType; }
+}
