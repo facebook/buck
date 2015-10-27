926b556a8887941daab046e712505771841610c9
Shawn Wilsher <sdwilsh@fb.com>
2015-10-27 13:05:17 -0700

    Fix incorrect error message
    Test Plan: CI

--- src/com/facebook/buck/rules/Cell.java
+++ src/com/facebook/buck/rules/Cell.java
@@ -284,7 +284,7 @@ public class Cell {
   @SuppressWarnings("serial")
   public static class MissingBuildFileException extends BuildTargetException {
     public MissingBuildFileException(BuildTarget buildTarget, BuckConfig buckConfig) {
-      super(String.format("No build XXX at %s when resolving target %s.",
+      super(String.format("No build file at %s when resolving target %s.",
           buildTarget.getBasePathWithSlash() + new ParserConfig(buckConfig).getBuildFileName(),
           buildTarget.getFullyQualifiedName()));
     }
