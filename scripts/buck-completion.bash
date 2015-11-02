f5a40e350567ce37c6101d7c16eaf45ec1bfcf0a
Vladislav Alexeev <beefon@fb.com>
2015-11-02 09:25:53 -0800

    Add --build-with-buck into completion list
    Summary: Add --build-with-buck into completion list
    
    Test Plan: Use autocompletion

--- scripts/buck-completion.bash
+++ scripts/buck-completion.bash
@@ -156,6 +156,7 @@ function _buck_completion_try_install() {
 
 function _buck_completion_try_project() {
   _buck_completion_try_long_arg "
+      --build-with-buck
       --combined-project
       --help
       --ide
