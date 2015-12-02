bbf85589b84b9c8ab71b0f6d2cfb58d6b0894311
James Byatt <grumpyjames@gmail.com>
2015-12-02 12:33:51 -0800

    Break out DefaultSuggestBuildRules from DefaultJavaLibrary
    Summary:
    It feels like it would be nice to test it without having to rely on
    DefaultJavaLibrary, so we extract out the previously anonymous
    SuggestBuildRules implementation, rework its inputs to make it look a
    little more like a pure function. We also split the existing test into
    two cases and substantially rework the tests to make adding further
    cases a little simpler.
    
    Test-plan: CI
    Closes https://github.com/facebook/buck/pull/541
    
    Test Plan: Imported from GitHub, without a `Test Plan:` line.
    
    Reviewed By: svcscm
    
    Pulled By: Coneko
    
    fb-gh-sync-id: d7fa65d

---
+++ test/com/facebook/buck/jvm/java/DefaultSuggestBuildRulesTest.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2012-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.jvm.java;
+
+import static org.junit.Assert.assertEquals;
+
+import com.facebook.buck.cli.BuildTargetNodeToBuildRuleTransformer;
+import com.facebook.buck.io.ProjectFilesystem;
+import com.facebook.buck.jvm.core.SuggestBuildRules;
+import com.facebook.buck.model.BuildTarget;
+import com.facebook.buck.model.BuildTargetFactory;
+import com.facebook.buck.parser.NoSuchBuildTargetException;
+import com.facebook.buck.rules.BuildRule;
+import com.facebook.buck.rules.BuildRuleResolver;
+import com.facebook.buck.rules.TargetGraph;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class DefaultSuggestBuildRulesTest {
+  @Rule
+  public TemporaryFolder tmp = new TemporaryFolder();
+
+  private BuildRuleResolver ruleResolver;
+  private ProjectFilesystem projectFilesystem;
+
+  @Before
+  public void before() {
+    ruleResolver =
+        new BuildRuleResolver(TargetGraph.EMPTY, new BuildTargetNodeToBuildRuleTransformer());
+    projectFilesystem = new ProjectFilesystem(tmp.getRoot().toPath());
+  }
+
+  @Test
+  public void suggestTheTopologicallyNearestDependency() throws NoSuchBuildTargetException {
+    // TODO(grumpyjames): stop duplicating source/symbol names if possible
+    BuildRule libraryTwo = javaLibrary("//:libtwo", "com/facebook/Foo.java");
+    BuildRule parent = javaLibrary("//:parent", "com/facebook/Foo.java", libraryTwo);
+    BuildRule grandparent = javaLibrary("//:grandparent", "com/parent/OldManRiver.java", parent);
+
+    ImmutableMap<Path, String> jarPathToSymbols = ImmutableMap.of(
+        parent.getPathToOutput(), "com.facebook.Foo",
+        libraryTwo.getPathToOutput(), "com.facebook.Foo");
+    ImmutableSetMultimap<JavaLibrary, Path> transitiveClasspathEntries =
+        fromLibraries(libraryTwo, parent, grandparent);
+
+    SuggestBuildRules.JarResolver jarResolver = createJarResolver(jarPathToSymbols);
+
+    SuggestBuildRules suggestFn =
+        DefaultSuggestBuildRules.createSuggestBuildFunction(
+            jarResolver,
+            ImmutableSetMultimap.<JavaLibrary, Path>of(),
+            ImmutableSetMultimap.<JavaLibrary, Path>builder()
+                .putAll(transitiveClasspathEntries)
+                .build(),
+            ImmutableList.of(libraryTwo, parent, grandparent));
+
+    final ImmutableSet<String> suggestions =
+        suggestFn.suggest(
+            projectFilesystem,
+            ImmutableSet.of("com.facebook.Foo"));
+
+    assertEquals(ImmutableSet.of("//:parent"), suggestions);
+  }
+
+  @Test
+  public void suggestTopologicallyDistantDependency() throws NoSuchBuildTargetException {
+    // TODO(grumpyjames): stop duplicating source/symbol names if possible
+    BuildRule libraryTwo = javaLibrary("//:libtwo", "com/facebook/Bar.java");
+    BuildRule parent = javaLibrary("//:parent", "com/facebook/Foo.java", libraryTwo);
+    BuildRule grandparent = javaLibrary("//:grandparent", "com/parent/OldManRiver.java", parent);
+
+    ImmutableMap<Path, String> jarPathToSymbols = ImmutableMap.of(
+        parent.getPathToOutput(), "com.facebook.Foo",
+        libraryTwo.getPathToOutput(), "com.facebook.Bar");
+    ImmutableSetMultimap<JavaLibrary, Path> transitiveClasspathEntries =
+        fromLibraries(libraryTwo, parent, grandparent);
+
+    SuggestBuildRules.JarResolver jarResolver = createJarResolver(jarPathToSymbols);
+
+    SuggestBuildRules suggestFn =
+        DefaultSuggestBuildRules.createSuggestBuildFunction(
+            jarResolver,
+            ImmutableSetMultimap.<JavaLibrary, Path>of(),
+            ImmutableSetMultimap.<JavaLibrary, Path>builder()
+                .putAll(transitiveClasspathEntries)
+                .build(),
+            ImmutableList.of(libraryTwo, parent, grandparent));
+
+    final ImmutableSet<String> suggestions =
+        suggestFn.suggest(
+            projectFilesystem,
+            ImmutableSet.of("com.facebook.Bar"));
+
+    assertEquals(ImmutableSet.of("//:libtwo"), suggestions);
+  }
+
+  private ImmutableSetMultimap<JavaLibrary, Path> fromLibraries(BuildRule...buildRules) {
+    ImmutableSetMultimap.Builder<JavaLibrary, Path> builder =
+        ImmutableSetMultimap.builder();
+
+    for (BuildRule buildRule : buildRules) {
+      //noinspection ConstantConditions
+      builder.put((JavaLibrary) buildRule, buildRule.getPathToOutput());
+    }
+
+    return builder.build();
+  }
+
+  private SuggestBuildRules.JarResolver createJarResolver(
+      final ImmutableMap<Path, String> classToSymbols) {
+
+    ImmutableSetMultimap.Builder<Path, String> resolveMapBuilder =
+        ImmutableSetMultimap.builder();
+
+    for (Map.Entry<Path, String> entry : classToSymbols.entrySet()) {
+      String fullyQualified = entry.getValue();
+      String packageName = fullyQualified.substring(0, fullyQualified.lastIndexOf('.'));
+      String className = fullyQualified.substring(fullyQualified.lastIndexOf('.'));
+      resolveMapBuilder.putAll(entry.getKey(), fullyQualified, packageName, className);
+    }
+
+    final ImmutableSetMultimap<Path, String> resolveMap = resolveMapBuilder.build();
+
+    return new SuggestBuildRules.JarResolver() {
+      @Override
+      public ImmutableSet<String> resolve(ProjectFilesystem filesystem, Path relativeClassPath) {
+        if (resolveMap.containsKey(relativeClassPath)) {
+          return resolveMap.get(relativeClassPath);
+        } else {
+          return ImmutableSet.of();
+        }
+      }
+    };
+  }
+
+  private BuildRule javaLibrary(
+      String name,
+      String pathToClass,
+      BuildRule... deps) throws NoSuchBuildTargetException {
+    BuildTarget target = BuildTargetFactory.newInstance(name);
+
+    JavaLibraryBuilder builder = JavaLibraryBuilder
+        .createBuilder(target)
+        .addSrc(Paths.get("java/src/" + pathToClass));
+    for (BuildRule dep : deps) {
+      builder = builder.addDep(dep.getBuildTarget());
+    }
+    return builder.build(ruleResolver);
+  }
+}
