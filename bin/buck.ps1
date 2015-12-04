74fdf940a3a8fbca5ca63d9a4e51aab8911a9cf9
Shawn Wilsher <sdwilsh@fb.com>
2015-12-04 13:03:49 -0800

    Set the PYTHONPATH on Windows
    Summary: A previous change broke building Buck on Windows due to a python import.
    
    Test Plan: `./bin/buck build buck`
    
    Reviewed By: @bhamiltoncx
    
    fb-gh-sync-id: d1fa8d9

--- bin/buck.ps1
+++ bin/buck.ps1
@@ -1,2 +1,3 @@
 # Invoke the buck wrappers scripts with Python.
+$env:PYTHONPATH=(Join-Path (Join-Path (Split-Path (Split-Path $PSCommandPath)) third-party) nailgun)
 python (Join-Path (Join-Path (Split-Path (Split-Path $PSCommandPath)) programs) buck.py) $args
