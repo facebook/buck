e0ea7a5b0bf8926f438e21d69e0cf0ac040a7f41
Ben Hamilton <beng@fb.com>
2015-12-10 10:39:10 -0800

    Perf: New class AsynchronousDirectoryContentsCleaner
    Summary:
    Upon upgrade, Buck currently blocks for a long time at
    startup cleaning up the `buck-out` directory synchronously.
    
    This work can be done asynchronously, so this is a new class
    `AsynchronousDirectoryContentsCleaner` which we will use to asynchronously
    delete `buck-out/.trash` directory upon upgrade.
    
    Test Plan: Unit tests included. `ant java-test -Dtest.class=AsynchronousDirectoryContentsCleaner`
    
    Reviewed By: k21
    
    fb-gh-sync-id: c660774

---
+++ src/com/facebook/buck/io/AsynchronousDirectoryContentsCleaner.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.io;
+
+import static com.facebook.buck.util.concurrent.MoreExecutors.newSingleThreadExecutor;
+
+import com.facebook.buck.log.Logger;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import java.io.IOException;
+
+import java.nio.file.Path;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.EnumSet;
+
+/**
+ * Asynchronously cleans the contents of a directory.
+ */
+public class AsynchronousDirectoryContentsCleaner {
+  private static final Logger LOG = Logger.get(AsynchronousDirectoryContentsCleaner.class);
+
+  private final Path pathToClean;
+  private final Executor executor;
+
+  /**
+   * A ThreadFactory which ensures the spawned threads do not keep the JVM alive at
+   * exit time.
+   */
+  private static class DaemonThreadFactory implements ThreadFactory {
+
+    private final String threadName;
+
+    public DaemonThreadFactory(String threadName) {
+      this.threadName = threadName;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+      Thread newThread = Executors.defaultThreadFactory().newThread(r);
+      newThread.setDaemon(true);
+      newThread.setName(threadName);
+      return newThread;
+    }
+  }
+
+  public AsynchronousDirectoryContentsCleaner(Path pathToClean) {
+    this(
+        pathToClean,
+        newSingleThreadExecutor(
+            new DaemonThreadFactory(
+                "AsynchronousDirectoryContentsCleaner-" + pathToClean.toString())));
+  }
+
+  @VisibleForTesting
+  AsynchronousDirectoryContentsCleaner(Path pathToClean, Executor executor) {
+    this.pathToClean = pathToClean;
+    this.executor = executor;
+  }
+
+  /**
+   * Starts cleaning the configured directory in the background.
+   *
+   * Multiple calls to this method will be serialized, so only one
+   * instance of directory cleaning will occur at a time.
+   */
+  public void startCleaningDirectory() {
+    executor.execute(
+        new Runnable() {
+          @Override
+          public void run() {
+            LOG.debug("Starting to clean %s", pathToClean);
+            try {
+              MoreFiles.deleteRecursivelyWithOptions(
+                  pathToClean,
+                  EnumSet.of(
+                      MoreFiles.DeleteRecursivelyOptions.IGNORE_NO_SUCH_FILE_EXCEPTION,
+                      MoreFiles.DeleteRecursivelyOptions.DELETE_CONTENTS_ONLY));
+            } catch (IOException e) {
+              LOG.warn(e, "I/O error cleaning trash");
+            } finally {
+              LOG.debug("Done cleaning %s", pathToClean);
+            }
+          }
+        });
+  }
+}
---
+++ test/com/facebook/buck/io/AsynchronousDirectoryContentsCleanerTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.io;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import static org.hamcrest.Matchers.is;
+
+import static org.junit.Assert.assertThat;
+
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link AsynchronousDirectoryContentsCleaner}.
+ */
+public class AsynchronousDirectoryContentsCleanerTest {
+  @Test
+  public void contentsOfTrashDirectoryCleanedAsynchronously() throws Exception {
+    FileSystem vfs = Jimfs.newFileSystem(Configuration.unix());
+    Path dirToDelete = vfs.getPath("/tmp/fake-tmp-dir");
+    Path fooDir = dirToDelete.resolve("foo");
+    Path fooBarDir = fooDir.resolve("bar");
+    Files.createDirectories(fooBarDir.resolve("baz"));
+
+    Path fooBarBlechTxtFile = fooBarDir.resolve("blech.txt");
+    Files.write(fooBarBlechTxtFile, "hello world\n".getBytes(UTF_8));
+
+    AsynchronousDirectoryContentsCleaner cleaner =
+        new AsynchronousDirectoryContentsCleaner(dirToDelete, MoreExecutors.directExecutor());
+
+    assertThat(Files.exists(dirToDelete), is(true));
+    assertThat(Files.exists(fooBarDir), is(true));
+    assertThat(Files.exists(fooBarBlechTxtFile), is(true));
+
+    // This executes synchronously, since we passed in a direct executor above.
+    cleaner.startCleaningDirectory();
+
+    assertThat(Files.exists(dirToDelete), is(true));
+    assertThat(Files.exists(fooBarDir), is(false));
+    assertThat(Files.exists(fooBarBlechTxtFile), is(false));
+  }
+}
