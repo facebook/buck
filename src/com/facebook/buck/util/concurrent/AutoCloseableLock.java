ed616ab34db064c0dfb8249464727004adfa55d3
Shawn Wilsher <sdwilsh@fb.com>
2015-12-11 12:25:59 -0800

    Add an AutoCloseable ReadWriteUpdateLock
    Test Plan: Added test
    
    Reviewed By: shs96c, marcinkosiba
    
    fb-gh-sync-id: 68d2033

---
+++ src/com/facebook/buck/util/concurrent/AutoCloseableLock.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util.concurrent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class AutoCloseableLock implements AutoCloseable, Lock {
+  private final Lock lock;
+
+  public AutoCloseableLock(Lock lock) {
+    this.lock = lock;
+  }
+
+  @Override
+  public void close() {
+    unlock();
+  }
+
+  @Override
+  public void lock() {
+    lock.lock();
+  }
+
+  @Override
+  public void lockInterruptibly() throws InterruptedException {
+    lock.lockInterruptibly();
+  }
+
+  @Override
+  public boolean tryLock() {
+    return lock.tryLock();
+  }
+
+  @Override
+  public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+    return lock.tryLock(time, unit);
+  }
+
+  @Override
+  public void unlock() {
+    lock.unlock();
+  }
+
+  @Override
+  public Condition newCondition() {
+    return lock.newCondition();
+  }
+}
---
+++ src/com/facebook/buck/util/concurrent/AutoCloseableReadWriteUpdateLock.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util.concurrent;
+
+import com.googlecode.concurentlocks.ReadWriteUpdateLock;
+import com.googlecode.concurentlocks.ReentrantReadWriteUpdateLock;
+
+public class AutoCloseableReadWriteUpdateLock implements ReadWriteUpdateLock {
+
+  private final ReentrantReadWriteUpdateLock reentrantReadWriteUpdateLock;
+
+  public AutoCloseableReadWriteUpdateLock() {
+    reentrantReadWriteUpdateLock = new ReentrantReadWriteUpdateLock();
+  }
+
+  @Override
+  public AutoCloseableLock readLock() {
+    AutoCloseableLock lock = new AutoCloseableLock(reentrantReadWriteUpdateLock.readLock());
+    lock.lock();
+    return lock;
+  }
+
+  @Override
+  public AutoCloseableLock updateLock() {
+    AutoCloseableLock lock = new AutoCloseableLock(reentrantReadWriteUpdateLock.updateLock());
+    lock.lock();
+    return lock;
+  }
+
+  @Override
+  public AutoCloseableLock writeLock() {
+    AutoCloseableLock lock = new AutoCloseableLock(reentrantReadWriteUpdateLock.writeLock());
+    lock.lock();
+    return lock;
+  }
+}
