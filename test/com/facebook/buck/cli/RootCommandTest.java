0396c75a450b0cc5ef895c7e9ed4ce299752027d
Delyan Kratunov <delyank@fb.com>
2015-12-29 16:19:17 -0800

    [buck] Add a root command to help scripts
    Summary:
    Mercurial has an `hg root` command to help scripts navigate the checkout.
    This introduces a similar command to buck (I was inspired by a few bad
    clones I stumbled upon).
    
    Since the find-buckconfig logic lives in the python launcher, this command
    doesn't do much, just prints the normalized path to `ProjectFilesystem` root.
    
    One can imagine extending this to cross-cell support,
    `buck root FOO` or some such.
    
    Test Plan: New silly unit test.
    
    Reviewed By: sdwilsh
    
    fb-gh-sync-id: ed24db8

---
+++ test/com/facebook/buck/cli/RootCommandTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.cli;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+
+import com.facebook.buck.testutil.TestConsole;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class RootCommandTest {
+
+  @Test
+  public void testPrintsProjectRootToStdout() throws IOException, InterruptedException {
+    TestConsole console = new TestConsole();
+    CommandRunnerParams commandRunnerParams = CommandRunnerParamsForTesting
+        .builder()
+        .setConsole(console)
+        .build();
+
+    RootCommand runCommand = new RootCommand();
+    int exitCode = runCommand.run(commandRunnerParams);
+    String testRoot = commandRunnerParams
+        .getCell()
+        .getFilesystem()
+        .getRootPath()
+        .normalize()
+        .toAbsolutePath()
+        .toString();
+
+    assertThat(console.getTextWrittenToStdOut(), containsString(testRoot));
+    assertThat(exitCode, equalTo(0));
+  }
+}
