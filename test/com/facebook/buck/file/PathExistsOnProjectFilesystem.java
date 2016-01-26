137163edafd4985d30164daeae472d236e6091b1
Shawn Wilsher <sdwilsh@fb.com>
2016-01-26 14:26:29 -0800

    Add new matchers for `ProjectFilesystem` existance
    Summary:
    This provides much better error messages than asserting if the `exists`
    method returns `true` or `false`.
    
    Test Plan:
    * `./bin/buck build //test/com/facebook/buck/file:matchers`
    * Used in a subsequent change
    
    Reviewed By: marcinkosiba
    
    fb-gh-sync-id: bf0daa4

---
+++ test/com/facebook/buck/file/PathExistsOnProjectFilesystem.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.file;
+
+import com.facebook.buck.io.ProjectFilesystem;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+import java.nio.file.Path;
+
+public class PathExistsOnProjectFilesystem extends BaseMatcher<ProjectFilesystem> {
+  final Path expectedPath;
+
+  public PathExistsOnProjectFilesystem(
+      Path expectedPath) {
+    this.expectedPath = expectedPath;
+  }
+
+  @Override
+  public boolean matches(Object o) {
+    if (o instanceof ProjectFilesystem) {
+      ProjectFilesystem filesystem = (ProjectFilesystem) o;
+      return filesystem.exists(expectedPath);
+    }
+    return false;
+  }
+
+  @Override
+  public void describeTo(Description description) {
+    description.appendText(String.format("%s exists", expectedPath.toString()));
+  }
+
+  @Override
+  public void describeMismatch(Object item, Description description) {
+    description.appendText(String.format("%s does not exist", expectedPath.toString()));
+  }
+}
