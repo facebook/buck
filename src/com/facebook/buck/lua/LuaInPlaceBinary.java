4c3a43abdf300d3328bc3ace79b0805ad0a03371
Andrew Gallagher <agallagher@fb.com>
2016-02-16 23:43:45 -0800

    lua: create parent dirs for in-place binaries
    Summary: This would cause occasional test failures.
    
    Test Plan: CI
    
    Reviewed By: yiding
    
    fb-gh-sync-id: 7d8b581
    shipit-source-id: 7d8b581

--- src/com/facebook/buck/lua/LuaInPlaceBinary.java
+++ src/com/facebook/buck/lua/LuaInPlaceBinary.java
@@ -27,6 +27,7 @@ import com.facebook.buck.rules.SourcePathResolver;
 import com.facebook.buck.rules.SymlinkTree;
 import com.facebook.buck.rules.Tool;
 import com.facebook.buck.step.Step;
+import com.facebook.buck.step.fs.MkdirStep;
 import com.facebook.buck.step.fs.WriteFileStep;
 import com.facebook.buck.util.Escaper;
 import com.google.common.base.Charsets;
@@ -121,7 +122,8 @@ public class LuaInPlaceBinary extends LuaBinary {
       BuildableContext buildableContext) {
     Path binPath = getBinPath();
     buildableContext.recordArtifact(binPath);
-    return ImmutableList.<Step>of(
+    return ImmutableList.of(
+        new MkdirStep(getProjectFilesystem(), binPath.getParent()),
         new WriteFileStep(getProjectFilesystem(), script, binPath, /* executable */ true));
   }
 
