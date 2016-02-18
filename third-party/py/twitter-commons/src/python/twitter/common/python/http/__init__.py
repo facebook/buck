8d84d18ce2b9b4a6ced81d672680f30152c9ef0d
Shawn Wilsher <sdwilsh@fb.com>
2016-02-18 10:28:50 -0800

    Remove twitter-commons
    Summary: This appears to be unused now.
    
    Test Plan: CI
    
    Reviewed By: bhamiltoncx
    
    fb-gh-sync-id: a405379
    shipit-source-id: a405379

--- third-party/py/twitter-commons/src/python/twitter/common/python/http/__init__.py
+++
@@ -1,9 +0,0 @@
-from .crawler import Crawler
-from .http import CachedWeb, FetchError, Web
-
-__all__ = (
-  CachedWeb,
-  Crawler,
-  FetchError,
-  Web,
-)
--- third-party/py/twitter-commons/src/python/twitter/common/python/http/tracer.py
+++
@@ -1,6 +0,0 @@
-from ..tracer import Tracer
-
-__all__ = ('TRACER',)
-
-TRACER = Tracer(predicate=Tracer.env_filter('TWITTER_COMMON_PYTHON_HTTP'),
-                prefix='twitter.common.python.http: ')
