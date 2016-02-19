17270f823411e6bd3210ec5495a50b88f2ae5051
Marshall Roch <mroch@fb.com>
2016-02-19 04:50:24 -0800

    split native and bytecode linkable inputs (take 2)
    Summary:native inputs were being passed around as inputs to both the native and bytecode rules, and converted by regex.
    
    this diff instead threads bytecode inputs through to the bytecode rules and native to native (as before). this eliminates the restriction that native and bytecode have the same filename (and in a followup diff, that native even exists at all).
    
    (take 2: this was reverted because native inputs were being used in bytecode rules and this split exposed it. that underlying issue has been fixed)
    
    Test Plan: tests
    
    Reviewed By: int3
    
    fb-gh-sync-id: 7a5c23b
    shipit-source-id: 7a5c23b

--- src/com/facebook/buck/ocaml/OCamlStaticLibrary.java
+++ src/com/facebook/buck/ocaml/OCamlStaticLibrary.java
@@ -18,6 +18,7 @@ package com.facebook.buck.ocaml;
 
 import com.facebook.buck.cxx.NativeLinkableInput;
 import com.facebook.buck.model.BuildTarget;
+import com.facebook.buck.model.UnflavoredBuildTarget;
 import com.facebook.buck.rules.BuildRule;
 import com.facebook.buck.rules.BuildRuleParams;
 import com.facebook.buck.rules.BuildTargetSourcePath;
@@ -64,22 +65,22 @@ class OCamlStaticLibrary extends NoopBuildRule implements OCamlLibrary {
         compileParams.getBuildTarget());
   }
 
-  @Override
-  public NativeLinkableInput getLinkableInput() {
+  private NativeLinkableInput getLinkableInput(boolean isBytecode) {
     NativeLinkableInput.Builder inputBuilder = NativeLinkableInput.builder();
 
     // Add linker flags.
     inputBuilder.addAllArgs(StringArg.from(linkerFlags));
 
     // Add arg and input for static library.
+    UnflavoredBuildTarget staticBuildTarget = staticLibraryTarget.getUnflavoredBuildTarget();
     inputBuilder.addArgs(
         new SourcePathArg(
             getResolver(),
             new BuildTargetSourcePath(
                 ocamlLibraryBuild.getBuildTarget(),
-                OCamlBuildContext.getNativeOutputPath(
-                    staticLibraryTarget.getUnflavoredBuildTarget(),
-                    /* isLibrary */ true))));
+                isBytecode
+                ? OCamlBuildContext.getBytecodeOutputPath(staticBuildTarget, /* isLibrary */ true)
+                : OCamlBuildContext.getNativeOutputPath(staticBuildTarget, /* isLibrary */ true))));
 
     // Add args and inputs for C object files.
     for (SourcePath objFile : objFiles) {
@@ -87,6 +88,16 @@ class OCamlStaticLibrary extends NoopBuildRule implements OCamlLibrary {
     }
 
     return inputBuilder.build();
+  }
+
+  @Override
+  public NativeLinkableInput getNativeLinkableInput() {
+    return getLinkableInput(false);
+  }
+
+  @Override
+  public NativeLinkableInput getBytecodeLinkableInput() {
+    return getLinkableInput(true);
   }
 
   @Override
--- src/com/facebook/buck/ocaml/PrebuiltOCamlLibraryDescription.java
+++ src/com/facebook/buck/ocaml/PrebuiltOCamlLibraryDescription.java
@@ -76,6 +76,9 @@ public class PrebuiltOCamlLibraryDescription
     final SourcePath staticNativeLibraryPath = new PathSourcePath(
         params.getProjectFilesystem(),
         libPath.resolve(nativeLib));
+    final SourcePath staticBytecodeLibraryPath = new PathSourcePath(
+        params.getProjectFilesystem(),
+        libPath.resolve(bytecodeLib));
     final ImmutableList<SourcePath> staticCLibraryPaths =
         FluentIterable.from(cLibs)
           .transform(new Function<String, SourcePath>() {
@@ -94,9 +97,8 @@ public class PrebuiltOCamlLibraryDescription
     return new PrebuiltOCamlLibrary(
         params,
         new SourcePathResolver(resolver),
-        nativeLib,
-        bytecodeLib,
         staticNativeLibraryPath,
+        staticBytecodeLibraryPath,
         staticCLibraryPaths,
         bytecodeLibraryPath,
         libPath,
