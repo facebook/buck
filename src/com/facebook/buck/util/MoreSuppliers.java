4e9087be9e9b43eb124b46558ac7239f64b2fa84
Yiding Jia <yiding@fb.com>
2016-02-22 13:58:09 -0800

    Add MoreSuppliers.weakMemoize to cache a value weakly
    Test Plan: A unit test.
    
    Reviewed By: k21
    
    fb-gh-sync-id: 835488c
    shipit-source-id: 835488c

---
+++ src/com/facebook/buck/util/MoreSuppliers.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util;
+
+import com.google.common.base.Supplier;
+
+import java.lang.ref.WeakReference;
+
+import javax.annotation.Nullable;
+
+public final class MoreSuppliers {
+  private MoreSuppliers() {}
+
+  public static <T> Supplier<T> weakMemoize(Supplier<T> delegate) {
+    return (delegate instanceof WeakMemoizingSupplier)
+        ? delegate
+        : new WeakMemoizingSupplier<>(delegate);
+  }
+
+  private static class WeakMemoizingSupplier<T> implements Supplier<T> {
+    private final Supplier<T> delegate;
+    private WeakReference<T> valueRef;
+
+    public WeakMemoizingSupplier(Supplier<T> delegate) {
+      this.delegate = delegate;
+      this.valueRef = new WeakReference<>(null);
+    }
+
+    @Override
+    public T get() {
+      @Nullable T value = valueRef.get();
+      if (value == null) {
+        synchronized (this) {
+          // Check again in case someone else has populated the cache.
+          value = valueRef.get();
+          if (value == null) {
+            value = delegate.get();
+            valueRef = new WeakReference<>(value);
+          }
+        }
+      }
+      return value;
+    }
+  }
+
+}
---
+++ test/com/facebook/buck/util/MoreSuppliersTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util;
+
+import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class MoreSuppliersTest {
+  @Test
+  public void weakMemoizeShouldMemoize() {
+    Supplier<Object> supplier = MoreSuppliers.weakMemoize(new Supplier<Object>() {
+      @Override
+      public Object get() {
+        return new Object();
+      }
+    });
+    Object a = supplier.get();
+    Object b = supplier.get();
+    Assert.assertSame("Supplier should have cached the instance", a, b);
+  }
+
+  @Test
+  public void weakMemoizeShouldRunDelegateOnlyOnceOnConcurrentAccess() throws Exception {
+    final int numFetchers = 10;
+
+    final Semaphore semaphore = new Semaphore(0);
+
+    class TestDelegate implements Supplier<Object> {
+      private int timesCalled = 0;
+
+      public int getTimesCalled() {
+        return timesCalled;
+      }
+
+      @Override
+      public Object get() {
+        try {
+          // Wait for all the fetch threads to be ready.
+          semaphore.acquire(numFetchers);
+          Thread.sleep(50); // Give other threads a chance to catch up.
+          timesCalled++;
+          return new Object();
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        } finally {
+          semaphore.release(numFetchers);
+        }
+      }
+    }
+
+    TestDelegate delegate = new TestDelegate();
+    final Supplier<Object> supplier = MoreSuppliers.weakMemoize(delegate);
+    ListeningExecutorService executor =
+        MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(numFetchers));
+
+    class Fetcher implements Callable<Object> {
+      @Override
+      public Object call() {
+        // Signal that this particular fetcher is ready.
+        semaphore.release();
+        return supplier.get();
+      }
+    }
+
+    ImmutableList.Builder<Callable<Object>> fetcherBuilder = ImmutableList.builder();
+    for (int i = 0; i < numFetchers; i++) {
+      fetcherBuilder.add(new Fetcher());
+    }
+    @SuppressWarnings("unchecked")
+    List<ListenableFuture<Object>> futures =
+        (List<ListenableFuture<Object>>) (List<?>) executor.invokeAll(fetcherBuilder.build());
+
+    // Wait for all fetchers to finish.
+    List<Object> results = Futures.allAsList(futures).get();
+
+    Assert.assertEquals("should only have been called once", 1, delegate.getTimesCalled());
+    Assert.assertThat(
+        "all result items are the same",
+        ImmutableSet.copyOf(results),
+        Matchers.hasSize(1));
+  }
+
+  @Test
+  public void weakMemoizeShouldNotMemoizeSupplierThatIsAlreadyWeakMemoized() {
+    Supplier<Object> supplier = MoreSuppliers.weakMemoize(new Supplier<Object>() {
+      @Override
+      public Object get() {
+        return new Object();
+      }
+    });
+    Supplier<Object> twiceMemoized = MoreSuppliers.weakMemoize(supplier);
+    Assert.assertSame("should have just returned the same instance", supplier, twiceMemoized);
+  }
+}
