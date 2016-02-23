10942e9e65ce4905bbc7a276e4a18d1d40a2807d
Bob Haarman <robbert@fb.com>
2016-02-22 16:56:13 -0800

    allow arbitrary characters in buckconfig values through quoting
    Summary: Adds a quoting mechanism for .buckconfig files. Quoted part start and end with double quotes. Within double quotes, splitting on separator characters is inhibited, and double quote, backslash, and newline can be encoded as "\"", "\\", and "\n", respectively. With this change, buckconfig values can contain arbitrary characters, including ones that would normally have special significance.
    
    Test Plan: bin/buck test test/com/facebook/buck/cli
    
    Reviewed By: dcolascione
    
    fb-gh-sync-id: 950867d
    shipit-source-id: 950867d

--- src/com/facebook/buck/cli/Config.java
+++ src/com/facebook/buck/cli/Config.java
@@ -22,11 +22,8 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Predicate;
-import com.google.common.base.Splitter;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -168,35 +165,38 @@ public class Config {
   }
   public Optional<ImmutableList<String>> getOptionalListWithoutComments(
       String sectionName, String propertyName, char splitChar) {
-    Optional<String> value = getValue(sectionName, propertyName);
-    if (!value.isPresent()) {
+    Optional<String> rawValue = getRawValue(sectionName, propertyName);
+    if (!rawValue.isPresent()) {
+      return Optional.absent();
+    }
+    String value = rawValue.get();
+    if (value.isEmpty()) {
       return Optional.absent();
     }
 
-    Iterable<String> allValues = Splitter.on(splitChar)
-        .omitEmptyStrings()
-        .trimResults()
-        .split(value.get());
-    return Optional.of(
-        FluentIterable.from(allValues)
-            .filter(
-                new Predicate<String>() {
-                  @Override
-                  public boolean apply(String input) {
-                    // Reject if the first printable character is an ini comment char (';' or '#')
-                    return !Pattern.compile("^\\s*[#;]").matcher(input).find();
-                  }
-                })
-            .toList());
+    // Reject if the first nonspace character is an ini comment char (';' or '#')
+    if (Pattern.compile("^\\s*[#;]").matcher(value).find()) {
+      return Optional.absent();
+    }
+
+    return Optional.of(decodeQuotedParts(
+            value, Optional.of(splitChar), sectionName, propertyName));
   }
 
   public Optional<String> getValue(String sectionName, String propertyName) {
-    ImmutableMap<String, String> properties = get(sectionName);
-    String value = properties.get(propertyName);
-    if (value != null && value.length() == 0) {
-      value = null;
+    Optional<String> rawValue = getRawValue(sectionName, propertyName);
+    if (rawValue.isPresent()) {
+      String value = rawValue.get();
+      if (value.isEmpty()) {
+        return Optional.absent();
+      } else {
+        return Optional.of(decodeQuotedParts(
+                value, Optional.<Character>absent(),
+                sectionName, propertyName).get(0));
+      }
+    } else {
+      return rawValue;
     }
-    return Optional.fromNullable(value);
   }
 
   public Optional<Long> getLong(String sectionName, String propertyName) {
@@ -344,5 +344,188 @@ public class Config {
     }
 
     return toReturn.build();
+  }
+
+  /**
+   * Decodes from a string to a list of strings, splitting on separators.
+   * The encoded string may contain double quotes. These inhibit the special
+   * meaning of characters inside them, except for backslash and double quote.
+   * Double quote ends the quoted part, and backslash begins an escape
+   * sequence. The following escape sequences are supported:
+   *
+   * \          backslash
+   * "          double quote
+   * n          newline
+   * r          carriage return
+   * t          tab
+   * x##        unicode character with code point ## (in hex)
+   * u####      unicode character with code point #### (in hex)
+   * U########  unicode character with code point ######## (in hex)
+   *
+   * Using this decoding, the resulting strings can contain any unicode
+   * character, even the ones that normally would have special meaning.
+   *
+   * When the splitting character is absent, no splitting is performed and a
+   * list containing a single string is returned.
+   *
+   * Unquoted whitespace is trimmed from the front of values.
+   *
+   * @param input string to decode
+   * @param splitChar character to split on (if absent, no splitting performed)
+   * @param section section in the configuration file
+   * @param field field in the configuration file
+   *
+   * @return list of decoded parts (single-item list if splitChar is absent)
+   */
+  private static ImmutableList<String> decodeQuotedParts(
+      String input,
+      Optional<Character> splitChar,
+      String section,
+      String field) {
+    ImmutableList.Builder<String> listBuilder = ImmutableList.<String>builder();
+    StringBuilder stringBuilder = new StringBuilder();
+    boolean inQuotes = false;
+    int quoteIndex = 0;
+    for (int i = 0; i < input.length(); i++) {
+      char c = input.charAt(i);
+      if (inQuotes) {
+        if (c == '"') {
+          inQuotes = false;
+          continue;
+        } else if (c == '\\') {
+          ++i;
+          if (i >= input.length()) {
+            throw new HumanReadableException(
+                ".buckconfig: %s:%s: Input ends inside escape sequence: %s",
+                section, field, input.substring(i - 1));
+          }
+          c = input.charAt(i);
+          switch(c) {
+            case 'n':
+              stringBuilder.append('\n');
+              continue;
+            case 'r':
+              stringBuilder.append('\r');
+              continue;
+            case 't':
+              stringBuilder.append('\t');
+              continue;
+            case 'U':
+              int codePoint = hexDecode(
+                  input, i + 1, 8, "\\U", section, field);
+              stringBuilder.append(Character.toChars(codePoint));
+              i += 8;
+              continue;
+            case 'u':
+              stringBuilder.append((char) hexDecode(
+                      input, i + 1, 4, "\\u", section, field));
+              i += 4;
+              continue;
+            case 'x':
+              stringBuilder.append((char) hexDecode(
+                      input, i + 1, 2, "\\x", section, field));
+              i += 2;
+              continue;
+            case '\\':
+            case '"':
+              // These characters are added literally.
+              break;
+            default:
+              throw new HumanReadableException(
+                  ".buckconfig: %s:%s: Invalid escape sequence: %s",
+                  section, field, input.substring(i - 1, i + 1));
+          }
+        }
+      } else if (c == '"') {
+        quoteIndex = i;
+        inQuotes = true;
+        continue;
+      } else if (splitChar.isPresent() && c == splitChar.get()) {
+        listBuilder.add(stringBuilder.toString());
+        stringBuilder = new StringBuilder();
+        continue;
+      } else if (stringBuilder.length() == 0 && c == ' ' || c == '\t') {
+        // Skip unquoted whitespace before value.
+        continue;
+      }
+      // default case: add the actual character
+      stringBuilder.append(c);
+    }
+
+    if (inQuotes) {
+      // We reached the end of the input without finding the closing quote.
+      // Show a short sample of the quoted part in the error message.
+      int lastIndex = quoteIndex + 10;
+      if (lastIndex >= input.length()) {
+        lastIndex = input.length();
+      }
+      throw new HumanReadableException(
+          ".buckconfig: %s:%s: " +
+          "Input ends inside quoted string: %s...",
+          section, field, input.substring(quoteIndex, lastIndex));
+    }
+
+    listBuilder.add(stringBuilder.toString());
+    return listBuilder.build();
+  }
+
+  /**
+   * @return the value at sectionName, propertyName, without any decoding,
+   *   or absent() if the key is not present.
+   */
+  private Optional<String> getRawValue(String sectionName, String propertyName) {
+    ImmutableMap<String, String> properties = get(sectionName);
+    return Optional.fromNullable(properties.get(propertyName));
+  }
+
+  /**
+   * Decodes hexadecimal digits from a string.
+   * @param string the string to decode the digits from
+   * @param begin position to start decoding from
+   * @param length number of digits to decode
+   * @param prefix characters before the hexadecimal digits (e.g. "\\x")
+   * @param section section name in configuration file
+   * @param field field name in configuration file
+   *
+   * @return the decoded value.
+   */
+  private static int hexDecode(
+      String string,
+      int begin,
+      int length,
+      String prefix,
+      String section,
+      String field) {
+    int result = 0;
+    for (int i = begin; i < begin + length; i++) {
+      if (i >= string.length()) {
+        throw new HumanReadableException(
+            ".buckconfig: %s:%s: " +
+            "Input ends inside hexadecimal sequence: %s%s",
+            section, field, prefix, string.substring(begin));
+      }
+      char c = string.charAt(i);
+      if (c >= 'a') {
+        // 'a' has value 97. Subtract 87, so 'a' becomes 10, 'b' 11, etc.
+        c -= 87;
+      } else if (c >= 'A') {
+        // 'A' has value 65. Subtract 55, so 'A' becomes 10, 'B' 11, etc.
+        c -= 55;
+      } else if (c >= '0' && c <= '9') {
+        // '0' has value 48, so subtract that, making '0' 0, '1' 1, etc.
+        c -= 48;
+      } else {
+        // not a valid hex char; set c to an invalid value to trigger
+        // the exception below.
+        c = 255;
+      }
+      if (c > 16) {
+        throw new HumanReadableException(
+            ".buckconfig: %s:%s: Invalid hexadecimal digit in sequence: %s%s",
+            section, field, prefix, string.substring(begin, i + 1));
+      }
+      result = (result << 4) | c;
+    }
+    return result;
   }
 }
