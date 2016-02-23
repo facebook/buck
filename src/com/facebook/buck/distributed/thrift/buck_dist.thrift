ec1ece37f8a54067fa56ba2df5dbf500c78b67fd
Shivanker Goel <shivanker@fb.com>
2016-02-23 05:01:37 -0800

    Update buck_dist.thrift and gen sources
    Summary: Sync up with the frontend thrift file and update sources
    
    Test Plan:buck test
    ci
    
    Reviewed By: marcinkosiba
    
    fb-gh-sync-id: 399273a
    shipit-source-id: 399273a

--- src/com/facebook/buck/distributed/thrift/buck_dist.thrift
+++ src/com/facebook/buck/distributed/thrift/buck_dist.thrift
@@ -1,11 +1,78 @@
-#!/usr/local/bin/thrift -cpp -py
+#!/usr/local/bin/thrift -cpp -py -java
+#
+# This .thrift file contains the protocol required by the buck client to
+# communicate with the buck-frontend server.
+# This protocol is under active development and
+# will likely be changed in non-compatible ways
 
 namespace cpp buck.common
 namespace java com.facebook.buck.distributed.thrift
 
-# This protocol is under active development and
-# will likely be changed in non-compatible ways
-struct BuckJob {
-  1: string id,
-  2: i64 timestamp_ms,
+##############################################################################
+## DataTypes
+##############################################################################
+
+struct LogRecord {
+  1: optional string name;
+  2: optional i64 timestampMillis;
+}
+
+struct DebugInfo {
+  1: optional list<LogRecord> logBook;
+}
+
+struct BuildId {
+  1 : optional string id;
+}
+
+struct BuildJob {
+  1: optional BuildId buildId;
+  2: optional DebugInfo debug;
+}
+
+
+##############################################################################
+## Request/Response structs
+##############################################################################
+
+struct StartBuildRequest {
+  1: optional i64 startTimestampMillis;
+}
+
+struct StartBuildResponse {
+  1: optional BuildJob buildJob;
+}
+
+struct BuildStatusRequest {
+  1: optional BuildId buildId;
+}
+
+struct BuildStatusResponse {
+  1: optional BuildJob buildJob;
+}
+
+
+##############################################################################
+## Top-Level Buck-Frontend HTTP body thrift Request/Response format
+##############################################################################
+
+enum FrontendRequestType {
+  UNKNOWN,
+  START_BUILD,
+  BUILD_STATUS,
+}
+
+struct FrontendRequest {
+  1: optional FrontendRequestType type = FrontendRequestType.UNKNOWN;
+  2: optional StartBuildRequest startBuild;
+  3: optional BuildStatusRequest buildStatus;
+}
+
+struct FrontendResponse {
+  1: optional bool wasSuccessful;
+  2: optional string errorMessage;
+
+  10: optional FrontendRequestType type = FrontendRequestType.UNKNOWN;
+  11: optional StartBuildResponse startBuild;
+  12: optional BuildStatusResponse buildStatus;
 }
