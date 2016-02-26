e13b6cee30fda88f7b2e5af09c57375f86ab935b
Ben Hamilton <beng@fb.com>
2016-02-26 12:33:59 -0800

    Easy: Ignore BUCK_TTY when checking for changes to environment
    Summary:In `Set env var when Buck is run from a TTY`, andrewjcg
    added a new feature to allow Buck to log whether or not it was
    connected to a TTY.
    
    Since this feature used an environment variable, changes in that
    variable were causing buckd to dump its parser cache.
    
    This adds `$BUCK_TTY` to the list of ignored enviroment variables.
    We don't filter it since it's used for logging.
    
    Test Plan: CI
    
    Reviewed By: ryu2
    
    fb-gh-sync-id: 4832e5f
    shipit-source-id: 4832e5f

--- src/com/facebook/buck/util/environment/EnvironmentFilter.java
+++ src/com/facebook/buck/util/environment/EnvironmentFilter.java
@@ -63,6 +63,7 @@ public class EnvironmentFilter {
   // Ignore the environment variables with these names when comparing environments.
   private static final ImmutableSet<String> ENV_TO_IGNORE = ImmutableSet.of(
       "ANDROID_SERIAL", // Serial of the target Android device/emulator.
+      "BUCK_TTY",       // Whether buck's stdin is connected to a TTY
       "SCRIPT",         // Populated by script command: http://www.freebsd.org/cgi/man.cgi?script
       "NAILGUN_TTY_0",  // Nailgun stdin supports ANSI escape sequences.
       "NAILGUN_TTY_1",  // Nailgun stdout supports ANSI escape sequences.
