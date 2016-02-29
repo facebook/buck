3edda1883ddd768da1b7c73e08d0f2a55d2d574e
Ben Hamilton <beng@fb.com>
2016-02-29 13:54:51 -0800

    Easy: New immutable value test utility FakeExecutionEnvironment
    Summary:`ExecutionEnvironment` currently has no fake for unit tests,
    so we can't write any tests which depend on its values.
    
    This introduces a new class `AbstractFakeExecutionEnvironment` which
    is an immutable value type implementing `ExecutionEnvironment`.
    
    I looked into replacing the existing `DefaultExecutionEnvironment` with
    this type, but it was a bit tricky due to laziness and default values.
    We can still do so separately.
    
    Test Plan:`buck build //test/com/facebook/buck/util/environment:testutil`.
    Used from subsequent diff.
    
    Reviewed By: Coneko
    
    fb-gh-sync-id: ffb42e2
    shipit-source-id: ffb42e2

---
+++ test/com/facebook/buck/util/environment/AbstractFakeExecutionEnvironment.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util.environment;
+
+import com.facebook.buck.util.immutables.BuckStyleTuple;
+import com.google.common.base.Optional;
+
+import java.util.Map;
+
+import org.immutables.value.Value;
+
+/**
+ * Test utility implementation of {@link ExecutionEnvironment} based
+ * on an immutable value type.
+ */
+@Value.Immutable
+@BuckStyleTuple
+abstract class AbstractFakeExecutionEnvironment implements ExecutionEnvironment {
+  @Override
+  public abstract String getHostname();
+
+  @Override
+  public abstract String getUsername();
+
+  @Override
+  public abstract int getAvailableCores();
+
+  @Override
+  public abstract long getTotalMemory();
+
+  @Override
+  public abstract Platform getPlatform();
+
+  @Override
+  public abstract Optional<String> getWifiSsid();
+
+  public abstract Map<String, String> getEnvironment();
+  public abstract Map<String, String> getProperties();
+
+  @Override
+  public String getenv(String key, String defaultValue) {
+    return getWithDefault(getEnvironment(), key, defaultValue);
+  }
+
+  @Override
+  public String getProperty(String key, String defaultValue) {
+    return getWithDefault(getProperties(), key, defaultValue);
+  }
+
+  private static String getWithDefault(
+      Map<String, String> values,
+      String key,
+      String defaultValue) {
+    String result = values.get(key);
+    if (result != null) {
+      return result;
+    } else {
+      return defaultValue;
+    }
+  }
+}
