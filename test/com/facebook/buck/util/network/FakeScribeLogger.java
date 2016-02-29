2c9a0a8a9c08c503f43f58dd78b92b579cc0a88b
Ben Hamilton <beng@fb.com>
2016-02-29 13:54:51 -0800

    Easy: New test utility FakeScribeLogger
    Summary:We use `ScribeLogger` to abstract logging to Scribe, but we have no
    fake implementation for tests.
    
    This adds a new test utility `FakeScribeLogger` which we can use for tests.
    
    Test Plan: `buck build //test/com/facebook/buck/util/network:testutil`
    
    Reviewed By: Coneko
    
    fb-gh-sync-id: 786dd83
    shipit-source-id: 786dd83

---
+++ test/com/facebook/buck/util/network/FakeScribeLogger.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util.network;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.Collection;
+
+import javax.annotation.concurrent.GuardedBy;
+
+/**
+ * Fake implementation of {@link ScribeLogger} which records logged lines to memory.
+ */
+public final class FakeScribeLogger implements ScribeLogger {
+  // Keep the lines in the order in which they were written.
+  @GuardedBy("this")
+  private final Multimap<String, String> loggedCategoryLines = LinkedListMultimap.create();
+
+  @Override
+  public ListenableFuture<Void> log(String category, Iterable<String> lines) {
+    synchronized (this) {
+      loggedCategoryLines.putAll(category, lines);
+      return Futures.immediateFuture(null);
+    }
+  }
+
+  public ImmutableList<String> getLinesForCategory(String category) {
+    synchronized (this) {
+      // This is guaranteed to be non-null even if no lines were logged to category.
+      Collection<String> lines = loggedCategoryLines.get(category);
+
+      // Make a defensive copy to ensure new lines aren't logged to the category after we return.
+      return ImmutableList.copyOf(lines);
+    }
+  }
+}
