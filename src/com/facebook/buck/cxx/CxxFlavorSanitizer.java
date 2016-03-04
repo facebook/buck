344c05bc3ea05b90d39f290d0a0496af8349365a
Jakub Zika <jakubzika@fb.com>
2016-03-04 03:04:08 -0800

    Use shorter flavor names for cxx rules
    Summary:Instead of using file path in the cxx rule flavor, only use its
    last element. This will shorten the directory names used for
    rules' outputs, which are currently gettting very close to the
    filesystem limit. This should be safe because we still append a
    hash of the full path to the flavor and it should not be too
    confusing because we still use the file name in the flavor.
    
    Test Plan: CI.
    
    Reviewed By: Coneko
    
    fb-gh-sync-id: bf46338
    shipit-source-id: bf46338

--- src/com/facebook/buck/cxx/CxxFlavorSanitizer.java
+++ src/com/facebook/buck/cxx/CxxFlavorSanitizer.java
@@ -20,14 +20,16 @@ import com.facebook.buck.model.Flavor;
 import com.google.common.hash.Hashing;
 
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 
 public class CxxFlavorSanitizer {
   private CxxFlavorSanitizer() {
   }
 
   public static String sanitize(String name) {
+    String fileName = Paths.get(name).getFileName().toString();
     // The hash prevents collisions between "an/example.c", "an_example.c" etc.
-    return Flavor.replaceInvalidCharacters(name) +
+    return Flavor.replaceInvalidCharacters(fileName) +
         Hashing.murmur3_32().hashString(name, StandardCharsets.UTF_8);
   }
 }
