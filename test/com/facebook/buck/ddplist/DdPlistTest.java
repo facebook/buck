4543dd8e969dd7bdf5dafea8ead88cb98756b813
Mark Wang <markwang@fb.com>
2016-03-11 10:13:46 -0800

    Update to newest version of dd-plist
    Summary:Update to the newest version which fixes some cases where binary `.plist`s were being written that were causing errors with iOS's validation for device builds.
    
    https://github.com/3breadt/dd-plist/commit/3fa1fa56c82df169e06079feb54ba281ad41cae6
    
    ```
    dd-plist$ ant jar
    dd-plist/src/main/java$ jar cf dd-plist-sources.jar com
    ```
    
    Test Plan: CI
    
    Reviewed By: Coneko
    
    fb-gh-sync-id: 9c36b72
    shipit-source-id: 9c36b72

--- test/com/facebook/buck/ddplist/DdPlistTest.java
+++ test/com/facebook/buck/ddplist/DdPlistTest.java
@@ -38,10 +38,10 @@ public class DdPlistTest {
   }
 
   @Test
-  public void testASCIIWriting() throws Exception {
+  public void testXMLWriting() throws Exception {
     InputStream in = getClass().getResourceAsStream("test-files/test1.plist");
     NSDictionary x = (NSDictionary) PropertyListParser.parse(in);
-    PropertyListParser.saveAsASCII(x, outputFile);
+    PropertyListParser.saveAsXML(x, outputFile);
     NSDictionary y = (NSDictionary) PropertyListParser.parse(outputFile);
     assertEquals(x, y);
   }
