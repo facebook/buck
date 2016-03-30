cf17ce8df7f0acc355e86cab1c4d89f5e0a01c30
Yiding Jia <yiding@fb.com>
2016-03-29 19:39:02 -0700

    Unset proxy env vars so curl calls to localhost avoids proxy
    Summary:Erase any proxy settings when calling soy2html.py, this prevents the underlying
    curl calls to localhost from going over the proxy, which would then fail.
    
    Test Plan:Set http proxy to an active proxy which rejects proxying to localhost, and call the script.
    See that the script completes successfully.
    
    Reviewed By: bolinfest
    
    fb-gh-sync-id: a14a079
    fbshipit-source-id: a14a079

--- docs/soy2html.sh
+++ docs/soy2html.sh
@@ -19,17 +19,19 @@
 set -e
 
 # Always run this script from the root of the Buck project directory.
-cd $(git rev-parse --show-toplevel)
+cd "$(git rev-parse --show-toplevel)"
 
 # The output directory should be the one and only argument.
 OUTPUT_DIR="$1"
 
-# Run soy2html.py
-cd docs
-python soy2html.py $OUTPUT_DIR
-cd -
+# Run soy2html.py, taking care to unset proxy env variables as the script will
+# use curl internally.
+(
+  cd docs
+  HTTP_PROXY="" HTTPS_PROXY="" http_proxy="" https_proxy="" python soy2html.py "$OUTPUT_DIR"
+)
 
 # Generate javadoc and include it in the output directory.
 ant javadoc-with-android
-mkdir -p ${OUTPUT_DIR}/javadoc/
-cp -r build/javadoc-with-android/* ${OUTPUT_DIR}/javadoc/
+mkdir -p "${OUTPUT_DIR}"/javadoc/
+cp -r build/javadoc-with-android/* "${OUTPUT_DIR}"/javadoc/
