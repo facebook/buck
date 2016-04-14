b6b682e314851baf58d07451cf316071bcf7d291
Jonathan Keljo <jkeljo@fb.com>
2016-04-14 08:30:37 -0700

    Small refactor of ABI class generation.
    Summary: Moving some stuff around so I can more conveniently hash the classes while writing them to the jar.
    
    Test Plan: CI
    
    Reviewed By: Coneko
    
    fb-gh-sync-id: 524828d
    fbshipit-source-id: 524828d

--- src/com/facebook/buck/jvm/java/abi/StubJar.java
+++ src/com/facebook/buck/jvm/java/abi/StubJar.java
@@ -21,7 +21,9 @@ import static org.objectweb.asm.ClassReader.SKIP_DEBUG;
 import static org.objectweb.asm.ClassReader.SKIP_FRAMES;
 
 import com.facebook.buck.io.ProjectFilesystem;
+import com.facebook.buck.zip.ZipConstants;
 import com.google.common.base.Preconditions;
+import com.google.common.io.ByteSource;
 
 import org.objectweb.asm.ClassReader;
 
@@ -29,6 +31,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
+import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 
 public class StubJar {
@@ -50,21 +53,44 @@ public class StubJar {
     try (
         OutputStream fos = filesystem.newFileOutputStream(path);
         JarOutputStream jar = new JarOutputStream(fos)) {
-      walker.walk(
-          new FileAction() {
-            @Override
-            public void visit(Path relativizedPath, InputStream stream) throws IOException {
-              String fileName = relativizedPath.toString();
-              if (!fileName.endsWith(".class")) {
-                return;
-              }
+      final CreateStubAction createStubAction = new CreateStubAction(jar);
+      walker.walk(createStubAction);
+    }
+  }
 
-              ClassReader classReader = new ClassReader(stream);
-              ClassMirror visitor = new ClassMirror(fileName);
-              classReader.accept(visitor, SKIP_CODE | SKIP_DEBUG | SKIP_FRAMES);
-              visitor.writeTo(jar);
-            }
-          });
+  private static class CreateStubAction implements FileAction {
+    private final JarOutputStream jar;
+
+    public CreateStubAction(JarOutputStream jar) {
+      this.jar = jar;
+    }
+
+    @Override
+    public void visit(Path relativizedPath, InputStream stream) throws IOException {
+      String fileName = relativizedPath.toString();
+      if (!fileName.endsWith(".class")) {
+        return;
+      }
+
+      ByteSource stubClassBytes = getStubClassBytes(stream, fileName);
+      writeToJar(fileName, stubClassBytes);
+    }
+
+    private ByteSource getStubClassBytes(InputStream stream,
+        String fileName) throws IOException {
+      ClassReader classReader = new ClassReader(stream);
+      ClassMirror visitor = new ClassMirror(fileName);
+      classReader.accept(visitor, SKIP_CODE | SKIP_DEBUG | SKIP_FRAMES);
+      return visitor.getStubClassBytes();
+    }
+
+    private void writeToJar(String fileName, ByteSource stubClassBytes) throws IOException {
+      JarEntry entry = new JarEntry(fileName);
+      // We want deterministic JARs, so avoid mtimes.
+      entry.setTime(ZipConstants.getFakeTime());
+      jar.putNextEntry(entry);
+      stubClassBytes.copyTo(jar);
+      jar.closeEntry();
     }
   }
 }
