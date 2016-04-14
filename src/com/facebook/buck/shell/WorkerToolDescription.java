41c758e6ffad1462089bc4d85a83fa1927be5644
Sam Swarr <swarr@fb.com>
2016-04-14 14:56:47 -0700

    Support $(location) macros in startup args
    Summary: ##worker_tool## definitions now support the use of ##$(location)## macros in the ##args## param. This will allow you to pass the path to another Buck-managed resource (such as a config file) to the worker process when starting up.
    
    Test Plan:Added unit test:
      buck test //test/com/facebook/buck/shell:shell
    
    Reviewed By: Coneko
    
    fb-gh-sync-id: aa9d04a
    fbshipit-source-id: aa9d04a

--- src/com/facebook/buck/shell/WorkerToolDescription.java
+++ src/com/facebook/buck/shell/WorkerToolDescription.java
@@ -12,7 +12,6 @@
  *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  *  License for the specific language governing permissions and limitations
  *  under the License.
- *  test
  */
 
 package com.facebook.buck.shell;
@@ -26,15 +25,31 @@ import com.facebook.buck.rules.BuildRuleParams;
 import com.facebook.buck.rules.BuildRuleResolver;
 import com.facebook.buck.rules.BuildRuleType;
 import com.facebook.buck.rules.Description;
+import com.facebook.buck.rules.ImplicitDepsInferringDescription;
 import com.facebook.buck.rules.SourcePathResolver;
 import com.facebook.buck.rules.TargetGraph;
+import com.facebook.buck.rules.macros.LocationMacroExpander;
+import com.facebook.buck.rules.macros.MacroException;
+import com.facebook.buck.rules.macros.MacroExpander;
+import com.facebook.buck.rules.macros.MacroHandler;
 import com.facebook.buck.util.HumanReadableException;
 import com.facebook.infer.annotation.SuppressFieldNotInitialized;
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
-public class WorkerToolDescription implements Description<WorkerToolDescription.Arg> {
+import java.nio.file.Path;
+
+public class WorkerToolDescription implements Description<WorkerToolDescription.Arg>,
+    ImplicitDepsInferringDescription<WorkerToolDescription.Arg> {
 
   public static final BuildRuleType TYPE = BuildRuleType.of("worker_tool");
+
+  public static final MacroHandler MACRO_HANDLER = new MacroHandler(
+      ImmutableMap.<String, MacroExpander>builder()
+        .put("location", new LocationMacroExpander())
+        .build());
 
   @Override
   public BuildRuleType getBuildRuleType() {
@@ -61,11 +76,40 @@ public class WorkerToolDescription implements Description<WorkerToolDescription.
           args.exe.getFullyQualifiedName());
     }
 
+    String startupArgs;
+    try {
+      startupArgs = MACRO_HANDLER.expand(
+          params.getBuildTarget(),
+          params.getCellRoots(),
+          resolver,
+          args.args.or(""));
+    } catch (MacroException e) {
+      throw new HumanReadableException(e, "%s: %s", params.getBuildTarget(), e.getMessage());
+    }
+
     return new WorkerTool(
         params,
         new SourcePathResolver(resolver),
         (BinaryBuildRule) rule,
-        args.args.or(""));
+        startupArgs);
+  }
+
+  @Override
+  public Iterable<BuildTarget> findDepsForTargetFromConstructorArgs(
+      BuildTarget buildTarget,
+      Function<Optional<String>, Path> cellRoots,
+      WorkerToolDescription.Arg constructorArg) {
+    ImmutableSet.Builder<BuildTarget> targets = ImmutableSet.builder();
+    if (constructorArg.args.isPresent()) {
+      try {
+        targets.addAll(
+            MACRO_HANDLER.extractParseTimeDeps(
+                buildTarget, cellRoots, constructorArg.args.get()));
+      } catch (MacroException e) {
+        throw new HumanReadableException(e, "%s: %s", buildTarget, e.getMessage());
+      }
+    }
+    return targets.build();
   }
 
   @SuppressFieldNotInitialized
