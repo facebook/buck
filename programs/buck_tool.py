6e729a9c77d8529041a56209939b52d4834426d5
Josiah Gaskin <jcg@fb.com>
2016-04-15 03:18:21 -0700

    Add scandir to compiled pex
    Summary: Adds scandir to build rules
    
    Test Plan: ant py-test
    
    Reviewed By: alsutton
    
    fb-gh-sync-id: 74a1c56
    fbshipit-source-id: 74a1c56

--- programs/buck_tool.py
+++ programs/buck_tool.py
@@ -49,6 +49,7 @@ EXPORTED_RESOURCES = [
     Resource("path_to_intellij_py"),
     Resource("path_to_pex"),
     Resource("path_to_pywatchman"),
+    Resource("path_to_scandir_py", basename='scandir.py'),
     Resource("path_to_sh_binary_template"),
     Resource("jacoco_agent_jar"),
     Resource("report_generator_jar"),
