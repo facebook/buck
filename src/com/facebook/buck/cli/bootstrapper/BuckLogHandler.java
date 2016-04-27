e32ac85449a0b81db5c5a0d6ecc713cca14187b7
Andrew Gallagher <andrewjcg@fb.com>
2016-04-26 23:21:59 -0700

    Use a special `BuckLogHandler` to handle buck logs
    Summary:
    Previously, we used a `FileHandler` with it's properties defedin in
    the packaged logging.properties to setup logging to the buck-out/log
    files.  This meant we had to hard-code the log path in both the
    properties file and inside Buck.  This diff adds a custom `FileHandler`
    subclass, `BuckLogHandler`, so that the log path can be determined
    inside of Buck, using the same single source of truth as other code.
    
    Test Plan: ran a build and verified logs were in correct place
    
    Reviewed By: Coneko
    
    fb-gh-sync-id: 8f06a68
    fbshipit-source-id: 8f06a68

---
+++ src/com/facebook/buck/cli/bootstrapper/BuckLogHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.cli.bootstrapper;
+
+import java.util.concurrent.Callable;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public final class BuckLogHandler extends Handler implements Callable<Handler> {
+  private static final Class<?> BUCK_LOG_HANDLER_CLASS =
+      ClassLoaderBootstrapper.loadClass("com.facebook.buck.log.BuckLogHandler");
+
+  private final Handler inner;
+
+  public BuckLogHandler() throws IllegalAccessException, InstantiationException {
+    inner = (Handler) BUCK_LOG_HANDLER_CLASS.newInstance();
+  }
+
+  @Override
+  public void publish(LogRecord record) {
+    inner.publish(record);
+  }
+
+  @Override
+  public void flush() {
+    inner.flush();
+  }
+
+  @Override
+  public void close() throws SecurityException {
+    inner.close();
+  }
+
+  @Override
+  public Handler call() {
+    return inner;
+  }
+}
