baf394d8a442e40257ef61875e2a7ed0a0d47628
Michael Bolin <mbolin@fb.com>
2016-05-21 12:35:06 -0700

    Make `buck build test/...` work.
    Summary:
    As noted in the new comment in the code, there was a dependency that `buck autodeps`
    could not see because it was part of a generated file, so we had to add it manually.
    
    The reason we did not see any failures in CI is because the dependency was part of
    a `java_library()` that powered a benchmark, and apparently this benchmark code is
    only run when someone is interested in it, so it did not appear to be built by CI.
    
    I added logic in our `travisci_run.sh` script to ensure this gets run in the future.
    
    Test Plan: Verify that `buck build test/...` failed before this change, but now succeeds.
    
    Reviewed By: andrewjcg
    
    fbshipit-source-id: 00fe264

--- scripts/travisci_run.sh
+++ scripts/travisci_run.sh
@@ -8,13 +8,18 @@ export TERM=dumb
 # Use Buck to run the tests first because its output is more
 # conducive to diagnosing failed tests than Ant's is.
 ./bin/buck build buck || { cat buck-out/log/ant.log; exit 1; }
+
 # There are only two cores in the containers, but Buck thinks there are 12
 # and tries to use all of them.  As a result, we seem to have our test
 # processes killed if we do not limit our threads.
+export BUCK_NUM_THREADS=3
+
+# Make sure that everything builds in case a library is not covered by a test.
+./bin/buck build --num-threads=$BUCK_NUM_THREADS src/... test/...
+
 # Until we get https://github.com/facebook/buck/pull/479, we cannot run the
 # Go tests in Travis
-./bin/buck test \
-  --num-threads=3
+./bin/buck test --num-threads=$BUCK_NUM_THREADS
 
 # Run all the other checks with ant.
 ant travis
