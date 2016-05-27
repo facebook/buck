62dfab948aa995ccc0cf6f5dfab2776f6536b2d6
Vladislav Alexeev <beefon@fb.com>
2016-05-27 08:46:01 -0700

    LC_SYMTAB
    Summary: SymTabCommand to work with string table and symbol tables
    
    Test Plan: added tests
    
    Reviewed By: Coneko
    
    fbshipit-source-id: 18fd8cd

---
+++ src/com/facebook/buck/macho/SymTabCommandUtils.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.macho;
+
+import com.facebook.buck.charset.NulTerminatedCharsetDecoder;
+import com.google.common.base.Charsets;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.primitives.UnsignedInteger;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class SymTabCommandUtils {
+  private SymTabCommandUtils() {}
+
+  public static SymTabCommand createFromBuffer(ByteBuffer buffer) {
+    return SymTabCommand.of(
+        LoadCommand.of(
+            buffer.position(),
+            UnsignedInteger.fromIntBits(buffer.getInt()),
+            UnsignedInteger.fromIntBits(buffer.getInt())),
+        UnsignedInteger.fromIntBits(buffer.getInt()),
+        UnsignedInteger.fromIntBits(buffer.getInt()),
+        UnsignedInteger.fromIntBits(buffer.getInt()),
+        UnsignedInteger.fromIntBits(buffer.getInt()));
+  }
+
+  public static void writeCommandToBuffer(SymTabCommand command, ByteBuffer buffer) {
+    LoadCommandUtils.writeCommandToBuffer(command.getLoadCommand(), buffer);
+    buffer
+        .putInt(command.getSymoff().intValue())
+        .putInt(command.getNsyms().intValue())
+        .putInt(command.getStroff().intValue())
+        .putInt(command.getStrsize().intValue());
+  }
+
+  /**
+   * Creates and returns the Nlist entry at the given index for the given SymTabCommand.
+   * @param buffer The buffer which holds all data.
+   * @param command The command that describes the Nlist entries.
+   * @param index Index of the entry that should be returned.
+   * @param is64Bit Indicates if binary is 64 bit or not.
+   * @return The Nlist entry at given index for the given SymTabCommand.
+   * @throws IOException
+   */
+  public static Nlist getNlistAtIndex(
+      ByteBuffer buffer,
+      SymTabCommand command,
+      int index,
+      boolean is64Bit) throws IOException {
+    final int nlistSizeInBytes = NlistUtils.getSizeInBytes(is64Bit);
+    final int offset = command.getSymoff().intValue() + index * nlistSizeInBytes;
+    buffer.position(offset);
+    return NlistUtils.createFromBuffer(buffer, is64Bit);
+  }
+
+  /**
+   * Returns the string entry from the string table described by the given Nlist entry of the given
+   * command.
+   * @param buffer The buffer which holds all data.
+   * @param command SymTabCommand that has reference to the string table.
+   * @param nlist The entry which describes the reference to the string table which value
+   *              should be returned.
+   * @return Optional which may contain String object if Nlist has non-null value in the string
+   * table; otherwise returns absent value.
+   * @throws IOException
+   */
+  public static Optional<String> getStringTableEntryForNlist(
+      ByteBuffer buffer,
+      SymTabCommand command,
+      Nlist nlist) throws IOException {
+    int entryIndex = nlist.getN_strx().intValue();
+    if (entryIndex == 0) {
+      return Optional.absent();
+    }
+
+    int offset = command.getStroff().intValue() + nlist.getN_strx().intValue();
+    buffer.position(offset);
+    String result = NulTerminatedCharsetDecoder.decodeUTF8String(buffer);
+    return Optional.of(result);
+  }
+
+  /**
+   * This method updates the given buffer by updating the given SymTabCommand to reflect the
+   * insertion of the entry into string table.
+   * @param buffer The buffer which holds all the data that needs to be updated.
+   * @param command The old SymTabCommand that does not reflect the updated string table yet.
+   * @param newEntryContents Contents of the new string table entry that previously has been
+   *                         inserted into the string table.
+   * @return New SymTabCommand that is updated to reflect the availability of the new entry in its
+   * string table.
+   * @throws IOException
+   */
+  public static SymTabCommand updateSymTabCommand(
+      ByteBuffer buffer,
+      SymTabCommand command,
+      String newEntryContents) throws IOException {
+    SymTabCommand updatedCommand = getUpdatedSymTabCommandWithNewEntryContents(
+        command,
+        newEntryContents);
+    writeCommand(buffer, command, updatedCommand);
+    return updatedCommand;
+  }
+
+  /**
+   * @param entry The contents of the string table entry.
+   * @return The actual size of the entry for the given string.
+   */
+  public static int sizeOfStringTableEntryWithContents(String entry) {
+    return entry.getBytes(StandardCharsets.UTF_8).length + 1; // + 1 for null terminator
+  }
+
+  /**
+   * Inserts the given string into the string buffer and returns its location in the string table.
+   * @param buffer the buffer which contains all data that should be modified. Buffer should be
+   *               ready to accept new bytes at its end.
+   * @param command SymTabCommand which string table is being updated.
+   * @param newEntryContents the value for new entry.
+   * @return the location of the newly inserted entry in the string table.
+   * @throws IOException
+   */
+  public static UnsignedInteger insertNewStringTableEntry(
+      ByteBuffer buffer,
+      SymTabCommand command,
+      String newEntryContents) throws IOException {
+    buffer.position(command.getStroff().intValue() + command.getStrsize().intValue());
+    buffer.put(newEntryContents.getBytes(Charsets.UTF_8));
+    buffer.put((byte) 0x00);
+    return command.getStrsize();
+  }
+
+  /**
+   * Updates the given SymTabCommand with the new data in the given buffer.
+   * @param buffer The buffer which holds the data for the old command
+   * @param command Old command
+   * @param updatedCommand New command that should replace the old command
+   * @throws IOException
+   */
+  private static void writeCommand(
+      ByteBuffer buffer,
+      SymTabCommand command,
+      SymTabCommand updatedCommand) throws IOException {
+    Preconditions.checkArgument(
+        command.getLoadCommand().getOffsetInBinary() ==
+            updatedCommand.getLoadCommand().getOffsetInBinary());
+    Preconditions.checkArgument(
+        command.getLoadCommand().getCmd().equals(
+            updatedCommand.getLoadCommand().getCmd()));
+    Preconditions.checkArgument(
+        command.getLoadCommand().getCmdsize().equals(
+            updatedCommand.getLoadCommand().getCmdsize()));
+
+    buffer.position(command.getLoadCommand().getOffsetInBinary());
+    writeCommandToBuffer(updatedCommand, buffer);
+  }
+
+  /**
+   * Constructs new SymTabCommand which would contain the given string table entry.
+   * @param command The old command which must be updated to include the given string table entry.
+   * @param newEntryContents String that was inserted into string table.
+   * @return new command which string table size is updated to include the new string table entry.
+   */
+  private static SymTabCommand getUpdatedSymTabCommandWithNewEntryContents(
+      SymTabCommand command,
+      String newEntryContents) {
+    return command.withStrsize(
+        command.getStrsize().plus(
+            UnsignedInteger.fromIntBits(
+                sizeOfStringTableEntryWithContents(newEntryContents))));
+  }
+}
---
+++ test/com/facebook/buck/macho/SymTabCommandUtilsTest.java
@@ -0,0 +1,278 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.macho;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToObject;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Optional;
+import com.google.common.primitives.UnsignedInteger;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+public class SymTabCommandUtilsTest {
+  @Test
+  public void testGettingNlistAtIndex64BitBigEndian() throws Exception {
+    checkWithBytes(NlistTestData.getBigEndian64Bit(), true, false);
+  }
+
+  @Test
+  public void testGettingNlistAtIndex32BitBigEndian() throws Exception {
+    checkWithBytes(NlistTestData.getBigEndian32Bit(), false, false);
+  }
+
+  @Test
+  public void testGettingNlistAtIndex64BitLittleEndian() throws Exception {
+    checkWithBytes(NlistTestData.getLittleEndian64Bit(), true, true);
+  }
+
+  @Test
+  public void testGettingNlistAtIndex32BitLittleEndian() throws Exception {
+    checkWithBytes(NlistTestData.getLittleEndian32Bit(), false, true);
+  }
+
+  private void checkWithBytes(byte[] nlistTemplateBytes, boolean is64Bit, boolean isSwapped)
+      throws IOException {
+
+    byte[] nlistBytes1 = Arrays.copyOf(nlistTemplateBytes, nlistTemplateBytes.length);
+    if (isSwapped) {
+      nlistBytes1[0] = (byte) 0x01;   // strx
+    } else {
+      nlistBytes1[3] = (byte) 0x01;   // strx
+    }
+    nlistBytes1[4] = (byte) 0x11;     // type
+
+    byte[] nlistBytes2 = Arrays.copyOf(nlistTemplateBytes, nlistTemplateBytes.length);
+    if (isSwapped) {
+      nlistBytes2[0] = (byte) 0x02;   // strx
+    } else {
+      nlistBytes2[3] = (byte) 0x02;   // strx
+    }
+    nlistBytes2[4] = (byte) 0x22;     // type
+
+    byte[] commandBytes;
+    if (isSwapped) {
+      commandBytes = SymTabCommandTestData.getLittleEndian();
+    } else {
+      commandBytes = SymTabCommandTestData.getBigEndian();
+    }
+    final int cmdSize = commandBytes.length;
+    if (isSwapped) {
+      commandBytes[8] = (byte) cmdSize;     // symoff
+      commandBytes[12] = (byte) 2;          // nsyms
+    } else {
+      commandBytes[11] = (byte) cmdSize;    // symoff
+      commandBytes[15] = (byte) 2;          // nsyms
+    }
+
+    ByteBuffer commandBuffer = ByteBuffer
+        .wrap(commandBytes)
+        .order(isSwapped ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+
+    SymTabCommand symTabCommand = SymTabCommandUtils.createFromBuffer(commandBuffer);
+    assertThat(
+        symTabCommand.getSymoff(),
+        equalToObject(UnsignedInteger.fromIntBits(cmdSize)));
+    assertThat(
+        symTabCommand.getNsyms(),
+        equalToObject(UnsignedInteger.fromIntBits(2)));
+
+    ByteBuffer byteBuffer = ByteBuffer.allocate(cmdSize + nlistTemplateBytes.length * 2)
+        .order(commandBuffer.order())
+        .put(commandBytes)
+        .put(nlistBytes1)
+        .put(nlistBytes2);
+
+    Nlist entry1 = SymTabCommandUtils.getNlistAtIndex(byteBuffer, symTabCommand, 0, is64Bit);
+    assertThat(entry1.getN_strx(), equalToObject(UnsignedInteger.fromIntBits(0x01)));
+    assertThat(entry1.getN_type(), equalToObject(UnsignedInteger.fromIntBits(0x11)));
+
+    Nlist entry2 = SymTabCommandUtils.getNlistAtIndex(byteBuffer, symTabCommand, 1, is64Bit);
+    assertThat(entry2.getN_strx(), equalToObject(UnsignedInteger.fromIntBits(0x02)));
+    assertThat(entry2.getN_type(), equalToObject(UnsignedInteger.fromIntBits(0x22)));
+  }
+
+  @Test
+  public void testGettingStringTableValueForNlist() throws Exception {
+    final String stringTableEntry = "string_table_entry";
+
+    byte[] nlistBytes = NlistTestData.getBigEndian64Bit();
+    nlistBytes[3] = (byte) (0x01);      // strx - first entry
+
+    byte[] commandBytes = SymTabCommandTestData.getBigEndian();
+    final int cmdSize = commandBytes.length;
+    commandBytes[11] = (byte) cmdSize;   // symoff
+    commandBytes[15] = (byte) 1;         // nsyms
+    commandBytes[19] = (byte) (cmdSize + nlistBytes.length);   // stroff
+    commandBytes[23] = (byte) (stringTableEntry.length() + 1); // strsize
+
+    ByteBuffer byteBuffer = ByteBuffer
+        .allocate(commandBytes.length + nlistBytes.length + 1 + stringTableEntry.length() + 1)
+        .order(ByteOrder.BIG_ENDIAN)
+        .put(commandBytes)
+        .put(nlistBytes)
+        .put((byte) 0x00)
+        .put(stringTableEntry.getBytes(Charsets.UTF_8))
+        .put((byte) 0x00);
+
+    byteBuffer.position(0);
+    SymTabCommand symTabCommand = SymTabCommandUtils.createFromBuffer(byteBuffer);
+    assertThat(
+        symTabCommand.getSymoff(),
+        equalToObject(UnsignedInteger.fromIntBits(cmdSize)));
+    assertThat(
+        symTabCommand.getNsyms(),
+        equalToObject(UnsignedInteger.fromIntBits(1)));
+
+    byteBuffer.position(cmdSize);
+    Nlist nlist = NlistUtils.createFromBuffer(byteBuffer, true);
+    assertThat(nlist.getN_strx(), equalToObject(UnsignedInteger.fromIntBits(1)));
+
+    Optional<String> result = SymTabCommandUtils.getStringTableEntryForNlist(
+        byteBuffer,
+        symTabCommand,
+        nlist);
+    assertThat(result.isPresent(), equalTo(true));
+    assertThat(result.get(), equalToObject(stringTableEntry));
+  }
+
+  @Test
+  public void testGettingStringTableNullValueForNlist() throws Exception {
+    byte[] nlistBytes = NlistTestData.getBigEndian64Bit();
+    nlistBytes[3] = (byte) 0x00;  // strx - first entry
+
+    byte[] commandBytes = SymTabCommandTestData.getBigEndian();
+    final int cmdSize = commandBytes.length;
+    commandBytes[11] = (byte) cmdSize;   // symoff
+    commandBytes[15] = (byte) 1;         // nsyms
+    commandBytes[19] = (byte) (cmdSize + nlistBytes.length);   // stroff
+    commandBytes[23] = (byte) 0x00;                            // strsize
+
+    ByteBuffer byteBuffer = ByteBuffer
+        .allocate(cmdSize + nlistBytes.length)
+        .order(ByteOrder.BIG_ENDIAN)
+        .put(commandBytes)
+        .put(nlistBytes);
+
+    byteBuffer.position(cmdSize);
+    Nlist nlist = NlistUtils.createFromBuffer(byteBuffer, false);
+    assertThat(nlist.getN_strx(), equalToObject(UnsignedInteger.ZERO));
+
+    SymTabCommand symTabCommand = SymTabCommandUtils.createFromBuffer(
+        ByteBuffer.wrap(commandBytes).order(ByteOrder.BIG_ENDIAN));
+    assertThat(
+        symTabCommand.getSymoff(),
+        equalToObject(UnsignedInteger.fromIntBits(cmdSize)));
+    assertThat(
+        symTabCommand.getNsyms(),
+        equalToObject(UnsignedInteger.fromIntBits(1)));
+
+    Optional<String> result = SymTabCommandUtils.getStringTableEntryForNlist(
+        byteBuffer,
+        symTabCommand,
+        nlist);
+    assertThat(result.isPresent(), equalTo(false));
+  }
+
+  @Test
+  public void testGettingStringTableEntrySize() throws Exception {
+    assertThat(SymTabCommandUtils.sizeOfStringTableEntryWithContents("abc"), equalTo(4));
+  }
+
+  @Test
+  public void testInsertingNewStringTableEntry() throws Exception {
+    byte[] commandBytes = SymTabCommandTestData.getBigEndian();
+    final int cmdSize = commandBytes.length;
+    commandBytes[11] = (byte) cmdSize;   // symoff
+    commandBytes[15] = (byte) 0;         // nsyms
+    commandBytes[19] = (byte) cmdSize;   // stroff
+    commandBytes[23] = (byte) 20;        // strsize
+
+    final String content = "new_entry";
+
+    ByteBuffer byteBuffer = ByteBuffer
+        .allocate(cmdSize + 20 + SymTabCommandUtils.sizeOfStringTableEntryWithContents(content))
+        .order(ByteOrder.BIG_ENDIAN)
+        .put(commandBytes)
+        .put(new byte[20]);
+
+    byteBuffer.position(0);
+    SymTabCommand symTabCommand = SymTabCommandUtils.createFromBuffer(byteBuffer);
+
+    UnsignedInteger offset = SymTabCommandUtils.insertNewStringTableEntry(
+        byteBuffer,
+        symTabCommand,
+        content);
+    assertThat(offset, equalToObject(UnsignedInteger.fromIntBits(20)));
+
+    byteBuffer.position(symTabCommand.getStroff().plus(offset).intValue());
+    byte[] entryBytes = new byte[content.length()];
+    byteBuffer.get(entryBytes, 0, content.length());
+    assertThat(entryBytes, equalTo(content.getBytes(Charsets.UTF_8)));
+  }
+
+  @Test
+  public void testUpdatingSymTabCommand() throws Exception {
+    byte[] commandBytes = SymTabCommandTestData.getBigEndian();
+    final int cmdSize = commandBytes.length;
+    commandBytes[11] = (byte) cmdSize;   // symoff
+    commandBytes[15] = (byte) 3;         // nsyms
+    commandBytes[18] = (byte) cmdSize;   // stroff
+    commandBytes[23] = (byte) 20;        // strsize
+
+    SymTabCommand symTabCommand = SymTabCommandUtils.createFromBuffer(
+        ByteBuffer.wrap(commandBytes).order(ByteOrder.BIG_ENDIAN));
+
+    final String content = "new_entry";
+
+    ByteBuffer byteBuffer = ByteBuffer
+        .allocate(cmdSize + 20 + SymTabCommandUtils.sizeOfStringTableEntryWithContents(content))
+        .order(ByteOrder.BIG_ENDIAN)
+        .putInt(SymTabCommand.LC_SYMTAB.intValue())
+        .putInt(cmdSize)
+        .put(commandBytes);
+
+    SymTabCommand updated = SymTabCommandUtils.updateSymTabCommand(
+        byteBuffer,
+        symTabCommand,
+        content);
+
+    assertThat(
+        updated.getStrsize(),
+        equalToObject(symTabCommand.getStrsize().plus(
+            UnsignedInteger.fromIntBits(
+                SymTabCommandUtils.sizeOfStringTableEntryWithContents(content)))));
+
+    byteBuffer.position(updated.getLoadCommand().getOffsetInBinary());
+
+    byte[] updatedBytes = new byte[commandBytes.length];
+    byteBuffer.get(updatedBytes, 0, updatedBytes.length);
+
+    SymTabCommand commandFromBuffer = SymTabCommandUtils.createFromBuffer(
+        ByteBuffer.wrap(updatedBytes).order(ByteOrder.BIG_ENDIAN));
+    assertThat(commandFromBuffer.getSymoff(), equalToObject(updated.getSymoff()));
+    assertThat(commandFromBuffer.getNsyms(), equalToObject(updated.getNsyms()));
+    assertThat(commandFromBuffer.getStroff(), equalToObject(updated.getStroff()));
+    assertThat(commandFromBuffer.getStrsize(), equalToObject(updated.getStrsize()));
+  }
+}
