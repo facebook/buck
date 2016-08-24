db43d068175484dbb6224ddb740b4cc56a0aefdd
Rui Barbosa Martins <ruibm@fb.com>
2016-08-24 02:22:49 -0700

    Adding new ThriftUtil method that can serialize to streams.
    Summary: Adding new ThriftUtil method that can serialize to streams.
    
    Test Plan: buck test
    
    Reviewed By: shivanker
    
    fbshipit-source-id: fa76736

--- src/com/facebook/buck/slb/ThriftUtil.java
+++ src/com/facebook/buck/slb/ThriftUtil.java
@@ -32,6 +32,7 @@ import org.apache.thrift.transport.TIOStreamTransport;
 import org.apache.thrift.transport.TTransport;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public final class ThriftUtil {
   private static final Logger LOGGER = Logger.get(ThriftUtil.class);
@@ -62,6 +63,20 @@ public final class ThriftUtil {
 
   public static TProtocol newProtocolInstance(ThriftProtocol protocol, TTransport transport) {
     return getProtocolFactory(protocol).getProtocol(transport);
+  }
+
+  public static void serialize(
+      ThriftProtocol protocol,
+      TBase<?, ?> source,
+      OutputStream stream) throws ThriftException {
+    try (TTransport transport = new TIOStreamTransport(stream)) {
+      TProtocol thriftProtocol = getProtocolFactory(protocol).getProtocol(transport);
+      try {
+        source.write(thriftProtocol);
+      } catch (TException e) {
+        throw new ThriftException(e);
+      }
+    }
   }
 
   public static byte[] serialize(
---
+++ test/com/facebook/buck/slb/ThriftUtilTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.slb;
+
+import com.facebook.buck.distributed.thrift.BuildId;
+import com.facebook.buck.distributed.thrift.BuildJob;
+import com.facebook.buck.distributed.thrift.BuildStatus;
+import com.google.common.collect.ImmutableList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ThriftUtilTest {
+
+  @Test
+  public void testSerializations() throws IOException {
+    ImmutableList<Serializer> serializers = ImmutableList.<Serializer>of(
+        new ByteSerializer(),
+        new StreamSerializer());
+    ImmutableList<Deserializer> deserializers = ImmutableList.<Deserializer>of(
+        new ByteSerializer(),
+        new StreamSerializer());
+
+    for (Serializer serializer : serializers) {
+      for (Deserializer deserializer : deserializers) {
+        for (ThriftProtocol protocol : ThriftProtocol.values()) {
+          BuildJob expectedJob = createBuildJob();
+
+          byte[] data = serializer.serialize(protocol, expectedJob);
+          BuildJob actualJob = deserializer.deserialize(protocol, data);
+          Assert.assertEquals(
+              String.format(
+                  "Serializer=[%s] Deserializer=[%s] Protocol=[%s] Expected=[%s] Actual=[%s]",
+                  serializer.getClass().getName(),
+                  deserializer.getClass().getName(),
+                  protocol.toString(),
+                  expectedJob,
+                  actualJob),
+              expectedJob,
+              actualJob
+          );
+        }
+      }
+    }
+  }
+
+  private BuildJob createBuildJob() {
+    BuildJob job = new BuildJob();
+    job.setStatus(BuildStatus.FINISHED_SUCCESSFULLY);
+    BuildId buildId = new BuildId();
+    buildId.setId("all will be well");
+    job.setBuildId(buildId);
+    return job;
+  }
+
+  public interface Serializer {
+    byte[] serialize(ThriftProtocol protocol, BuildJob job) throws IOException;
+  }
+
+  public interface Deserializer {
+    BuildJob deserialize(ThriftProtocol protocol, byte[] data) throws IOException;
+  }
+
+  public static class ByteSerializer implements Serializer, Deserializer {
+
+    @Override
+    public byte[] serialize(
+        ThriftProtocol protocol, BuildJob job) throws ThriftException {
+      return ThriftUtil.serialize(protocol, job);
+    }
+
+    @Override
+    public BuildJob deserialize(ThriftProtocol protocol, byte[] data) throws IOException {
+      BuildJob job = new BuildJob();
+      ThriftUtil.deserialize(protocol, data, job);
+      return job;
+    }
+  }
+
+  public static class StreamSerializer implements Serializer, Deserializer {
+
+    @Override
+    public byte[] serialize(
+        ThriftProtocol protocol, BuildJob job) throws IOException {
+      try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+        ThriftUtil.serialize(protocol, job, stream);
+        return stream.toByteArray();
+      }
+    }
+
+    @Override
+    public BuildJob deserialize(ThriftProtocol protocol, byte[] data) throws IOException {
+      BuildJob job = new BuildJob();
+      try (ByteArrayInputStream stream = new ByteArrayInputStream(data)) {
+        ThriftUtil.deserialize(protocol, stream, job);
+      }
+      return job;
+    }
+  }
+}
