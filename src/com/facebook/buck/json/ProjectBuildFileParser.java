17a50db92b59adf72c98bf6590764faf1da0a5cc
Michael Lee <mzlee@fb.com>
2016-08-30 07:18:58 -0700

    Switch type propagation to ImmutableList
    Summary: Also bserDeserialize can return a Null value, so annotate
    
    Test Plan: Type checkers
    
    Reviewed By: marcinkosiba
    
    fbshipit-source-id: c9426a0

--- src/com/facebook/buck/json/ProjectBuildFileParser.java
+++ src/com/facebook/buck/json/ProjectBuildFileParser.java
@@ -330,7 +330,7 @@ public class ProjectBuildFileParser implements AutoCloseable {
    */
   public List<Map<String, Object>> getAll(Path buildFile)
       throws BuildFileParseException, InterruptedException {
-    List<Map<String, Object>> result = getAllRulesAndMetaRules(buildFile);
+    ImmutableList<Map<String, Object>> result = getAllRulesAndMetaRules(buildFile);
 
     // Strip out the __includes, __configs, and __env meta rules, which are the last rules.
     return Collections.unmodifiableList(result.subList(0, result.size() - 3));
@@ -342,7 +342,7 @@ public class ProjectBuildFileParser implements AutoCloseable {
    *
    * @param buildFile should be an absolute path to a build file. Must have rootPath as its prefix.
    */
-  public List<Map<String, Object>> getAllRulesAndMetaRules(Path buildFile)
+  public ImmutableList<Map<String, Object>> getAllRulesAndMetaRules(Path buildFile)
       throws BuildFileParseException, InterruptedException {
     try {
       return getAllRulesInternal(buildFile);
@@ -353,7 +353,7 @@ public class ProjectBuildFileParser implements AutoCloseable {
   }
 
   @VisibleForTesting
-  protected List<Map<String, Object>> getAllRulesInternal(Path buildFile)
+  protected ImmutableList<Map<String, Object>> getAllRulesInternal(Path buildFile)
       throws IOException, BuildFileParseException {
     ensureNotClosed();
     initIfNeeded();
@@ -365,7 +365,7 @@ public class ProjectBuildFileParser implements AutoCloseable {
     ParseBuckFileEvent.Started parseBuckFileStarted = ParseBuckFileEvent.started(buildFile);
     buckEventBus.post(parseBuckFileStarted);
 
-    List<Map<String, Object>> values = null;
+    ImmutableList<Map<String, Object>> values = ImmutableList.of();
     String profile = "";
     try (AssertScopeExclusiveAccess.Scope scope = assertSingleThreadedParsing.scope()) {
       String buildFileString = buildFile.toString();
@@ -403,7 +403,7 @@ public class ProjectBuildFileParser implements AutoCloseable {
   }
 
   @SuppressWarnings("unchecked")
-  private static BuildFilePythonResult handleDeserializedValue(Object deserializedValue)
+  private static BuildFilePythonResult handleDeserializedValue(@Nullable Object deserializedValue)
       throws IOException {
     if (!(deserializedValue instanceof Map<?, ?>)) {
       throw new IOException(
--- test/com/facebook/buck/parser/ProjectBuildFileParserPoolTest.java
+++ test/com/facebook/buck/parser/ProjectBuildFileParserPoolTest.java
@@ -68,10 +68,10 @@ public class ProjectBuildFileParserPoolTest {
               @Override
               public ProjectBuildFileParser apply(Cell input) {
                 createCount.incrementAndGet();
-                return createMockPaser(
-                    new IAnswer<List<Map<String, Object>>>() {
+                return createMockParser(
+                    new IAnswer<ImmutableList<Map<String, Object>>>() {
                       @Override
-                      public List<Map<String, Object>> answer() throws Throwable {
+                      public ImmutableList<Map<String, Object>> answer() throws Throwable {
                         createParserLatch.countDown();
                         boolean didntTimeout = false;
                         try {
@@ -135,9 +135,9 @@ public class ProjectBuildFileParserPoolTest {
                 try {
                   EasyMock.expect(parser.getAllRulesAndMetaRules(EasyMock.anyObject(Path.class)))
                       .andAnswer(
-                          new IAnswer<List<Map<String, Object>>>() {
+                          new IAnswer<ImmutableList<Map<String, Object>>>() {
                             @Override
-                            public List<Map<String, Object>> answer() throws Throwable {
+                            public ImmutableList<Map<String, Object>> answer() throws Throwable {
                               createParserLatch.countDown();
                               createParserLatch.await();
 
@@ -192,10 +192,10 @@ public class ProjectBuildFileParserPoolTest {
                    @Override
                    public ProjectBuildFileParser apply(Cell input) {
                      final AtomicInteger sleepCallCount = new AtomicInteger(0);
-                     return createMockPaser(
-                         new IAnswer<List<Map<String, Object>>>() {
+                     return createMockParser(
+                         new IAnswer<ImmutableList<Map<String, Object>>>() {
                            @Override
-                           public List<Map<String, Object>> answer() throws Throwable {
+                           public ImmutableList<Map<String, Object>> answer() throws Throwable {
                              int numCalls = sleepCallCount.incrementAndGet();
                              Preconditions.checkState(numCalls == 1);
                              try {
@@ -227,10 +227,10 @@ public class ProjectBuildFileParserPoolTest {
     try (ProjectBuildFileParserPool parserPool =
              new ProjectBuildFileParserPool(
                  /* maxParsers */ 1,
-                 createMockPaserFactory(
-                     new IAnswer<List<Map<String, Object>>>() {
+                 createMockParserFactory(
+                     new IAnswer<ImmutableList<Map<String, Object>>>() {
                        @Override
-                       public List<Map<String, Object>> answer() throws Throwable {
+                       public ImmutableList<Map<String, Object>> answer() throws Throwable {
                          waitTillCanceled.await();
                          waitTillAllWorkIsDone.countDown();
                          return ImmutableList.of();
@@ -266,10 +266,10 @@ public class ProjectBuildFileParserPoolTest {
     try (ProjectBuildFileParserPool parserPool =
              new ProjectBuildFileParserPool(
                  /* maxParsers */ 1,
-                 createMockPaserFactory(
-                     new IAnswer<List<Map<String, Object>>>() {
+                 createMockParserFactory(
+                     new IAnswer<ImmutableList<Map<String, Object>>>() {
                        @Override
-                       public List<Map<String, Object>> answer() throws Throwable {
+                       public ImmutableList<Map<String, Object>> answer() throws Throwable {
                          firstJobRunning.countDown();
                          waitTillClosed.await();
                          return ImmutableList.of();
@@ -318,10 +318,10 @@ public class ProjectBuildFileParserPoolTest {
     try (ProjectBuildFileParserPool parserPool =
              new ProjectBuildFileParserPool(
                  /* maxParsers */ 2,
-                 createMockPaserFactory(
-                     new IAnswer<List<Map<String, Object>>>() {
+                 createMockParserFactory(
+                     new IAnswer<ImmutableList<Map<String, Object>>>() {
                        @Override
-                       public List<Map<String, Object>> answer() throws Throwable {
+                       public ImmutableList<Map<String, Object>> answer() throws Throwable {
                          if (throwWhileParsing.get()) {
                            throw new Exception(exceptionMessage);
                          }
@@ -360,8 +360,8 @@ public class ProjectBuildFileParserPoolTest {
     return futures.build();
   }
 
-  private ProjectBuildFileParser createMockPaser(
-      IAnswer<List<Map<String, Object>>> parseFn) {
+  private ProjectBuildFileParser createMockParser(
+      IAnswer<ImmutableList<Map<String, Object>>> parseFn) {
     ProjectBuildFileParser mock = EasyMock.createMock(ProjectBuildFileParser.class);
     try {
       EasyMock.expect(mock.getAllRulesAndMetaRules(EasyMock.anyObject(Path.class)))
@@ -374,17 +374,17 @@ public class ProjectBuildFileParserPoolTest {
     return mock;
   }
 
-  private Function<Cell, ProjectBuildFileParser> createMockPaserFactory(
-      final IAnswer<List<Map<String, Object>>> parseFn) {
+  private Function<Cell, ProjectBuildFileParser> createMockParserFactory(
+      final IAnswer<ImmutableList<Map<String, Object>>> parseFn) {
     return new Function<Cell, ProjectBuildFileParser>() {
       @Override
       public ProjectBuildFileParser apply(Cell input) {
         final AssertScopeExclusiveAccess exclusiveAccess =
             new AssertScopeExclusiveAccess();
-        return createMockPaser(
-            new IAnswer<List<Map<String, Object>>>() {
+        return createMockParser(
+            new IAnswer<ImmutableList<Map<String, Object>>>() {
               @Override
-              public List<Map<String, Object>> answer() throws Throwable {
+              public ImmutableList<Map<String, Object>> answer() throws Throwable {
                 try (AssertScopeExclusiveAccess.Scope scope = exclusiveAccess.scope()) {
                   return parseFn.answer();
                 }
