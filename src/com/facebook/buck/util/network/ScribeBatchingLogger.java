746d979dd94448bade035056a1043c3124b31c0d
Michal Sienkiewicz <msienkiewicz@fb.com>
2016-09-06 09:36:15 -0700

    Remove RemoteLoggerFactory
    Summary: Removing the RemoteLoggerFactory (and HttpPutLogger used only there). Leaving only ScribeBatchingLogger (pulled out of ...Factory) as we use it elsewhere.
    
    Test Plan: buck test
    
    Reviewed By: ruibm
    
    fbshipit-source-id: 905f41a

---
+++ src/com/facebook/buck/util/network/ScribeBatchingLogger.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util.network;
+
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ * Uploads log entries to the given scribe category.
+ */
+public class ScribeBatchingLogger extends BatchingLogger {
+
+  private final ScribeLogger scribeLogger;
+  private final String category;
+
+  public ScribeBatchingLogger(String category, ScribeLogger scribeLogger) {
+    this.category = category;
+    this.scribeLogger = scribeLogger;
+  }
+
+  @Override
+  protected ListenableFuture<Void> logMultiple(ImmutableCollection<BatchEntry> data) {
+    ImmutableList<String> lines = FluentIterable.from(data)
+        .transform(
+            new Function<BatchEntry, String>() {
+              @Override
+              public String apply(BatchEntry input) {
+                return input.getLine();
+              }
+            }).toList();
+    return scribeLogger.log(category, lines);
+  }
+}
