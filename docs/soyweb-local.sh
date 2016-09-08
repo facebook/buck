c74d103db86f9e7cc69b8a5635a127815e4cb91f
Philip Jameson <pjameson@fb.com>
2016-09-08 09:34:38 -0700

    Allow extra arguments to be passed to soyweb (e.g. --port)
    Summary: Pass any extra arguments down to the soyweb executable in soyweb-local.sh
    
    Test Plan:
    ```
    $ ./docs/soyweb-local.sh  --port 8080
    Listening on /0:0:0:0:0:0:0:0:8080
    $ ./docs/soyweb-local.sh
    Listening on /0:0:0:0:0:0:0:0:9811
    ```
    
    Reviewed By: Coneko
    
    fbshipit-source-id: f6f28ef

--- docs/soyweb-local.sh
+++ docs/soyweb-local.sh
@@ -2,4 +2,4 @@
 
 # Always run this script from the root of the Buck project directory.
 cd "$(git rev-parse --show-toplevel)/docs" || exit
-exec java -jar plovr-81ed862.jar soyweb --dir . --globals globals.json
+exec java -jar plovr-81ed862.jar soyweb --dir . --globals globals.json $@
