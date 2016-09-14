03f8d0094b5d0dd82d44a0f4f1127f6b7dc2a1d3
Yiding Jia <yiding@fb.com>
2016-09-14 12:36:03 -0700

    Propagate exception cause in methods of BuckQueryEnvironment
    Summary:
    The lack of exception propagation makes debugging errors during buck query
    somewhat difficult.
    
    Test Plan: CI
    
    Reviewed By: Coneko
    
    fbshipit-source-id: 74503c0

--- src/com/facebook/buck/cli/BuckQueryEnvironment.java
+++ src/com/facebook/buck/cli/BuckQueryEnvironment.java
@@ -103,9 +103,13 @@ public class BuckQueryEnvironment implements QueryEnvironment {
     try {
       targetPatternEvaluator.preloadTargetPatterns(patterns, executor);
     } catch (IOException e) {
-      throw new QueryException("Error in preloading targets. %s: %s", e.getClass(), e.getMessage());
+      throw new QueryException(
+          e,
+          "Error in preloading targets. %s: %s",
+          e.getClass(),
+          e.getMessage());
     } catch (BuildTargetException | BuildFileParseException e) {
-      throw new QueryException("Error in preloading targets. %s", e.getMessage());
+      throw new QueryException(e, "Error in preloading targets. %s", e.getMessage());
     }
   }
 
@@ -138,7 +142,7 @@ public class BuckQueryEnvironment implements QueryEnvironment {
               targetPatternEvaluator.resolveTargetPatterns(ImmutableList.of(pattern), executor)
                   .values()));
     } catch (BuildTargetException | BuildFileParseException | IOException e) {
-      throw new QueryException("Error in resolving targets matching %s", pattern);
+      throw new QueryException(e, "Error in resolving targets matching %s", pattern);
     }
   }
 
@@ -151,7 +155,7 @@ public class BuckQueryEnvironment implements QueryEnvironment {
           MostExecutors.newSingleThreadExecutor("buck query.getNode"));
       return parserState.getTargetNode(((QueryBuildTarget) target).getBuildTarget());
     } catch (BuildTargetException | BuildFileParseException e) {
-      throw new QueryException("Error getting target node for %s\n%s", target, e.getMessage());
+      throw new QueryException(e, "Error getting target node for %s\n%s", target, e.getMessage());
     } finally {
       if (executor != null) {
         executor.shutdown();
@@ -234,7 +238,7 @@ public class BuckQueryEnvironment implements QueryEnvironment {
     try {
       graph = parserState.buildTargetGraph(targets);
     } catch (BuildFileParseException | BuildTargetException | IOException e) {
-      throw new QueryException("Error in building dependency graph");
+      throw new QueryException(e, "Error in building dependency graph");
     }
   }
 
@@ -323,7 +327,7 @@ public class BuckQueryEnvironment implements QueryEnvironment {
           files);
       return getTargetsFromBuildTargetsContainer(report.owners.keySet());
     } catch (BuildFileParseException | BuildTargetException | IOException e) {
-      throw new QueryException("Could not parse build targets.\n%s", e.getMessage());
+      throw new QueryException(e, "Could not parse build targets.\n%s", e.getMessage());
     }
   }
 
