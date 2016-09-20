33f9127876ca76411c43d9f8900fd1f166f513e1
Richard Howell <richardh@uber.com>
2016-09-20 15:54:47 +0200

    disable COPY_PHASE_STRIP, fails for codesigned embedded code

--- src/com/facebook/buck/apple/project_generator/ProjectGenerator.java
+++ src/com/facebook/buck/apple/project_generator/ProjectGenerator.java
@@ -1274,7 +1274,8 @@ public class ProjectGenerator {
     ImmutableMap.Builder<String, String> extraSettingsBuilder = ImmutableMap.builder();
     extraSettingsBuilder
         .put("TARGET_NAME", buildTargetName)
-        .put("SRCROOT", pathRelativizer.outputPathToBuildTargetPath(buildTarget).toString());
+        .put("SRCROOT", pathRelativizer.outputPathToBuildTargetPath(buildTarget).toString())
+        .put("COPY_PHASE_STRIP", "NO");  // this produces warnings on xcode8 and up
     if (productType == ProductType.UI_TEST && isFocusedOnTarget) {
       if (bundleLoaderNode.isPresent()) {
         BuildTarget testTarget = bundleLoaderNode.get().getBuildTarget();
