1661ee109bbf2a0383f4323022f9679b40c87a3b
Michael Lee <mzlee@fb.com>
2016-09-27 15:58:42 -0700

    Add an AbstractWatchmanQuery type
    Summary:
    The watchman query is currently a `List<Object>` consisting of
    
        ["query", watchman_root_path, parameters]
    
    Changing from an untyped list to an actual type.
    
    Test Plan: Compilation. Code is currently unused.
    
    Reviewed By: marcinkosiba
    
    fbshipit-source-id: d8ed67d

---
+++ src/com/facebook/buck/io/AbstractWatchmanQuery.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.io;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.facebook.buck.util.immutables.BuckStyleImmutable;
+
+import org.immutables.value.Value;
+
+@Value.Immutable
+@BuckStyleImmutable
+abstract class AbstractWatchmanQuery {
+
+  @Value.Parameter
+  abstract String getQueryPath();
+
+  @Value.Parameter
+  abstract ImmutableMap<String, Object> getQueryParams();
+
+  public ImmutableList<Object> toList(String sinceCursor) {
+    return ImmutableList.<Object>of(
+        "query",
+        getQueryPath(),
+        ImmutableMap.<String, Object>builder()
+            .put("since", sinceCursor)
+            .putAll(getQueryParams())
+            .build());
+  }
+}
