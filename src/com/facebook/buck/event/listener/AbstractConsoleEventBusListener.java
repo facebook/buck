183a486796daac1b286a10afbaab13407cf86b67
Gautam Korlam <gkorlam@uber.com>
2016-10-10 23:43:50 -0700

    Add a space in time format

--- src/com/facebook/buck/event/listener/AbstractConsoleEventBusListener.java
+++ src/com/facebook/buck/event/listener/AbstractConsoleEventBusListener.java
@@ -197,7 +197,7 @@ public abstract class AbstractConsoleEventBusListener implements BuckEventListen
   protected String formatElapsedTime(long elapsedTimeMs) {
     long minutes = elapsedTimeMs / 60_000L;
     String seconds = TIME_FORMATTER.format(locale, elapsedTimeMs / 1000.0 - (minutes * 60));
-    return String.format(minutes == 0 ? "%s" : "%2$dm%1$s", seconds, minutes);
+    return String.format(minutes == 0 ? "%s" : "%2$dm %1$s", seconds, minutes);
   }
 
   protected Optional<Double> getApproximateBuildProgress() {
