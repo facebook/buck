89299d1eb25ac023bc5bc63ecbc8535f6d91e416
Tho Nguyen <tho@uber.com>
2016-10-11 12:15:08 +0200

    Validate correctness of test rules

--- scripts/travisci_run.sh
+++ scripts/travisci_run.sh
@@ -17,12 +17,7 @@ export BUCK_NUM_THREADS=3
 # Make sure that everything builds in case a library is not covered by a test.
 ./bin/buck build --num-threads=$BUCK_NUM_THREADS src/... test/...
 
-# Validate existence of all test rules.
-./bin/buck test --dry-run --all
-
-# Until we get https://github.com/facebook/buck/pull/479, we cannot run the
-# Go tests in Travis
-./bin/buck test --num-threads=$BUCK_NUM_THREADS
+./bin/buck test --num-threads=$BUCK_NUM_THREADS //...
 
 # Run all the other checks with ant.
 ant travis
