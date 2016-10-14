8ec6869695a022e0ac0b451f48039c7cc75be143
Andrew Zhao <andrew.zhao@snapchat.com>
2016-10-14 15:14:53 -0700

    clean up the logic a bit

--- programs/buck_version.py
+++ programs/buck_version.py
@@ -34,17 +34,17 @@ class EmptyTempFile(object):
 
 def is_git(dirpath):
     dot_git = os.path.join(dirpath, '.git')
-    if(all([
-        os.path.exists(dot_git),
-        os.path.isdir(dot_git),
-        which('git'),
-        sys.platform != 'cygwin',
-    ])):
-        return True
-    output = check_output(
-        ['git', 'rev-parse', '--is-inside-work-tree'],
-        cwd=dirpath)
-    return output.strip() == 'true'
+    if(which('git') and sys.platform != 'cygwin'):
+        if(os.path.exists(dot_git) and os.path.isdir(dot_git)):
+            return True
+        output = check_output(
+            ['git', 'rev-parse', '--is-inside-work-tree'],
+            cwd=dirpath)
+        return output.strip() == 'true'
+    return False
+
+
+
 
 
 def is_dirty(dirpath):
