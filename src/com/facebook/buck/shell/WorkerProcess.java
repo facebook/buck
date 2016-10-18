0a10d0d813653f16fcc8db7f00944d155db76dae
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2016-10-17 21:15:04 -0700

    Dump worker_tool stderr if it fails to shutdown cleanly

--- src/com/facebook/buck/shell/WorkerProcess.java
+++ src/com/facebook/buck/shell/WorkerProcess.java
@@ -25,6 +25,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
+import com.google.common.io.LineReader;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
@@ -33,6 +34,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -147,6 +149,26 @@ public class WorkerProcess {
       Files.deleteIfExists(stdErr);
     } catch (Exception e) {
       LOG.debug(e, "Error closing worker process %s.", this.hashCode());
+
+      LOG.debug("Worker process stderr at %s", this.stdErr.toString());
+      Optional<Reader> fileReader = filesystem.getReaderIfFileExists(this.stdErr);
+      if (fileReader.isPresent()) {
+        LineReader lineReader = new LineReader(fileReader.get());
+        try {
+          for (String line = lineReader.readLine(); line != null; line = lineReader.readLine()) {
+            LOG.error("stderr: %s", line);
+          }
+        } catch (IOException e2) {
+          LOG.debug(e2, "Failed to read stderr file");
+        } finally {
+          try {
+            fileReader.get().close();
+          } catch (IOException e3) {
+            LOG.debug(e3, "Failed to close stderr reader");
+          }
+        }
+      }
+
       throw new HumanReadableException(e,
           "Error while trying to close the process %s at the end of the build.",
           Joiner.on(' ').join(processParams.getCommand()));
