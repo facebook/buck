60c46d32358c81e6bccad55f88c11edb1ae5fa34
Tho Nguyen <tho@uber.com>
2016-10-18 12:08:10 +0200

    Stop trolling people

--- test/com/facebook/buck/swift/testdata/swift_on_objc/Test0.swift
+++ test/com/facebook/buck/swift/testdata/swift_on_objc/Test0.swift
@@ -3,7 +3,7 @@ import UIKit
 public class Test0 : NSObject {
     public class func func0(_ str: String) {
         let alert = UIAlertView()
-        alert.title = "From Test1.swift"
+        alert.title = "From Test0.swift"
         alert.message = str
         alert.addButton(withTitle: "Yes")
         alert.addButton(withTitle: "No")
--- test/com/facebook/buck/swift/testdata/swift_on_objc/Test1.swift
+++ test/com/facebook/buck/swift/testdata/swift_on_objc/Test1.swift
@@ -3,7 +3,7 @@ import UIKit
 public class Test1 : NSObject {
     public class func func1(_ str: String) {
         let alert = UIAlertView()
-        alert.title = "From Test2.swift"
+        alert.title = "From Test1.swift"
         alert.message = str
         alert.addButton(withTitle: "Yes")
         alert.addButton(withTitle: "No")
