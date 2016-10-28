39b39520e2c6fcd8db2a2358492d10cefb914152
Brett Wooldridge <brett.wooldridge@gmail.com>
2016-10-28 04:02:17 -0700

    Demote error log to warning. The parser is going to continue anyway,…
    Summary:
    … so a log level of warn makes more sense and is less alarming to the user.
    Closes https://github.com/facebook/buck/pull/975
    
    Reviewed By: Coneko
    
    fbshipit-source-id: a073ce7

--- src/com/facebook/buck/jvm/java/JavaFileParser.java
+++ src/com/facebook/buck/jvm/java/JavaFileParser.java
@@ -642,7 +642,7 @@ public class JavaFileParser {
 
           simpleImportedTypes.put(name, enclosingType);
         } else {
-          LOG.error("Suspicious import lacks obvious enclosing type: %s", fullyQualifiedName);
+          LOG.warn("Suspicious import lacks obvious enclosing type: %s", fullyQualifiedName);
           // The one example we have seen of this in the wild is
           // "org.whispersystems.curve25519.java.curve_sigs". In practice, we still need to add it
           // as a required symbol in this case.
