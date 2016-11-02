12bd24c4c0d1d416160c080ce8ad0e166a299319
Alisdair Johnstone <alisdair@fb.com>
2016-11-02 05:19:27 -0700

    Added script which runs local and distributed build then compares output.
    Summary: This is to automate the process of finding discrepancies between the outputs, if any.
    
    Test Plan: run the script
    
    Reviewed By: ruibm
    
    fbshipit-source-id: 1dcb04a

---
+++ scripts/stampede/compare_local_vs_distributed_build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x # print command trace before running
+set -e # exit immediately if command fails
+set -v # print shell lines as read
+
+if [ -z "$1" ]
+  then
+    echo "Please invoke with format: <script.sh> <buck build target>"
+    exit -1
+fi
+
+TARGET=$1
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEMP_DIR=`mktemp -d`
+BUCK_OUT_DIR=`pwd`/buck-out
+LOCAL_BUILD_TEMP_DIR="$TEMP_DIR/local_buck_out"
+DIST_BUILD_TEMP_DIR="$TEMP_DIR/dist_buck_out"
+
+mkdir "$LOCAL_BUILD_TEMP_DIR"
+mkdir "$DIST_BUILD_TEMP_DIR"
+
+rm -rf ./buck-out
+buck build "$TARGET" --config=cache.mode=dir --deep
+cp -r "$BUCK_OUT_DIR"/. "$LOCAL_BUILD_TEMP_DIR"
+
+rm -rf ./buck-out
+buck build "$TARGET" --distributed --deep
+cp -r "$BUCK_OUT_DIR"/. "$DIST_BUILD_TEMP_DIR"
+
+echo Copied local buck-out to "$LOCAL_BUILD_TEMP_DIR"
+echo Copied distributed buck-out to "$DIST_BUILD_TEMP_DIR"
+
+echo Running diff between local and distributed builds
+python "$SCRIPTS_DIR"/../diff_buck_out.py "$LOCAL_BUILD_TEMP_DIR" "$DIST_BUILD_TEMP_DIR"
