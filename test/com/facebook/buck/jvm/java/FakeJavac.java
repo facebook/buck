e2449ee94c94c3096cd8ed99d7cdf09385e5ce51
Ben Hamilton <beng@fb.com>
2016-11-10 08:58:09 -0800

    Easy: New testutil class FakeJavac
    Summary:
    I needed to write a test for `JavacStep`, but
    there was no fake implementation of its dependency `Javac`.
    
    This adds a fake class `FakeJavac` which currently just
    throws. I'll fill it out in a subsequent diff.
    
    Test Plan: CI
    
    Reviewed By: andrewjcg
    
    fbshipit-source-id: 713001c

---
+++ test/com/facebook/buck/jvm/java/FakeJavac.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.jvm.java;
+
+import com.facebook.buck.model.BuildTarget;
+import com.facebook.buck.rules.BuildRule;
+import com.facebook.buck.rules.RuleKeyObjectSink;
+import com.facebook.buck.rules.SourcePath;
+import com.facebook.buck.rules.SourcePathResolver;
+
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+/**
+ * Fake implementation of {@link Javac} for tests.
+ */
+public class FakeJavac implements Javac {
+  @Override
+  public void appendToRuleKey(RuleKeyObjectSink sink) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ImmutableCollection<BuildRule> getDeps(SourcePathResolver resolver) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ImmutableCollection<SourcePath> getInputs() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ImmutableList<String> getCommandPrefix(SourcePathResolver resolver) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ImmutableMap<String, String> getEnvironment(SourcePathResolver resolver) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public JavacVersion getVersion() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public int buildWithClasspath(
+      JavacExecutionContext context,
+      BuildTarget invokingRule,
+      ImmutableList<String> options,
+      ImmutableSet<String> safeAnnotationProcessors,
+      ImmutableSortedSet<Path> javaSourceFilePaths,
+      Path pathToSrcsList,
+      Optional<Path> workingDirectory) throws InterruptedException {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String getDescription(
+      ImmutableList<String> options,
+      ImmutableSortedSet<Path> javaSourceFilePaths,
+      Path pathToSrcsList) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String getShortName() {
+    throw new UnsupportedOperationException();
+  }
+
+}
