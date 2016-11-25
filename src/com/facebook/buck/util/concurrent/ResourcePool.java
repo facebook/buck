938e016176a12c86394ef753ce0657c2708cd996
Martin Kosiba <mkosiba@fb.com>
2016-11-25 02:11:03 -0800

    Introduce a ResourcePool class
    Summary: Introduce a ResourcePool class, similar to ProjectBuildFileParserPool
    
    Test Plan: new tests
    
    Reviewed By: illicitonion
    
    fbshipit-source-id: bbf2649

---
+++ src/com/facebook/buck/util/concurrent/ResourcePool.java
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util.concurrent;
+
+import com.facebook.buck.log.Logger;
+import com.facebook.buck.model.Either;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.SettableFuture;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
+
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.GuardedBy;
+
+/**
+ * Allows multiple concurrently executing futures to share a constrained number of resources.
+ *
+ * Resources are lazily created up till a fixed maximum. If more than max resources are
+ * requested the associated 'requests' are queued up in the resourceRequests field. As soon as
+ * a resource is returned it will be used to satisfy the first pending request, otherwise it
+ * is stored in the parkedResources queue.
+ *
+ * If the resourceSupplier throws a RuntimeException the Future associated with the failed attempt
+ * to create the resource will contain the relevant exception. Any subsequent requests the pool
+ * will get will attempt to create a new resource.
+ *
+ * If the {@link ResourceUsageErrorPolicy#RECYCLE} error usage policy is specified then, in case of
+ * errors when "using" a resource it is assumed to be defective, will be retired
+ * and a new resource will be requested from the supplier. The Future associated with the failed
+ * attempt to use the resource will contain the relevant exception.
+ */
+public class ResourcePool<R extends AutoCloseable> implements AutoCloseable {
+  private static final Logger LOG = Logger.get(ResourcePool.class);
+
+  private final int maxResources;
+  private final ResourceUsageErrorPolicy resourceUsageErrorPolicy;
+  @GuardedBy("this")
+  private final Supplier<R> resourceSupplier;
+  @GuardedBy("this")
+  private final List<R> createdResources;
+  @GuardedBy("this")
+  private final Deque<R> parkedResources;
+  @GuardedBy("this")
+  private final Deque<SettableFuture<Void>> resourceRequests;
+  private final AtomicBoolean closing;
+  @GuardedBy("this")
+  private @Nullable ListenableFuture<Void> shutdownFuture;
+  @GuardedBy("this")
+  private final Set<ListenableFuture<?>> pendingWork;
+
+  /**
+   * @param maxResources maximum number of resources to use concurrently.
+   * @param resourceSupplier function used to create a new resource. It should never block, it may
+   *                      be called more than maxResources times if processing resources throws
+   *                      exceptions.
+   */
+  public ResourcePool(
+      int maxResources,
+      ResourceUsageErrorPolicy resourceUsageErrorPolicy,
+      Supplier<R> resourceSupplier) {
+    Preconditions.checkArgument(maxResources > 0);
+
+    this.maxResources = maxResources;
+    this.resourceUsageErrorPolicy = resourceUsageErrorPolicy;
+    this.resourceSupplier = resourceSupplier;
+    this.createdResources = new ArrayList<>();
+    this.parkedResources = new ArrayDeque<>();
+    this.resourceRequests = new ArrayDeque<>();
+    this.closing = new AtomicBoolean(false);
+    this.shutdownFuture = null;
+    this.pendingWork = new HashSet<>();
+  }
+
+  /**
+   * @param executorService where to perform the resource processing. Should really be a "real"
+   *                        executor (not a directExecutor).
+   * @return a {@link ListenableFuture} containing the result of the processing. The future will be
+   *         cancelled if the {@link ResourcePool#close()} method is called.
+   */
+  public synchronized <T> ListenableFuture<T> scheduleOperationWithResource(
+      ThrowingFunction<R, T> withResource,
+      final ListeningExecutorService executorService) {
+    Preconditions.checkState(!closing.get());
+
+    final ListenableFuture<T> futureWork = Futures.transformAsync(
+        initialSchedule(),
+        new AsyncFunction<Void, T>() {
+          @Override
+          public ListenableFuture<T> apply(Void input) throws Exception {
+            Either<R, ListenableFuture<Void>> resourceRequest = requestResource();
+            if (resourceRequest.isLeft()) {
+              R resource = resourceRequest.getLeft();
+              boolean resourceIsDefunct = false;
+              try {
+                return Futures.immediateFuture(withResource.apply(resource));
+              } catch (Exception e) {
+                resourceIsDefunct =
+                    (resourceUsageErrorPolicy == ResourceUsageErrorPolicy.RETIRE);
+                throw e;
+              } finally {
+                returnResource(resource, resourceIsDefunct);
+              }
+            } else {
+              return Futures.transformAsync(resourceRequest.getRight(), this, executorService);
+            }
+          }
+        },
+        executorService);
+
+    pendingWork.add(futureWork);
+    futureWork.addListener(
+        () -> {
+          synchronized (ResourcePool.this) {
+            pendingWork.remove(futureWork);
+          }
+        },
+        executorService);
+
+    // If someone else calls cancel on `futureWork` it makes it impossible to wait for that future
+    // to finish using the resource, which then makes shutdown code exit too early.
+    return Futures.nonCancellationPropagating(futureWork);
+  }
+
+  private synchronized ListenableFuture<Void> initialSchedule() {
+    // If we'll (potentially) be allowed to create a resource or there are some parked then we'll
+    // take the chance and attempt to run immediately.
+    if (allowedToCreateResource() || !parkedResources.isEmpty()) {
+      return Futures.immediateFuture(null);
+    }
+    // All possible resources are currently occupied. Because we're in a synchronized block, even
+    // if one becomes available immediately after this call returns it will simply make this future
+    // runnable, so we'll be able to progress.
+    return scheduleNewResourceRequest();
+  }
+
+  private synchronized Either<R, ListenableFuture<Void>> requestResource() {
+    Optional<R> resource = obtainResource();
+    if (resource.isPresent()) {
+      return Either.ofLeft(resource.get());
+    }
+    return Either.ofRight(scheduleNewResourceRequest());
+  }
+
+  private synchronized ListenableFuture<Void> scheduleNewResourceRequest() {
+    if (closing.get()) {
+      return Futures.immediateCancelledFuture();
+    }
+    SettableFuture<Void> resourceFuture = SettableFuture.create();
+    resourceRequests.add(resourceFuture);
+    return resourceFuture;
+  }
+
+  private synchronized Optional<R> obtainResource() {
+    if (closing.get()) {
+      return Optional.empty();
+    }
+    R resource = parkedResources.pollFirst();
+    if (resource != null) {
+      return Optional.of(resource);
+    }
+    return createIfAllowed();
+  }
+
+  private synchronized void returnResource(R resource, boolean resourceIsDefunct) {
+    if (resourceIsDefunct) {
+      createdResources.remove(resource);
+      try {
+        resource.close();
+      } catch (Exception e) {
+        LOG.info(e, "Error shutting down a defunct resource.");
+      }
+    } else {
+      parkedResources.add(resource);
+    }
+    scheduleNextRequest();
+  }
+
+  private synchronized void scheduleNextRequest() {
+    while (true) {
+      SettableFuture<Void> nextRequest = resourceRequests.pollFirst();
+      // Queue empty.
+      if (nextRequest == null) {
+        return;
+      }
+      // A false return value means the future was failed/cancelled, so we ignore it.
+      if (nextRequest.set(null)) {
+        return;
+      }
+    }
+  }
+
+  private synchronized boolean allowedToCreateResource() {
+    return !closing.get() && (createdResources.size() < maxResources);
+  }
+
+  private synchronized Optional<R> createIfAllowed() {
+    if (!allowedToCreateResource()) {
+      return Optional.empty();
+    }
+    R resource = Preconditions.checkNotNull(resourceSupplier.get());
+    createdResources.add(resource);
+    return Optional.of(resource);
+  }
+
+  @Nullable
+  public ListenableFuture<Void> getShutdownFullyCompleteFuture() {
+    Preconditions.checkState(
+        closing.get(),
+        "This method should not be called before the .close() method is called.");
+    return Preconditions.checkNotNull(shutdownFuture);
+  }
+
+  @Override
+  public synchronized void close() {
+    Preconditions.checkState(!closing.get());
+    closing.set(true);
+
+    // Unblock all waiting requests.
+    for (SettableFuture<Void> request : resourceRequests) {
+      request.set(null);
+    }
+    resourceRequests.clear();
+
+    // Any processing that is currently taking place will be allowed to complete (as it won't notice
+    // `closing` is true.
+    // Any scheduled (but not executing) resource requests should notice `closing` is true and
+    // mark themselves as cancelled.
+    // Therefore `closeFuture` should allow us to wait for any resources that are in use.
+    ListenableFuture<List<Object>> closeFuture = Futures.successfulAsList(pendingWork);
+
+    // As silly as it seems this is the only reliable way to make sure we run the shutdown code.
+    // Reusing an external executor means we run the risk of it being shut down before the cleanup
+    // future is ready to run (which causes it to never run).
+    // Using a direct executor means we run the chance of executing shutdown synchronously (which
+    // we try to avoid).
+    final ExecutorService executorService =
+        MostExecutors.newSingleThreadExecutor("resource shutdown");
+
+    // It is possible that more requests for work are scheduled at this point, however they should
+    // all early-out due to `closing` being set to true, so we don't really care about those.
+    shutdownFuture = Futures.transformAsync(
+        closeFuture,
+        new AsyncFunction<List<Object>, Void>() {
+          @Override
+          public ListenableFuture<Void> apply(List<Object> input) throws Exception {
+            synchronized (ResourcePool.this) {
+              if (parkedResources.size() != createdResources.size()) {
+                LOG.error("Whoops! Some resource are still in use during shutdown.");
+              }
+              // Now that pending work is done we can close all resources.
+              for (R resource : createdResources) {
+                resource.close();
+              }
+              if (!resourceRequests.isEmpty()) {
+                LOG.error("Error shutting down ResourcePool: " +
+                    "there should be no enqueued resource requests.");
+              }
+            }
+            executorService.shutdown();
+            return Futures.immediateFuture(null);
+          }
+        },
+        executorService);
+  }
+
+  /**
+   * Describes how to handle errors that take place during resource usage.
+   */
+  public enum ResourceUsageErrorPolicy {
+    RETIRE,
+    RECYCLE
+  }
+
+  public interface ThrowingFunction<T, R> {
+    R apply(T t) throws Exception;
+  }
+}
---
+++ test/com/facebook/buck/util/concurrent/ResourcePoolTest.java
@@ -0,0 +1,250 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.util.concurrent;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ResourcePoolTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void doesNotCreateMoreThanMaxResources() throws Exception {
+    try (Fixture f = new Fixture()) {
+      Phaser phaser = new Phaser(f.getMaxResources());
+      List<ListenableFuture<?>> futures = new ArrayList<>();
+      for (int i = 0; i < f.getMaxResources() * 10; i++) {
+        futures.add(
+            f.getPool().scheduleOperationWithResource(
+                r -> {
+                  phaser.arrive();
+                  phaser.awaitAdvance(0);
+                  return r;
+                },
+                f.getExecutorService()));
+      }
+      Futures.allAsList(futures).get();
+      assertThat(f.getCreatedResources().get(), equalTo(2));
+    }
+  }
+
+  @Test
+  public void exceptionOnResourceCreation() throws Exception {
+    try (Fixture f = new Fixture(
+        /* maxResources */ 2,
+        (i) -> {
+          if (i == 0) {
+            throw new TestException();
+          }
+        },
+        ResourcePool.ResourceUsageErrorPolicy.RECYCLE)) {
+      List<ListenableFuture<TestResource>> results =
+          Stream.of(0, 1)
+              .map(i -> f.getPool().scheduleOperationWithResource(
+                  r -> r,
+                  f.getExecutorService()))
+              .collect(Collectors.toList());
+
+      assertThat(
+          Futures.successfulAsList(results).get().stream()
+              .filter(r -> r != null)
+              .count(),
+          equalTo(1L));
+      expectedException.expectCause(Matchers.instanceOf(TestException.class));
+      Futures.allAsList(results).get();
+    }
+  }
+
+  @Test
+  public void exceptionOnResourceUsageWithRecycleHandling() throws Exception {
+    try (Fixture f = new Fixture(/* maxResources */ 1)) {
+      ListeningExecutorService executorService = MoreExecutors.newDirectExecutorService();
+      List<ListenableFuture<TestResource>> results =
+          Stream.of(0, 1, 2)
+              .map(i -> f.getPool().scheduleOperationWithResource(
+                  r -> {
+                    if (i == 1) {
+                      throw new TestException();
+                    }
+                    return r;
+                  },
+                  executorService))
+              .collect(Collectors.toList());
+
+      assertThat(f.getCreatedResources().get(), equalTo(1));
+      // Only one resource in the pool, so both requests should use resource_id == 0
+      assertThat(results.get(0).get().getTestResourceId(), equalTo(0));
+      assertThat(results.get(2).get().getTestResourceId(), equalTo(0));
+      expectedException.expectCause(Matchers.instanceOf(TestException.class));
+      results.get(1).get();
+    }
+  }
+
+  @Test
+  public void exceptionOnResourceUsageWithRetireHandling() throws Exception {
+    try (Fixture f = new Fixture(
+        /* maxResources */ 1,
+        ResourcePool.ResourceUsageErrorPolicy.RETIRE)) {
+      ListeningExecutorService executorService = MoreExecutors.newDirectExecutorService();
+      List<ListenableFuture<TestResource>> results =
+          Stream.of(0, 1, 2)
+              .map(i -> f.getPool().scheduleOperationWithResource(
+                  r -> {
+                    if (i == 1) {
+                      throw new TestException();
+                    }
+                    return r;
+                  },
+                  executorService))
+              .collect(Collectors.toList());
+
+      Futures.successfulAsList(results).get();
+
+      assertThat(f.getCreatedResources().get(), equalTo(f.getMaxResources() + 1));
+      // First request gets the first resource (id == 0), second request errors out causing the
+      // resource to be retired and the third request gets a new resource (id == 1).
+      assertThat(results.get(0).get().getTestResourceId(), equalTo(0));
+      assertThat(results.get(2).get().getTestResourceId(), equalTo(1));
+      expectedException.expectCause(Matchers.instanceOf(TestException.class));
+      results.get(1).get();
+    }
+  }
+
+  private static class TestResource implements AutoCloseable {
+    private final int id;
+
+    public TestResource(int id) {
+      this.id = id;
+    }
+
+    public int getTestResourceId() {
+      return id;
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+  }
+
+  private static class TestException extends RuntimeException {
+  }
+
+  private static class Fixture implements AutoCloseable {
+    private final AtomicInteger createdResources;
+    private final int maxResources;
+    private final ResourcePool<TestResource> pool;
+    private final ListeningExecutorService executorService;
+    private final Set<TestResource> createdResourcesSet;
+    private final Set<TestResource> closedResourcesSet;
+
+    public Fixture() {
+      this(/* maxResources */ 2, ResourcePool.ResourceUsageErrorPolicy.RECYCLE);
+    }
+
+    public Fixture(int maxResources) {
+      this(maxResources, ResourcePool.ResourceUsageErrorPolicy.RECYCLE);
+    }
+
+    public Fixture(int maxResources, ResourcePool.ResourceUsageErrorPolicy errorPolicy) {
+      this(maxResources, (id) -> {}, errorPolicy);
+    }
+
+    public Fixture(
+        int maxResources,
+        Consumer<Integer> beforeResourceCreatedFunction,
+        ResourcePool.ResourceUsageErrorPolicy errorPolicy) {
+      this.maxResources = maxResources;
+      this.createdResources = new AtomicInteger(0);
+      this.createdResourcesSet = new HashSet<>();
+      this.closedResourcesSet = new HashSet<>();
+      this.pool = new ResourcePool<>(
+          /* maxResources */ maxResources,
+          errorPolicy,
+          () -> {
+            int id = createdResources.getAndIncrement();
+            beforeResourceCreatedFunction.accept(id);
+            TestResource testResource = new TestResource(id) {
+              @Override
+              public void close() throws Exception {
+                synchronized (closedResourcesSet) {
+                  closedResourcesSet.add(this);
+                }
+              }
+            };
+            synchronized (createdResourcesSet) {
+              createdResourcesSet.add(testResource);
+            }
+            return testResource;
+          }
+      );
+      executorService =
+          MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(maxResources));
+    }
+
+    public ListeningExecutorService getExecutorService() {
+      return executorService;
+    }
+
+    public AtomicInteger getCreatedResources() {
+      return createdResources;
+    }
+
+    public int getMaxResources() {
+      return maxResources;
+    }
+
+    public ResourcePool<TestResource> getPool() {
+      return pool;
+    }
+
+    @Override
+    public void close() throws Exception {
+      getPool().close();
+      getPool().getShutdownFullyCompleteFuture().get(1, TimeUnit.SECONDS);
+      synchronized (createdResourcesSet) {
+        synchronized (closedResourcesSet) {
+          assertEquals("Not all resources were closed.", createdResourcesSet, closedResourcesSet);
+        }
+      }
+    }
+  }
+}
