b9a73a1cde15f1661ab2c2e06e3d26a6e07d2b13
Vladislav Alexeev <beefon@fb.com>
2016-12-09 04:09:29 -0800

    Serialze ReturnResultMessage and deserialize InvocationMessage
    Summary:
    Serialze ReturnResultMessage and deserialize InvocationMessage
    We will need this functionality for OOP javac.
    
    Test Plan: CI
    
    Reviewed By: plamenko
    
    fbshipit-source-id: c74f5ef

--- src/com/facebook/buck/message_ipc/MessageSerializer.java
+++ src/com/facebook/buck/message_ipc/MessageSerializer.java
@@ -20,32 +20,65 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class MessageSerializer {
+  private static final String TYPE = "type";
+  private static final String NAME = "name";
+  private static final String ARGS = "args";
+  private static final String VALUE = "value";
   private final ObjectMapper objectMapper;
 
   public MessageSerializer(ObjectMapper objectMapper) {
     this.objectMapper = objectMapper;
   }
 
-  String serializeInvocation(InvocationMessage invocation) throws JsonProcessingException {
+  public String serializeInvocation(InvocationMessage invocation) throws JsonProcessingException {
     ImmutableMap.Builder<String, Object> builder = ImmutableMap.builder();
-    builder.put("type", InvocationMessage.class.getSimpleName());
-    builder.put("name", invocation.getMethodName());
-    builder.put("args", invocation.getArguments());
+    builder.put(TYPE, InvocationMessage.class.getSimpleName());
+    builder.put(NAME, invocation.getMethodName());
+    builder.put(ARGS, invocation.getArguments());
     return objectMapper.writeValueAsString(builder.build());
   }
 
-  ReturnResultMessage deserializeResult(String data) throws IOException {
+  @SuppressWarnings("unchecked")
+  public InvocationMessage deserializeInvocation(String data) throws IOException {
     Map<String, Object> rep = objectMapper.readValue(
         data,
         new TypeReference<Map<String, Object>>() {});
-    String type = (String) rep.get("type");
+    String type = (String) rep.get(TYPE);
+    Preconditions.checkNotNull(
+        type,
+        "Unable to deserialize %s: no field %s in data (%s)",
+        InvocationMessage.class.getSimpleName(), TYPE, data);
+    Preconditions.checkArgument(type.equals(InvocationMessage.class.getSimpleName()));
+    return new InvocationMessage(
+        (String) rep.get(NAME),
+        ImmutableList.copyOf((List<Object>) rep.get(ARGS)));
+  }
+
+  public String serializeResult(ReturnResultMessage message) throws JsonProcessingException {
+    ImmutableMap.Builder<String, Object> builder = ImmutableMap.builder();
+    builder.put(TYPE, ReturnResultMessage.class.getSimpleName());
+    builder.put(VALUE, message.getValue());
+    return objectMapper.writeValueAsString(builder.build());
+  }
+
+  public ReturnResultMessage deserializeResult(String data) throws IOException {
+    Map<String, Object> rep = objectMapper.readValue(
+        data,
+        new TypeReference<Map<String, Object>>() {});
+    String type = (String) rep.get(TYPE);
+    Preconditions.checkNotNull(
+        type,
+        "Unable to deserialize %s: no field %s in data (%s)",
+        ReturnResultMessage.class.getSimpleName(), TYPE, data);
     Preconditions.checkArgument(type.equals(ReturnResultMessage.class.getSimpleName()));
-    return new ReturnResultMessage(rep.get("value"));
+    return new ReturnResultMessage(rep.get(VALUE));
   }
 }
