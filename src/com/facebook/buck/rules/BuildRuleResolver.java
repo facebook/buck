b62f9fa5d2d56f4bd6e489b36c850e4bb820da01
Jakub Zika <jakubzika@fb.com>
2016-12-12 06:19:54 -0800

    Forbid multiple rules for the same target
    Summary:
    Throw an exception if multiple rules are generated for the same
    target. This can happen if a rule is created and registered with
    `addToIndex` while another rule for the same target is being
    created.
    
    The previous check only checked equality, which for most (all?)
    rules only compares the rule type and the build target which would
    not detect when different rules are generated for the same target.
    
    Test Plan: CI.
    
    Reviewed By: Coneko, illicitonion
    
    fbshipit-source-id: ed33793

--- src/com/facebook/buck/rules/BuildRuleResolver.java
+++ src/com/facebook/buck/rules/BuildRuleResolver.java
@@ -136,9 +136,12 @@ public class BuildRuleResolver {
     rule = buildRuleGenerator.transform(targetGraph, this, node);
     BuildRule oldRule = buildRuleIndex.put(target, rule);
     Preconditions.checkState(
-        oldRule == null || oldRule.equals(rule),
-        "Race condition while requiring rule for target '%s':\n" +
-            "created rule '%s' does not match existing rule '%s'.",
+        // TODO(k21): Eventually we should be able to remove the oldRule == rule part.
+        // For now we need it to handle cases where a description adds a rule to the index before
+        // returning it.
+        oldRule == null || oldRule == rule,
+        "Multiple rules created for target '%s':\n" +
+            "new rule '%s' does not match existing rule '%s'.",
         target,
         rule,
         oldRule);
