6d7c9f0cb6bcb70ca1da9df9ca3fcead1a954b13
Shivanker Goel <shivanker@fb.com>
2016-12-21 13:45:02 -0800

    Optimizations in the build comparison script
    Summary: Move the buck-out directories instead of copying.
    
    Test Plan: ci
    
    Reviewed By: ruibm, illicitonion, marcinkosiba
    
    fbshipit-source-id: 67c163c

--- scripts/stampede/compare_local_vs_distributed_build.sh
+++ scripts/stampede/compare_local_vs_distributed_build.sh
@@ -12,24 +12,25 @@ fi
 
 TARGET=$1
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEMP_DIR_BASE=~/local/tmp
 TEMP_DIR=`mktemp -d`
+mkdir -p $TEMP_DIR_BASE
+mv $TEMP_DIR $TEMP_DIR_BASE
+TEMP_DIR=$TEMP_DIR_BASE/`basename $TEMP_DIR`
+
 BUCK_OUT_DIR=`pwd`/buck-out
 LOCAL_BUILD_TEMP_DIR="$TEMP_DIR/local_buck_out"
 DIST_BUILD_TEMP_DIR="$TEMP_DIR/dist_buck_out"
 
-mkdir "$LOCAL_BUILD_TEMP_DIR"
-mkdir "$DIST_BUILD_TEMP_DIR"
-
 rm -rf ./buck-out
 buck build "$TARGET" --config=cache.mode=dir --deep
-cp -r "$BUCK_OUT_DIR"/. "$LOCAL_BUILD_TEMP_DIR"
+mv "$BUCK_OUT_DIR" "$LOCAL_BUILD_TEMP_DIR"
 
-rm -rf ./buck-out
 buck build "$TARGET" --distributed --deep
-cp -r "$BUCK_OUT_DIR"/. "$DIST_BUILD_TEMP_DIR"
+mv "$BUCK_OUT_DIR" "$DIST_BUILD_TEMP_DIR"
 
-echo Copied local buck-out to "$LOCAL_BUILD_TEMP_DIR"
-echo Copied distributed buck-out to "$DIST_BUILD_TEMP_DIR"
+echo Moved local buck-out to "$LOCAL_BUILD_TEMP_DIR"
+echo Moved distributed buck-out to "$DIST_BUILD_TEMP_DIR"
 echo Results dir: "$TEMP_DIR"
 
 echo Running diff between local and distributed builds
