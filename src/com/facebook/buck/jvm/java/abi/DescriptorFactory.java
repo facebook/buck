42c1e9c8f0e77f440db1dbe9549d7b4e52deecc7
Jonathan Keljo <jkeljo@fb.com>
2017-01-31 15:22:50 -0800

    Compute descriptors from Elements and TypeMirrors
    Summary:
    We're going to be producing ABI jars from `Element`s and `TypeMirror`s instead
    of from the fully-compiled bytecode. We'll still be using the ASM-based
    ABI jar generation code, though, so we need some adapters.
    
    This commit adds `DescriptorFactory`, which can compute type descriptors.
    
    Test Plan: New unit tests
    
    Reviewed By: dreiss
    
    fbshipit-source-id: ca6f17e

---
+++ src/com/facebook/buck/jvm/java/abi/DescriptorFactory.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.jvm.java.abi;
+
+import com.google.common.base.Preconditions;
+
+import org.objectweb.asm.Type;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.TypeParameterElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.IntersectionType;
+import javax.lang.model.type.NoType;
+import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVariable;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.SimpleElementVisitor8;
+import javax.lang.model.util.SimpleTypeVisitor8;
+
+/**
+ * Computes type descriptors from {@link Element}s or {@link TypeMirror}s.
+ */
+class DescriptorFactory {
+  private final Elements elements;
+
+  public DescriptorFactory(Elements elements) {
+    this.elements = elements;
+  }
+
+  public String getDescriptor(TypeMirror type) {
+    return getType(type).getDescriptor();
+  }
+
+  public String getDescriptor(Element element) {
+    return getType(element).getDescriptor();
+  }
+
+  private Type getType(Element element) {
+    return Preconditions.checkNotNull(element.accept(new SimpleElementVisitor8<Type, Void>() {
+      @Override
+      protected Type defaultAction(Element e, Void aVoid) {
+        throw new IllegalArgumentException(
+            String.format("Unexpected element kind: %s", element.getKind()));
+      }
+
+      @Override
+      public Type visitType(TypeElement e, Void aVoid) {
+        return Type.getObjectType(getInternalName(e));
+      }
+
+      @Override
+      public Type visitExecutable(ExecutableElement e, Void aVoid) {
+        return Type.getMethodType(
+            getType(e.getReturnType()),
+            e.getParameters().stream()
+                .map(DescriptorFactory.this::getType)
+                .toArray(size -> new Type[size]));
+      }
+
+      @Override
+      public Type visitVariable(VariableElement e, Void aVoid) {
+        return getType(e.asType());
+      }
+
+      @Override
+      public Type visitTypeParameter(TypeParameterElement e, Void aVoid) {
+        return getType(e.asType());
+      }
+    }, null));
+  }
+
+  private Type getType(TypeMirror typeMirror) {
+    return Preconditions.checkNotNull(typeMirror.accept(new SimpleTypeVisitor8<Type, Void>() {
+      @Override
+      protected Type defaultAction(TypeMirror t, Void aVoid) {
+        throw new IllegalArgumentException(String.format("Unexpected type kind: %s", t.getKind()));
+      }
+
+      @Override
+      public Type visitPrimitive(PrimitiveType type, Void aVoid) {
+        switch (type.getKind()) {
+          case BOOLEAN:
+            return Type.BOOLEAN_TYPE;
+          case BYTE:
+            return Type.BYTE_TYPE;
+          case CHAR:
+            return Type.CHAR_TYPE;
+          case SHORT:
+            return Type.SHORT_TYPE;
+          case INT:
+            return Type.INT_TYPE;
+          case LONG:
+            return Type.LONG_TYPE;
+          case FLOAT:
+            return Type.FLOAT_TYPE;
+          case DOUBLE:
+            return Type.DOUBLE_TYPE;
+          // $CASES-OMITTED$
+          default:
+            throw new IllegalArgumentException(String.format(
+                "Unexpected type kind: %s",
+                type.getKind()));
+        }
+      }
+
+      @Override
+      public Type visitNoType(NoType t, Void aVoid) {
+        if (t.getKind() != TypeKind.VOID) {
+          throw new IllegalArgumentException(String.format(
+              "Unexpected type kind: %s",
+              t.getKind()));
+        }
+
+        return Type.VOID_TYPE;
+      }
+
+      @Override
+      public Type visitArray(ArrayType t, Void aVoid) {
+        return Type.getObjectType("[" + getDescriptor(t.getComponentType()));
+      }
+
+      @Override
+      public Type visitDeclared(DeclaredType t, Void aVoid) {
+        // The erasure of a parameterized type is just the unparameterized version (JLS8 4.6)
+        return Type.getObjectType(getInternalName((TypeElement) t.asElement()));
+      }
+
+      @Override
+      public Type visitTypeVariable(TypeVariable t, Void aVoid) {
+        // The erasure of a type variable is the erasure of its leftmost bound (JLS8 4.6)
+        // If there's only one bound, getUpperBound returns it directly; if there's more than
+        // one it returns an IntersectionType
+        return getType(t.getUpperBound());
+      }
+
+      @Override
+      public Type visitIntersection(IntersectionType t, Void aVoid) {
+        // The erasure of a type variable is the erasure of its leftmost bound (JLS8 4.6)
+        return getType(t.getBounds().get(0));
+      }
+    }, null));
+  }
+
+  /**
+   * Gets the internal form of the binary name; see JVMS8 4.2.1
+   */
+  public String getInternalName(TypeMirror typeMirror) {
+    return getType(typeMirror).getInternalName();
+  }
+
+  /**
+   * Gets the internal form of the binary name; see JVMS8 4.2.1
+   */
+  public String getInternalName(TypeElement typeElement) {
+    return elements.getBinaryName(typeElement).toString().replace('.', '/');
+  }
+}
---
+++ test/com/facebook/buck/jvm/java/abi/DescriptorFactoryTest.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.jvm.java.abi;
+
+import static org.junit.Assert.assertEquals;
+
+import com.facebook.buck.jvm.java.testutil.CompilerTreeApiTest;
+import com.facebook.buck.jvm.java.testutil.CompilerTreeApiTestRunner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+
+@RunWith(CompilerTreeApiTestRunner.class)
+public class DescriptorFactoryTest extends CompilerTreeApiTest {
+
+  private DescriptorFactory descriptorFactory;
+
+  @Before
+  public void setUp() throws IOException {
+    initCompiler();
+    descriptorFactory = new DescriptorFactory(elements);
+  }
+
+  @Test
+  public void testVoidDescriptor() {
+    assertEquals("V", descriptorFactory.getDescriptor(types.getNoType(TypeKind.VOID)));
+  }
+
+  @Test
+  public void testBooleanDescriptor() {
+    assertEquals("Z", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.BOOLEAN)));
+  }
+
+  @Test
+  public void testByteDescriptor() {
+    assertEquals("B", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.BYTE)));
+  }
+
+  @Test
+  public void testCharDescriptor() {
+    assertEquals("C", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.CHAR)));
+  }
+
+  @Test
+  public void testShortDescriptor() {
+    assertEquals("S", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.SHORT)));
+  }
+
+  @Test
+  public void testIntDescriptor() {
+    assertEquals("I", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.INT)));
+  }
+
+  @Test
+  public void testLongDescriptor() {
+    assertEquals("J", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.LONG)));
+  }
+
+  @Test
+  public void testFloatDescriptor() {
+    assertEquals("F", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.FLOAT)));
+  }
+
+  @Test
+  public void testDoubleDescriptor() {
+    assertEquals("D", descriptorFactory.getDescriptor(types.getPrimitiveType(TypeKind.DOUBLE)));
+  }
+
+  @Test
+  public void testPrimitiveArrayDescriptor() {
+    assertEquals(
+        "[D",
+        descriptorFactory.getDescriptor(
+            types.getArrayType(
+                types.getPrimitiveType(TypeKind.DOUBLE))));
+  }
+
+  @Test
+  public void testTopLevelClassDescriptor() {
+    assertEquals(
+        "Ljava/lang/String;",
+        descriptorFactory.getDescriptor(elements.getTypeElement("java.lang.String")));
+  }
+
+  @Test
+  public void testNestedClassDescriptor() {
+    assertEquals(
+        "Ljava/util/Map$Entry;",
+        descriptorFactory.getDescriptor(elements.getTypeElement("java.util.Map.Entry")));
+  }
+
+  @Test
+  public void testObjectArrayDescriptor() {
+    assertEquals(
+        "[Ljava/lang/Object;",
+        descriptorFactory.getDescriptor(
+            types.getArrayType(elements.getTypeElement("java.lang.Object").asType())));
+  }
+
+  @Test
+  public void testMultiDimObjectArrayDescriptor() {
+    assertEquals(
+        "[[Ljava/lang/Object;",
+        descriptorFactory.getDescriptor(
+            types.getArrayType(
+                types.getArrayType(
+                    elements.getTypeElement("java.lang.Object").asType()))));
+  }
+
+  @Test
+  public void testParameterlessMethodDescriptor() {
+    assertEquals(
+        "()Ljava/lang/String;",
+        descriptorFactory.getDescriptor(findMethod(
+            "toString",
+            elements.getTypeElement("java.lang.Object"))));
+  }
+
+  @Test
+  public void testMethodWithParametersDescriptor() {
+    assertEquals(
+        "(II)I",
+        descriptorFactory.getDescriptor(findMethod(
+            "codePointCount",
+            elements.getTypeElement("java.lang.String"))));
+  }
+
+  @Test
+  public void testMethodWithGenericParametersDescriptor() {
+    assertEquals(
+        "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+        descriptorFactory.getDescriptor(findMethod(
+            "put",
+            elements.getTypeElement("java.util.Map"))));
+  }
+
+  @Test
+  public void testTypeVarWithMultipleBoundsDescriptor() throws IOException {
+    compile("class Foo <T extends Runnable & CharSequence> { }");
+
+    assertEquals(
+        "Ljava/lang/Runnable;",
+        descriptorFactory.getDescriptor(elements.getTypeElement("Foo").getTypeParameters().get(0)));
+  }
+
+  @Test
+  public void testTypeVarInternalName() throws IOException {
+    compile("class Foo<T extends java.util.List<U>, U> { }");
+
+    assertEquals(
+        "java/util/List",
+        descriptorFactory.getInternalName(
+            elements.getTypeElement("Foo").getTypeParameters().get(0).asType()));
+  }
+
+  @Test
+  public void testTypeVarWithMultipleBoundsInDifferentOrderDescriptor() throws IOException {
+    compile("class Foo <T extends CharSequence & Runnable> { }");
+
+    assertEquals(
+        "Ljava/lang/CharSequence;",
+        descriptorFactory.getDescriptor(elements.getTypeElement("Foo").getTypeParameters().get(0)));
+  }
+
+  private ExecutableElement findMethod(String name, TypeElement typeElement) {
+    for (Element element : typeElement.getEnclosedElements()) {
+      if (element.getKind() == ElementKind.METHOD && element.getSimpleName().contentEquals(name)) {
+        return (ExecutableElement) element;
+      }
+    }
+
+    throw new IllegalArgumentException(String.format(
+        "No such method in %s: %s",
+        typeElement.getQualifiedName(),
+        name));
+  }
+}
