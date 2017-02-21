4198755b3e8cee267172c15f06d3ea82262a7109
Mike Athanasakis <mikath@fb.com>
2017-02-21 05:53:24 -0800

    switch to java stream
    Summary: Change the FluentIterable to a Java 8 stream.
    
    Test Plan: CI
    
    Reviewed By: Coneko
    
    fbshipit-source-id: 0ebfdcb

--- src/com/facebook/buck/util/cache/DefaultFileHashCache.java
+++ src/com/facebook/buck/util/cache/DefaultFileHashCache.java
@@ -19,13 +19,13 @@ package com.facebook.buck.util.cache;
 import com.facebook.buck.hashing.PathHashing;
 import com.facebook.buck.io.ArchiveMemberPath;
 import com.facebook.buck.io.ProjectFilesystem;
+import com.facebook.buck.util.MoreCollectors;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hasher;
@@ -248,10 +248,9 @@ public class DefaultFileHashCache implements ProjectFileHashCache {
     if (projectFilesystem.isDirectory(path)) {
       value = HashCodeAndFileType.ofDirectory(
           hashCode,
-          ImmutableSet.copyOf(
-              FluentIterable.from(projectFilesystem.getFilesUnderPath(path))
-                  .transform(
-                      path::relativize)));
+          projectFilesystem.getFilesUnderPath(path).stream()
+              .map(path::relativize)
+              .collect(MoreCollectors.toImmutableSet()));
     } else if (rawPath.toString().endsWith(".jar")) {
       value = HashCodeAndFileType.ofArchive(
           hashCode,
