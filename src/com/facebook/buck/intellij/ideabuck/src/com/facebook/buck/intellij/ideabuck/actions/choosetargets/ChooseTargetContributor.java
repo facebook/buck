5dca2f6c61de1b7a397125c90b0d58e131dc4002
Weiwen Liu <weiwen@fb.com>
2017-03-29 09:46:57 -0700

    restructure code to make it easy to understand and add new features
    Summary:
    break down the original huge function into smaller functions.  This
    makes it easier to read and to add new features.
    
    Test Plan: unit tests and manual test in intellij
    
    Reviewed By: styurin
    
    fbshipit-source-id: b782da6

--- src/com/facebook/buck/intellij/ideabuck/src/com/facebook/buck/intellij/ideabuck/actions/choosetargets/ChooseTargetContributor.java
+++ src/com/facebook/buck/intellij/ideabuck/src/com/facebook/buck/intellij/ideabuck/actions/choosetargets/ChooseTargetContributor.java
@@ -27,6 +27,8 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
+import java.io.File;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
@@ -36,65 +38,124 @@ import javax.annotation.Nullable;
 
 public class ChooseTargetContributor implements ChooseByNameContributor {
 
-  public static final String ALIAS_SEPARATOR = "::";
-  public static final String TARGET_NAME_SEPARATOR = ":";
+  private static final String ALIAS_SEPARATOR = "::";
+  private static final String TARGET_NAME_SEPARATOR = ":";
+  private static final String BUILD_DIR_SEPARATOR = "/";
 
-  public void addPathSugestions(final List<String> names, final Project project) {
-    String currentText = project.getUserData(
-            ChooseByNamePopup.CHOOSE_BY_NAME_POPUP_IN_PROJECT_KEY).getEnteredText();
+  @Override
+  public String[] getNames(Project project, boolean includeNonProjectItems) {
+    List<String> names = new ArrayList<>();
+    names.addAll(getNamesFromPathSuggestions(project));
+    names.addAll(getNamesFromBuildTargetAlias(project));
+    return names.toArray(new String[names.size()]);
+  }
 
-    // Remove the begining //
-    currentText = currentText.replaceFirst("^/*", "");
-
-    // check if we have as input a proper target
-    int currentTargetSeparatorIndex = currentText.lastIndexOf(TARGET_NAME_SEPARATOR);
-    if (currentTargetSeparatorIndex != -1) {
-      currentText = currentText.substring(0, currentText.lastIndexOf(TARGET_NAME_SEPARATOR));
+  private List<String> getNamesFromBuildTargetAlias(Project project) {
+    List<String> names = new ArrayList<>();
+    BuckBuildTargetAliasParser.parseAlias(project.getBasePath());
+    for (Map.Entry<String, Set<String>> entry :
+        BuckBuildTargetAliasParser.sTargetAlias.entrySet()) {
+      String target = entry.getKey();
+      Set<String> alias = entry.getValue();
+      target += ALIAS_SEPARATOR + Joiner.on(',').join(alias);
+      names.add(target);
     }
-    // Try to get the relative path to the current input folder
-    VirtualFile baseDir = project.getBaseDir().findFileByRelativePath(currentText + "/");
+    return names;
+  }
 
-    if (baseDir == null) {
-      // Try to get the relative path to the previous input folder
-      if (currentText.lastIndexOf("/") != -1) {
-        currentText = currentText.substring(0, currentText.lastIndexOf("/"));
-      } else {
-        // Get the base path if there is no previous folder
-        currentText = "";
-      }
-      baseDir = project.getBaseDir().findFileByRelativePath(currentText);
+  private List<String> getNamesFromPathSuggestions(Project project) {
+    CurrentInputText currentInputText = new CurrentInputText(project);
+    if (currentInputText.buildDir == null) {
+      return Collections.emptyList();
+    }
+    List<String> names = new ArrayList<>();
+    names.addAll(getNameSuggestionUnderPath(project, currentInputText.buildDir));
+    if (!currentInputText.hasBuildRule) {
+      names.addAll(
+          getNameSuggestionUnderPath(project, getParentDir(currentInputText.buildDir)));
+    }
+    return names;
+  }
 
-      // If the base dir is still null, then we have a bad relative path
-      if (baseDir == null) {
+  private static class CurrentInputText {
+    public final String buildDir;
+    public final boolean hasBuildRule;
+
+    CurrentInputText(Project project) {
+      ChooseByNamePopup chooseByNamePopup = project.getUserData(
+          ChooseByNamePopup.CHOOSE_BY_NAME_POPUP_IN_PROJECT_KEY);
+      if (chooseByNamePopup == null) {
+        buildDir = null;
+        hasBuildRule = false;
         return;
       }
+      String currentText = chooseByNamePopup.getEnteredText()
+          // Remove the begining //
+          .replaceFirst("^/*", "");
+
+      // check if we have as input a proper target
+      int targetSeparatorIndex = currentText.lastIndexOf(TARGET_NAME_SEPARATOR);
+      if (targetSeparatorIndex != -1) {
+        hasBuildRule = true;
+        buildDir = currentText.substring(0, targetSeparatorIndex);
+      } else {
+        hasBuildRule = false;
+        buildDir = currentText;
+      }
     }
+  }
+
+  private String getParentDir(String currentDir) {
+    int lastDirSeparatorPosition = currentDir.lastIndexOf(File.separator);
+    if (lastDirSeparatorPosition == -1) {
+      return "";
+    } else {
+      return currentDir.substring(0, lastDirSeparatorPosition);
+    }
+  }
+
+  private List<String> getNameSuggestionUnderPath(Project project, String buildDir) {
+    List<String> names = new ArrayList<>();
+    // Try to get the relative path to the current input folder
+    VirtualFile baseDir = project.getBaseDir().findFileByRelativePath(
+        appendAffixIfNotEmpty(buildDir, File.separator));
+    if (baseDir == null) {
+      return names;
+    }
+
     // get the files under the base folder
     VirtualFile[] files = baseDir.getChildren();
-
-    if (!currentText.isEmpty()) {
-      currentText += "/";
-    }
-
     for (VirtualFile file : files) {
-      // if the file is a directory we add it to the targets
-      if (file.isDirectory()) {
-        names.add("//" + currentText + file.getName());
-      }
-      //if the file is a buck file  we parse it and add its target names to the list
-      if (file.getName().equals("BUCK")) {
-        int end = currentText.length() - 1;
-        // Handle if the BUCK file is the main directory of the project
-        if (currentText.isEmpty()) {
-          end = 0;
-        }
-        String target = "//" + currentText.substring(0, end) + ":";
+      names.addAll(getNameSuggestionForVirtualFile(project, file, buildDir));
+    }
+    return names;
+  }
 
-        // we need the aliases now
-        names.addAll(
-            BuckQueryAction.execute(
+  private String appendAffixIfNotEmpty(String source, String suffix) {
+    if (!source.isEmpty()) {
+      source += suffix;
+    }
+    return source;
+  }
+
+  private List<String> getNameSuggestionForVirtualFile(Project project, VirtualFile file,
+      String buildDir) {
+    // if the file is a directory we add it to the targets
+    if (file.isDirectory()) {
+      return new ArrayList<String>(Collections.singletonList(
+          "//" + appendAffixIfNotEmpty(buildDir, BUILD_DIR_SEPARATOR) + file.getName()));
+    } else if (file.getName().equals("BUCK")) {
+      //if the file is a buck file  we parse it and add its target names to the list
+      return getBuildTargetFromBuildProjectFile(project, buildDir);
+    }
+    return Collections.emptyList();
+  }
+
+  private List<String> getBuildTargetFromBuildProjectFile(Project project, String buildDir) {
+    return
+        BuckQueryAction.execute(
             project,
-            target,
+            "//" + buildDir + TARGET_NAME_SEPARATOR,
             new Function<List<String>, Void>() {
               @Nullable
               @Override
@@ -115,27 +176,7 @@ public class ChooseTargetContributor implements ChooseByNameContributor {
                 return null;
               }
             }
-        ));
-      }
-    }
-  }
-
-  @Override
-  public String[] getNames(Project project, boolean includeNonProjectItems) {
-    BuckBuildTargetAliasParser.parseAlias(project.getBasePath());
-    List<String> names = new ArrayList<String>();
-
-    addPathSugestions(names, project);
-
-    for (Map.Entry<String, Set<String>> entry :
-        BuckBuildTargetAliasParser.sTargetAlias.entrySet()) {
-      String target = entry.getKey();
-      Set<String> alias = entry.getValue();
-      target += ALIAS_SEPARATOR + Joiner.on(',').join(alias);
-      names.add(target);
-    }
-
-    return names.toArray(new String[names.size()]);
+        );
   }
 
   @Override
