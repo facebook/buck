c01d1e20eb053dea5ca487b620e18f5e21ecdc91
Uri Baghin <coneko@fb.com>
2017-04-25 08:17:27 -0700

    Autoformat java files in src/com/facebook/buck/build_type.
    Test Plan: CI
    
    Reviewed By: aiked
    
    fbshipit-source-id: ad44eda

--- src/com/facebook/buck/build_type/BuildType.java
+++ src/com/facebook/buck/build_type/BuildType.java
@@ -20,7 +20,6 @@ import com.facebook.buck.log.Logger;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.io.Files;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
@@ -36,25 +35,26 @@ public enum BuildType {
   /**
    * Check in runtime to see what current build type is.
    *
-   * To run buck in different modes you can invoke: buck run buck --config build.type=enum_value
+   * <p>To run buck in different modes you can invoke: buck run buck --config build.type=enum_value
    */
-  public static final Supplier<BuildType> CURRENT_BUILD_TYPE = Suppliers.memoize(
-      new Supplier<BuildType>() {
-        @Override
-        public BuildType get() {
-          String buildTypeFilename = System.getProperty("buck.buck_build_type_info");
-          if (buildTypeFilename == null) {
-            return LOCAL_ANT;
-          }
-          try {
-            String contents = Files.readFirstLine(
-                Paths.get(buildTypeFilename).toFile(),
-                StandardCharsets.UTF_8);
-            return BuildType.valueOf(contents);
-          } catch (IOException e) {
-            LOG.error(e, "Failed to read build type, using LOCAL_ANT type.");
-            return LOCAL_ANT;
-          }
-        }
-      });
+  public static final Supplier<BuildType> CURRENT_BUILD_TYPE =
+      Suppliers.memoize(
+          new Supplier<BuildType>() {
+            @Override
+            public BuildType get() {
+              String buildTypeFilename = System.getProperty("buck.buck_build_type_info");
+              if (buildTypeFilename == null) {
+                return LOCAL_ANT;
+              }
+              try {
+                String contents =
+                    Files.readFirstLine(
+                        Paths.get(buildTypeFilename).toFile(), StandardCharsets.UTF_8);
+                return BuildType.valueOf(contents);
+              } catch (IOException e) {
+                LOG.error(e, "Failed to read build type, using LOCAL_ANT type.");
+                return LOCAL_ANT;
+              }
+            }
+          });
 }
