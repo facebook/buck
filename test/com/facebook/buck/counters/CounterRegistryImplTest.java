4bd6d8c39caa239eb16f362dfe280557f5e8b856
Uri Baghin <coneko@fb.com>
2017-04-25 08:44:20 -0700

    Autoformat java files in test/com/facebook/buck/counters.
    Test Plan: CI
    
    Reviewed By: marcinkosiba
    
    fbshipit-source-id: 707d62b

--- test/com/facebook/buck/counters/CounterRegistryImplTest.java
+++ test/com/facebook/buck/counters/CounterRegistryImplTest.java
@@ -31,27 +31,24 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.eventbus.Subscribe;
-
-import org.easymock.Capture;
-import org.easymock.EasyMock;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import org.easymock.Capture;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CounterRegistryImplTest {
 
   private static final String CATEGORY = "Counter_Category";
   private static final String NAME = "Counter_Name";
-  public static final ImmutableMap<String, String> TAGS = ImmutableMap.of(
-      "My super Tag Key", "And the according value!"
-  );
+  public static final ImmutableMap<String, String> TAGS =
+      ImmutableMap.of("My super Tag Key", "And the according value!");
 
   private BuckEventBus eventBus;
   private ScheduledThreadPoolExecutor executor;
@@ -84,12 +81,12 @@ public class CounterRegistryImplTest {
 
     this.eventBus.post(EasyMock.capture(countersEvent));
     EasyMock.expectLastCall();
-    EasyMock.expect(this.executor
-        .scheduleAtFixedRate(
-            EasyMock.capture(flushCountersRunnable),
-            EasyMock.anyInt(),
-            EasyMock.anyInt(),
-            EasyMock.anyObject(TimeUnit.class)))
+    EasyMock.expect(
+            this.executor.scheduleAtFixedRate(
+                EasyMock.capture(flushCountersRunnable),
+                EasyMock.anyInt(),
+                EasyMock.anyInt(),
+                EasyMock.anyObject(TimeUnit.class)))
         .andReturn(this.mockFuture);
     EasyMock.expect(this.mockFuture.cancel(EasyMock.anyBoolean())).andReturn(true).once();
 
@@ -116,8 +113,7 @@ public class CounterRegistryImplTest {
       Assert.assertTrue(flushCountersRunnable.hasCaptured());
       flushCountersRunnable.getValue().run();
       Assert.assertTrue(countersEvent.hasCaptured());
-      CountersSnapshotEvent event =
-          (CountersSnapshotEvent) countersEvent.getValue();
+      CountersSnapshotEvent event = (CountersSnapshotEvent) countersEvent.getValue();
       Assert.assertEquals(2, event.getSnapshots().size());
       Assert.assertEquals(42, (long) event.getSnapshots().get(0).getValues().values().toArray()[0]);
       Assert.assertEquals(
@@ -131,48 +127,33 @@ public class CounterRegistryImplTest {
 
   @Test
   public void noEventsFlushedIfNoCountersRegistered() throws IOException {
-    BuckEventBus fakeEventBus = new DefaultBuckEventBus(
-        new FakeClock(0),
-        false,
-        new BuildId("12345"),
-        1000);
+    BuckEventBus fakeEventBus =
+        new DefaultBuckEventBus(new FakeClock(0), false, new BuildId("12345"), 1000);
     SnapshotEventListener listener = new SnapshotEventListener();
     fakeEventBus.register(listener);
     FakeExecutor fakeExecutor = new FakeExecutor();
     try (CounterRegistryImpl registry = new CounterRegistryImpl(fakeExecutor, fakeEventBus)) {
       assertThat(
-          "No events should be flushed before timer fires",
-          listener.snapshotEvents,
-          empty());
+          "No events should be flushed before timer fires", listener.snapshotEvents, empty());
       fakeExecutor.drain();
-      assertThat(
-          "No events should be flushed after timer fires",
-          listener.snapshotEvents,
-          empty());
+      assertThat("No events should be flushed after timer fires", listener.snapshotEvents, empty());
     }
 
     assertThat(
-        "No events should be flushed after registry closed",
-        listener.snapshotEvents,
-        empty());
+        "No events should be flushed after registry closed", listener.snapshotEvents, empty());
   }
 
   @Test
   public void noEventsFlushedIfCounterRegisteredButHasNoData() throws IOException {
-    BuckEventBus fakeEventBus = new DefaultBuckEventBus(
-        new FakeClock(0),
-        false,
-        new BuildId("12345"),
-        1000);
+    BuckEventBus fakeEventBus =
+        new DefaultBuckEventBus(new FakeClock(0), false, new BuildId("12345"), 1000);
     SnapshotEventListener listener = new SnapshotEventListener();
     fakeEventBus.register(listener);
     FakeExecutor fakeExecutor = new FakeExecutor();
     try (CounterRegistryImpl registry = new CounterRegistryImpl(fakeExecutor, fakeEventBus)) {
       registry.newIntegerCounter(CATEGORY, NAME, TAGS);
       assertThat(
-          "No events should be flushed before timer fires",
-          listener.snapshotEvents,
-          empty());
+          "No events should be flushed before timer fires", listener.snapshotEvents, empty());
       fakeExecutor.drain();
       assertThat(
           "No events should be flushed after timer fires when counter has no data",
@@ -181,18 +162,13 @@ public class CounterRegistryImplTest {
     }
 
     assertThat(
-        "No events should be flushed after registry closed",
-        listener.snapshotEvents,
-        empty());
+        "No events should be flushed after registry closed", listener.snapshotEvents, empty());
   }
 
   @Test
   public void eventIsFlushedIfCounterRegisteredWithData() throws IOException {
-    BuckEventBus fakeEventBus = new DefaultBuckEventBus(
-        new FakeClock(0),
-        false,
-        new BuildId("12345"),
-        1000);
+    BuckEventBus fakeEventBus =
+        new DefaultBuckEventBus(new FakeClock(0), false, new BuildId("12345"), 1000);
     SnapshotEventListener listener = new SnapshotEventListener();
     fakeEventBus.register(listener);
     FakeExecutor fakeExecutor = new FakeExecutor();
@@ -201,14 +177,10 @@ public class CounterRegistryImplTest {
       IntegerCounter counter = registry.newIntegerCounter(CATEGORY, NAME, TAGS);
       counter.inc(42);
       assertThat(
-          "No events should be flushed before timer fires",
-          listener.snapshotEvents,
-          empty());
+          "No events should be flushed before timer fires", listener.snapshotEvents, empty());
       fakeExecutor.drain();
       assertThat(
-          "One event should be flushed after timer fires",
-          listener.snapshotEvents,
-          hasSize(1));
+          "One event should be flushed after timer fires", listener.snapshotEvents, hasSize(1));
       assertThat(
           "Counter with expected value should be flushed after timer fires",
           listener.snapshotEvents.get(0).getSnapshots(),
@@ -223,11 +195,8 @@ public class CounterRegistryImplTest {
 
   @Test
   public void closingRegistryBeforeTimerFiresFlushesCounters() throws IOException {
-    BuckEventBus fakeEventBus = new DefaultBuckEventBus(
-        new FakeClock(0),
-        false,
-        new BuildId("12345"),
-        1000);
+    BuckEventBus fakeEventBus =
+        new DefaultBuckEventBus(new FakeClock(0), false, new BuildId("12345"), 1000);
     SnapshotEventListener listener = new SnapshotEventListener();
     fakeEventBus.register(listener);
     FakeExecutor fakeExecutor = new FakeExecutor();
@@ -236,9 +205,7 @@ public class CounterRegistryImplTest {
       IntegerCounter counter = registry.newIntegerCounter(CATEGORY, NAME, TAGS);
       counter.inc(42);
       assertThat(
-          "No events should be flushed before timer fires",
-          listener.snapshotEvents,
-          empty());
+          "No events should be flushed before timer fires", listener.snapshotEvents, empty());
     }
 
     // We explicitly do not call fakeExecutor.drain() here, because we want to simulate what
