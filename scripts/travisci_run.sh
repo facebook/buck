54c733b0fce3542b750c836a25509d4a6f3f0503
Christian Legnitto <christian@legnitto.com>
2017-05-10 04:12:07 -0700

    Fix travis
    Summary:
    WIP, don't merge yet!
    Closes https://github.com/facebook/buck/pull/1331
    
    Reviewed By: Coneko
    
    Pulled By: Coneko
    
    fbshipit-source-id: 21595da

--- scripts/travisci_run.sh
+++ scripts/travisci_run.sh
@@ -5,8 +5,8 @@ set -eux
 # because otherwise Travis thinks our logs are too long.
 export TERM=dumb
 
-# Use Buck to run the tests first because its output is more
-# conducive to diagnosing failed tests than Ant's is.
+# Use Buck to build first because its output is more
+# conducive to diagnosing failed builds than Ant's is.
 ./bin/buck build buck || { cat buck-out/log/ant.log; exit 1; }
 
 # There are only two cores in the containers, but Buck thinks there are 12
@@ -14,10 +14,20 @@ export TERM=dumb
 # processes killed if we do not limit our threads.
 export BUCK_NUM_THREADS=3
 
-# Make sure that everything builds in case a library is not covered by a test.
-./bin/buck build --num-threads=$BUCK_NUM_THREADS src/... test/...
+if [ "$CI_ACTION" = "build" ]; then
+  # Make sure that everything builds in case a library is not covered by a test.
+  ./bin/buck build --num-threads=$BUCK_NUM_THREADS src/... test/...
+fi
 
-./bin/buck test --num-threads=$BUCK_NUM_THREADS //...
+if [ "$CI_ACTION" = "unit" ]; then
+  ./bin/buck test --num-threads=$BUCK_NUM_THREADS --all --test-selectors '!.*[Ii]ntegration.*'
+fi
 
-# Run all the other checks with ant.
-ant travis
+if [ "$CI_ACTION" = "integration" ]; then
+  ./bin/buck test --num-threads=$BUCK_NUM_THREADS --all --filter '.*[Ii]ntegration.*'
+fi
+
+if [ "$CI_ACTION" = "ant" ]; then
+  # Run all the other checks with ant.
+  ant travis
+fi
