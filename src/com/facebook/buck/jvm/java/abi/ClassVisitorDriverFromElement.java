1168c516e82cf1e5cb012bbf5d69169054cc1086
Yiding Jia <yiding@fb.com>
2017-05-24 17:24:19 -0700

    fix build
    Summary: missing import
    
    Test Plan: Command F9 to build in intellij.
    
    Reviewed By: jkeljo
    
    fbshipit-source-id: 109f5a8

--- src/com/facebook/buck/jvm/java/abi/ClassVisitorDriverFromElement.java
+++ src/com/facebook/buck/jvm/java/abi/ClassVisitorDriverFromElement.java
@@ -24,6 +24,7 @@ import javax.annotation.Nullable;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
