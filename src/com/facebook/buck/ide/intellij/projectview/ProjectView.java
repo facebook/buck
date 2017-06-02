ba068732cd9ce40addab348a4951fc1335d48008
Jon Shemitz <shemitz@fb.com>
2017-06-02 11:01:20 -0700

    Refactor to decrease the size of the parameter lists
    Summary:
    I disapprove of the monster-parameter-list pattern. It makes for big code that's hard to follow and tedious to maintain; passing a single `interface` that can 'reach into' calling code to extract state on-demand is slightly cheaper, and makes it a lot easier to update the parameter list, especially when (as here) the parameters are passed through (and stored in) a 'helper' class.
    
    This diff introduces the smaller, more-maintainable parameter-extraction pattern. The interfaces are Just Good Enough for a 1st pass and can undoubtedly be improved and/or generalized, but the code is already smaller, clearer, and slightly faster. This diff doesn't touch iOS project generation, and makes only minimal changes to the current standard `buck project` IJ generator.
    
    Test Plan: `buck test //test/com/facebook/buck/ide/intellij/projectview && buck test //test/com/facebook/buck/ide/intellij`
    
    Reviewed By: styurin
    
    fbshipit-source-id: f131c65

--- src/com/facebook/buck/ide/intellij/projectview/ProjectView.java
+++ src/com/facebook/buck/ide/intellij/projectview/ProjectView.java
@@ -20,6 +20,7 @@ import static com.facebook.buck.ide.intellij.projectview.Patterns.capture;
 import static com.facebook.buck.ide.intellij.projectview.Patterns.noncapture;
 import static com.facebook.buck.ide.intellij.projectview.Patterns.optional;
 
+import com.facebook.buck.cli.parameter_extractors.ProjectViewParameters;
 import com.facebook.buck.config.Config;
 import com.facebook.buck.graph.AbstractBreadthFirstTraversal;
 import com.facebook.buck.io.ProjectFilesystem;
@@ -37,6 +38,7 @@ import com.facebook.buck.rules.TargetGraph;
 import com.facebook.buck.rules.TargetNode;
 import com.facebook.buck.rules.TargetNodes;
 import com.facebook.buck.util.DirtyPrintStreamDecorator;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import java.io.File;
 import java.io.FileWriter;
@@ -74,17 +76,11 @@ public class ProjectView {
   // region Public API
 
   public static int run(
-      DirtyPrintStreamDecorator stderr,
-      boolean dryRun,
-      boolean withTests,
-      String viewPath,
+      ProjectViewParameters projectViewParameters,
       TargetGraph targetGraph,
       ImmutableSet<BuildTarget> buildTargets,
-      ActionGraphAndResolver actionGraph,
-      Config config) {
-    return new ProjectView(
-            stderr, dryRun, withTests, viewPath, targetGraph, buildTargets, actionGraph, config)
-        .run();
+      ActionGraphAndResolver actionGraph) {
+    return new ProjectView(projectViewParameters, targetGraph, buildTargets, actionGraph).run();
   }
 
   // endregion Public API
@@ -117,24 +113,20 @@ public class ProjectView {
   private final String repository = new File("").getAbsolutePath();
 
   private ProjectView(
-      DirtyPrintStreamDecorator stdErr,
-      boolean dryRun,
-      boolean withTests,
-      String viewPath,
+      ProjectViewParameters projectViewParameters,
       TargetGraph targetGraph,
       ImmutableSet<BuildTarget> buildTargets,
-      ActionGraphAndResolver actionGraph,
-      Config config) {
-    this.stdErr = stdErr;
-    this.viewPath = viewPath;
-    this.dryRun = dryRun;
-    this.withTests = withTests;
+      ActionGraphAndResolver actionGraph) {
+    this.stdErr = projectViewParameters.getStdErr();
+    this.viewPath = Preconditions.checkNotNull(projectViewParameters.getViewPath());
+    this.dryRun = projectViewParameters.isDryRun();
+    this.withTests = projectViewParameters.isWithTests();
 
     this.targetGraph = targetGraph;
     this.buildTargets = buildTargets;
     this.actionGraph = actionGraph;
 
-    this.config = config;
+    this.config = projectViewParameters.getConfig();
   }
 
   private int run() {
