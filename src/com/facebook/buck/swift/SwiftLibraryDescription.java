65f950b84346eaca4f1d00b7ed3045fcefd29bbb
Yiding Jia <yiding@fb.com>
2017-06-27 16:29:54 -0700

    Fix a bug where BuildRules referenced by swift srcs were not added to build-deps
    Summary:
    Revert back to using copyAppendingExtraDeps, which should already include
    dependencies to rules named in SourcePaths from fields in the arg.
    
    Test Plan: Build swift target with a build target in a source file.
    
    Reviewed By: scottrice
    
    fbshipit-source-id: 17db5ef

--- src/com/facebook/buck/swift/SwiftLibraryDescription.java
+++ src/com/facebook/buck/swift/SwiftLibraryDescription.java
@@ -260,20 +260,15 @@ public class SwiftLibraryDescription implements Description<SwiftLibraryDescript
       return new SwiftCompile(
           cxxPlatform,
           swiftBuckConfig,
-          new BuildRuleParams(
-              params.getBuildTarget(),
+          params.copyAppendingExtraDeps(
               () ->
-                  ImmutableSortedSet.<BuildRule>naturalOrder()
+                  ImmutableSet.<BuildRule>builder()
                       .addAll(swiftCompileRules)
                       .addAll(implicitSwiftCompileRules)
                       .addAll(cxxDeps.getDeps(ruleFinder))
-                      // This is only used for generating include args, but adding it for local
-                      // reasoning correctness.
+                      // This is only used for generating include args and may not be actually needed.
                       .addAll(preprocessor.getDeps(ruleFinder))
-                      .build(),
-              ImmutableSortedSet::of,
-              ImmutableSortedSet.of(),
-              params.getProjectFilesystem()),
+                      .build()),
           swiftPlatform.get().getSwiftc(),
           args.getFrameworks(),
           args.getModuleName().orElse(buildTarget.getShortName()),
