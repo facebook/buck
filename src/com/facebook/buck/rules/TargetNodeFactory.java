cc29f119a855cb39665f54584ce779d1708c99eb
Daniel Wagner-Hall <dwh@fb.com>
2017-06-29 03:51:04 -0700

    TargetNodeFactory implements equals and hashCode
    Test Plan: CI
    
    fbshipit-source-id: 8327bbe

--- src/com/facebook/buck/rules/TargetNodeFactory.java
+++ src/com/facebook/buck/rules/TargetNodeFactory.java
@@ -27,6 +27,7 @@ import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Sets;
 import com.google.common.hash.HashCode;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Optional;
 
 public class TargetNodeFactory {
@@ -202,5 +203,18 @@ public class TargetNodeFactory {
               originalNode.getBuildTarget()),
           e);
     }
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof TargetNodeFactory)) {
+      return false;
+    }
+    return Objects.equals(typeCoercerFactory, ((TargetNodeFactory) obj).typeCoercerFactory);
+  }
+
+  @Override
+  public int hashCode() {
+    return typeCoercerFactory.hashCode();
   }
 }
