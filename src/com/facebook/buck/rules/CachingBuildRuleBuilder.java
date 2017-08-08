ac02329c1f7e5e29e18a68522225dfbf82bb4e01
Jonathan Keljo <jkeljo@fb.com>
2017-08-08 16:18:13 -0700

    CachingBuildRuleBuilder: Fix refactoring error
    Summary:
    In an earlier refactor, I inadvertently changed an error path from catching
    `Throwable` to catching `Exception`. This resulted in log error spew and odd behavior
    when non-`Exception`s were thrown.
    
    Test Plan: Introduced such a throw, verified that it's handled gracefully now.
    
    Reviewed By: bertmaher
    
    fbshipit-source-id: 1ec0a09

--- src/com/facebook/buck/rules/CachingBuildRuleBuilder.java
+++ src/com/facebook/buck/rules/CachingBuildRuleBuilder.java
@@ -465,7 +465,7 @@ class CachingBuildRuleBuilder {
     buildResult =
         Futures.catchingAsync(
             buildResult,
-            Exception.class,
+            Throwable.class,
             thrown -> {
               LOG.debug(thrown, "Building rule [%s] failed.", rule.getBuildTarget());
 
@@ -1052,7 +1052,7 @@ class CachingBuildRuleBuilder {
         new BuildRuleDiagnosticData(ruleDeps.get(rule), diagnosticKeysBuilder.build()));
   }
 
-  private Exception maybeAttachBuildRuleNameToException(@Nonnull Exception thrown) {
+  private Throwable maybeAttachBuildRuleNameToException(@Nonnull Throwable thrown) {
     if ((thrown instanceof HumanReadableException) || (thrown instanceof InterruptedException)) {
       return thrown;
     }
