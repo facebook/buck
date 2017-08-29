564a879ac95443a2d58502b27f1533b112d7aa5c
Robbert Van Ginkel <robbert@uber.com>
2017-08-29 13:45:57 -0700

    Fix shouldRefuseToRunAppleTestIfXctestNotPresent
    Summary:
    This currently fails if the xctool is available in the PATH, because
    it auto resolves. Making the path fake makes this test consistently
    fail regardless of environment.
    Closes https://github.com/facebook/buck/pull/1486
    
    Reviewed By: dinhviethoa, ttsugriy
    
    Pulled By: ttsugriy
    
    fbshipit-source-id: dd203fe

--- test/com/facebook/buck/apple/AppleTestIntegrationTest.java
+++ test/com/facebook/buck/apple/AppleTestIntegrationTest.java
@@ -284,7 +284,7 @@ public class AppleTestIntegrationTest {
         containsString(
             "Set xctool_path = /path/to/xctool or xctool_zip_target = //path/to:xctool-zip in the "
                 + "[apple] section of .buckconfig to run this test"));
-    workspace.runBuckCommand("test", "//:foo");
+    workspace.runBuckCommand("test", "//:foo", "--config", "apple.xctool_path=does/not/exist");
   }
 
   @Test
