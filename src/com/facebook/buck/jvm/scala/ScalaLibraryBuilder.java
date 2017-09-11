4745e617096a180dece95695e20026db06076047
Gautam Korlam <gkorlam@uber.com>
2017-09-11 15:05:02 -0700

    Added comment

--- src/com/facebook/buck/jvm/scala/ScalaLibraryBuilder.java
+++ src/com/facebook/buck/jvm/scala/ScalaLibraryBuilder.java
@@ -51,6 +51,8 @@ public class ScalaLibraryBuilder extends DefaultJavaLibraryBuilder {
         cellRoots,
         javaBuckConfig);
     this.scalaBuckConfig = scalaBuckConfig;
+
+    // See https://github.com/facebook/buck/issues/1386
     setCompileAgainstAbis(false);
   }
 
