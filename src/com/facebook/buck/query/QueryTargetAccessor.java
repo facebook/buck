82991089e9a7c207014bfb3f67de3141d17cac44
Simon Stewart <simon.m.stewart@gmail.com>
2017-09-13 13:17:38 +0100

    Make `QueryFileTarget` take a `SourcePath`
    This allows them to be aware of cells, and to work when the
    base directory is redirected.

--- src/com/facebook/buck/query/QueryTargetAccessor.java
+++ src/com/facebook/buck/query/QueryTargetAccessor.java
@@ -17,7 +17,6 @@
 package com.facebook.buck.query;
 
 import com.facebook.buck.model.BuildTarget;
-import com.facebook.buck.rules.BuildTargetSourcePath;
 import com.facebook.buck.rules.PathSourcePath;
 import com.facebook.buck.rules.SourcePath;
 import com.facebook.buck.rules.TargetNode;
@@ -25,7 +24,6 @@ import com.facebook.buck.rules.coercer.CoercedTypeCache;
 import com.facebook.buck.rules.coercer.DefaultTypeCoercerFactory;
 import com.facebook.buck.rules.coercer.ParamInfo;
 import com.facebook.buck.rules.coercer.TypeCoercerFactory;
-import com.facebook.buck.util.HumanReadableException;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
@@ -52,9 +50,9 @@ public class QueryTargetAccessor {
     info.traverse(
         value -> {
           if (value instanceof Path) {
-            builder.add(QueryFileTarget.of((Path) value));
+            builder.add(QueryFileTarget.of(new PathSourcePath(node.getFilesystem(), (Path) value)));
           } else if (value instanceof SourcePath) {
-            builder.add(extractSourcePath((SourcePath) value));
+            builder.add(QueryFileTarget.of((SourcePath) value));
           } else if (value instanceof BuildTarget) {
             builder.add(extractBuildTargetContainer((BuildTarget) value));
           }
@@ -84,15 +82,6 @@ public class QueryTargetAccessor {
         },
         node.getConstructorArg());
     return builder.build();
-  }
-
-  public static QueryTarget extractSourcePath(SourcePath sourcePath) {
-    if (sourcePath instanceof PathSourcePath) {
-      return QueryFileTarget.of(((PathSourcePath) sourcePath).getRelativePath());
-    } else if (sourcePath instanceof BuildTargetSourcePath) {
-      return QueryBuildTarget.of(((BuildTargetSourcePath) sourcePath).getTarget());
-    }
-    throw new HumanReadableException("Unsupported source path type: %s", sourcePath.getClass());
   }
 
   public static QueryTarget extractBuildTargetContainer(BuildTarget buildTargetContainer) {
