f5e5c324c875e22c6dc068ccc56aea3765aedca7
Chris Hopman <cjhopman@fb.com>
2017-09-25 16:23:51 -0700

    Add tests for the error output of builds
    Summary:
    Adds integration tests that assert on the "main" lines of error output
    from builds. Individual stack frame lines are not matched (instead they are
    clumped into a single " <stacktrace> " line).
    
    Following changes in this stack dramatically change the outputs and I want a
    clear way to understand the effects.
    
    Test Plan: CI
    
    Reviewed By: ttsugriy
    
    fbshipit-source-id: f93866b

---
+++ test/com/facebook/buck/cli/BuildCommandErrorsIntegrationTest.java
@@ -0,0 +1,483 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.cli;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import com.facebook.buck.android.FakeAndroidDirectoryResolver;
+import com.facebook.buck.config.BuckConfig;
+import com.facebook.buck.cxx.toolchain.CxxPlatform;
+import com.facebook.buck.cxx.toolchain.CxxPlatformUtils;
+import com.facebook.buck.io.filesystem.ProjectFilesystem;
+import com.facebook.buck.model.BuildTarget;
+import com.facebook.buck.model.FlavorDomain;
+import com.facebook.buck.rules.AbstractBuildRule;
+import com.facebook.buck.rules.BuildContext;
+import com.facebook.buck.rules.BuildRule;
+import com.facebook.buck.rules.BuildRuleParams;
+import com.facebook.buck.rules.BuildRuleResolver;
+import com.facebook.buck.rules.BuildableContext;
+import com.facebook.buck.rules.CellPathResolver;
+import com.facebook.buck.rules.Description;
+import com.facebook.buck.rules.KnownBuildRuleTypes;
+import com.facebook.buck.rules.KnownBuildRuleTypesFactory;
+import com.facebook.buck.rules.NoopBuildRule;
+import com.facebook.buck.rules.SourcePath;
+import com.facebook.buck.rules.TargetGraph;
+import com.facebook.buck.step.AbstractExecutionStep;
+import com.facebook.buck.step.ExecutionContext;
+import com.facebook.buck.step.Step;
+import com.facebook.buck.step.StepExecutionResult;
+import com.facebook.buck.testutil.integration.ProjectWorkspace;
+import com.facebook.buck.testutil.integration.TemporaryPaths;
+import com.facebook.buck.testutil.integration.TestDataHelper;
+import com.facebook.buck.util.FakeProcessExecutor;
+import com.facebook.buck.util.HumanReadableException;
+import com.facebook.buck.util.immutables.BuckStyleImmutable;
+import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Lists;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.SortedSet;
+import javax.annotation.Nullable;
+import org.immutables.value.Value;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class BuildCommandErrorsIntegrationTest {
+  private static final boolean DEBUG = false;
+
+  @Rule public TemporaryPaths tmp = new TemporaryPaths();
+  private ProjectWorkspace workspace;
+  private MockDescription mockDescription;
+
+  @Before
+  public void setUp() throws IOException {
+    workspace = TestDataHelper.createProjectWorkspaceForScenario(this, "errors", tmp);
+    workspace.setUp();
+    mockDescription = new MockDescription();
+    workspace.setKnownBuildRuleTypesFactoryFactory(
+        (processExecutor, androidDirectoryResolver, sdkEnvironment) ->
+            new KnownBuildRuleTypesFactory(
+                new FakeProcessExecutor(), new FakeAndroidDirectoryResolver(), sdkEnvironment) {
+              @Override
+              public KnownBuildRuleTypes create(BuckConfig config, ProjectFilesystem filesystem)
+                  throws IOException, InterruptedException {
+                FlavorDomain<CxxPlatform> cxxPlatforms = FlavorDomain.of("C/C++ platform");
+                CxxPlatform defaultPlatform = CxxPlatformUtils.DEFAULT_PLATFORM;
+
+                KnownBuildRuleTypes.Builder buildRuleTypesBuilder = KnownBuildRuleTypes.builder();
+                buildRuleTypesBuilder.setCxxPlatforms(cxxPlatforms);
+                buildRuleTypesBuilder.setDefaultCxxPlatform(defaultPlatform);
+                return buildRuleTypesBuilder.register(mockDescription).build();
+              }
+            });
+  }
+
+  // TODO(cjhopman): Add cases for errors in other phases of the build (watchman, parsing,
+  // action graph, etc.).
+
+  // TODO(cjhopman): Add tests for buck.log.
+
+  private String getStderr(ProjectWorkspace.ProcessResult result) {
+    String stderr = result.getStderr();
+    if (DEBUG) {
+      System.out.println("=== STDERR ===");
+      System.out.print(stderr);
+      System.out.println("==============");
+    }
+    return stderr;
+  }
+
+  @Test
+  // TODO(cjhopman): The exception shouldn't be propagated out of the build.
+  public void exceptionWithCauseThrown() throws Exception {
+    try {
+      mockDescription.buildRuleFactory =
+          exceptionTargetFactory("failure message", RuntimeException.class, RuntimeException.class);
+      workspace.runBuckBuild(":target_name");
+      fail();
+    } catch (Exception e) {
+      if (DEBUG) e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void exceptionWithCauseThrownInStep() throws Exception {
+    mockDescription.buildRuleFactory =
+        stepExceptionTargetFactory(
+            "failure message", RuntimeException.class, RuntimeException.class);
+    workspace.runBuckBuild(":target_name");
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild(":target_name");
+    result.assertFailure();
+    assertEquals(
+        "BUILD FAILED: //:target_name failed on step failing_step with an exception:\n"
+            + " <- failure message -> \n"
+            + "java.lang.RuntimeException:  <- failure message -> \n"
+            + "<stacktrace>\n"
+            + "Caused by: java.lang.RuntimeException: failure message\n"
+            + "<stacktrace>\n",
+        getError(getStderr(result)));
+  }
+
+  @Test
+  // TODO(cjhopman): The exception shouldn't be propagated out of the build.
+  public void runtimeExceptionThrown() throws Exception {
+    try {
+      mockDescription.buildRuleFactory =
+          exceptionTargetFactory("failure message", RuntimeException.class);
+      workspace.runBuckBuild(":target_name");
+      fail();
+    } catch (Exception e) {
+      if (DEBUG) e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void runtimeExceptionThrownInStep() throws Exception {
+    mockDescription.buildRuleFactory =
+        stepExceptionTargetFactory("failure message", RuntimeException.class);
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild(":target_name");
+    result.assertFailure();
+    assertEquals(
+        "BUILD FAILED: //:target_name failed on step failing_step with an exception:\n"
+            + "failure message\n"
+            + "java.lang.RuntimeException: failure message\n"
+            + "<stacktrace>\n",
+        getError(getStderr(result)));
+  }
+
+  @Test
+  public void ioExceptionThrownInStep() throws Exception {
+    mockDescription.buildRuleFactory =
+        stepExceptionTargetFactory("failure message", IOException.class);
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild(":target_name");
+    result.assertFailure();
+    assertEquals(
+        "BUILD FAILED: //:target_name failed on step failing_step with an exception:\n"
+            + "failure message\n"
+            + "java.io.IOException: failure message\n"
+            + "<stacktrace>\n",
+        getError(getStderr(result)));
+  }
+
+  @Test
+  // TODO(cjhopman): The exception shouldn't be propagated out of the build.
+  public void ioExceptionThrown() throws Exception {
+    try {
+      mockDescription.buildRuleFactory =
+          exceptionTargetFactory("failure message", IOException.class);
+      workspace.runBuckBuild(":target_name");
+      fail();
+    } catch (Exception e) {
+      if (DEBUG) e.printStackTrace();
+    }
+  }
+
+  @Test
+  // TODO(cjhopman): The exception shouldn't be propagated out of the build.
+  public void ioExceptionWithRuleInMessageThrown() throws Exception {
+    try {
+      mockDescription.buildRuleFactory =
+          exceptionTargetFactory("failure message //:target_name", IOException.class);
+      workspace.runBuckBuild(":target_name");
+      fail();
+    } catch (Exception e) {
+      if (DEBUG) e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void humanReadableExceptionThrownInStep() throws Exception {
+    mockDescription.buildRuleFactory =
+        stepExceptionTargetFactory("failure message", HumanReadableException.class);
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild(":target_name");
+    result.assertFailure();
+    assertEquals(
+        "BUILD FAILED: //:target_name failed:\n" + "failing_step\n" + "failure message",
+        getError(getStderr(result)));
+  }
+
+  @Test
+  // TODO(cjhopman): The exception shouldn't be propagated out of the build.
+  public void humanReadableExceptionThrown() throws Exception {
+    try {
+      mockDescription.buildRuleFactory =
+          exceptionTargetFactory("failure message", HumanReadableException.class);
+      workspace.runBuckBuild(":target_name");
+      fail();
+    } catch (Exception e) {
+      if (DEBUG) e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void withStepReturningFailure() throws Exception {
+    mockDescription.buildRuleFactory = exitCodeTargetFactory("failure message", 1);
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild(":target_name");
+    result.assertFailure();
+    assertEquals(
+        "BUILD FAILED: //:target_name failed with exit code 1:\n"
+            + "step_with_exit_code_1\n"
+            + "stderr: failure message",
+        getError(getStderr(result)));
+  }
+
+  @Test
+  public void withSuccessfulStep() throws Exception {
+    mockDescription.buildRuleFactory = exitCodeTargetFactory("success message", 0);
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild(":target_name");
+    result.assertSuccess();
+    assertEquals("", getError(getStderr(result)));
+  }
+
+  @Test
+  public void successWithNoSteps() throws Exception {
+    mockDescription.buildRuleFactory = successTargetFactory();
+    ProjectWorkspace.ProcessResult result = workspace.runBuckBuild(":target_name");
+    result.assertSuccess();
+    assertEquals("", getError(getStderr(result)));
+  }
+
+  private String getError(String stderr) {
+    String[] lines = stderr.split("\n");
+    int start = 0;
+    while (start < lines.length) {
+      if (lines[start].startsWith("BUILD FAILED:")) {
+        break;
+      }
+      start++;
+    }
+    List<String> filtered = new ArrayList<>();
+    boolean previousWasFrame = false;
+    while (start < lines.length) {
+      String line = lines[start];
+      if (line.startsWith("DOWNLOADED")) {
+        break;
+      }
+      boolean isFrame = line.matches("^\\s*(at .*|\\.\\.\\..*more)");
+      if (isFrame) {
+        if (!previousWasFrame) {
+          filtered.add("<stacktrace>");
+        }
+        previousWasFrame = true;
+      } else {
+        previousWasFrame = false;
+        filtered.add(line);
+      }
+      start++;
+    }
+    return Joiner.on("\n").join(filtered);
+  }
+
+  @SafeVarargs
+  private final BuildRuleFactory exceptionTargetFactory(
+      String message, Class<? extends Throwable>... exceptionClasses) {
+    return exceptionTargetFactory(message, false, exceptionClasses);
+  }
+
+  @SafeVarargs
+  private final BuildRuleFactory stepExceptionTargetFactory(
+      String message, Class<? extends Throwable>... exceptionClasses) {
+    return exceptionTargetFactory(message, true, exceptionClasses);
+  }
+
+  @SafeVarargs
+  private final BuildRuleFactory exceptionTargetFactory(
+      String message, boolean inStep, Class<? extends Throwable>... exceptionClasses) {
+    return ((buildTarget, projectFilesystem) ->
+        new ExceptionRule(
+            buildTarget,
+            projectFilesystem,
+            ImmutableList.copyOf(exceptionClasses),
+            message,
+            inStep));
+  }
+
+  private BuildRuleFactory exitCodeTargetFactory(String message, int exitCode) {
+    return ((buildTarget, projectFilesystem) ->
+        new ExitCodeRule(buildTarget, projectFilesystem, exitCode, message));
+  }
+
+  private BuildRuleFactory successTargetFactory() {
+    return ((buildTarget, projectFilesystem) ->
+        new NoopBuildRule(buildTarget, projectFilesystem) {
+          @Override
+          public SortedSet<BuildRule> getBuildDeps() {
+            return ImmutableSortedSet.of();
+          }
+        });
+  }
+
+  private static class ExceptionRule extends AbstractBuildRule {
+    private final String message;
+    private final boolean inStep;
+    private final ImmutableList<Class<? extends Throwable>> exceptionClasses;
+
+    public ExceptionRule(
+        BuildTarget buildTarget,
+        ProjectFilesystem projectFilesystem,
+        ImmutableList<Class<? extends Throwable>> exceptionClasses,
+        String message,
+        boolean inStep) {
+      super(buildTarget, projectFilesystem);
+      this.exceptionClasses = exceptionClasses;
+      this.message = message;
+      this.inStep = inStep;
+    }
+
+    @Override
+    public SortedSet<BuildRule> getBuildDeps() {
+      return ImmutableSortedSet.of();
+    }
+
+    @Override
+    public ImmutableList<? extends Step> getBuildSteps(
+        BuildContext context, BuildableContext buildableContext) {
+      Throwable throwable = getException(exceptionClasses, message);
+      if (!inStep) {
+        Throwables.throwIfUnchecked(throwable);
+        throw new RuntimeException(throwable);
+      }
+      return ImmutableList.of(
+          new AbstractExecutionStep("failing_step") {
+            @Override
+            public StepExecutionResult execute(ExecutionContext context)
+                throws IOException, InterruptedException {
+              Throwables.throwIfInstanceOf(throwable, IOException.class);
+              Throwables.throwIfInstanceOf(throwable, InterruptedException.class);
+              Throwables.throwIfUnchecked(throwable);
+              throw new RuntimeException(throwable);
+            }
+          });
+    }
+
+    @Nullable
+    @Override
+    public SourcePath getSourcePathToOutput() {
+      return null;
+    }
+
+    private static Throwable getException(
+        ImmutableList<Class<? extends Throwable>> exceptionClasses, String message) {
+      Throwable cause = null;
+      for (Class<? extends Throwable> clz : Lists.reverse(exceptionClasses)) {
+        try {
+          Object obj;
+          if (cause == null) {
+            Constructor<?> constructor = clz.getConstructor(String.class);
+            obj = constructor.newInstance(message);
+          } else {
+            Constructor<?> constructor = clz.getConstructor(String.class, Throwable.class);
+            obj = constructor.newInstance(message, cause);
+          }
+          cause = Throwable.class.cast(obj);
+          // Modify the message a little so that we can differentiate the message at different
+          // levels of nesting.
+          message = " <- " + message + " -> ";
+        } catch (NoSuchMethodException
+            | IllegalAccessException
+            | InstantiationException
+            | InvocationTargetException e) {
+          // Throw an error to hopefully skip all the exception processing and don't accidentally
+          // pass the test.
+          throw new AssertionError(e);
+        }
+      }
+      return cause;
+    }
+  }
+
+  private class ExitCodeRule extends AbstractBuildRule {
+    private final int exitCode;
+    private final String message;
+
+    public ExitCodeRule(
+        BuildTarget buildTarget,
+        ProjectFilesystem projectFilesystem,
+        int exitCode,
+        String message) {
+      super(buildTarget, projectFilesystem);
+
+      this.exitCode = exitCode;
+      this.message = message;
+    }
+
+    @Override
+    public SortedSet<BuildRule> getBuildDeps() {
+      return ImmutableSortedSet.of();
+    }
+
+    @Override
+    public ImmutableList<? extends Step> getBuildSteps(
+        BuildContext context, BuildableContext buildableContext) {
+      return ImmutableList.of(
+          new AbstractExecutionStep("step_with_exit_code_" + exitCode) {
+            @Override
+            public StepExecutionResult execute(ExecutionContext context)
+                throws IOException, InterruptedException {
+              return StepExecutionResult.of(exitCode, Optional.of(message));
+            }
+          });
+    }
+
+    @Nullable
+    @Override
+    public SourcePath getSourcePathToOutput() {
+      return null;
+    }
+  }
+
+  @BuckStyleImmutable
+  @Value.Immutable
+  interface AbstractMockArg {}
+
+  private class MockDescription implements Description<MockArg> {
+    private BuildRuleFactory buildRuleFactory = null;
+
+    @Override
+    public Class<MockArg> getConstructorArgType() {
+      return MockArg.class;
+    }
+
+    @Override
+    public BuildRule createBuildRule(
+        TargetGraph targetGraph,
+        BuildTarget buildTarget,
+        ProjectFilesystem projectFilesystem,
+        BuildRuleParams params,
+        BuildRuleResolver resolver,
+        CellPathResolver cellRoots,
+        MockArg args) {
+      return Preconditions.checkNotNull(buildRuleFactory).create(buildTarget, projectFilesystem);
+    }
+  }
+
+  private interface BuildRuleFactory {
+    BuildRule create(BuildTarget buildTarget, ProjectFilesystem projectFilesystem);
+  }
+}
