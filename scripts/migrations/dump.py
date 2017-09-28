ac4433e9ea707eb9b7a333f3833ca3451b825396
Taras Tsugrii <ttsugrii@fb.com>
2017-09-27 23:17:50 -0700

    Extend dump script to print export map
    Summary:
    This command will be used to print all symbols transitively included
    in a particular build file, which will help to undestand how to migrate
    from include_defs to load function.
    
    Test Plan: python3 -m unittest *_test.py
    
    Reviewed By: sbalabanov
    
    fbshipit-source-id: e5fa5c4

--- scripts/migrations/dump.py
+++ scripts/migrations/dump.py
@@ -7,9 +7,21 @@ import argparse
 import os
 
 
+class StoreKeyValuePair(argparse.Action):
+    """Parser action that populates a dictionary with '=' separated key-value pairs."""
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        key, value = values.split('=')
+        dest_dict = {}
+        if hasattr(namespace, self.dest) and getattr(namespace, self.dest):
+            dest_dict = getattr(namespace, self.dest)
+        dest_dict[key] = value
+        setattr(namespace, self.dest, dest_dict)
+
+
 def dump_exported_symbols(args):
     bf = build_file.from_path(args.build_file)
-    repo = repository.Repository({})
+    repo = repository.Repository(args.cell_roots)
     symbols = bf.get_exported_symbols_transitive_closure(repo)
     if args.json:
         import json
@@ -18,14 +30,35 @@ def dump_exported_symbols(args):
         print(os.linesep.join(symbols))
 
 
+def dump_export_map(args):
+    """Prints export map that includes all included definitions and symbols they export."""
+    bf = build_file.from_path(args.build_file)
+    repo = repository.Repository(args.cell_roots)
+    export_map = bf.get_export_map(repo)
+    if args.json:
+        import json
+        print(json.dumps(export_map))
+    else:
+        for import_string, exported_symbols in export_map.items():
+            print(import_string + ':')
+            for exported_symbol in exported_symbols:
+                print(' ' * 2 + exported_symbol)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Dumps requested build file information.')
     subparsers = parser.add_subparsers()
 
     exported_symbols_parser = subparsers.add_parser('exported_symbols')
     exported_symbols_parser.set_defaults(func=dump_exported_symbols)
+
+    export_map_parser = subparsers.add_parser('export_map')
+    export_map_parser.set_defaults(func=dump_export_map)
+
     parser.add_argument('build_file', metavar='FILE')
     parser.add_argument('--json', action='store_true')
+    parser.add_argument('--cell_root', action=StoreKeyValuePair, metavar='CELL=PATH',
+                        dest='cell_roots')
     args = parser.parse_args()
     args.func(args)
 
