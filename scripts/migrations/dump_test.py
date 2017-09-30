7fea769780bb18d9b9d1efb5f6e6ce9351d11372
Taras Tsugrii <ttsugrii@fb.com>
2017-09-30 13:47:18 -0700

    Refactor dump script test
    Summary: This is in preparation to introducing a repository flag.
    
    Test Plan: python3 -m unittest *_test.py
    
    Reviewed By: styurin
    
    fbshipit-source-id: 5e06a7c

--- scripts/migrations/dump_test.py
+++ scripts/migrations/dump_test.py
@@ -3,30 +3,33 @@ import os
 import subprocess
 import tempfile
 
+from typing import Any
+
 
 class DumpTest(unittest.TestCase):
     SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+    def _dump(self, *args: Any) -> str:
+        return subprocess.check_output(
+            [os.path.join(self.SCRIPT_DIR, 'dump.py')] + list(args)).decode()
 
     def test_can_dump_exported_symbols_in_plain_text(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             build_file_path = os.path.join(temp_dir, 'BUCK')
             with open(build_file_path, 'w') as build_file:
                 build_file.write('foo = "FOO"')
-            output = subprocess.check_output(
-                [os.path.join(self.SCRIPT_DIR, 'dump.py'), 'exported_symbols', build_file_path])
+            output = self._dump('exported_symbols', build_file_path)
 
-        self.assertEqual(b'foo', output.strip())
+        self.assertEqual('foo', output.strip())
 
     def test_can_dump_exported_symbols_in_json(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             build_file_path = os.path.join(temp_dir, 'BUCK')
             with open(build_file_path, 'w') as build_file:
                 build_file.write('foo = "FOO"')
-            output = subprocess.check_output(
-                [os.path.join(self.SCRIPT_DIR, 'dump.py'), '--json', 'exported_symbols',
-                 build_file_path])
+            output = self._dump('--json', 'exported_symbols', build_file_path)
 
-        self.assertEqual(b'["foo"]', output.strip())
+        self.assertEqual('["foo"]', output.strip())
 
     def test_can_dump_export_map_in_plain_text(self):
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -35,11 +38,9 @@ class DumpTest(unittest.TestCase):
                 build_file.write('include_defs("cell//DEFS")')
             with open(os.path.join(temp_dir, 'DEFS'), 'w') as defs_file:
                 defs_file.write('foo = "FOO"')
-            output = subprocess.check_output(
-                [os.path.join(self.SCRIPT_DIR, 'dump.py'), '--cell_root', 'cell=' + temp_dir,
-                 'export_map', build_file_path])
+            output = self._dump('--cell_root', 'cell=' + temp_dir, 'export_map', build_file_path)
 
-        self.assertEqual(b"cell//DEFS:\n  foo", output.strip())
+        self.assertEqual("cell//DEFS:\n  foo", output.strip())
 
     def test_can_dump_export_map_in_json(self):
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -48,11 +49,10 @@ class DumpTest(unittest.TestCase):
                 build_file.write('include_defs("cell//DEFS")')
             with open(os.path.join(temp_dir, 'DEFS'), 'w') as defs_file:
                 defs_file.write('foo = "FOO"')
-            output = subprocess.check_output(
-                [os.path.join(self.SCRIPT_DIR, 'dump.py'), '--json', '--cell_root',
-                 'cell=' + temp_dir, 'export_map', build_file_path])
+            output = self._dump('--json', '--cell_root',
+                                'cell=' + temp_dir, 'export_map', build_file_path)
 
-        self.assertEqual(b'{"cell//DEFS": ["foo"]}', output.strip())
+        self.assertEqual('{"cell//DEFS": ["foo"]}', output.strip())
 
     def test_can_dump_export_map_as_load_functions_in_plain_text(self):
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -61,11 +61,10 @@ class DumpTest(unittest.TestCase):
                 build_file.write('include_defs("cell//DEFS")')
             with open(os.path.join(temp_dir, 'DEFS'), 'w') as defs_file:
                 defs_file.write('foo = "FOO"')
-            output = subprocess.check_output(
-                [os.path.join(self.SCRIPT_DIR, 'dump.py'), '--cell_root', 'cell=' + temp_dir,
-                 'export_map', '--print_as_load_functions', build_file_path])
+            output = self._dump('--cell_root', 'cell=' + temp_dir,
+                                'export_map', '--print_as_load_functions', build_file_path)
 
-        self.assertEqual(b'load("@cell//:DEFS", "foo")', output.strip())
+        self.assertEqual('load("@cell//:DEFS", "foo")', output.strip())
 
     def test_can_dump_export_map_as_load_functions_in_json(self):
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -74,8 +73,8 @@ class DumpTest(unittest.TestCase):
                 build_file.write('include_defs("cell//DEFS")')
             with open(os.path.join(temp_dir, 'DEFS'), 'w') as defs_file:
                 defs_file.write('foo = "FOO"')
-            output = subprocess.check_output(
-                [os.path.join(self.SCRIPT_DIR, 'dump.py'), '--json', '--cell_root',
-                 'cell=' + temp_dir, 'export_map', '--print_as_load_functions', build_file_path])
+            output = self._dump('--json', '--cell_root',
+                                'cell=' + temp_dir, 'export_map', '--print_as_load_functions',
+                                build_file_path)
 
-        self.assertEqual(b'["load(\\"@cell//:DEFS\\", \\"foo\\")"]', output.strip())
+        self.assertEqual('["load(\\"@cell//:DEFS\\", \\"foo\\")"]', output.strip())
