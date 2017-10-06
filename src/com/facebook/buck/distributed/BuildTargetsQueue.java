3bb07d1a95f6bd2c82c6d1b16a5e30e9df2fa1eb
Alisdair Johnstone <alisdair@fb.com>
2017-10-05 19:04:45 -0700

    Reverse dependency graph now takes into account runtime dependencies
    Summary:
    The existing behavior ignores runtime dependencies, which results in rules being built multiple times by different minions.
    
    Additionally added safety checks to ensure that any dependencies counted down are genuine dependencies and also not duplicated.
    
    Test Plan: added new unit tests
    
    Reviewed By: shivanker
    
    fbshipit-source-id: 7e2d515

--- src/com/facebook/buck/distributed/BuildTargetsQueue.java
+++ src/com/facebook/buck/distributed/BuildTargetsQueue.java
@@ -15,12 +15,18 @@
  */
 package com.facebook.buck.distributed;
 
+import com.facebook.buck.log.Logger;
 import com.facebook.buck.model.BuildTarget;
 import com.facebook.buck.rules.BuildRule;
 import com.facebook.buck.rules.BuildRuleResolver;
+import com.facebook.buck.rules.HasRuntimeDeps;
+import com.facebook.buck.rules.SourcePathRuleFinder;
+import com.facebook.buck.util.MoreCollectors;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,8 +35,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class BuildTargetsQueue {
+  private static final Logger LOG = Logger.get(BuildTargetsQueue.class);
+
   private List<EnqueuedTarget> zeroDependencyTargets;
   private final Map<String, EnqueuedTarget> allEnqueuedTargets;
 
@@ -46,9 +55,10 @@ public class BuildTargetsQueue {
 
   public static BuildTargetsQueue newQueue(
       BuildRuleResolver resolver, Iterable<BuildTarget> targetsToBuild) {
+    SourcePathRuleFinder ruleFinder = new SourcePathRuleFinder(resolver);
     // Build the reverse dependency graph by traversing the action graph Top-Down.
     Map<String, Set<String>> allReverseDeps = new HashMap<>();
-    Map<String, Integer> numberOfDependencies = new HashMap<>();
+    Map<String, Set<String>> allForwardDeps = new HashMap<>();
     Set<String> visitedTargets = new HashSet<>();
     Queue<BuildRule> buildRulesToProcess =
         Lists.newLinkedList(
@@ -61,14 +71,34 @@ public class BuildTargetsQueue {
                     }));
     while (!buildRulesToProcess.isEmpty()) {
       BuildRule rule = buildRulesToProcess.remove();
+
       String target = ruleToTarget(rule);
-      numberOfDependencies.put(target, rule.getBuildDeps().size());
-      for (BuildRule dependencyRule : rule.getBuildDeps()) {
+      allForwardDeps.put(target, new HashSet<>());
+
+      ImmutableSortedSet.Builder<BuildRule> allDependencies = ImmutableSortedSet.naturalOrder();
+
+      // Get all standard build dependencies
+      allDependencies.addAll(rule.getBuildDeps());
+
+      // Optionally add in any run-time deps
+      if (rule instanceof HasRuntimeDeps) {
+        LOG.debug(String.format("[%s] has runtime deps", rule.getFullyQualifiedName()));
+
+        Stream<BuildTarget> runtimeDepPaths = ((HasRuntimeDeps) rule).getRuntimeDeps(ruleFinder);
+        ImmutableSet<BuildRule> runtimeDeps =
+            resolver.getAllRules(runtimeDepPaths.collect(MoreCollectors.toImmutableSet()));
+
+        allDependencies.addAll(runtimeDeps);
+      }
+
+      for (BuildRule dependencyRule : allDependencies.build()) {
         String dependencyTarget = ruleToTarget(dependencyRule);
+
         if (!allReverseDeps.containsKey(dependencyTarget)) {
           allReverseDeps.put(dependencyTarget, new HashSet<>());
         }
         allReverseDeps.get(dependencyTarget).add(target);
+        allForwardDeps.get(target).add(dependencyTarget);
 
         if (!visitedTargets.contains(dependencyTarget)) {
           visitedTargets.add(dependencyTarget);
@@ -92,7 +122,8 @@ public class BuildTargetsQueue {
           new EnqueuedTarget(
               target,
               ImmutableList.copyOf(currentRevDeps),
-              Preconditions.checkNotNull(numberOfDependencies.get(target)));
+              allForwardDeps.get(target).size(),
+              ImmutableSet.copyOf(allForwardDeps.get(target)));
       allEnqueuedTargets.put(target, enqueuedTarget);
 
       if (enqueuedTarget.areAllDependenciesResolved()) {
@@ -109,7 +140,7 @@ public class BuildTargetsQueue {
       EnqueuedTarget target = Preconditions.checkNotNull(allEnqueuedTargets.get(node));
       for (String dependent : target.getDependentTargets()) {
         EnqueuedTarget dep = Preconditions.checkNotNull(allEnqueuedTargets.get(dependent));
-        dep.decrementUnsatisfiedDeps();
+        dep.decrementUnsatisfiedDeps(node);
         if (dep.areAllDependenciesResolved()) {
           zeroDependencyTargets.add(dep);
         }
@@ -131,13 +162,20 @@ public class BuildTargetsQueue {
   private static class EnqueuedTarget {
     private final String buildTarget;
     private final ImmutableList<String> dependentTargets;
+    private final Set<String> allDependencies;
+    private final Set<String> dependenciesRemaining;
     private int unsatisfiedDependencies;
 
     private EnqueuedTarget(
-        String buildTarget, ImmutableList<String> dependentTargets, int numberOfDependencies) {
+        String buildTarget,
+        ImmutableList<String> dependentTargets,
+        int numberOfDependencies,
+        ImmutableSet<String> dependenciesRemaining) {
       this.buildTarget = buildTarget;
       this.dependentTargets = dependentTargets;
       this.unsatisfiedDependencies = numberOfDependencies;
+      this.dependenciesRemaining = new HashSet<>(dependenciesRemaining);
+      this.allDependencies = new HashSet<>(dependenciesRemaining);
     }
 
     public boolean areAllDependenciesResolved() {
@@ -152,7 +190,21 @@ public class BuildTargetsQueue {
       return dependentTargets;
     }
 
-    public void decrementUnsatisfiedDeps() {
+    public void decrementUnsatisfiedDeps(String dependency) {
+      if (!dependenciesRemaining.contains(dependency)) {
+        boolean isActualDependency = allDependencies.contains(dependency);
+        String errorMessage =
+            String.format(
+                "[%s] is not a remaining dependency of [%s]. Is real dependency [%s]",
+                dependency, buildTarget, isActualDependency);
+        LOG.error(errorMessage);
+        throw new RuntimeException(errorMessage);
+      }
+
+      LOG.debug(
+          String.format(
+              "Removing [%s] from remaining dependencies for [%s]", dependency, buildTarget));
+      dependenciesRemaining.remove(dependency);
       --unsatisfiedDependencies;
       Preconditions.checkArgument(
           unsatisfiedDependencies >= 0,
