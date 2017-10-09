552c0b0b98ff9bf2d7c8753dc91ec3ec8eb48910
David Reiss <dreiss@fb.com>
2017-10-09 13:43:59 -0700

    Use R.txt from aapt2
    Summary:
    Previous versions of aapt2 did not generate R.txt, so we had to make it
    ourselves by parsing R.java.  Current versions support R.txt directly,
    so use that and delete all of the conversion code.
    
    Test Plan: R.txt for FB4A is identical other than whitespace.
    
    fbshipit-source-id: b85644b

--- src/com/facebook/buck/android/Aapt2Link.java
+++ src/com/facebook/buck/android/Aapt2Link.java
@@ -16,7 +16,6 @@
 
 package com.facebook.buck.android;
 
-import com.android.annotations.VisibleForTesting;
 import com.facebook.buck.io.BuildCellRelativePath;
 import com.facebook.buck.io.filesystem.ProjectFilesystem;
 import com.facebook.buck.model.BuildTarget;
@@ -33,29 +32,21 @@ import com.facebook.buck.rules.coercer.ManifestEntries;
 import com.facebook.buck.shell.ShellStep;
 import com.facebook.buck.step.ExecutionContext;
 import com.facebook.buck.step.Step;
-import com.facebook.buck.step.StepExecutionResult;
 import com.facebook.buck.step.fs.MakeCleanDirectoryStep;
 import com.facebook.buck.step.fs.RmStep;
 import com.facebook.buck.step.fs.SymlinkTreeStep;
 import com.facebook.buck.util.RichStream;
 import com.facebook.buck.zip.ZipScrubberStep;
-import com.google.common.base.Charsets;
-import com.google.common.base.Joiner;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Lists;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.annotation.Nullable;
 
 /** Perform the "aapt2 link" step of building an Android app. */
@@ -149,9 +140,6 @@ public class Aapt2Link extends AbstractBuildRule {
         new Aapt2LinkStep(getProjectFilesystem().resolve(linkTreePath), symlinkPaths.build()));
     steps.add(ZipScrubberStep.of(getProjectFilesystem().resolve(getResourceApkPath())));
 
-    steps.add(
-        new MakeRDotTxtStep(getProjectFilesystem(), getInitialRDotJavaDir(), getRDotTxtPath()));
-
     buildableContext.recordArtifact(getFinalManifestPath());
     buildableContext.recordArtifact(getResourceApkPath());
     buildableContext.recordArtifact(getProguardConfigPath());
@@ -238,142 +226,14 @@ public class Aapt2Link extends AbstractBuildRule {
       builder.add("--proguard", pf.resolve(getProguardConfigPath()).toString());
       builder.add("--manifest", pf.resolve(getFinalManifestPath()).toString());
       builder.add("-I", pf.resolve(androidPlatformTarget.getAndroidJar()).toString());
+      // We don't need the R.java output, but aapt2 won't output R.txt
+      // unless we also request R.java.
       builder.add("--java", pf.resolve(getInitialRDotJavaDir()).toString());
-      // Generate a custom-package R.java just for the purpose of generating R.txt.
-      builder.add("--custom-package", "make.r.txt");
+      builder.add("--output-text-symbols", pf.resolve(getRDotTxtPath()).toString());
 
       compiledResourcePaths.forEach(r -> builder.add("-R", r.toString()));
 
       return builder.build();
-    }
-  }
-
-  @VisibleForTesting
-  static class MakeRDotTxtStep implements Step {
-    private static final Pattern NO_OP_LINE =
-        Pattern.compile(
-            "^package |"
-                + // Don't care about package.
-                "^public final class R \\{|"
-                + // Don't care about top-level class.
-                "^[/*]" // Comment.
-            );
-    private static final Pattern CLASS_LINE =
-        Pattern.compile("public static final class (\\w+) \\{");
-    private static final Pattern INT_LINE =
-        Pattern.compile("public static final int (\\w+)=(\\w+);");
-    private static final Pattern ARRAY_LINE =
-        Pattern.compile("public static final int\\[\\] (\\w+)=\\{");
-    private static final Pattern VALUES_LINE = Pattern.compile("[x0-9a-f, ]*");
-
-    private final ProjectFilesystem projectFilesystem;
-    private final Path initialRDotJavaDir;
-    private final Path rDotTxtPath;
-
-    public MakeRDotTxtStep(
-        ProjectFilesystem projectFilesystem, Path initialRDotJavaDir, Path rDotTxtPath) {
-      this.projectFilesystem = projectFilesystem;
-      this.initialRDotJavaDir = initialRDotJavaDir;
-      this.rDotTxtPath = rDotTxtPath;
-    }
-
-    @Override
-    public StepExecutionResult execute(ExecutionContext context)
-        throws IOException, InterruptedException {
-      List<String> javaLines =
-          projectFilesystem.readLines(initialRDotJavaDir.resolve("make/r/txt/R.java"));
-      List<String> txtLines = convertLines(javaLines);
-      projectFilesystem.writeLinesToPath(txtLines, rDotTxtPath);
-      return StepExecutionResult.SUCCESS;
-    }
-
-    @VisibleForTesting
-    static List<String> convertLines(List<String> javaLines) {
-      ImmutableList.Builder<String> txtLines = ImmutableList.builder();
-
-      @Nullable String inClass = null;
-      @Nullable String currentArrayName = null;
-      @Nullable List<String> currentArrayValues = null;
-
-      for (String line : javaLines) {
-        line = line.trim();
-
-        if (line.isEmpty() || NO_OP_LINE.matcher(line).lookingAt()) {
-          continue;
-        }
-
-        if (currentArrayName != null) {
-
-          if (line.equals("};")) {
-            txtLines.add(
-                String.format(
-                    "int[] %s %s { %s }",
-                    inClass, currentArrayName, Joiner.on(',').join(currentArrayValues)));
-            currentArrayName = null;
-            currentArrayValues = null;
-            continue;
-          }
-
-          if (VALUES_LINE.matcher(line).matches()) {
-            RichStream.of(line.split(","))
-                .map(String::trim)
-                .filter(s -> !s.isEmpty())
-                .forEach(currentArrayValues::add);
-            continue;
-          }
-
-          throw new IllegalArgumentException("Got unexpected line in array data: " + line);
-        }
-
-        Matcher classMatch = CLASS_LINE.matcher(line);
-        if (classMatch.matches()) {
-          inClass = classMatch.group(1);
-          continue;
-        }
-
-        if (line.equals("}")) {
-          inClass = null;
-          continue;
-        }
-
-        if (inClass == null) {
-          throw new IllegalArgumentException("Got unexpected line with no active class: " + line);
-        }
-
-        Matcher intMatch = INT_LINE.matcher(line);
-        if (intMatch.matches()) {
-          txtLines.add(
-              String.format("int %s %s %s", inClass, intMatch.group(1), intMatch.group(2)));
-          continue;
-        }
-
-        Matcher arrayMatch = ARRAY_LINE.matcher(line);
-        if (arrayMatch.matches()) {
-          currentArrayName = arrayMatch.group(1);
-          currentArrayValues = new ArrayList<>();
-          continue;
-        }
-
-        throw new IllegalArgumentException("Got unexpected line: " + line);
-      }
-
-      return txtLines.build();
-    }
-
-    public static void main(String[] args) throws IOException {
-      List<String> javaLines = Files.readAllLines(Paths.get(args[0]), Charsets.UTF_8);
-      List<String> txtLines = convertLines(javaLines);
-      txtLines.forEach(System.out::println);
-    }
-
-    @Override
-    public String getShortName() {
-      return "make_r_dot_txt";
-    }
-
-    @Override
-    public String getDescription(ExecutionContext context) {
-      return String.format("make_r_dot_txt %s > %s", initialRDotJavaDir, rDotTxtPath);
     }
   }
 }
