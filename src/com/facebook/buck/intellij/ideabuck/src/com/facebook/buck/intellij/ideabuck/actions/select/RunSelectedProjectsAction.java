8c682938778124e28ca858c43cba3e5eb8b4a1fe
Ryan Sonnenberg <ryanson@uber.com>
2017-10-13 12:48:59 -0700

    Add support to right click and generate projects.

---
+++ src/com/facebook/buck/intellij/ideabuck/src/com/facebook/buck/intellij/ideabuck/actions/select/RunSelectedProjectsAction.java
@@ -0,0 +1,60 @@
+package com.facebook.buck.intellij.ideabuck.actions.select;
+
+import com.facebook.buck.intellij.ideabuck.build.BuckBuildCommandHandler;
+import com.facebook.buck.intellij.ideabuck.build.BuckBuildManager;
+import com.facebook.buck.intellij.ideabuck.build.BuckCommand;
+import com.facebook.buck.intellij.ideabuck.config.BuckModule;
+import com.intellij.ide.IdeView;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDirectory;
+
+/**
+ * Action to generate projects from the project tool view.
+ */
+public class RunSelectedProjectsAction extends AnAction {
+
+  @Override
+  public void actionPerformed(AnActionEvent anActionEvent) {
+    final Project project = anActionEvent.getProject();
+    if (project == null) {
+      return;
+    }
+
+    BuckModule buckModule = project.getComponent(BuckModule.class);
+    DataContext dataContext = anActionEvent.getDataContext();
+    final IdeView view = LangDataKeys.IDE_VIEW.getData(dataContext);
+    if (view == null) {
+      return;
+    }
+
+    final PsiDirectory directory = view.getOrChooseDirectory();
+    if (directory == null) {
+      return;
+    }
+
+    String directoryString = directory.getVirtualFile().getPath();
+    String basepath = project.getBasePath();
+    if (basepath == null) {
+      return;
+    }
+
+    String relative = directoryString.replace(basepath, "");
+    String target = "/" + relative + "/...";
+
+    BuckBuildCommandHandler handler =
+        new BuckBuildCommandHandler(project, project.getBaseDir(), BuckCommand.PROJECT);
+    handler.command().addParameter(target);
+    handler.command().addParameter("--ide");
+    handler.command().addParameter("INTELLIJ");
+
+    BuckBuildManager buildManager = BuckBuildManager.getInstance(project);
+    buildManager.setBuilding(project, true);
+    buckModule.attach(target);
+    buildManager.runBuckCommand(handler, "Running buck project on selected groups.");
+    buildManager.setBuilding(project, false);
+  }
+}
