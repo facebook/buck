d98df0d680ab406f442776dbc0ff1ec33c992873
Philip Jameson <pjameson@fb.com>
2017-10-16 13:29:41 -0700

    Add rulekey file parser for consistency checker
    Summary: This adds a simple rulekey file parser for the consistency checker
    
    Test Plan: Added unit tests + CI
    
    Reviewed By: styurin
    
    fbshipit-source-id: dc79a64

---
+++ tools/consistency_checker/src/com/facebook/buck/tools/consistency/RuleKeyFileParser.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.tools.consistency;
+
+import com.facebook.buck.log.thrift.rulekeys.FullRuleKey;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.thrift.TDeserializer;
+import org.apache.thrift.protocol.TCompactProtocol;
+
+/** A class that can parse out length prefixed, thrift compact encoded rule keys. */
+public class RuleKeyFileParser {
+
+  /**
+   * A node in the set of all rule keys. When parsing, it tracks whether the node has been visited
+   * yet or not.
+   */
+  static class RuleKeyNode {
+
+    public final FullRuleKey ruleKey;
+    public boolean visited = false;
+
+    public RuleKeyNode(FullRuleKey ruleKey) {
+      this.ruleKey = ruleKey;
+    }
+  }
+
+  /** A parsed rule key file with details of the parse, and all rules contained in the file. */
+  static class ParsedFile {
+
+    public final String filename;
+    public final String rootHash;
+    public final Map<String, RuleKeyNode> rules;
+    public final Duration parseTime;
+
+    public ParsedFile(
+        String filename, String rootHash, Map<String, RuleKeyNode> rules, Duration parseTime) {
+      this.filename = filename;
+      this.rootHash = rootHash;
+      this.rules = rules;
+      this.parseTime = parseTime;
+    }
+  }
+
+  /**
+   * Represents an error that occurred when parsing the log file. Wraps any internal exceptions such
+   * as IO exceptions and thrift errors.
+   */
+  public class ParseException extends Exception {
+
+    public ParseException(String formatString, Object... formatArgs) {
+      super(String.format(formatString, formatArgs));
+    }
+
+    public ParseException(Throwable originalException, String formatString, Object... formatArgs) {
+      super(String.format(formatString, formatArgs), originalException);
+    }
+  }
+
+  /** Arbitrary estimate of an average rule key size when serialized */
+  public final int THRIFT_STRUCT_SIZE = 300;
+
+  /**
+   * Parse a thrift compact serialized file
+   *
+   * @param filename The name of the file
+   * @param targetName The name of the target that should be found
+   * @return A {@link ParsedFile} object that all deserialized rules, and the rule key hash of the
+   *     specified target
+   * @throws ParseException If an IO or serialization error occurs, or if the target could not be
+   *     found in the file
+   */
+  public ParsedFile parseFile(String filename, String targetName) throws ParseException {
+    long startNanos = System.nanoTime();
+
+    HashMap<String, RuleKeyNode> ret = null;
+    String rootHashKey = "";
+
+    ByteBuffer lengthBuf = ByteBuffer.allocate(4);
+    try (FileInputStream fileInputStream = new FileInputStream(filename)) {
+      ret = new HashMap<>(fileInputStream.available() / THRIFT_STRUCT_SIZE);
+
+      while (fileInputStream.available() >= 4) {
+        fileInputStream.read(lengthBuf.array());
+        int length = lengthBuf.getInt();
+        lengthBuf.rewind();
+
+        byte[] serialized = new byte[length];
+        int bytesRead = fileInputStream.read(serialized);
+        if (bytesRead != length) {
+          throw new ParseException(
+              "Invalid length specified. Expected %s bytes, only got %s", length, bytesRead);
+        }
+        TDeserializer deserializer = new TDeserializer(new TCompactProtocol.Factory());
+        FullRuleKey ruleKey = new FullRuleKey();
+        deserializer.deserialize(ruleKey, serialized);
+        // The thrift deserializer doesn't blow up on invalid data, it just sets all fields to
+        // null. 'key' is required, so if it's null, we failed to deserialize. Yes, deserialize()
+        // /should/ throw a TException, but it doesn't.
+        if (ruleKey.key == null) {
+          throw new ParseException("Could not deserialize array of size %s", serialized.length);
+        }
+        if ("DEFAULT".equals(ruleKey.type) && targetName.equals(ruleKey.name)) {
+          rootHashKey = ruleKey.key;
+        }
+        ret.put(ruleKey.key, new RuleKeyNode(ruleKey));
+      }
+    } catch (Exception e) {
+      throw new ParseException(e, "Error reading %s: %s", filename, e);
+    }
+    if (rootHashKey.isEmpty()) {
+      throw new ParseException("Could not find %s in %s", targetName, filename);
+    }
+    Duration runtime = Duration.ofNanos(System.nanoTime() - startNanos);
+
+    return new ParsedFile(filename, rootHashKey, ret, runtime);
+  }
+}
---
+++ tools/consistency_checker/test/com/facebook/buck/tools/consistency/RuleKeyFileParserTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.tools.consistency;
+
+import com.facebook.buck.log.thrift.ThriftRuleKeyLogger;
+import com.facebook.buck.log.thrift.rulekeys.FullRuleKey;
+import com.facebook.buck.testutil.integration.TemporaryPaths;
+import com.facebook.buck.tools.consistency.RuleKeyFileParser.ParseException;
+import com.facebook.buck.tools.consistency.RuleKeyFileParser.ParsedFile;
+import com.google.common.collect.ImmutableMap;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class RuleKeyFileParserTest {
+
+  @Rule public TemporaryPaths temporaryFolder = new TemporaryPaths();
+
+  private String logPath;
+
+  @Before
+  public void setUp() throws InterruptedException, IOException {
+    logPath = temporaryFolder.newFile("out.bin.log").toAbsolutePath().toString();
+  }
+
+  @Test
+  public void parsesFileProperly() throws FileNotFoundException, ParseException {
+    FullRuleKey ruleKey1 = new FullRuleKey("key1", "//:name1", "DEFAULT", ImmutableMap.of());
+    FullRuleKey ruleKey2 = new FullRuleKey("key2", "//:name1", "other_type", ImmutableMap.of());
+    FullRuleKey ruleKey3 = new FullRuleKey("key3", "//:name2", "OTHER", ImmutableMap.of());
+    try (ThriftRuleKeyLogger logger = ThriftRuleKeyLogger.create(logPath)) {
+      logger.write(ruleKey1);
+      logger.write(ruleKey2);
+      logger.write(ruleKey3);
+    }
+
+    RuleKeyFileParser parser = new RuleKeyFileParser();
+    ParsedFile parsedFile = parser.parseFile(logPath, "//:name1");
+
+    Assert.assertEquals("key1", parsedFile.rootHash);
+    Assert.assertEquals(logPath, parsedFile.filename);
+    Assert.assertTrue(parsedFile.parseTime.toNanos() > 0);
+    Assert.assertEquals(3, parsedFile.rules.size());
+    Assert.assertEquals(ruleKey1, parsedFile.rules.get("key1").ruleKey);
+    Assert.assertEquals(ruleKey2, parsedFile.rules.get("key2").ruleKey);
+    Assert.assertEquals(ruleKey3, parsedFile.rules.get("key3").ruleKey);
+  }
+
+  @Test(expected = ParseException.class)
+  public void throwsIfTargetNotFound() throws FileNotFoundException, ParseException {
+    FullRuleKey ruleKey1 = new FullRuleKey("key1", "//:name1", "DEFAULT", ImmutableMap.of());
+    try (ThriftRuleKeyLogger logger = ThriftRuleKeyLogger.create(logPath)) {
+      logger.write(ruleKey1);
+    }
+
+    RuleKeyFileParser parser = new RuleKeyFileParser();
+    parser.parseFile(logPath, "//:invalid_name");
+  }
+
+  @Test(expected = ParseException.class)
+  public void throwsOnBadLength() throws ParseException, IOException {
+    FullRuleKey ruleKey1 = new FullRuleKey("key1", "//:name1", "DEFAULT", ImmutableMap.of());
+    try (ThriftRuleKeyLogger logger = ThriftRuleKeyLogger.create(logPath)) {
+      logger.write(ruleKey1);
+    }
+    try (DataOutputStream outputStream =
+        new DataOutputStream(new FileOutputStream(logPath, true))) {
+      outputStream.writeInt(1234);
+      outputStream.flush();
+    }
+
+    RuleKeyFileParser parser = new RuleKeyFileParser();
+    parser.parseFile(logPath, "//:name1");
+  }
+
+  @Test(expected = ParseException.class)
+  public void throwsOnDeserializationError() throws ParseException, IOException {
+    FullRuleKey ruleKey1 = new FullRuleKey("key1", "//:name1", "DEFAULT", ImmutableMap.of());
+    try (ThriftRuleKeyLogger logger = ThriftRuleKeyLogger.create(logPath)) {
+      logger.write(ruleKey1);
+    }
+    try (DataOutputStream outputStream =
+        new DataOutputStream(new FileOutputStream(logPath, true))) {
+      outputStream.writeInt(8);
+      outputStream.writeInt(1234);
+      outputStream.writeInt(2345);
+      outputStream.flush();
+    }
+
+    RuleKeyFileParser parser = new RuleKeyFileParser();
+    parser.parseFile(logPath, "//:name1");
+  }
+}
