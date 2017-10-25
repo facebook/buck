a2416bd8c055ea5e1ca693679d054aac462a0c09
Taras Tsugrii <ttsugrii@fb.com>
2017-10-24 22:36:35 -0700

    Move NativeModule class to functions package
    Summary: It's not part of build file parser, so shouldn't be there.
    
    Test Plan: CI
    
    Reviewed By: styurin
    
    fbshipit-source-id: 5bfb94c

--- src/com/facebook/buck/skylark/parser/SkylarkProjectBuildFileParser.java
+++ src/com/facebook/buck/skylark/parser/SkylarkProjectBuildFileParser.java
@@ -16,7 +16,6 @@
 
 package com.facebook.buck.skylark.parser;
 
-import bazel.shaded.com.google.common.collect.ImmutableCollection;
 import com.facebook.buck.event.BuckEventBus;
 import com.facebook.buck.io.file.MorePaths;
 import com.facebook.buck.log.Logger;
@@ -29,30 +28,25 @@ import com.facebook.buck.rules.coercer.CoercedTypeCache;
 import com.facebook.buck.rules.coercer.ParamInfo;
 import com.facebook.buck.rules.coercer.TypeCoercerFactory;
 import com.facebook.buck.skylark.function.Glob;
+import com.facebook.buck.skylark.function.NativeModule;
 import com.facebook.buck.skylark.io.impl.SimpleGlobber;
 import com.facebook.buck.skylark.packages.PackageContext;
 import com.facebook.buck.skylark.packages.PackageFactory;
 import com.facebook.buck.util.MoreCollectors;
 import com.google.common.base.CaseFormat;
-import com.google.common.base.Joiner;
-import com.google.common.base.Objects;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
-import com.google.common.collect.Ordering;
 import com.google.devtools.build.lib.cmdline.Label;
 import com.google.devtools.build.lib.cmdline.PackageIdentifier;
 import com.google.devtools.build.lib.cmdline.RepositoryName;
 import com.google.devtools.build.lib.events.EventKind;
 import com.google.devtools.build.lib.events.PrintingEventHandler;
-import com.google.devtools.build.lib.skylarkinterface.SkylarkPrinter;
-import com.google.devtools.build.lib.skylarkinterface.SkylarkValue;
 import com.google.devtools.build.lib.syntax.BazelLibrary;
 import com.google.devtools.build.lib.syntax.BuildFileAST;
 import com.google.devtools.build.lib.syntax.BuiltinFunction;
-import com.google.devtools.build.lib.syntax.ClassObject;
 import com.google.devtools.build.lib.syntax.Environment;
 import com.google.devtools.build.lib.syntax.EvalException;
 import com.google.devtools.build.lib.syntax.FuncallExpression;
@@ -65,10 +59,7 @@ import com.google.devtools.build.lib.vfs.FileSystem;
 import com.google.devtools.build.lib.vfs.PathFragment;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.EnumSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
@@ -436,90 +427,6 @@ public class SkylarkProjectBuildFileParser implements ProjectBuildFileParser {
   @Override
   public void close() throws BuildFileParseException, InterruptedException, IOException {
     // nothing to do
-  }
-
-  /**
-   * Represents a {@code native} variable available in Skylark extension files. It's responsible for
-   * handling calls like {@code native.java_library(...)} in {@code .bzl} files.
-   */
-  private static class NativeModule implements ClassObject, SkylarkValue {
-    private final ImmutableMap<String, BuiltinFunction> buckRuleFunctionRegistry;
-
-    private NativeModule(
-        ImmutableList<BuiltinFunction> buckRuleFunctions, BuiltinFunction... otherFunctions) {
-      ImmutableMap.Builder<String, BuiltinFunction> registryBuilder = ImmutableMap.builder();
-      for (BuiltinFunction buckRuleFunction : buckRuleFunctions) {
-        registryBuilder.put(buckRuleFunction.getName(), buckRuleFunction);
-      }
-      for (BuiltinFunction builtinFunction : otherFunctions) {
-        registryBuilder.put(builtinFunction.getName(), builtinFunction);
-      }
-      buckRuleFunctionRegistry = registryBuilder.build();
-    }
-
-    @Nullable
-    @Override
-    public BuiltinFunction getValue(String name) {
-      return buckRuleFunctionRegistry.get(name);
-    }
-
-    @Override
-    public ImmutableCollection<String> getKeys() {
-      // TODO(ttsugrii): Remove this unnecessary copying once guava version in Skylark and Buck match
-      return bazel.shaded.com.google.common.collect.ImmutableSet.copyOf(
-          buckRuleFunctionRegistry.keySet());
-    }
-
-    @Nullable
-    @Override
-    public String errorMessage(String name) {
-      String suffix =
-          "Available attributes: " + Joiner.on(", ").join(Ordering.natural().sortedCopy(getKeys()));
-      return "native object does not have an attribute " + name + "\n" + suffix;
-    }
-
-    @Override
-    public void repr(SkylarkPrinter printer) {
-      boolean first = true;
-      printer.append("struct(");
-      // Sort by key to ensure deterministic output.
-      for (String key : Ordering.natural().sortedCopy(getKeys())) {
-        if (!first) {
-          printer.append(", ");
-        }
-        first = false;
-        printer.append(key);
-        printer.append(" = ");
-        printer.repr(getValue(key));
-      }
-      printer.append(")");
-    }
-
-    @Override
-    public boolean isImmutable() {
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      List<String> keys = new ArrayList<>(getKeys());
-      Collections.sort(keys);
-      List<Object> objectsToHash = new ArrayList<>();
-      for (String key : keys) {
-        objectsToHash.add(key);
-        objectsToHash.add(getValue(key));
-      }
-      return Objects.hashCode(objectsToHash.toArray());
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof NativeModule)) {
-        return false;
-      }
-      NativeModule other = (NativeModule) obj;
-      return this == other || this.buckRuleFunctionRegistry.equals(other.buckRuleFunctionRegistry);
-    }
   }
 
   /**
