258d77b126d41ae014d5664db74efb1d9ae95322
Taras Tsugrii <ttsugrii@fb.com>
2017-10-30 13:55:07 -0700

    Extract parse lifecycle event tracking into separate class
    Summary:
    This logic should be shared by all top-level parsers used in Buck.
    Currently PythonDSL parser hard-codes this logic, which means that in case
    any other parser is used by Buck parse start and end will not be reported.
    This will be used in follow-up commit to start tracking parse time when
    hybrid parser is used.
    
    Test Plan: CI
    
    Reviewed By: styurin
    
    fbshipit-source-id: aafb813

---
+++ src/com/facebook/buck/parser/decorators/EventReportingProjectBuildFileParser.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.parser.decorators;
+
+import com.facebook.buck.event.BuckEventBus;
+import com.facebook.buck.json.ProjectBuildFileParseEvents;
+import com.facebook.buck.parser.api.ProjectBuildFileParser;
+import com.facebook.buck.parser.exceptions.BuildFileParseException;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.GuardedBy;
+
+/**
+ * Decorator for {@link ProjectBuildFileParser} that is responsible for reporting parse lifecycle
+ * events like start and finish.
+ *
+ * <p>This decouples status reporting functionality so that it can be used with different underlying
+ * {@link ProjectBuildFileParser}s.
+ */
+public class EventReportingProjectBuildFileParser implements ProjectBuildFileParser {
+
+  private final ProjectBuildFileParser delegate;
+  private final BuckEventBus eventBus;
+  private final Object eventLock;
+
+  @GuardedBy("eventLock")
+  @Nullable
+  private ProjectBuildFileParseEvents.Started projectBuildFileParseEventStarted;
+
+  private EventReportingProjectBuildFileParser(
+      ProjectBuildFileParser delegate, BuckEventBus eventBus) {
+    this.delegate = delegate;
+    this.eventBus = eventBus;
+    this.eventLock = new Object();
+  }
+
+  @Override
+  public ImmutableList<Map<String, Object>> getAll(Path buildFile, AtomicLong processedBytes)
+      throws BuildFileParseException, InterruptedException, IOException {
+    maybePostStartEvent();
+    return delegate.getAll(buildFile, processedBytes);
+  }
+
+  /** Possibly post a start event making sure it's done only once. */
+  private void maybePostStartEvent() {
+    synchronized (eventLock) {
+      if (projectBuildFileParseEventStarted == null) {
+        projectBuildFileParseEventStarted = new ProjectBuildFileParseEvents.Started();
+        eventBus.post(projectBuildFileParseEventStarted);
+      }
+    }
+  }
+
+  @Override
+  public ImmutableList<Map<String, Object>> getAllRulesAndMetaRules(
+      Path buildFile, AtomicLong processedBytes)
+      throws BuildFileParseException, InterruptedException, IOException {
+    maybePostStartEvent();
+    return delegate.getAllRulesAndMetaRules(buildFile, processedBytes);
+  }
+
+  @Override
+  public void reportProfile() throws IOException {
+    delegate.reportProfile();
+  }
+
+  @Override
+  public void close() throws BuildFileParseException, InterruptedException, IOException {
+    maybePostFinishedEvent();
+    delegate.close();
+  }
+
+  /** Post a finished event in case start event was posted previously. */
+  private void maybePostFinishedEvent() {
+    synchronized (eventLock) {
+      if (projectBuildFileParseEventStarted != null) {
+        eventBus.post(
+            new ProjectBuildFileParseEvents.Finished(
+                Preconditions.checkNotNull(projectBuildFileParseEventStarted)));
+      }
+    }
+  }
+
+  /**
+   * Static factory method for producing instances of {@link
+   * com.facebook.buck.parser.decorators.EventReportingProjectBuildFileParser}.
+   */
+  public static EventReportingProjectBuildFileParser of(
+      ProjectBuildFileParser delegate, BuckEventBus eventBus) {
+    return new EventReportingProjectBuildFileParser(delegate, eventBus);
+  }
+}
---
+++ test/com/facebook/buck/parser/decorators/EventReportingProjectBuildFileParserTest.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.parser.decorators;
+
+import static org.junit.Assert.*;
+
+import com.facebook.buck.event.BuckEventBus;
+import com.facebook.buck.event.BuckEventBusForTests;
+import com.facebook.buck.json.ProjectBuildFileParseEvents;
+import com.facebook.buck.parser.api.ProjectBuildFileParser;
+import com.google.common.collect.ImmutableList;
+import com.google.common.eventbus.Subscribe;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventReportingProjectBuildFileParserTest {
+
+  private static final Path SOME_PATH = Paths.get("some_path");
+  private TestProjectBuildFileParser delegate;
+
+  private EventReportingProjectBuildFileParser parser;
+  private ProjectBuildFileParseEventListener listener;
+  private AtomicLong processedBytes;
+  private ImmutableList<Map<String, Object>> allRules;
+  private ImmutableList<Map<String, Object>> allRulesAndMetadata;
+
+  private static class ProjectBuildFileParseEventListener {
+    private int startedCount;
+    private int finishedCount;
+
+    @Subscribe
+    public void onProjectBuildFileParseStarted(
+        @SuppressWarnings("unused") ProjectBuildFileParseEvents.Started started) {
+      startedCount += 1;
+    }
+
+    @Subscribe
+    public void onProjectBuildFileParseFinished(
+        @SuppressWarnings("unused") ProjectBuildFileParseEvents.Finished finished) {
+      finishedCount += 1;
+    }
+
+    public int getStartedCount() {
+      return startedCount;
+    }
+
+    public boolean isStarted() {
+      return startedCount > 0;
+    }
+
+    public boolean isFinished() {
+      return finishedCount > 0;
+    }
+  }
+
+  private class TestProjectBuildFileParser implements ProjectBuildFileParser {
+
+    private boolean isProfileReported;
+    private boolean isClosed;
+
+    @Override
+    public ImmutableList<Map<String, Object>> getAll(Path buildFile, AtomicLong processedBytes) {
+      return allRules;
+    }
+
+    @Override
+    public ImmutableList<Map<String, Object>> getAllRulesAndMetaRules(
+        Path buildFile, AtomicLong processedBytes) {
+      return allRulesAndMetadata;
+    }
+
+    @Override
+    public void reportProfile() {
+      isProfileReported = true;
+    }
+
+    @Override
+    public void close() {
+      isClosed = true;
+    }
+
+    public boolean isProfileReported() {
+      return isProfileReported;
+    }
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    BuckEventBus eventBus = BuckEventBusForTests.newInstance();
+    delegate = new TestProjectBuildFileParser();
+    listener = new ProjectBuildFileParseEventListener();
+    eventBus.register(listener);
+    parser = EventReportingProjectBuildFileParser.of(delegate, eventBus);
+    processedBytes = new AtomicLong();
+  }
+
+  @Test
+  public void getAllRecordsStartEvent() throws Exception {
+    assertFalse(listener.isStarted());
+    parser.getAll(SOME_PATH, processedBytes);
+    assertTrue(listener.isStarted());
+  }
+
+  @Test
+  public void getAllReturnsUnderlyingRules() throws Exception {
+    allRules = ImmutableList.of();
+    assertSame(allRules, parser.getAll(SOME_PATH, processedBytes));
+  }
+
+  @Test
+  public void startEventIsRecordedOnlyOnce() throws Exception {
+    assertFalse(listener.isStarted());
+    parser.getAll(SOME_PATH, processedBytes);
+    parser.getAllRulesAndMetaRules(SOME_PATH, processedBytes);
+    parser.getAll(SOME_PATH, processedBytes);
+    assertTrue(listener.isStarted());
+    assertThat(listener.getStartedCount(), Matchers.is(1));
+  }
+
+  @Test
+  public void getAllRulesAndMetaRulesFiresStartEvent() throws Exception {
+    assertFalse(listener.isStarted());
+    parser.getAllRulesAndMetaRules(SOME_PATH, processedBytes);
+    assertTrue(listener.isStarted());
+  }
+
+  @Test
+  public void getAllRulesAndMetaRulesReturnsUnderlyingRules() throws Exception {
+    allRulesAndMetadata = ImmutableList.of();
+    assertSame(allRulesAndMetadata, parser.getAllRulesAndMetaRules(SOME_PATH, processedBytes));
+  }
+
+  @Test
+  public void reportProfileDelegates() throws Exception {
+    assertFalse(delegate.isProfileReported());
+    parser.reportProfile();
+    assertTrue(delegate.isProfileReported());
+  }
+
+  @Test
+  public void closeReportsFinishedEvent() throws Exception {
+    parser.getAll(SOME_PATH, processedBytes);
+    assertFalse(listener.isFinished());
+    parser.close();
+    assertTrue(listener.isFinished());
+  }
+
+  @Test
+  public void closeDoesNotFireFinishedEventWithoutStart() throws Exception {
+    assertFalse(listener.isFinished());
+    parser.close();
+    assertFalse(listener.isFinished());
+  }
+
+  @Test
+  public void closeClosesUnderlyingParser() throws Exception {
+    assertFalse(delegate.isClosed);
+    parser.close();
+    assertTrue(delegate.isClosed);
+  }
+}
