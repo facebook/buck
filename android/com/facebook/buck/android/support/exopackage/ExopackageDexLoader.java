62f4f0d0d9086c1e36733a84401b98db89759363
David Reiss <dreiss@fb.com>
2017-11-08 14:42:09 -0800

    Exopackage support: load list of secondary dex files from metadata
    Summary:
    Previously, this code was just listing the secondary dex directory and
    loading everything in there.  However, with concurrent install, we might
    have not-yet-ready files sitting in that directory for an indefinite
    time.  Now, we instead load the list from metadata.txt which is updated
    not quite atomically, but much more quickly and with fewer opportunities
    for invalid intermediate states.
    
    Side note, remember what Java is like why you don't have
    try-with-resources or Guava?  Ugh.
    
    Test Plan: Ran exopackage_test_single.sh on one emulator.
    
    fbshipit-source-id: b22515a

--- android/com/facebook/buck/android/support/exopackage/ExopackageDexLoader.java
+++ android/com/facebook/buck/android/support/exopackage/ExopackageDexLoader.java
@@ -18,7 +18,11 @@ package com.facebook.buck.android.support.exopackage;
 
 import android.content.Context;
 import android.util.Log;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -51,19 +55,27 @@ public class ExopackageDexLoader {
     List<File> dexJars = new ArrayList<>();
     Set<String> expectedOdexSet = new HashSet<>();
 
-    File[] files = containingDirectory.listFiles();
-    if (files != null) {
-      for (File file : files) {
-        if (file.getName().equals("metadata.txt")) {
-          continue;
+    try {
+      BufferedReader metadataReader =
+          new BufferedReader(
+              new InputStreamReader(
+                  new FileInputStream(new File(containingDirectory, "metadata.txt"))));
+      try {
+        String line;
+        while ((line = metadataReader.readLine()) != null) {
+          int space = line.indexOf(' ');
+          if (space == -1) {
+            throw new RuntimeException("Found no space in line: " + line);
+          }
+          String baseName = line.substring(0, space);
+          dexJars.add(new File(containingDirectory, baseName));
+          expectedOdexSet.add(baseName.replaceFirst("\\.jar$", ".dex"));
         }
-        if (!file.getName().endsWith(".dex.jar")) {
-          Log.w(TAG, "Skipping unexpected file in exopackage directory: " + file.getName());
-          continue;
-        }
-        dexJars.add(file);
-        expectedOdexSet.add(file.getName().replaceFirst("\\.jar$", ".dex"));
+      } finally {
+        metadataReader.close();
       }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
 
     File dexOptDir = context.getDir("exopackage_dex_opt", Context.MODE_PRIVATE);
