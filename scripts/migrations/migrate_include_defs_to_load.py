780f9861b857ed50f4e467e0f6c8c4f87ac4767d
Taras Tsugrii <ttsugrii@fb.com>
2017-11-11 10:28:22 -0800

    Make cell prefix configurable when running migrations
    Summary:
    This is so that we can control which cell prefix to use.
    For some reason linter started complaining about some python syntax issues
    so I had to fix them in this revision.
    
    Test Plan: run migrations on our internal code
    
    Reviewed By: styurin
    
    fbshipit-source-id: 09efbcb

--- scripts/migrations/migrate_include_defs_to_load.py
+++ scripts/migrations/migrate_include_defs_to_load.py
@@ -15,9 +15,11 @@ MY_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def load_cell_roots(repo_dir: str) -> Dict[str, str]:
     """Returns a map with cell keys and their roots as values."""
-    cell_config = subprocess.check_output(['buck', 'audit', 'cell'], cwd=repo_dir).decode().strip()
+    cell_config = subprocess.check_output(
+        ['buck', 'audit', 'cell'], cwd=repo_dir,
+    ).decode().strip()
     logging.debug('Cell config: %r' % cell_config)
-    cell_roots = {}
+    cell_roots = {}  # type: Dict[str, str]
     if not cell_config:
         return cell_roots
     for config in cell_config.split(os.linesep):
@@ -27,23 +29,38 @@ def load_cell_roots(repo_dir: str) -> Dict[str, str]:
     return cell_roots
 
 
-def load_export_map(repo: str, cell_roots: Dict[str, str], build_file: str, verbose: bool = False):
-    """Returns a dictionary with import string keys and all symbols they export as values."""
+def load_export_map(
+        repo: str,
+        cell_roots: Dict[str, str],
+        build_file: str,
+        cell_prefix: str = None,
+        verbose: bool = False):
+    """
+    Returns a dictionary with import string keys and all symbols they export as
+    values.
+    """
     cell_root_args = []
     for cell, root in cell_roots.items():
         cell_root_args.extend(['--cell_root', cell + '=' + root])
     dump_script = os.path.join(MY_DIR, 'dump.py')
     verbosity_flags = ['-v'] if verbose else []
-    export_map_flags = ['export_map', '--use_load_function_import_string_format']
-    dump_command = [dump_script, '--json', '--repository',
-                    repo] + verbosity_flags + cell_root_args + export_map_flags + [build_file]
+    export_map_flags = [
+        'export_map', '--use_load_function_import_string_format',
+    ]
+    cell_prefix_flags = [
+        '--cell_prefix', cell_prefix
+    ] if cell_prefix is not None else []
+    dump_command = [
+        dump_script, '--json', '--repository', repo
+    ] + verbosity_flags + cell_root_args + export_map_flags + \
+        cell_prefix_flags + [build_file]
     return json.loads(subprocess.check_output(dump_command).decode().strip())
 
 
 class Buildozer:
     """Represents a buildozer tool."""
 
-    def __init__(self, path: str, repo: str):
+    def __init__(self, path: str, repo: str) -> None:
         self.path = path
         self.repo = repo
 
@@ -94,9 +111,16 @@ def main():
     parser.add_argument('build_file', metavar='FILE', help='Build file path.')
     parser.add_argument('--repository', metavar='DIRECTORY', required='True',
                         help='Repository path.')
-    parser.add_argument('--buildozer', metavar='FILE', required=True, help='Buildozer path.')
-    parser.add_argument('--test', action='store_true',
-                        help='Whether new build file should be tested in the end.')
+    parser.add_argument(
+        '--buildozer', metavar='FILE', required=True, help='Buildozer path.',
+    )
+    parser.add_argument('--cell_prefix', default='@',
+                        help='The prefix to use for cells in import strings.')
+    parser.add_argument(
+        '--test',
+        action='store_true',
+        help='Whether new build file should be tested in the end.',
+    )
     args = parser.parse_args()
     logging_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=logging_level, format=(
@@ -106,7 +130,13 @@ def main():
     buildozer = Buildozer(args.buildozer, args.repository)
     package_dir = os.path.dirname(args.build_file)
     package = str(pathlib.Path(package_dir).relative_to(args.repository))
-    load_funcs = load_export_map(args.repository, cell_roots, args.build_file, args.verbose)
+    load_funcs = load_export_map(
+        repo=args.repository,
+        cell_roots=cell_roots,
+        build_file=args.build_file,
+        cell_prefix=args.cell_prefix,
+        verbose=args.verbose,
+    )
     logging.debug(load_funcs)
     add_load_funcs(buildozer, load_funcs, package)
     remove_include_defs(buildozer, package)
