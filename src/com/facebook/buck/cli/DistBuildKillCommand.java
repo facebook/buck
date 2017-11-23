a705f2c5afbf395e6342c0b7c9e5b8f8d2d7ed10
Rui Barbosa Martins <ruibm@fb.com>
2017-11-23 09:44:04 -0800

    Implement a buck distbuild command to kill a distributed build.
    Summary: [stampede] Implement a buck distbuild command to kill a distributed build.
    
    Test Plan: ci
    
    Reviewed By: stepancheg
    
    fbshipit-source-id: 1ddd203

---
+++ src/com/facebook/buck/cli/DistBuildKillCommand.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2017-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.cli;
+
+import com.facebook.buck.distributed.BuildStatusUtil;
+import com.facebook.buck.distributed.DistBuildService;
+import com.facebook.buck.distributed.thrift.BuildJob;
+import com.facebook.buck.distributed.thrift.BuildStatus;
+import com.facebook.buck.distributed.thrift.StampedeId;
+import com.facebook.buck.util.Console;
+import com.facebook.buck.util.network.hostname.HostnameFetching;
+import com.google.common.base.Stopwatch;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.concurrent.TimeUnit;
+
+/** Kills a stampede/distributed build. */
+public class DistBuildKillCommand extends AbstractDistBuildCommand {
+
+  @Override
+  public boolean isReadOnly() {
+    return true;
+  }
+
+  @Override
+  public String getShortDescription() {
+    return "kills a distributed build.";
+  }
+
+  @Override
+  public int runWithoutHelp(CommandRunnerParams params) throws IOException, InterruptedException {
+    Console console = params.getConsole();
+    PrintStream stdout = console.getStdOut();
+    StampedeId stampedeId = getStampedeId();
+
+    try (DistBuildService service = DistBuildFactory.newDistBuildService(params)) {
+      stdout.println(
+          String.format("Fetching build information for StampedeId=[%s].", stampedeId.getId()));
+      Stopwatch stopwatch = Stopwatch.createStarted();
+      BuildJob buildJob = service.getCurrentBuildJobState(getStampedeId());
+
+      stdout.println(
+          String.format(
+              "The build is currently in status [%s:%s].",
+              buildJob.getStatus().toString(), buildJob.getStatusMessage()));
+      if (BuildStatusUtil.isTerminalBuildStatus(buildJob.getStatus())) {
+        console.printSuccess(
+            String.format(
+                "Build is already finished so doing nothing. Took [%d millis] to execute.",
+                stopwatch.elapsed(TimeUnit.MILLISECONDS)));
+        return 0;
+      }
+
+      String statusMessage =
+          String.format(
+              "Build killed via 'buck distbuild kill' command by user=[%s] from host=[%s].",
+              System.getProperty("user.name"), HostnameFetching.getHostname());
+      stdout.println(
+          String.format("Killing the build and setting statusMessage to [%s].", statusMessage));
+
+      service.setFinalBuildStatus(stampedeId, BuildStatus.FAILED, statusMessage);
+      console.printSuccess(
+          String.format(
+              "Successfully killed the build in [%d millis].",
+              stopwatch.elapsed(TimeUnit.MILLISECONDS)));
+      return 0;
+    }
+  }
+}
