a9a842531300757e221fc6e9290fd8e2d852c86b
Josiah Gaskin <jcg@fb.com>
2017-11-30 17:13:43 -0800

    Save mapping of class->dexfile and go directly to dex which contains it
    Summary:
    We already save the list of classes which we've encountered in our dex files.
    This change adds in the mapping of the class to the `DexFile` which contains the definition.
    This prevents a linear scan when doing a class lookup and allows us to go directly to the
    defining file.
    
    Test Plan: `./scripts/exopackage_test_single.sh`
    
    Reviewed By: dreiss
    
    fbshipit-source-id: cded2c8

--- android/com/facebook/buck/android/support/exopackage/DelegatingClassLoader.java
+++ android/com/facebook/buck/android/support/exopackage/DelegatingClassLoader.java
@@ -33,7 +33,6 @@
 package com.facebook.buck.android.support.exopackage;
 
 import android.util.Log;
-import dalvik.system.BaseDexClassLoader;
 import dalvik.system.DexFile;
 import dalvik.system.PathClassLoader;
 import java.io.File;
@@ -41,9 +40,9 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Enumeration;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 /**
  * A {@link ClassLoader} similar to {@link dalvik.system.DexClassLoader}. This loader aims to allow
@@ -69,16 +68,21 @@ class DelegatingClassLoader extends ClassLoader {
   private File mDexOptDir;
 
   private PathClassLoader mDelegate;
-  private Set<String> mManagedClasses = new HashSet<>();
+
+  // Map classes to the dex files where they live. In the future, once we can read the modular
+  // metadata in order to build a package name => module name mapping, here we will just want to map
+  // module name => list of dex files, which will be marginally less efficient for lookups, and take
+  // far less memory/time to instantiate
+  private Map<String, DexFile> mManagedClassesToDexFile = new HashMap<String, DexFile>();
 
   private static DelegatingClassLoader sInstalledClassLoader;
 
-  private static final Method sFIND_CLASS;
+  private static final Method sFIND_LOADED_CLASS;
 
   static {
     try {
-      sFIND_CLASS = BaseDexClassLoader.class.getDeclaredMethod("findClass", String.class);
-      sFIND_CLASS.setAccessible(true);
+      sFIND_LOADED_CLASS = ClassLoader.class.getDeclaredMethod("findLoadedClass", String.class);
+      sFIND_LOADED_CLASS.setAccessible(true);
     } catch (NoSuchMethodException e) {
       throw new RuntimeException(e);
     }
@@ -132,7 +136,7 @@ class DelegatingClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
       final DelegatingClassLoader instance = DelegatingClassLoader.getInstance();
-      if (mManagedClasses.contains(name)) {
+      if (mManagedClassesToDexFile.containsKey(name)) {
         return instance.loadManagedClass(name);
       } else {
         throw new ClassNotFoundException(name);
@@ -148,7 +152,7 @@ class DelegatingClassLoader extends ClassLoader {
   @Override
   public Class<?> loadClass(String className, boolean resolve) throws ClassNotFoundException {
     Class<?> clazz;
-    if (mManagedClasses.contains(className)) {
+    if (mManagedClassesToDexFile.containsKey(className)) {
       clazz = loadManagedClass(className);
     } else {
       clazz = getParent().loadClass(className);
@@ -170,7 +174,19 @@ class DelegatingClassLoader extends ClassLoader {
           "DelegatingCL was not initialized via ExopackageDexLoader.loadExopackageJars");
     }
     try {
-      return (Class) sFIND_CLASS.invoke(mDelegate, className);
+      // First try loading the class from the cache
+      Class<?> clazz = (Class<?>) sFIND_LOADED_CLASS.invoke(mDelegate, className);
+      if (clazz == null) {
+        // Otherwise, load it from the relevant dex file
+        DexFile origin = mManagedClassesToDexFile.get(className);
+        if (origin == null) {
+          throw new ClassNotFoundException("Unable to find class " + className);
+        }
+        // attribute all classloads to the same classloader so that package-scoping works correctly
+        // and so that we can use its loaded class cache
+        clazz = origin.loadClass(className, mDelegate);
+      }
+      return clazz;
     } catch (Exception e) {
       throw new ClassNotFoundException("Unable to find class " + className, e.getCause());
     }
@@ -188,14 +204,13 @@ class DelegatingClassLoader extends ClassLoader {
    */
   void resetDelegate(List<File> dexJars) {
     mDelegate = new PathClassLoader("", "", this);
-    mManagedClasses.clear();
-    SystemClassLoaderAdder.installDexJars(mDelegate, mDexOptDir, dexJars);
+    mManagedClassesToDexFile.clear();
     for (File dexJar : dexJars) {
       try {
         DexFile dexFile = new DexFile(dexJar);
         final Enumeration<String> entries = dexFile.entries();
         while (entries.hasMoreElements()) {
-          mManagedClasses.add(entries.nextElement());
+          mManagedClassesToDexFile.put(entries.nextElement(), dexFile);
         }
       } catch (IOException e) {
         // Pass for now
