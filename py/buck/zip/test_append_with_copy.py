432ecf5d692cc221314c5f910c722d3d7bc0f15e
Sergey Tyurin <tyurins@fb.com>
2017-12-04 10:02:46 -0800

    Compute module binary hashes separately from Buck binary hash
    Summary: This change separates hashes of the main Buck binary and Buck modules. This separation will allow us to use module hashes in rule keys of the descriptions from these modules.
    
    Test Plan:
    Tested both with Buck built by ant and by Buck:
    
    - change code in some module, check that Buck binary hash doesn't change and module's hash changes;
    - change code in buck and check that Buck binary hash changes and module hashes are the same.
    
    Reviewed By: sbalabanov
    
    fbshipit-source-id: 53b82b3

---
+++ py/buck/zip/test_append_with_copy.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Copyright 2017-present Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os.path
+import shutil
+import tempfile
+import unittest
+import uuid
+import zipfile
+
+from py.buck.zip import append_with_copy
+
+
+class TestAppend(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_file_included(self):
+        src_zip_name = 'src.zip'
+        src_zip_path = os.path.join(self.test_dir, src_zip_name)
+
+        existing_entry = 'existing_entry'
+        existing_entry_content = uuid.uuid4().hex
+        self.__create_zip_file(src_zip_path, existing_entry, existing_entry_content)
+
+        appended_entry_name = 'appended_entry'
+        appended_file_name = 'appended_file'
+        appended_file_path = os.path.join(self.test_dir, appended_file_name)
+        appended_file_content = uuid.uuid4().hex
+        self.__create_file(appended_file_path, appended_file_content)
+
+        dst_file_path = os.path.join(self.test_dir, 'dst.zip')
+
+        append_with_copy.copy_and_append_to_zip_file(src_zip_path, dst_file_path,
+                                                     [appended_entry_name, appended_file_path])
+
+        self.assertEqual(existing_entry_content,
+                         self.__read_file_entry(dst_file_path, existing_entry))
+        self.assertEqual(appended_file_content,
+                         self.__read_file_entry(dst_file_path, appended_entry_name))
+
+    def __create_zip_file(self, zip_file_path, entry, entry_content):
+        with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
+            zip_file.writestr(entry, entry_content)
+
+    def __create_file(self, file_path, file_content):
+        with open(file_path, 'w') as new_file:
+            new_file.write(file_content)
+
+    def __read_file_entry(self, zip_file_path, entry_name):
+        with zipfile.ZipFile(zip_file_path) as zip_file:
+            return zip_file.read(entry_name)
--- tools/build_rules/module_rules.bzl
+++ tools/build_rules/module_rules.bzl
@@ -17,14 +17,38 @@ def buck_module(
         **kwargs
     )
 
+    jar_without_hash_name = name + '_jar_without_hash'
+
     java_binary(
+        name = jar_without_hash_name,
+        deps = [
+            ":" + name,
+        ],
+    )
+
+    calculate_module_hash_name = name + '_calculate_module_hash'
+
+    genrule(
+        name = calculate_module_hash_name,
+        out = "module-binary-hash.txt",
+        cmd = " ".join([
+            "$(exe //py/hash:hash_files)",
+            "$(location :{})".format(jar_without_hash_name),
+            "$(location //py/hash:hash_files.py) > $OUT"
+        ]),
+    )
+
+    genrule(
         name = name + "-module",
+        out = "{}.jar".format(name),
+        cmd = " ".join([
+            "$(exe //py/buck/zip:append_with_copy)",
+            "$(location :{}) $OUT".format(jar_without_hash_name),
+            "META-INF/module-binary-hash.txt $(location :{})".format(calculate_module_hash_name)
+        ]),
         visibility = [
             "//programs:bucklib",
             "//programs:calculate-buck-binary-hash",
-        ],
-        deps = [
-            ":" + name,
         ],
     )
 
