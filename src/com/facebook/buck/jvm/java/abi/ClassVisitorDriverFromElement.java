ad03ff9b981f2dede3e4df06571475a5d1849e59
Elan Kugelmass <elan@addepar.com>
2017-12-12 00:02:04 -0500

    Opcodes.V9

--- src/com/facebook/buck/jvm/java/abi/ClassVisitorDriverFromElement.java
+++ src/com/facebook/buck/jvm/java/abi/ClassVisitorDriverFromElement.java
@@ -109,7 +109,8 @@ class ClassVisitorDriverFromElement {
         return Opcodes.V1_7;
       case RELEASE_8:
         return Opcodes.V1_8;
-      //$CASES-OMITTED$ No RELEASE_9 support yet
+      case RELEASE_9:
+        return Opcodes.V9;
       default:
         throw new IllegalArgumentException(String.format("Unexpected source version: %s", version));
     }
