547387863ef14ba71e3cf28ebe01df634021f3be
Bob Yang <bobyf@fb.com>
2018-01-02 11:04:29 -0800

    Improve IOS AG time by caching result of getDeps
    Summary: Add caching on computer getDeps of AbstractCxxSymlinkTreeHeaders to improve action graph construction time.
    
    Test Plan:
    all buck tests work.
    built wilde with master and this diff, and compared md5 of the FacebookBinary#iphonesimulator binary
    
    Reviewed By: sbalabanov
    
    fbshipit-source-id: 218b941

--- src/com/facebook/buck/cxx/AbstractCxxSymlinkTreeHeaders.java
+++ src/com/facebook/buck/cxx/AbstractCxxSymlinkTreeHeaders.java
@@ -27,6 +27,7 @@ import com.facebook.buck.rules.SourcePath;
 import com.facebook.buck.rules.SourcePathResolver;
 import com.facebook.buck.rules.SourcePathRuleFinder;
 import com.facebook.buck.util.immutables.BuckStyleImmutable;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedMap;
 import java.nio.file.Path;
 import java.util.Optional;
@@ -37,6 +38,10 @@ import org.immutables.value.Value;
 @Value.Immutable(prehash = true)
 @BuckStyleImmutable
 abstract class AbstractCxxSymlinkTreeHeaders extends CxxHeaders implements RuleKeyAppendable {
+
+  @SuppressWarnings("immutables")
+  private Optional<ImmutableList<BuildRule>> computedDeps = Optional.empty();
+
   @Override
   @AddToRuleKey
   public abstract CxxPreprocessables.IncludeType getIncludeType();
@@ -76,13 +81,28 @@ abstract class AbstractCxxSymlinkTreeHeaders extends CxxHeaders implements RuleK
   // rulekey is really slow to compute.
   public Stream<BuildRule> getDeps(SourcePathRuleFinder ruleFinder) {
     Stream.Builder<BuildRule> builder = Stream.builder();
-    getNameToPathMap().values().forEach(value -> ruleFinder.getRule(value).ifPresent(builder));
     ruleFinder.getRule(getRoot()).ifPresent(builder);
     if (getIncludeRoot().isRight()) {
       ruleFinder.getRule(getIncludeRoot().getRight()).ifPresent(builder);
     }
     getHeaderMap().flatMap(ruleFinder::getRule).ifPresent(builder);
-    return builder.build().distinct();
+
+    // return a stream of the cached dependencies, or compute and store it
+    return Stream.concat(
+            computedDeps
+                .orElseGet(
+                    () -> {
+                      ImmutableList.Builder<BuildRule> cachedBuilder = ImmutableList.builder();
+                      getNameToPathMap()
+                          .values()
+                          .forEach(
+                              value -> ruleFinder.getRule(value).ifPresent(cachedBuilder::add));
+                      computedDeps = Optional.of(cachedBuilder.build());
+                      return computedDeps.get();
+                    })
+                .stream(),
+            builder.build())
+        .distinct();
   }
 
   @Override
