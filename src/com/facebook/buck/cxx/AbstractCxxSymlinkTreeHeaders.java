f25328f3b16d93d09ee5758e9db820d7b87713b0
Taras Tsugrii <ttsugrii@fb.com>
2018-01-04 10:40:12 -0800

    Revert "Improve IOS AG time by caching result of getDeps"
    Summary:
    This reverts commit 218b941596a2c81d78d4be86cc5f18d98e8f055b.
    
    Reverted commit is a suspect in Android build time regression.
    
    Test Plan: CI
    
    Reviewed By: sbalabanov
    
    fbshipit-source-id: 695dfb0

--- src/com/facebook/buck/cxx/AbstractCxxSymlinkTreeHeaders.java
+++ src/com/facebook/buck/cxx/AbstractCxxSymlinkTreeHeaders.java
@@ -27,7 +27,6 @@ import com.facebook.buck.rules.SourcePath;
 import com.facebook.buck.rules.SourcePathResolver;
 import com.facebook.buck.rules.SourcePathRuleFinder;
 import com.facebook.buck.util.immutables.BuckStyleImmutable;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedMap;
 import java.nio.file.Path;
 import java.util.Optional;
@@ -38,10 +37,6 @@ import org.immutables.value.Value;
 @Value.Immutable(prehash = true)
 @BuckStyleImmutable
 abstract class AbstractCxxSymlinkTreeHeaders extends CxxHeaders implements RuleKeyAppendable {
-
-  @SuppressWarnings("immutables")
-  private Optional<ImmutableList<BuildRule>> computedDeps = Optional.empty();
-
   @Override
   @AddToRuleKey
   public abstract CxxPreprocessables.IncludeType getIncludeType();
@@ -81,28 +76,13 @@ abstract class AbstractCxxSymlinkTreeHeaders extends CxxHeaders implements RuleK
   // rulekey is really slow to compute.
   public Stream<BuildRule> getDeps(SourcePathRuleFinder ruleFinder) {
     Stream.Builder<BuildRule> builder = Stream.builder();
+    getNameToPathMap().values().forEach(value -> ruleFinder.getRule(value).ifPresent(builder));
     ruleFinder.getRule(getRoot()).ifPresent(builder);
     if (getIncludeRoot().isRight()) {
       ruleFinder.getRule(getIncludeRoot().getRight()).ifPresent(builder);
     }
     getHeaderMap().flatMap(ruleFinder::getRule).ifPresent(builder);
-
-    // return a stream of the cached dependencies, or compute and store it
-    return Stream.concat(
-            computedDeps
-                .orElseGet(
-                    () -> {
-                      ImmutableList.Builder<BuildRule> cachedBuilder = ImmutableList.builder();
-                      getNameToPathMap()
-                          .values()
-                          .forEach(
-                              value -> ruleFinder.getRule(value).ifPresent(cachedBuilder::add));
-                      computedDeps = Optional.of(cachedBuilder.build());
-                      return computedDeps.get();
-                    })
-                .stream(),
-            builder.build())
-        .distinct();
+    return builder.build().distinct();
   }
 
   @Override
