17b8078c95223792e7aec74645ee8731f00cc030
Damien Jiang <damien@addepar.com>
2018-01-29 19:56:13 -0800

    fix semantic merge conflicts

--- test/com/facebook/buck/testrunner/TestNGLoggingIntegrationTest.java
+++ test/com/facebook/buck/testrunner/TestNGLoggingIntegrationTest.java
@@ -20,8 +20,9 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
+import com.facebook.buck.testutil.ProcessResult;
+import com.facebook.buck.testutil.TemporaryPaths;
 import com.facebook.buck.testutil.integration.ProjectWorkspace;
-import com.facebook.buck.testutil.integration.TemporaryPaths;
 import com.facebook.buck.testutil.integration.TestDataHelper;
 import java.io.IOException;
 import org.junit.Before;
@@ -43,7 +44,7 @@ public class TestNGLoggingIntegrationTest {
 
   @Test
   public void logOutputIsOnlyReportedForTestWhichFails() throws IOException {
-    ProjectWorkspace.ProcessResult result = workspace.runBuckCommand("test", "//:testng-logging");
+    ProcessResult result = workspace.runBuckCommand("test", "//:testng-logging");
     result.assertTestFailure();
 
     // stdout should get all debug messages and up when a test fails.
@@ -62,7 +63,7 @@ public class TestNGLoggingIntegrationTest {
 
   @Test
   public void logParametersForFailingTest() throws IOException {
-    ProjectWorkspace.ProcessResult result =
+    ProcessResult result =
         workspace.runBuckCommand("test", "//:testng-test-output-parameters");
     result.assertTestFailure();
 
