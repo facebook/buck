7b9ae3c58f8fd883c49e252c84b49c0a6c975fd7
Shivanker Goel <shivanker@fb.com>
2018-03-01 01:30:18 -0800

    Tone down non-critical error messages which might pop up when local build finishes first.
    Test Plan: thereisnoneedtobeupset
    
    Reviewed By: michsien
    
    fbshipit-source-id: 2d86d69

--- src/com/facebook/buck/distributed/build_client/BuildPhase.java
+++ src/com/facebook/buck/distributed/build_client/BuildPhase.java
@@ -538,7 +538,7 @@ public class BuildPhase {
                   elapseMillisSinceFirstFinishedStatus));
           return; // Events are still missing, and we haven't timed out, so poll again.
         } else {
-          LOG.error(
+          LOG.warn(
               String.format(
                   "%d ms elapsed since build job marked as finished, but still missing finished status from some workers.",
                   elapseMillisSinceFirstFinishedStatus));
@@ -553,7 +553,7 @@ public class BuildPhase {
                   elapseMillisSinceFirstFinishedStatus));
           return; // Events are still missing, and we haven't timed out, so poll again.
         } else {
-          LOG.error(
+          LOG.warn(
               String.format(
                   "%d ms elapsed since build job marked as finished, but still missing build rule finished events.",
                   elapseMillisSinceFirstFinishedStatus));
--- src/com/facebook/buck/distributed/build_client/DistBuildController.java
+++ src/com/facebook/buck/distributed/build_client/DistBuildController.java
@@ -170,7 +170,8 @@ public class DistBuildController {
               invocationInfo,
               ruleKeyCalculatorFuture);
     } catch (IOException | RuntimeException ex) { // Important: Don't swallow InterruptedException
-      LOG.error(ex, "Distributed build step failed.");
+      // Don't print an error here, because we might have failed due to local finishing first.
+      LOG.warn(ex, "Distributed build step failed.");
       return createFailedExecutionResult(
           Preconditions.checkNotNull(stampedeIdReference.get()),
           ExitCode.DISTRIBUTED_BUILD_STEP_LOCAL_EXCEPTION);
