055935c8e1c378ae78d2cca1205d6b2c17f5b240
Chris Hopman <cjhopman@fb.com>
2018-03-02 17:46:37 -0800

    Regenerate thrift with undated annotations
    Summary:
    Having these be dated makes it more difficult to review changes.
    
    Added several blacklisted files that are inexplicably not generated with
    current thrift.
    
    Test Plan: run third-party/java/thrift/gen.sh
    
    Reviewed By: ttsugriy
    
    fbshipit-source-id: 8dcc200

--- third-party/java/thrift/gen.sh
+++ third-party/java/thrift/gen.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash -ex
-for f in $(git ls-files -- *.thrift); do
-  /usr/local/bin/thrift --gen java -out src-gen/ $f
+# TODO(alisdair): update coordinator.thrift to be generated with current thrift and un-blacklist it.
+for f in $(git ls-files -- *.thrift | grep -v coordinator.thrift); do
+  /usr/local/bin/thrift --gen java:generated_annotations=undated -out src-gen/ $f
 done
