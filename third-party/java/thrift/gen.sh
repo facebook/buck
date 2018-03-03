5d2db782082d262cf185741884a0ddd1f9e14899
Chris Hopman <cjhopman@fb.com>
2018-03-03 03:43:33 -0800

    Generate coordinator.thrift w/ thrift 0.10.0
    Summary: And un-blacklist it.
    
    Test Plan: dogscience
    
    Reviewed By: alisdair04
    
    fbshipit-source-id: e91be0a

--- third-party/java/thrift/gen.sh
+++ third-party/java/thrift/gen.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash -ex
-# TODO(alisdair): update coordinator.thrift to be generated with current thrift and un-blacklist it.
-for f in $(git ls-files -- *.thrift | grep -v coordinator.thrift); do
+for f in $(git ls-files -- *.thrift); do
   /usr/local/bin/thrift --gen java:generated_annotations=undated -out src-gen/ $f
 done
