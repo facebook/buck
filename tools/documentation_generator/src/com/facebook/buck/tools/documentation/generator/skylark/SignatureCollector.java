b248a1ca5ef1c1dc409e04d3fddd7a3d53258928
Taras Tsugrii <ttsugrii@fb.com>
2018-03-18 22:38:15 -0700

    Start a Skylark documentation generator project.
    Summary:
    This project will be used to scan for all Skylark functions in the
    classpath and generate their documentation.
    
    Test Plan: CI
    
    Reviewed By: philipjameson
    
    fbshipit-source-id: 3410a23

---
+++ tools/documentation_generator/src/com/facebook/buck/tools/documentation/generator/skylark/SignatureCollector.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.tools.documentation.generator.skylark;
+
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
+import com.google.devtools.build.lib.skylarkinterface.SkylarkSignature;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/** Class responsible discovering Skylark signature metadata in the classpath. */
+class SignatureCollector {
+  /**
+   * Returns a stream of signatures found in the current classpath.
+   *
+   * @param classInfoPredicate predicate to use in order to filter out classes that should not be
+   *     loaded. It's best to make it as precise as possible to avoid expensive loading - checking
+   *     for class name and package is ideal.
+   */
+  static Stream<SkylarkSignature> getSkylarkSignatures(Predicate<ClassInfo> classInfoPredicate)
+      throws IOException {
+    return ClassPath.from(ClassPath.class.getClassLoader())
+        .getAllClasses()
+        .stream()
+        .filter(classInfoPredicate)
+        .map(ClassInfo::load)
+        .flatMap(clazz -> Arrays.stream(clazz.getDeclaredFields()))
+        .map(field -> field.getAnnotation(SkylarkSignature.class))
+        .filter(Objects::nonNull);
+  }
+}
---
+++ tools/documentation_generator/test/com/facebook/buck/tools/documentation/generator/skylark/SignatureCollectorTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.tools.documentation.generator.skylark;
+
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeTrue;
+
+import com.facebook.buck.util.environment.Platform;
+import com.google.common.reflect.ClassPath.ClassInfo;
+import com.google.devtools.build.lib.skylarkinterface.SkylarkSignature;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import org.junit.Test;
+
+public class SignatureCollectorTest {
+
+  private static final Predicate<ClassInfo> CLASS_INFO_PREDICATE =
+      classInfo ->
+          classInfo
+              .getPackageName()
+              .startsWith("com.facebook.buck.tools.documentation.generator.skylark.signatures");
+
+  @Test
+  public void findsAtLeastSkylarkSignature() throws Exception {
+    // ClassPath$Scanner has issues scanning classpath on Windows and results in
+    // "The filename, directory name, or volume label syntax is incorrect" exception.
+    assumeTrue(Platform.detect() != Platform.WINDOWS);
+
+    assertThat(
+        SignatureCollector.getSkylarkSignatures(CLASS_INFO_PREDICATE)
+            .map(SkylarkSignature::name)
+            .collect(Collectors.toList()),
+        contains("dummy"));
+  }
+}
