1323e0f7dd73eec84f81c2dbbbc249d75a0d8205
Keegan Parker <keeganp@fb.com>
2018-03-21 22:54:55 -0700

    iOS template: genrule the plist
    Summary:
    ```
                                              android_binary                                                                        apple_package
                                                     +                                                                                   +
          +------------+-------------+---------------+                                                                                   |
          v            v             v               v                                                                                   v
    export_file     manifest     keystore     android_library                                                                       apple_bundle
                       +                             +                                                                                   +
                       |                    +--------+---------+------------------+---------------------+-----------------+              +-----------------------------------------+
                       v                    v                  v                  v                     v                 v              v                                         v
                    genrule          android_resource   android_library      prebuilt_jar     android_build_config   cxx_library    apple_binary                           *genrule (plist)*
                       +                                       +                  +                                       +              +                                         +
       +---------------+                                       |                  |                                       +------------------------------------+                   |
       v               v                                       v                  v                                       v              v                     v                   v
    genrule      python_binary                      android_prebuilt_aar       genrule                               cxx_library    apple_resource     apple_asset_library  *python_binary*
                                                                                  +
                                                                                  |
                                                                                  v
                                                                            python_binary
    ```
    
    Test Plan: Built locally
    
    Reviewed By: bobyangyf
    
    fbshipit-source-id: a438c63

---
+++ test/com/facebook/buck/testutil/endtoend/testdata/mobile/ios/plist/py_cat.py
@@ -0,0 +1,21 @@
+# Copyright 2018-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+
+
+def parse_file_name():
+    parser = argparse.ArgumentParser(description="Outputs given file.")
+    parser.add_argument("file_name", type=str, help="Name of file to output")
+    return parser.parse_args().file_name
+
+
+def output_file(file_name):
+    with open(file_name, mode='r') as f:
+        print(f.read())
+
+if __name__ == "__main__":
+    output_file(parse_file_name())
