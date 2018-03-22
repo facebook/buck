88331c933a0753a214336d36e181a17eb2b0ba8d
Keegan Parker <keeganp@fb.com>
2018-03-21 22:55:10 -0700

    iOS E2E build test
    Summary: Test that verifies iOS build with given project structure
    
    Test Plan: Ran locally
    
    Reviewed By: bobyangyf
    
    fbshipit-source-id: 70d5215

---
+++ test/com/facebook/buck/apple/endtoend/AppleEndToEndTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple.endtoend;
+
+import static org.junit.Assume.assumeTrue;
+
+import com.facebook.buck.apple.AppleNativeIntegrationTestUtils;
+import com.facebook.buck.apple.toolchain.ApplePlatform;
+import com.facebook.buck.testutil.ProcessResult;
+import com.facebook.buck.testutil.endtoend.EndToEndEnvironment;
+import com.facebook.buck.testutil.endtoend.EndToEndRunner;
+import com.facebook.buck.testutil.endtoend.EndToEndTestDescriptor;
+import com.facebook.buck.testutil.endtoend.EndToEndWorkspace;
+import com.facebook.buck.testutil.endtoend.Environment;
+import com.facebook.buck.util.environment.Platform;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * E2E tests for buck's building process on an environment constructed like:
+ *
+ * <pre>
+ *                                            apple_package
+ *                                                 +
+ *                                                 |
+ *                                                 v
+ *                                            apple_bundle
+ *                                                 +
+ *                     +---------------------------+----------------------------+
+ *                     v                                                        v
+ *                apple_binary                                           genrule (plist)
+ *                     +                                                        +
+ *      +------------------------------------+-----------------+                |
+ *      v              v                     v                 v                v
+ * cxx_library    apple_resource   apple_asset_library   apple_library    python_binary
+ * </pre>
+ */
+@RunWith(EndToEndRunner.class)
+public class AppleEndToEndTest {
+  private static final String mainTarget = "//ios:BuckDemoApp";
+
+  @Before
+  public void assumeEnvironment() {
+    assumeTrue(Platform.detect() == Platform.MACOS);
+    assumeTrue(AppleNativeIntegrationTestUtils.isApplePlatformAvailable(ApplePlatform.IPHONEOS));
+  }
+
+  @Environment
+  public static EndToEndEnvironment baseEnvironment() {
+    return new EndToEndEnvironment()
+        .addTemplates("mobile")
+        .withCommand("build")
+        .withTargets(mainTarget);
+  }
+
+  /** Determines that buck successfully outputs proper programs */
+  @Test
+  public void shouldBuild(
+      EndToEndTestDescriptor test, EndToEndWorkspace workspace, ProcessResult result) {
+    result.assertSuccess("Did not successfully build");
+  }
+}
