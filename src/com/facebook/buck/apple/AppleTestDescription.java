a3a29c46587fba073c60c39def450c4e1b234631
Sergey Tyurin <tyurins@fb.com>
2018-04-02 15:56:14 -0700

    Use default platform when no other platforms are present in apple_test
    Summary: `cxx_test` uses the default platform when no platform flavors specified.
    
    Reviewed By: ttsugriy
    
    fbshipit-source-id: ea5bd68

--- src/com/facebook/buck/apple/AppleTestDescription.java
+++ src/com/facebook/buck/apple/AppleTestDescription.java
@@ -88,6 +88,7 @@ import com.google.common.hash.Hasher;
 import com.google.common.hash.Hashing;
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
 import org.immutables.value.Value;
@@ -478,9 +479,16 @@ public class AppleTestDescription
       extraDepsBuilder.add(xctoolZipTarget.get());
     }
     extraDepsBuilder.addAll(appleConfig.getCodesignProvider().getParseTimeDeps());
+
+    CxxPlatformsProvider cxxPlatformsProvider = getCxxPlatformsProvider();
+    ImmutableList<CxxPlatform> cxxPlatforms =
+        cxxPlatformsProvider.getCxxPlatforms().getValues(buildTarget);
+
     extraDepsBuilder.addAll(
         CxxPlatforms.getParseTimeDeps(
-            getCxxPlatformsProvider().getCxxPlatforms().getValues(buildTarget)));
+            cxxPlatforms.isEmpty()
+                ? Collections.singleton(cxxPlatformsProvider.getDefaultCxxPlatform())
+                : cxxPlatforms));
   }
 
   private AppleBundle getBuildRuleForTestHostAppTarget(
