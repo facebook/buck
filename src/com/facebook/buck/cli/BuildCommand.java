1cde8a8c3d43cd7b9f0b2e00f2f51f7cd9c380a5
Sergey Tyurin <tyurins@fb.com>
2018-04-19 15:58:04 -0700

    Fix BuildCommand to send finished event with correct exit code
    Summary: Refactoring went wrong
    
    Reviewed By: philipjameson
    
    fbshipit-source-id: fbcba32

--- src/com/facebook/buck/cli/BuildCommand.java
+++ src/com/facebook/buck/cli/BuildCommand.java
@@ -472,18 +472,18 @@ public class BuildCommand extends AbstractCommand {
       this.arguments.addAll(additionalTargets);
     }
     BuildEvent.Started started = postBuildStartedEvent(params);
-    ExitCode exitCode = ExitCode.BUILD_ERROR;
+    BuildRunResult result = ImmutableBuildRunResult.of(ExitCode.BUILD_ERROR, ImmutableList.of());
     try (CloseableMemoizedSupplier<ForkJoinPool> poolSupplier =
         getForkJoinPoolSupplier(params.getBuckConfig())) {
-      return executeBuildAndProcessResult(params, commandThreadManager, poolSupplier);
+      result = executeBuildAndProcessResult(params, commandThreadManager, poolSupplier);
     } catch (ActionGraphCreationException e) {
       params.getConsole().printBuildFailure(e.getMessage());
-      exitCode = ExitCode.PARSE_ERROR;
+      result = ImmutableBuildRunResult.of(ExitCode.PARSE_ERROR, ImmutableList.of());
     } finally {
-      params.getBuckEventBus().post(BuildEvent.finished(started, exitCode));
+      params.getBuckEventBus().post(BuildEvent.finished(started, result.getExitCode()));
     }
 
-    return ImmutableBuildRunResult.of(exitCode, ImmutableList.of());
+    return result;
   }
 
   private BuildEvent.Started postBuildStartedEvent(CommandRunnerParams params) {
