4fee7d000413af5bd9f19c63f22a35c18f950401
Chris Hopman <cjhopman@fb.com>
2018-04-25 10:19:08 -0700

    Add protos for grpc remote execution and generated code
    Summary: These protos are a small part of the googleapis (available at https://github.com/googleapis/googleapis).
    
    Reviewed By: ttsugriy
    
    fbshipit-source-id: b8f37d3

---
+++ third-party/java/googleapis/update.sh
@@ -0,0 +1,2 @@
+buck build third-party/java/googleapis:bundle --out third-party/java/googleapis/googleapis.jar
+buck build third-party/java/googleapis:proto-gen --out third-party/java/googleapis/googleapis.src.jar
