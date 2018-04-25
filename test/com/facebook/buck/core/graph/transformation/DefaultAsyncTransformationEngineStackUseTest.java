4529aa8107e4bdb83b7f0eeb56959d810dde5c02
Bob Yang <bobyf@fb.com>
2018-04-25 11:13:26 -0700

    Fix travis test for AsyncTransformationEngine

--- test/com/facebook/buck/core/graph/transformation/DefaultAsyncTransformationEngineStackUseTest.java
+++ test/com/facebook/buck/core/graph/transformation/DefaultAsyncTransformationEngineStackUseTest.java
@@ -28,7 +28,7 @@ public class DefaultAsyncTransformationEngineStackUseTest {
   public void largeGraphShouldNotStackOverflow() {
     MutableGraph<Long> graph = GraphBuilder.directed().build();
     // We set -Xss500k for the JVM for this test, so our stack is very small.
-    for (long i = 1L; i <= 7000L; i++) {
+    for (long i = 1L; i <= 6000L; i++) {
       graph.addNode(i);
       if (i > 1) {
         graph.putEdge(i - 1, i);
@@ -37,8 +37,8 @@ public class DefaultAsyncTransformationEngineStackUseTest {
 
     ChildrenAdder transformer = new ChildrenAdder(graph);
     assertEquals(
-        (Long) 24503500L, // arithmetic series from 1 to 7000
-        // https://www.wolframalpha.com/input/?i=sum+from+1+to+7000
+        (Long) 18003000L, // arithmetic series from 1 to 6000
+        // https://www.wolframalpha.com/input/?i=sum+from+1+to+6000
         new DefaultAsyncTransformationEngine<>(transformer, graph.nodes().size())
             .computeUnchecked((Long) 1L));
   }
