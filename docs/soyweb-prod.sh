e1e29534b2b413faa8e4d3a586cb685991aa67ed
Carl Parker <carljparker@fb.com>
2018-04-25 16:28:02 -0700

    Add `ant clean` to the script that publishes the docs
    Summary:
    - Add `ant clean` to the beginning of the script that publishes the
      documentation to GitHub pages. This command removes any residual files
      in the Buck source tree that could interfere with the process of
      publishing the docs.
    
    Reviewed By: philipjameson
    
    fbshipit-source-id: bc3e186

--- docs/soyweb-prod.sh
+++ docs/soyweb-prod.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 # Always run this script from the root of the Buck project directory.
+
+#
+# Remove any residual files that could derail build and publication.
+#
+cd "$(git rev-parse --show-toplevel)" || exit
+ant clean
+
 cd "$(git rev-parse --show-toplevel)/docs" || exit
 exec java -jar plovr-81ed862.jar soyweb --port 9814 --dir . --globals globals.json
+
