0e4dcf038c51a02f39331f745e79b08ac5ad801e
Chris Hopman <cjhopman@fb.com>
2018-04-30 12:35:12 -0700

    Have AbstractCxxSymlinkTreeHeaders compute inputs correctly
    Summary:
    This users CustomFieldBehavior to add inputs for fields that aren't
    added to the rulekey. These are pretty straightforward.
    
    Reviewed By: ttsugriy
    
    fbshipit-source-id: cdfd9de

--- src/com/facebook/buck/cxx/AbstractCxxSymlinkTreeHeaders.java
+++ src/com/facebook/buck/cxx/AbstractCxxSymlinkTreeHeaders.java
@@ -26,13 +26,17 @@ import com.facebook.buck.core.util.immutables.BuckStyleImmutable;
 import com.facebook.buck.cxx.toolchain.HeaderSymlinkTree;
 import com.facebook.buck.rules.BuildRule;
 import com.facebook.buck.rules.SourcePathRuleFinder;
+import com.facebook.buck.rules.modern.CustomFieldInputs;
+import com.facebook.buck.rules.modern.annotations.CustomFieldBehavior;
 import com.facebook.buck.util.types.Either;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedMap;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+import javax.annotation.Nullable;
 import org.immutables.value.Value;
 
 /** Encapsulates headers modeled using a {@link HeaderSymlinkTree}. */
@@ -49,12 +53,14 @@ abstract class AbstractCxxSymlinkTreeHeaders extends CxxHeaders implements RuleK
   public abstract CxxPreprocessables.IncludeType getIncludeType();
 
   @Override
+  @CustomFieldBehavior(RootInputsBehavior.class)
   public abstract SourcePath getRoot();
 
   /**
    * @return the path to add to the preprocessor search path to find the includes. This defaults to
    *     the root, but can be overridden to use an alternate path.
    */
+  @CustomFieldBehavior(IncludeRootInputsBehavior.class)
   public abstract Either<PathSourcePath, SourcePath> getIncludeRoot();
 
   @Override
@@ -66,9 +72,11 @@ abstract class AbstractCxxSymlinkTreeHeaders extends CxxHeaders implements RuleK
   }
 
   @Override
+  @CustomFieldBehavior(HeaderMapInputsBehavior.class)
   public abstract Optional<SourcePath> getHeaderMap();
 
   @Value.Auxiliary
+  @CustomFieldBehavior(NameToPathMapInputsBehavior.class)
   abstract ImmutableSortedMap<Path, SourcePath> getNameToPathMap();
 
   @Override
@@ -138,5 +146,40 @@ abstract class AbstractCxxSymlinkTreeHeaders extends CxxHeaders implements RuleK
       builder.setIncludeRoot(Either.ofRight(symlinkTree.getRootSourcePath()));
     }
     return builder.build();
+  }
+
+  private static class NameToPathMapInputsBehavior
+      implements CustomFieldInputs<ImmutableSortedMap<Path, SourcePath>> {
+    @Override
+    public void getInputs(
+        ImmutableSortedMap<Path, SourcePath> value, Consumer<SourcePath> consumer) {
+      value.values().forEach(consumer);
+    }
+  }
+
+  private static class IncludeRootInputsBehavior
+      implements CustomFieldInputs<Either<PathSourcePath, SourcePath>> {
+    @Override
+    public void getInputs(Either<PathSourcePath, SourcePath> value, Consumer<SourcePath> consumer) {
+      if (value.isRight()) {
+        consumer.accept(value.getRight());
+      }
+    }
+  }
+
+  private static class RootInputsBehavior implements CustomFieldInputs<SourcePath> {
+    @Override
+    public void getInputs(SourcePath value, Consumer<SourcePath> consumer) {
+      consumer.accept(value);
+    }
+  }
+
+  private static class HeaderMapInputsBehavior implements CustomFieldInputs<SourcePath> {
+    @Override
+    public void getInputs(@Nullable SourcePath value, Consumer<SourcePath> consumer) {
+      if (value != null) {
+        consumer.accept(value);
+      }
+    }
   }
 }
