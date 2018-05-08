502cd3995ada109dc4d168b0d9b6289d62dec829
Sergey Balabanov <sergeyb@fb.com>
2018-05-07 17:37:57 -0700

    Fix logging for background threads' exceptions
    Reviewed By: andrewjcg
    
    fbshipit-source-id: a279ab3

--- src/com/facebook/buck/cli/Main.java
+++ src/com/facebook/buck/cli/Main.java
@@ -1777,13 +1777,12 @@ public final class Main {
           }
 
           // Do not log anything in case we do not have space on the disk
-          if (exitCode == ExitCode.FATAL_DISK_FULL) {
+          if (exitCode != ExitCode.FATAL_DISK_FULL) {
             LOG.error(e, "Uncaught exception from thread %s", t);
           }
 
           if (context.isPresent()) {
             // Shut down the Nailgun server and make sure it stops trapping System.exit().
-            //
             // We pass false for exitVM because otherwise Nailgun exits with code 0.
             context.get().getNGServer().shutdown(/* exitVM */ false);
           }
