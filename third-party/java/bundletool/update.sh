a9b5b47637c5f91fcb74f247c593a1b67231288b
Yibo Liu <yiboliu@fb.com>
2018-06-18 15:45:16 -0700

    Add config.proto and all things generated from compiling it
    Summary:
    config.proto is downloaded from https://github.com/google/bundletool/blob/master/src/main/proto/config.proto
    This is used to create java class files for GenerateConfigStep to create BundleConfig.pb.
    
    Reviewed By: styurin
    
    fbshipit-source-id: db2e95f

---
+++ third-party/java/bundletool/update.sh
@@ -0,0 +1,2 @@
+buck build //third-party/java/bundletool:bundle --out third-party/java/bundletool/bundle.jar
+buck build //third-party/java/bundletool:proto-gen --out third-party/java/bundletool/bundle.src.jar
