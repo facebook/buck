8f4f8a41da801bbfbddddc7e9e6f7b5be9d76fbc
Yibo Liu <yiboliu@fb.com>
2018-06-18 15:46:13 -0700

    Create a test for app bundle
    Summary: fork a copy of android binary test so that we can do the same thing for android bundle
    
    fbshipit-source-id: 2b3c02c

---
+++ test/com/facebook/buck/android/AndroidAppBundleIntegrationTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.android;
+
+import static org.junit.Assert.assertThat;
+
+import com.facebook.buck.io.filesystem.ProjectFilesystem;
+import com.facebook.buck.io.filesystem.TestProjectFilesystems;
+import com.facebook.buck.jvm.java.testutil.AbiCompilationModeTest;
+import com.facebook.buck.model.BuildTargetFactory;
+import com.facebook.buck.model.BuildTargets;
+import com.facebook.buck.testutil.ProcessResult;
+import com.facebook.buck.testutil.TemporaryPaths;
+import com.facebook.buck.testutil.integration.ProjectWorkspace;
+import com.facebook.buck.testutil.integration.TestDataHelper;
+import com.facebook.buck.util.zip.ZipConstants;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import org.apache.commons.compress.archivers.zip.ZipUtil;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AndroidAppBundleIntegrationTest extends AbiCompilationModeTest {
+
+  private ProjectWorkspace workspace;
+  @Rule public TemporaryPaths tmpFolder = new TemporaryPaths(true);
+  private ProjectFilesystem filesystem;
+
+  @Before
+  public void setUp() throws InterruptedException, IOException {
+    AssumeAndroidPlatform.assumeSdkIsAvailable();
+    AssumeAndroidPlatform.assumeNdkIsAvailable();
+    workspace =
+        TestDataHelper.createProjectWorkspaceForScenario(this, "android_project", tmpFolder);
+    workspace.setUp();
+    setWorkspaceCompilationMode(workspace);
+    filesystem = TestProjectFilesystems.createProjectFilesystem(workspace.getDestPath());
+  }
+
+  @Test
+  public void testAppBundleHaveDeterministicTimestamps() throws IOException {
+    String target = "//apps/sample:app_bundle";
+    ProcessResult result = workspace.runBuckCommand("build", target);
+    result.assertSuccess();
+
+    // Iterate over each of the entries, expecting to see all zeros in the time fields.
+    Path apk =
+        workspace.getPath(
+            BuildTargets.getGenPath(filesystem, BuildTargetFactory.newInstance(target), "%s.apk"));
+    Date dosEpoch = new Date(ZipUtil.dosToJavaTime(ZipConstants.DOS_FAKE_TIME));
+    try (ZipInputStream is = new ZipInputStream(Files.newInputStream(apk))) {
+      for (ZipEntry entry = is.getNextEntry(); entry != null; entry = is.getNextEntry()) {
+        assertThat(entry.getName(), new Date(entry.getTime()), Matchers.equalTo(dosEpoch));
+      }
+    }
+  }
+}
