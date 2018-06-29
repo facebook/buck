61ff36bece3297143605cd1f1afeb4ffa7321376
Sergey Tyurin <tyurins@fb.com>
2018-06-28 22:05:03 -0700

    Run Android tests with Android 15 and 16 in Travis CI

--- scripts/travisci_run.sh
+++ scripts/travisci_run.sh
@@ -34,6 +34,11 @@ if [ "$CI_ACTION" = "heavy_integration" ]; then
   ./bin/buck test  --num-threads=1 //test/com/facebook/buck/android/... //test/com/facebook/buck/jvm/java/... --filter '.*[Ii]ntegration.*'
 fi
 
+if [ "$CI_ACTION" = "android_ndk_15" ] || [ "$CI_ACTION" = "android_ndk_16" ]; then
+  ./bin/buck build --num-threads=$BUCK_NUM_THREADS //test/com/facebook/buck/android/...
+  ./bin/buck test  --num-threads=1 //test/com/facebook/buck/android/... --filter '.*[Ii]ntegration.*'
+fi
+
 if [ "$CI_ACTION" = "ant" ]; then
   # Run all the other checks with ant.
   ant travis
