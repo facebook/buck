61f29548707079d54ba9457cfc6ccc76c0d597f6
Yibo Liu <yiboliu@fb.com>
2018-07-10 11:39:59 -0700

    Rename AndroidBinary to InstallableApk
    Summary: Rename AndroidBinary to InstallableApk to match its type
    
    Reviewed By: cjhopman
    
    fbshipit-source-id: 453d7d5

--- src/com/facebook/buck/android/AndroidBinaryInstallGraphEnhancer.java
+++ src/com/facebook/buck/android/AndroidBinaryInstallGraphEnhancer.java
@@ -47,17 +47,17 @@ class AndroidBinaryInstallGraphEnhancer {
 
   private ProjectFilesystem projectFilesystem;
   private BuildTarget buildTarget;
-  private HasInstallableApk androidBinary;
+  private HasInstallableApk installableApk;
   private AndroidInstallConfig androidInstallConfig;
 
   AndroidBinaryInstallGraphEnhancer(
       AndroidInstallConfig androidInstallConfig,
       ProjectFilesystem projectFilesystem,
       BuildTarget buildTarget,
-      HasInstallableApk androidBinary) {
+      HasInstallableApk installableApk) {
     this.projectFilesystem = projectFilesystem;
     this.buildTarget = buildTarget.withFlavors(INSTALL_FLAVOR);
-    this.androidBinary = androidBinary;
+    this.installableApk = installableApk;
     this.androidInstallConfig = androidInstallConfig;
   }
 
@@ -74,11 +74,11 @@ class AndroidBinaryInstallGraphEnhancer {
   }
 
   private boolean exopackageEnabled() {
-    return androidBinary.getApkInfo().getExopackageInfo().isPresent();
+    return installableApk.getApkInfo().getExopackageInfo().isPresent();
   }
 
   private void enhanceForConcurrentExopackageInstall(ActionGraphBuilder graphBuilder) {
-    ApkInfo apkInfo = androidBinary.getApkInfo();
+    ApkInfo apkInfo = installableApk.getApkInfo();
     Preconditions.checkState(apkInfo.getExopackageInfo().isPresent());
 
     ExopackageDeviceDirectoryLister directoryLister =
@@ -169,7 +169,7 @@ class AndroidBinaryInstallGraphEnhancer {
 
   private void enhanceForConcurrentInstall(ActionGraphBuilder graphBuilder) {
     graphBuilder.addToIndex(
-        new AndroidBinaryNonExoInstaller(buildTarget, projectFilesystem, androidBinary));
+        new AndroidBinaryNonExoInstaller(buildTarget, projectFilesystem, installableApk));
   }
 
   private void enhanceForLegacyInstall(ActionGraphBuilder graphBuilder) {
