e0403933d2a1f50a64cf5dcbc56a98712261fc31
Josiah Gaskin <jcg@fb.com>
2018-07-25 10:44:34 -0700

    Fix non-predexed builds by only augmenting R.java patterns for predex splitter

--- src/com/facebook/buck/android/DexSplitMode.java
+++ src/com/facebook/buck/android/DexSplitMode.java
@@ -17,6 +17,7 @@
 package com.facebook.buck.android;
 
 import com.facebook.buck.android.dalvik.ZipSplitter;
+import com.facebook.buck.android.dalvik.ZipSplitter.DexSplitStrategy;
 import com.facebook.buck.core.rulekey.AddToRuleKey;
 import com.facebook.buck.core.rulekey.AddsToRuleKey;
 import com.facebook.buck.core.sourcepath.SourcePath;
@@ -39,7 +40,8 @@ class DexSplitMode implements AddsToRuleKey {
           /* primaryDexScenarioFile */ Optional.empty(),
           /* isPrimaryDexScenarioOverflowAllowed */ false,
           /* secondaryDexHeadClassesFile */ Optional.empty(),
-          /* secondaryDexTailClassesFile */ Optional.empty());
+          /* secondaryDexTailClassesFile */ Optional.empty(),
+          /* allowRDotJavaInSecondaryDex */ false);
 
   /**
    * By default, assume we have 5MB of linear alloc, 1MB of which is taken up by the framework, so
@@ -115,26 +117,32 @@ class DexSplitMode implements AddsToRuleKey {
   @AddToRuleKey private final Optional<SourcePath> secondaryDexTailClassesFile;
 
   /**
+   * Boolean identifying whether we should allow the dex splitting to move R classes into secondary
+   * dex files.
+   */
+  @AddToRuleKey private boolean allowRDotJavaInSecondaryDex;
+
+  /**
    * @param primaryDexPatterns Set of substrings that, when matched, will cause individual input
    *     class or resource files to be placed into the primary jar (and thus the primary dex
    *     output). These classes are required for correctness.
    * @param primaryDexClassesFile Path to a file containing a list of classes that must be included
    *     in the primary dex. These classes are required for correctness.
    * @param primaryDexScenarioFile Path to a file containing a list of classes used in a scenario
-   *     that should be included in the primary dex along with all dependency classes required for
-   *     preverification. These dependencies will be calculated by buck. This list is used for
-   *     performance, not correctness.
+ *     that should be included in the primary dex along with all dependency classes required for
+ *     preverification. These dependencies will be calculated by buck. This list is used for
+ *     performance, not correctness.
    * @param isPrimaryDexScenarioOverflowAllowed A boolean indicating whether to fail the build if
-   *     any classes required by primaryDexScenarioFile cannot fit (false) or to allow the build to
-   *     to proceed on a best-effort basis (true).
+*     any classes required by primaryDexScenarioFile cannot fit (false) or to allow the build to
+*     to proceed on a best-effort basis (true).
    * @param secondaryDexHeadClassesFile Path to a file containing a list of classes that are put in
-   *     the first secondary dexes.
+*     the first secondary dexes.
    * @param secondaryDexTailClassesFile Path to a file containing a list of classes that are put in
-   *     the last secondary dexes.
+   * @param allowRDotJavaInSecondaryDex whether to allow R.java classes in the secondary dex files
    */
   public DexSplitMode(
       boolean shouldSplitDex,
-      ZipSplitter.DexSplitStrategy dexSplitStrategy,
+      DexSplitStrategy dexSplitStrategy,
       DexStore dexStore,
       long linearAllocHardLimit,
       Collection<String> primaryDexPatterns,
@@ -142,7 +150,8 @@ class DexSplitMode implements AddsToRuleKey {
       Optional<SourcePath> primaryDexScenarioFile,
       boolean isPrimaryDexScenarioOverflowAllowed,
       Optional<SourcePath> secondaryDexHeadClassesFile,
-      Optional<SourcePath> secondaryDexTailClassesFile) {
+      Optional<SourcePath> secondaryDexTailClassesFile,
+      boolean allowRDotJavaInSecondaryDex) {
     this.shouldSplitDex = shouldSplitDex;
     this.dexSplitStrategy = dexSplitStrategy;
     this.dexStore = dexStore;
@@ -153,6 +162,7 @@ class DexSplitMode implements AddsToRuleKey {
     this.isPrimaryDexScenarioOverflowAllowed = isPrimaryDexScenarioOverflowAllowed;
     this.secondaryDexHeadClassesFile = secondaryDexHeadClassesFile;
     this.secondaryDexTailClassesFile = secondaryDexTailClassesFile;
+    this.allowRDotJavaInSecondaryDex = allowRDotJavaInSecondaryDex;
   }
 
   public DexStore getDexStore() {
@@ -194,5 +204,9 @@ class DexSplitMode implements AddsToRuleKey {
 
   public Optional<SourcePath> getSecondaryDexTailClassesFile() {
     return secondaryDexTailClassesFile;
+  }
+
+  public boolean isAllowRDotJavaInSecondaryDex() {
+    return allowRDotJavaInSecondaryDex;
   }
 }
