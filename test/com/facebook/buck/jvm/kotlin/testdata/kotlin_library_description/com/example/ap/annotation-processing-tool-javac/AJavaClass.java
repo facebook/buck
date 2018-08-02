83213c36dfcc2d25b21412749a27359e82455ded
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2018-08-02 14:41:39 -0700

    Rm commented code

--- test/com/facebook/buck/jvm/kotlin/testdata/kotlin_library_description/com/example/ap/annotation-processing-tool-javac/AJavaClass.java
+++ test/com/facebook/buck/jvm/kotlin/testdata/kotlin_library_description/com/example/ap/annotation-processing-tool-javac/AJavaClass.java
@@ -3,8 +3,5 @@ package com.example.ap;
 class AJavaClass {
   public void foo() {
     new JavaClassWithJavaAnnotation_();
-    //new KotlinClassWithJavaAnnotation_();
-    //new JavaClassWithKotlinAnnotation_();
-    //new KotlinClassWithJavaAnnotation_();
   }
 }
--- test/com/facebook/buck/jvm/kotlin/testdata/kotlin_library_description/com/example/ap/annotation-processing-tool-javac/AKotlinClass.kt
+++ test/com/facebook/buck/jvm/kotlin/testdata/kotlin_library_description/com/example/ap/annotation-processing-tool-javac/AKotlinClass.kt
@@ -2,9 +2,5 @@ package com.example.ap
 
 class AKotlinClass {
     fun foo() {
-        //JavaClassWithJavaAnnotation_()
-        //KotlinClassWithJavaAnnotation_()
-        //JavaClassWithKotlinAnnotation_()
-        //KotlinClassWithJavaAnnotation_()
     }
 }
