234fe3fadfa9dbacf3e3d438a08f69d6460167eb
Rui Barbosa Martins <ruibm@fb.com>
2018-09-02 13:31:01 -0700

    Update third-party/java/thrift from version 0.10.0 to 0.11.0.
    Summary: [buck] Update third-party/java/thrift from version 0.10.0 to 0.11.0.
    
    fbshipit-source-id: d3141a52e2

--- third-party/java/thrift/gen.sh
+++ third-party/java/thrift/gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash -ex
-for f in $(git ls-files -- *.thrift); do
+for f in $(git ls-files -- *.thrift | grep -v "modern/builders/thrift"); do
   /usr/local/bin/thrift --gen java:generated_annotations=undated -out src-gen/ $f
 done
