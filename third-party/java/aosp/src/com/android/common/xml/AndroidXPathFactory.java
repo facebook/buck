30f82dbad95ccf096edee4b7e6a3f34c846c2631
J Torkkola <jtorkkola@fb.com>
2018-09-05 20:44:11 -0700

    Handle breaking change in javax.xml.namespace.NamepaceContext interface
    Summary:
    Java 10 changed the `javax.xml.namespace.NamespaceContext` interface in a breaking way.
    
    Java 9-: `Iterator getPrefixes​(String namespaceURI)`
    Java 10: `Iterator<String> getPrefixes​(String namespaceURI)`
    
    Luckily, due to type erasure, we can handle this update without breaking Java 8 builds. Note that Google's latest copy of [AndroidXPathFactor.java](https://android.googlesource.com/platform/tools/base/+/master/common/src/main/java/com/android/xml/AndroidXPathFactory.java) hasn't been updated to handle this.
    
    Reviewed By: styurin
    
    fbshipit-source-id: 5d8a300994

--- third-party/java/aosp/src/com/android/common/xml/AndroidXPathFactory.java
+++ third-party/java/aosp/src/com/android/common/xml/AndroidXPathFactory.java
@@ -81,7 +81,7 @@ public class AndroidXPathFactory {
         }
 
         @Override
-        public Iterator<?> getPrefixes(String namespaceURI) {
+        public Iterator<String> getPrefixes(String namespaceURI) {
             if (SdkConstants.NS_RESOURCES.equals(namespaceURI)) {
                 return mAndroidPrefixes.iterator();
             }
