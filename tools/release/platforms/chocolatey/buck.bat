e11805a973a817a5dd69e9cfd375bd2980a507fa
Philip Jameson <pjameson@fb.com>
2018-09-24 13:18:44 -0700

    Try to use c:\Python2.7\python.exe before defaulting to python.exe on path
    Summary:
    Attempt to use python2 binary directly before falling back to 'python.exe' on the path
    
    Fixes 2011
    
    Reviewed By: styurin
    
    fbshipit-source-id: 244e66f023

--- tools/release/platforms/chocolatey/buck.bat
+++ tools/release/platforms/chocolatey/buck.bat
@@ -1,3 +1,10 @@
 @echo off
 REM Invoke buck.pex in the same directory as this script with python
-python %~dp0\buck.pex %*
+REM We try to find a python2 installation first because python3 can
+REM install itself as 'python.exe' on the path, and we don't want that
+REM just yet
+if exist "C:\Python27\python.exe" (
+  C:\Python27\python.exe %~dp0\buck.pex %*
+) else (
+  python %~dp0\buck.pex %*
+)
