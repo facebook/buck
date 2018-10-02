febd03d677e58413f7d31ea93114f67dc930e278
Lubomir Litchev <lubol@fb.com>
2018-10-02 14:52:41 -0700

    Add Fingerprinter calculator.
    Summary: This is the implementation of weak and strong fingerprints.
    
    Reviewed By: ttsugriy
    
    fbshipit-source-id: a07f2623ac

---
+++ src/com/facebook/buck/parser/cache/Fingerprinter.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.parser.cache;
+
+import com.facebook.buck.io.filesystem.ProjectFilesystem;
+import com.facebook.buck.util.config.Config;
+import com.facebook.buck.util.environment.Architecture;
+import com.facebook.buck.util.environment.Platform;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+
+/**
+ * This class implements functionality to get the weak fingerprint and strong fingerprint hashes for
+ * caching parser result - {@link com.facebook.buck.parser.api.BuildFileManifest}.
+ *
+ * <p>Note: The Fingerprinter object provides fingerprints for the two-level cache used to identify
+ * the relevant {@link com.facebook.buck.parser.api.BuildFileManifest} objects in the cache. The
+ * first level cache is provided by the so called weak fingerprint. The weak fingerprint encodes
+ * some common configuration/environments used by the build file parser, and the name of the file
+ * that is associated with the cell currently being built. The weak fingerprint maps to a set of
+ * strong fingerprints that are relevant. Examples of entries that are currently included into a
+ * weak fingerprint are the buck configuration environment, the name of the build file associated
+ * with the cell object being built, platform and architecture for which the build is performed.
+ *
+ * <p>The strong fingerprint is used to narrow a specific version of the pre-parsed serialized
+ * objects - it adds another level of lookup rules that point to a specific match of the current
+ * build files states, from a set that use the same weak fingerprints. Currently the strong
+ * fingerprint includes the content hashes of all the loaded build files during the parsing of the
+ * build spec of interest.
+ *
+ * <p>Splitting the lookup into two phases reduces the data that needs to be transferred locally
+ * since only the entries that match the weak and strong fingerprint could be relevant. More
+ * importantly, it reduces the amount of glob operations that need to be performed to validate the
+ * use of the cached {@link com.facebook.buck.parser.api.BuildFileManifest} - only the globs for the
+ * potential match, both weak and strong fingerprints are performed.
+ */
+public final class Fingerprinter {
+  private Fingerprinter() {}
+
+  /**
+   * Gets the weak fingerprint for this configuration.
+   *
+   * @param buildFile the path to the BUCK file build spec of interest.
+   * @param config the {@link Config} object to calculate the weak fingerprint for
+   * @return a weak fingerprint - {@link com.google.common.hash.HashCode} that represent a unique
+   *     hash value.
+   */
+  public static HashCode getWeakFingerprint(Path buildFile, Config config) {
+    Hasher hasher = Hashing.sha256().newHasher();
+    return hasher
+        .putString(buildFile.toString(), StandardCharsets.UTF_8)
+        .putBytes(config.getOrderIndependentHashCode().asBytes())
+        .putString(Platform.detect().name(), StandardCharsets.UTF_8)
+        .putString(Architecture.detect().name(), StandardCharsets.UTF_8)
+        .hash();
+  }
+
+  private static void addContentHash(ProjectFilesystem fs, Path filePath, Hasher hasher)
+      throws IOException {
+    hasher.putString(fs.computeSha256(filePath), StandardCharsets.UTF_8);
+  }
+
+  private static void addIncludeEntryToFingerprint(
+      ProjectFilesystem fs, Hasher hasher, String value) throws IOException {
+    Path includePath = fs.getPath(value);
+    hasher.putString(fs.relativize(includePath).toString(), StandardCharsets.UTF_8);
+    addContentHash(fs, includePath, hasher);
+  }
+
+  /**
+   * Calculates a strong fingerprint.
+   *
+   * @param fs the {@Link ProjectFilesystem} that we use to calculate the strong fingerprint.
+   * @param includes the list of included build files for which we calculate the strong fingerprint.
+   * @return a strong fingerprint - {@Link HashCode} that represent a unique hash value.
+   * @throws IOException can throw if there is a problem getting the content of the main BUCK build
+   *     spec or an included file.
+   */
+  public static HashCode getStrongFingerprint(ProjectFilesystem fs, ImmutableList<String> includes)
+      throws IOException {
+    Hasher hasher = Hashing.sha256().newHasher();
+
+    // Now add the loaded paths.
+    ImmutableSortedSet<String> sortedIncludes = ImmutableSortedSet.copyOf(includes);
+    for (String value : sortedIncludes) {
+      addIncludeEntryToFingerprint(fs, hasher, value);
+    }
+
+    return hasher.hash();
+  }
+}
---
+++ test/com/facebook/buck/parser/cache/FingerprinterTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.parser.cache;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeThat;
+
+import com.facebook.buck.core.config.BuckConfig;
+import com.facebook.buck.core.config.FakeBuckConfig;
+import com.facebook.buck.io.filesystem.ProjectFilesystem;
+import com.facebook.buck.io.filesystem.impl.FakeProjectFilesystem;
+import com.facebook.buck.parser.api.BuildFileManifest;
+import com.facebook.buck.util.config.Config;
+import com.facebook.buck.util.environment.Architecture;
+import com.facebook.buck.util.environment.Platform;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.Optional;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class FingerprinterTest {
+  @Rule public ExpectedException expectedThrownException = ExpectedException.none();
+
+  private ProjectFilesystem fs;
+
+  @Before
+  public void setUp() {
+    // JimFS has issues with absolute and relative Windows paths.
+    assumeThat(Platform.detect(), Matchers.not(Platform.WINDOWS));
+
+    fs = FakeProjectFilesystem.createJavaOnlyFilesystem("/");
+  }
+
+  private BuckConfig getConfigForTest() {
+    return FakeBuckConfig.builder()
+        .setSections(
+            "[parser]", "local_parser_cache_location = foobar", "parser_caching_enabled = barFoo")
+        .setSections("[project]", "Z = Z", "Y = Y")
+        .setFilesystem(fs)
+        .build();
+  }
+
+  private static void addContentHash(Path filePath, ProjectFilesystem fs, Hasher hasher)
+      throws IOException {
+    hasher.putString(fs.computeSha256(filePath), StandardCharsets.UTF_8);
+  }
+
+  @Test
+  public void calculateWeakFingerprint() {
+    Config config = getConfigForTest().getConfig();
+    Path buildFilePath = fs.getPath("Foo/Bar");
+    HashCode weakFingerprintHash = Fingerprinter.getWeakFingerprint(buildFilePath, config);
+    HashCode expectedFingerprint =
+        Hashing.sha256()
+            .newHasher()
+            .putString(buildFilePath.toString(), StandardCharsets.UTF_8)
+            .putBytes(config.getOrderIndependentHashCode().asBytes())
+            .putString(Platform.detect().name(), StandardCharsets.UTF_8)
+            .putString(Architecture.detect().name(), StandardCharsets.UTF_8)
+            .hash();
+    assertEquals(expectedFingerprint, weakFingerprintHash);
+  }
+
+  @Test
+  public void calculateStrongFingerprintWithAllBuildFilesPresent() throws IOException {
+    fs.mkdirs(fs.getPath("foo/bar"));
+    fs.createNewFile(fs.getPath("foo/bar/FooBar.bzl"));
+    fs.createNewFile(fs.getPath("foo/bar/BUCK"));
+    fs.createNewFile(fs.getPath("foo/bar/BarFoo.bzl"));
+    ImmutableList<String> includes =
+        ImmutableList.of("/foo/bar/FooBar.bzl", "/foo/bar/BUCK", "/foo/bar/BarFoo.bzl");
+    BuildFileManifest buildFileManifest =
+        BuildFileManifest.of(
+            ImmutableList.of(), includes, ImmutableMap.of(), Optional.empty(), ImmutableList.of());
+    HashCode strongFingerprintHash = Fingerprinter.getStrongFingerprint(fs, includes);
+    ImmutableSortedSet<String> sortedIncludes =
+        ImmutableSortedSet.copyOf(buildFileManifest.getIncludes());
+    Hasher hasher = Hashing.sha256().newHasher();
+    for (String value : sortedIncludes) {
+      Path includePath = fs.getPath(value);
+      hasher.putString(fs.relativize(includePath).toString(), StandardCharsets.UTF_8);
+      addContentHash(includePath, fs, hasher);
+    }
+
+    HashCode expectedHash = hasher.hash();
+    assertEquals(expectedHash, strongFingerprintHash);
+  }
+
+  @Test
+  public void strongFingerprintWithMissingBuildFileThrows() throws IOException {
+    expectedThrownException.expect(NoSuchFileException.class);
+    expectedThrownException.expectMessage("/foo/bar/BUCK");
+
+    fs.mkdirs(fs.getPath("foo/bar"));
+    fs.createNewFile(fs.getPath("foo/bar/FooBar.bzl"));
+    fs.createNewFile(fs.getPath("foo/bar/BarFoo.bzl"));
+    ImmutableList<String> includes =
+        ImmutableList.of("/foo/bar/FooBar.bzl", "/foo/bar/BUCK", "/foo/bar/BarFoo.bzl");
+    BuildFileManifest buildFileManifest =
+        BuildFileManifest.of(
+            ImmutableList.of(), includes, ImmutableMap.of(), Optional.empty(), ImmutableList.of());
+
+    Fingerprinter.getStrongFingerprint(fs, buildFileManifest.getIncludes());
+  }
+}
