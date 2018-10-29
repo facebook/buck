37b10d707bb8987abdd9c60177ce4d12abb5c5a4
Bob Yang <bobyf@fb.com>
2018-10-29 11:30:23 -0700

    Fix missing rename for bazel third-party
    Summary:
    renaming skylark-lang to bazel caused some breakages in
    the third-party update script
    
    Reviewed By: styurin, ttsugriy
    
    fbshipit-source-id: 63ad33c461

--- third-party/java/bazel/update.sh
+++ third-party/java/bazel/update.sh
@@ -10,13 +10,13 @@ readonly FINAL_JAR_NAME="bazel"
 git -C "${WORK_DIR}" clone --depth 1 https://github.com/bazelbuild/bazel.git
 pushd "${BAZEL_DIR}"
 git apply "${MY_DIR}/bazel.patch"
-bazel build //src/main/java/com/google/devtools/build/lib:bazel_deploy.jar \
-  //src/main/java/com/google/devtools/build/lib:bazel_deploy-src.jar
+bazel build //src/main/java/com/google/devtools/build/lib:bazel-jar_deploy.jar \
+  //src/main/java/com/google/devtools/build/lib:bazel-jar_deploy-src.jar
 java -jar "${JARJAR_PATH}" process "${MY_DIR}/jarjar-rules.txt" \
-  bazel-bin/src/main/java/com/google/devtools/build/lib/bazel_deploy.jar \
+  bazel-bin/src/main/java/com/google/devtools/build/lib/bazel-jar_deploy.jar \
   "${MY_DIR}/${FINAL_JAR_NAME}_deploy.jar"
 java -jar "${JARJAR_PATH}" process "${MY_DIR}/jarjar-rules.txt" \
-  bazel-bin/src/main/java/com/google/devtools/build/lib/bazel_deploy-src.jar \
+  bazel-bin/src/main/java/com/google/devtools/build/lib/bazel-jar_deploy-src.jar \
   "${MY_DIR}/${FINAL_JAR_NAME}_deploy-src.jar"
 
 readonly commit_hash=$(git rev-parse HEAD)
