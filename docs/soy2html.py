6b65c00578b55998437c45a07102cf0c32b312ca
Bob Yang <bobyf@fb.com>
2018-12-02 18:17:24 -0800

    Add licensing to py files
    Summary: Add license
    
    Reviewed By: cjhopman
    
    fbshipit-source-id: 572a922dd7

--- docs/soy2html.py
+++ docs/soy2html.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+# Copyright 2018-present Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 #
 # This script is designed to be invoked by soy2html.sh.
 # Usage:
@@ -14,6 +27,7 @@
 #
 # When this script is run, soyweb should already be running locally on port
 # 9814 via ./docs/soyweb-prod.sh.
+
 
 import os
 import subprocess
