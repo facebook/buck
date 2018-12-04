4c5ae5499cb82dbb76ad73f30db93741bdd5af9a
Dulmandakh <dulmandakh@gmail.com>
2018-12-05 00:45:03 +0800

    BUCK_NUM_THREADS=3, echo -Xmx500m > .buckjavaargs.local

--- scripts/travisci_run.sh
+++ scripts/travisci_run.sh
@@ -14,7 +14,7 @@ ant
 # There are only two cores in the containers, but Buck thinks there are 12
 # and tries to use all of them.  As a result, we seem to have our test
 # processes killed if we do not limit our threads.
-export BUCK_NUM_THREADS=2
+export BUCK_NUM_THREADS=3
 
 if [ "$CI_ACTION" = "build" ]; then
   # Make sure that everything builds in case a library is not covered by a test.
