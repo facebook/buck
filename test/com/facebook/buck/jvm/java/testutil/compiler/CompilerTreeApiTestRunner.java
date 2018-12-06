dedc9fb5cdd2e016f01a8f7d5ad27a113819d70b
J Torkkola <jtorkkola@fb.com>
2018-12-05 18:04:28 -0800

    Fix Compiler Tree API test classloader crash on Java9+
    Summary: Fixes crash in the specialized test class loader for tests that use the Compiler Tree API. This API used to be in `tools.jar`, but this no longer exists in Java9+. The Compiler Tree API is now part of the JDK proper, and available via the system class loader (assuming the right module is pulled in). `ToolProvider.getSystemToolClassLoader()` actually always returns null in Java9+.
    
    Reviewed By: styurin
    
    fbshipit-source-id: 87537ee79d

--- test/com/facebook/buck/jvm/java/testutil/compiler/CompilerTreeApiTestRunner.java
+++ test/com/facebook/buck/jvm/java/testutil/compiler/CompilerTreeApiTestRunner.java
@@ -16,30 +16,40 @@
 
 package com.facebook.buck.jvm.java.testutil.compiler;
 
+import com.facebook.buck.util.JavaVersion;
+import com.facebook.buck.util.MoreSuppliers;
+import com.google.common.base.Preconditions;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.function.Supplier;
 import javax.tools.ToolProvider;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
 /**
  * Test runner that enables tests to work with the Compiler Tree API implementation corresponding to
- * the compiler returned by {@link ToolProvider#getSystemJavaCompiler()}. These are public APIs that
- * are not provided in rt.jar and thus are not usually on the classpath.
+ * the compiler returned by {@link ToolProvider#getSystemJavaCompiler()}. In Java 8 and earlier,
+ * these are public APIs that are not provided in rt.jar and thus are not usually on the classpath.
  */
 public class CompilerTreeApiTestRunner extends BlockJUnit4ClassRunner {
-  private static final TestClassLoader TEST_CLASS_LOADER = new TestClassLoader();
+  private static final Supplier<TestClassLoader> TEST_CLASS_LOADER =
+      MoreSuppliers.memoize(() -> new TestClassLoader());
 
   public CompilerTreeApiTestRunner(Class<?> klass) throws InitializationError {
     // The way runners work, the test class has already been loaded (that's how it knows which
     // runner to use). We'll ignore that one, though, and load the version from our class loader
-    // which has access to the Compiler Tree API. This is hacky and wrong, but it's test code. :-)
+    // which has access to the Compiler Tree API, in Java 8. This is hacky and wrong, but it's test
+    // code. :-) In Java 9+, the Compiler Tree API is part of the JCL proper, so we can skip this
+    // nonsense.
     super(reloadFromCompilerClassLoader(klass));
   }
 
   public static Class<?> reloadFromCompilerClassLoader(Class<?> clazz) throws InitializationError {
+    if (JavaVersion.getMajorVersion() >= 9) {
+      return clazz;
+    }
     try {
-      return Class.forName(clazz.getName(), true, TEST_CLASS_LOADER);
+      return Class.forName(clazz.getName(), true, TEST_CLASS_LOADER.get());
     } catch (ClassNotFoundException e) {
       throw new InitializationError(e);
     }
@@ -51,6 +61,9 @@ public class CompilerTreeApiTestRunner extends BlockJUnit4ClassRunner {
     }
 
     private static URL[] getSystemClassLoaderUrls() {
+      // Note: This only works on Java 8 and earlier, as in Java 9+, the system class loader is no
+      // longer a URLClassLoader.
+      Preconditions.checkState(JavaVersion.getMajorVersion() < 9);
       URLClassLoader systemClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
       return systemClassLoader.getURLs();
     }
