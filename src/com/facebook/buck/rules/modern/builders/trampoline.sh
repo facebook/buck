ba4b0e8a2c705aea668fee5c6f6314fb76b41cb0
Bob Yang <bobyf@fb.com>
2018-12-05 16:31:45 -0800

    Format sh files with license
    Summary: add license
    
    Reviewed By: carljparker
    
    fbshipit-source-id: 581ab669bf

--- src/com/facebook/buck/rules/modern/builders/trampoline.sh
+++ src/com/facebook/buck/rules/modern/builders/trampoline.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+# Copyright 2018-present Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 # In remote execution, a binary is run at the root directory. For Buck, this root directory may not
 # be the same as the build root (the root directory is the common ancestor of all the present
 # cells).
 # This trampoline is used to run the buck process in the correct subdirectory.
 # It also absolutizes the buck classpath and the bootstrap classpath.
+
 
 # Run with -e so the script will fail if any of the steps fail.
 set -e
