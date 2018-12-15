dd4dc24c6f6e497951038b17270654dc053e2917
Dulmandakh <dulmandakh@gmail.com>
2018-12-15 21:43:43 +0800

    use sdkmanager from PATH

--- scripts/android-sdk-setup.sh
+++ scripts/android-sdk-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$ANDROID_HOME/tools/bin/sdkmanager 'build-tools;23.0.2' 'platforms;android-23' 'platforms;android-21'  'add-ons;addon-google_apis-google-21' 'add-ons;addon-google_apis-google-23'
+sdkmanager 'build-tools;23.0.2' 'platforms;android-23' 'platforms;android-21'  'add-ons;addon-google_apis-google-21' 'add-ons;addon-google_apis-google-23'
