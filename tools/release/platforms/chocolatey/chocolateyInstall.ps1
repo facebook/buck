18c5808571a1ea7a07349b706d2bd1431172faf3
Bob Yang <bobyf@fb.com>
2018-12-14 16:37:59 -0800

    Add licenses to powershell scripts
    Summary: .
    
    Reviewed By: carljparker
    
    fbshipit-source-id: 496a505877

--- tools/release/platforms/chocolatey/chocolateyInstall.ps1
+++ tools/release/platforms/chocolatey/chocolateyInstall.ps1
@@ -1,3 +1,16 @@
+# Copyright 2018-present Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 # We use a .bat file instead of .ps1 because the chocolatey shim lib does not
 # properly execute .ps1 files, but it does .bat files
 Install-BinFile -Name buck -Path "${env:ChocolateyPackageFolder}\tools\buck.bat"
