093b9b0392f92bd35dfe657646fb71363a9c7671
Chris Tolliday <ctolliday@fb.com>
2018-12-21 14:49:00 -0800

    Fix StringTemplate race condition that produces malformed project files
    Summary:
    Found by running this inside IjProjectWriter.write(), setting a breakpoint and checking the ST objects that output different hashes, they have the same contents but the compiled templates do not match.
        projectDataPreparer
            .getModulesToBeWritten()
            .parallelStream()
            .forEach(
                module -> {
                  ConcurrentHashMap<Sha1HashCode, ST> roots = new ConcurrentHashMap<>();
                  IntStream.range(0, 1000).parallel().forEach(i -> {
                    try {
                      ST moduleContents = StringTemplateFile.MODULE_TEMPLATE.getST();
                      moduleContents.add("contentRoots", projectDataPreparer.getContentRoots(module));
                      moduleContents.add("dependencies", projectDataPreparer.getDependencies(module));
                      moduleContents.add(
                          "generatedSourceFolders", projectDataPreparer.getGeneratedSourceFolders(module));
                      moduleContents.add("androidFacet", projectDataPreparer.getAndroidProperties(module));
                      moduleContents.add("sdk", module.getModuleType().getSdkName(projectConfig).orElse(null));
                      moduleContents.add("sdkType", module.getModuleType().getSdkType(projectConfig));
                      moduleContents.add(
                          "languageLevel",
                          JavaLanguageLevelHelper.convertLanguageLevelToIjFormat(
                              module.getLanguageLevel().orElse(null)));
                      moduleContents.add("moduleType", module.getModuleType().getImlModuleType());
                      moduleContents.add(
                          "metaInfDirectory",
                          module
                              .getMetaInfDirectory()
                              .map((dir) -> getUrl(projectPaths.getModuleQualifiedPath(dir, module)))
                              .orElse(null));
                      Path tempPath = Files.createTempFile("file","module");
                      writeTemplate(moduleContents, tempPath);
                      Sha1HashCode hash = projectFilesystem.computeSha1(tempPath);
                      if (!roots.contains(hash)) {
                        roots.put(hash, moduleContents);
                      }
                    } catch (IOException exception) {
                      throw new RuntimeException(exception);
                    }
                  });
                  if (roots.size() != 1) {
                    throw new RuntimeException("multiple hashes!");
                  }
                }
            );
    
    Reviewed By: mkillianey
    
    fbshipit-source-id: 06ffb696ae

--- src/com/facebook/buck/features/project/intellij/StringTemplateFile.java
+++ src/com/facebook/buck/features/project/intellij/StringTemplateFile.java
@@ -48,7 +48,12 @@ enum StringTemplateFile {
     return fileName;
   }
 
-  public ST getST() throws IOException {
+  /**
+   * Not thread safe, see discussion in: https://github.com/antlr/stringtemplate4/issues/61 could be
+   * made faster by sharing STGroup across threads using a supplier, see {@link
+   * com.facebook.buck.parser.function.BuckPyFunction}. May be fixed in ST4.1
+   */
+  public synchronized ST getST() throws IOException {
     URL templateUrl = Resources.getResource(StringTemplateFile.class, "templates/" + fileName);
     String template = Resources.toString(templateUrl, StandardCharsets.UTF_8);
     return new ST(template, DELIMITER, DELIMITER);
