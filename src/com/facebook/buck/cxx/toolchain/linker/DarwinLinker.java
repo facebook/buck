7cd3f52203de962af16ba0ca2c210b0daa2a2d32
Richard Howell <richardh@uber.com>
2019-01-03 10:28:55 -0800

    fix import

--- src/com/facebook/buck/cxx/toolchain/linker/DarwinLinker.java
+++ src/com/facebook/buck/cxx/toolchain/linker/DarwinLinker.java
@@ -16,7 +16,6 @@
 
 package com.facebook.buck.cxx.toolchain.linker;
 
-import com.android.annotations.concurrency.Immutable;
 import com.facebook.buck.core.model.BuildTarget;
 import com.facebook.buck.core.rulekey.AddToRuleKey;
 import com.facebook.buck.core.rules.ActionGraphBuilder;
