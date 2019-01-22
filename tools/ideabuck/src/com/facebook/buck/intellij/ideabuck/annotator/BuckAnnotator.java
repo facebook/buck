0d4afcc7b05d9e1a2dbd7bb26b8128e2ac693c8a
Mick Killianey <mkillianey@fb.com>
2019-01-22 09:40:19 -0800

    Reduce error level of string parsing
    Summary:
    In `load()` statements, ideabuck can definitively parse paths to
    files and determine whether or not a target is valid.  In other
    contexts where a string is present, ideabuck is not yet smart
    enough to know whether or not the string is intended to refer
    to a target, so flagging it as an error is overly severe.  Change
    the annotation level from "error" to "warning" accordingly.
    
    This is part of an effort to make ideabuck's parsing more correct,
    so that ideabuck errors are truly errors that must be fixed before
    the user can build with buck.
    
    Reviewed By: cute-jumper
    
    fbshipit-source-id: 9a6d281ad7

--- tools/ideabuck/src/com/facebook/buck/intellij/ideabuck/annotator/BuckAnnotator.java
+++ tools/ideabuck/src/com/facebook/buck/intellij/ideabuck/annotator/BuckAnnotator.java
@@ -173,7 +173,7 @@ public class BuckAnnotator implements Annotator {
           buckTargetLocator.resolve(sourceFile, buckTargetPattern).orElse(null);
       if (absolutePattern == null) {
         Annotation annotation =
-            annotationHolder.createErrorAnnotation(expression, "Cannot resolve target pattern");
+            annotationHolder.createWarningAnnotation(expression, "Cannot resolve target pattern");
         annotation.setTextAttributes(BuckSyntaxHighlighter.BUCK_INVALID_TARGET);
         return true;
       }
@@ -200,7 +200,7 @@ public class BuckAnnotator implements Annotator {
               .map(path -> "Cannot find file at " + path.toString())
               .orElse("Cannot resolve path from target pattern");
       annotationHolder
-          .createErrorAnnotation(expression, message)
+          .createWarningAnnotation(expression, message)
           .setTextAttributes(BuckSyntaxHighlighter.BUCK_INVALID_TARGET);
       return true;
     }
