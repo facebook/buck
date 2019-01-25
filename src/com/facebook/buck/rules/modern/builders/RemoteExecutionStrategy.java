19a8c23f64e807f9be87bdd7dd30d369f9176cdb
Rui Barbosa Martins <ruibm@fb.com>
2019-01-25 03:18:32 -0800

    Fix deprecated calls in remoteexecution package.
    Summary: Currently both the src and the test remoteexecution packages are clean.
    
    Reviewed By: michsien
    
    fbshipit-source-id: 8cc2292175

--- src/com/facebook/buck/rules/modern/builders/RemoteExecutionStrategy.java
+++ src/com/facebook/buck/rules/modern/builders/RemoteExecutionStrategy.java
@@ -221,7 +221,9 @@ public class RemoteExecutionStrategy extends AbstractModernBuildRuleStrategy {
             service,
             () -> Futures.immediateFuture(getRemoteExecutionActionInfo(rule, strategyContext)));
     return Futures.transformAsync(
-        actionInfoFuture, actionInfo -> uploadInputs(rule.getBuildTarget(), actionInfo));
+        actionInfoFuture,
+        actionInfo -> uploadInputs(rule.getBuildTarget(), actionInfo),
+        MoreExecutors.directExecutor());
   }
 
   private ListenableFuture<RemoteExecutionActionInfo> uploadInputs(
@@ -242,7 +244,8 @@ public class RemoteExecutionStrategy extends AbstractModernBuildRuleStrategy {
           // actionInfo.getRequiredData() in particular may be very, very large and is unneeded once
           // uploading has completed.
           return actionInfo.withRequiredData(ImmutableMap.of());
-        });
+        },
+        MoreExecutors.directExecutor());
   }
 
   private ListenableFuture<Optional<BuildResult>> handleActionInfo(
@@ -322,7 +325,8 @@ public class RemoteExecutionStrategy extends AbstractModernBuildRuleStrategy {
                   result -> {
                     executingScope.close();
                     return result;
-                  });
+                  },
+                  MoreExecutors.directExecutor());
             });
 
     return Futures.transformAsync(
