4ca7a989dd3d913de2a83f5a5560133ae8c7ef0a
Thales Machado <thales@uber.com>
2019-02-14 13:06:29 +0100

    Run GJF on changed files

--- src/com/facebook/buck/jvm/java/JavaAnnotationProcessor.java
+++ src/com/facebook/buck/jvm/java/JavaAnnotationProcessor.java
@@ -20,12 +20,11 @@ import com.facebook.buck.core.model.BuildTarget;
 import com.facebook.buck.core.rules.BuildRuleParams;
 import com.facebook.buck.io.filesystem.ProjectFilesystem;
 
-/**
- * Represents a Java Annotation Processor Plugin for the Java Compiler
- */
+/** Represents a Java Annotation Processor Plugin for the Java Compiler */
 public class JavaAnnotationProcessor extends JavacPlugin {
 
-  public JavaAnnotationProcessor(BuildTarget buildTarget,
+  public JavaAnnotationProcessor(
+      BuildTarget buildTarget,
       ProjectFilesystem projectFilesystem,
       BuildRuleParams params,
       JavacPluginProperties properties) {
