4bed4558519dc4add63a1639aa8ba12adac55938
Sergey Tyurin <tyurins@fb.com>
2019-02-26 13:55:16 -0800

    Allow to specify git commit id when building bazel jar
    Summary: This is useful to rebuild bazel jar on the last known revision instead of using master in bazel repository.
    
    Reviewed By: bobyangyf
    
    fbshipit-source-id: 08cec5ed34

--- third-party/java/bazel/update.sh
+++ third-party/java/bazel/update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
+if [ ! -z "$1" ]; then
+  GIT_COMMIT_HASH=$1
+fi
+
 readonly WORK_DIR=$(mktemp -d)
 readonly BAZEL_DIR="${WORK_DIR}/bazel"
 readonly MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -7,8 +11,13 @@ readonly JARJAR_DIR="${MY_DIR}/../jarjar"
 readonly JARJAR_PATH="${JARJAR_DIR}/jarjar-1.4.1.jar"
 readonly FINAL_JAR_NAME="bazel"
 
-git -C "${WORK_DIR}" clone --depth 1 https://github.com/bazelbuild/bazel.git
+git -C "${WORK_DIR}" clone https://github.com/bazelbuild/bazel.git
 pushd "${BAZEL_DIR}"
+
+if [ ! -z "$GIT_COMMIT_HASH" ]; then
+  git checkout $GIT_COMMIT_HASH
+fi
+
 git apply "${MY_DIR}/bazel.patch"
 bazel build //src/main/java/com/google/devtools/build/lib:bazel-jar_deploy.jar \
   //src/main/java/com/google/devtools/build/lib:bazel-jar_deploy-src.jar
