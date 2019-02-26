b5689f1d0cf46967ee9284fcecd7827904dab960
Sergey Tyurin <tyurins@fb.com>
2019-02-26 13:55:27 -0800

    Remove entries from bazel jar instead of using jarjar
    Summary: `bazel_deploy.jar` contains some of the classes that are already present in other libraries and which should be removed by jarjar, but my testing showed that jarjar doesn't remove those classes. Since jarjar is used to remove classes only, it can be replaced with `zip` commands that remove jar entries.
    
    Reviewed By: bobyangyf
    
    fbshipit-source-id: 7cbd9cf6d8

--- third-party/java/bazel/update.sh
+++ third-party/java/bazel/update.sh
@@ -18,14 +18,24 @@ if [ ! -z "$GIT_COMMIT_HASH" ]; then
   git checkout $GIT_COMMIT_HASH
 fi
 
+remove_jar_entries() {
+  zip -d $1 com/google/common/\*
+  zip -d $1 com/google/gson/\*
+  zip -d $1 org/objectweb/asm/\*
+  zip -d $1 javax/annotation/\*
+  zip -d $1 com/google/errorprone/\*
+  zip -d $1 com/google/protobuf/\*
+}
+
 git apply "${MY_DIR}/bazel.patch"
 bazel build //src/main/java/com/google/devtools/build/lib:bazel-jar_deploy.jar \
   //src/main/java/com/google/devtools/build/lib:bazel-jar_deploy-src.jar
-java -jar "${JARJAR_PATH}" process "${MY_DIR}/jarjar-rules.txt" \
-  bazel-bin/src/main/java/com/google/devtools/build/lib/bazel-jar_deploy.jar \
+
+cp bazel-bin/src/main/java/com/google/devtools/build/lib/bazel-jar_deploy.jar \
   "${MY_DIR}/${FINAL_JAR_NAME}_deploy.jar"
-java -jar "${JARJAR_PATH}" process "${MY_DIR}/jarjar-rules.txt" \
-  bazel-bin/src/main/java/com/google/devtools/build/lib/bazel-jar_deploy-src.jar \
+remove_jar_entries "${MY_DIR}/${FINAL_JAR_NAME}_deploy.jar"
+
+cp bazel-bin/src/main/java/com/google/devtools/build/lib/bazel-jar_deploy-src.jar \
   "${MY_DIR}/${FINAL_JAR_NAME}_deploy-src.jar"
 
 readonly commit_hash=$(git rev-parse HEAD)
