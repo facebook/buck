c572770d294db37aef86bfacb9a1917a9a145479
Bob Yang <bobyf@fb.com>
2019-03-04 13:57:45 -0800

    Introduce Artifact as a type
    Summary:
    Add artifacts as a type to buck as a way to look up
    files, either source or built from other rules.
    Artifacts that are built will be able to be used to look up
    its corresponding action.
    
    Reviewed By: styurin
    
    fbshipit-source-id: 77a2937fd4

---
+++ src/com/facebook/buck/core/rules/actions/Artifact.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.core.rules.actions;
+
+import com.facebook.buck.core.sourcepath.ExplicitBuildTargetSourcePath;
+import com.facebook.buck.core.sourcepath.SourcePath;
+import org.immutables.value.Value;
+
+/**
+ * An {@link Artifact} is a file used during the build stage. It can either be a source file for the
+ * build or a generated file from a build step itself.
+ */
+public interface Artifact {
+
+  /** An artifact generated from build */
+  @Value.Immutable(builder = false, copy = false, prehash = true)
+  interface BuildArtifact extends Artifact {
+
+    /** @return the key to the {@link ActionAnalysisData} that owns this artifact */
+    @Value.Parameter
+    ActionAnalysisData.Key getActionDataKey();
+
+    /** @return the path to the artifact */
+    @Value.Parameter
+    ExplicitBuildTargetSourcePath getPath();
+  }
+
+  /** An artifact representing a source file */
+  @Value.Immutable(builder = false, copy = false, prehash = true)
+  interface SourceArtifact extends Artifact {
+
+    /** @return the path to the source file */
+    @Value.Parameter
+    SourcePath getPath();
+  }
+}
