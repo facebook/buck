52cb7743ba31b93a4b0055a61d17ad6cc90e3c14
Sergey Balabanov <sergeyb@fb.com>
2019-03-18 12:17:44 -0700

    Primitive DirectoryListCache implementation
    Reviewed By: bobyangyf
    
    fbshipit-source-id: 5717e30e35

---
+++ src/com/facebook/buck/core/files/DirectoryListCache.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.core.files;
+
+import com.facebook.buck.core.graph.transformation.GraphEngineCache;
+import com.facebook.buck.io.watchman.WatchmanOverflowEvent;
+import com.facebook.buck.io.watchman.WatchmanPathEvent;
+import com.google.common.eventbus.Subscribe;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+/** Stores a list of files and subfolders per each folder */
+public class DirectoryListCache implements GraphEngineCache<DirectoryListKey, DirectoryList> {
+
+  private ConcurrentHashMap<DirectoryListKey, DirectoryList> cache = new ConcurrentHashMap<>();
+  private final Path rootPath;
+  private final Invalidator invalidator;
+
+  private DirectoryListCache(Path rootPath) {
+    this.rootPath = rootPath;
+    this.invalidator = new Invalidator(this);
+  }
+
+  /**
+   * Create a new instance of {@link DirectoryListCache}
+   *
+   * @param rootPath Absolute path to the root folder for which files and subfolders are cached
+   */
+  public static DirectoryListCache of(Path rootPath) {
+    return new DirectoryListCache(rootPath);
+  }
+
+  @Override
+  public Optional<DirectoryList> get(DirectoryListKey key) {
+    return Optional.ofNullable(cache.get(key));
+  }
+
+  @Override
+  public void put(DirectoryListKey key, DirectoryList directoryList) {
+    cache.put(key, directoryList);
+  }
+
+  /** @return class that listens to watchman events and invalidates internal cache state */
+  public Invalidator getInvalidator() {
+    return invalidator;
+  }
+
+  /**
+   * Subscribes to watchman event and invalidates internal state of a provided {@link
+   * DirectoryListCache}
+   */
+  public static class Invalidator {
+
+    private final DirectoryListCache dirListCache;
+
+    public Invalidator(DirectoryListCache dirListCache) {
+      this.dirListCache = dirListCache;
+    }
+
+    /** Invoked asynchronously by event bus when file system change is detected with Watchman */
+    @Subscribe
+    public void onFileSystemChange(WatchmanPathEvent event) {
+      if (event.getKind() == WatchmanPathEvent.Kind.MODIFY) {
+        // file modifications do not change directory structure, do nothing
+        return;
+      }
+
+      if (!dirListCache.rootPath.equals(event.getCellPath())) {
+        // must be same cell
+        return;
+      }
+
+      // for CREATE and DELETE, invalidate containing folder
+      Path folderPath = event.getPath().getParent();
+      DirectoryListKey key = ImmutableDirectoryListKey.of(folderPath);
+      dirListCache.cache.remove(key);
+    }
+
+    /**
+     * Invoked asynchronously by event bus when Watchman detects too many files changed or unable to
+     * detect changes, this should drop the cache
+     */
+    @Subscribe
+    @SuppressWarnings("unused")
+    public void onFileSystemChange(WatchmanOverflowEvent event) {
+      dirListCache.cache = new ConcurrentHashMap<>();
+    }
+  }
+}
---
+++ test/com/facebook/buck/core/files/DirectoryListCacheTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.core.files;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.facebook.buck.io.watchman.WatchmanOverflowEvent;
+import com.facebook.buck.io.watchman.WatchmanPathEvent;
+import com.facebook.buck.testutil.TemporaryPaths;
+import com.google.common.collect.ImmutableSortedSet;
+import java.nio.file.Paths;
+import java.util.Optional;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
+public class DirectoryListCacheTest {
+
+  @Rule public TemporaryPaths tmp = new TemporaryPaths();
+
+  @Test
+  public void canReadAndWrite() {
+    DirectoryListCache cache = DirectoryListCache.of(tmp.getRoot());
+    cache.put(
+        ImmutableDirectoryListKey.of(Paths.get("")),
+        ImmutableDirectoryList.of(
+            ImmutableSortedSet.of(Paths.get("file")),
+            ImmutableSortedSet.of(Paths.get("folder")),
+            ImmutableSortedSet.of(Paths.get("symlink"))));
+
+    Optional<DirectoryList> dlist = cache.get(ImmutableDirectoryListKey.of(Paths.get("")));
+    assertTrue(dlist.isPresent());
+    assertTrue(dlist.get().getDirectories().contains(Paths.get("folder")));
+    assertTrue(dlist.get().getFiles().contains(Paths.get("file")));
+    assertTrue(dlist.get().getSymlinks().contains(Paths.get("symlink")));
+
+    Optional<DirectoryList> dlistEmpty =
+        cache.get(ImmutableDirectoryListKey.of(Paths.get("nonexisting")));
+    assertFalse(dlistEmpty.isPresent());
+  }
+
+  private Object getInvalidateParameters() {
+    return new Object[] {WatchmanPathEvent.Kind.CREATE, WatchmanPathEvent.Kind.DELETE};
+  }
+
+  @Test
+  @Parameters(method = "getInvalidateParameters")
+  public void whenFileListChangeThenInvalidate(WatchmanPathEvent.Kind kind) {
+    DirectoryListCache cache = DirectoryListCache.of(tmp.getRoot());
+    cache.put(
+        ImmutableDirectoryListKey.of(Paths.get("dir")),
+        ImmutableDirectoryList.of(
+            ImmutableSortedSet.of(Paths.get("file")),
+            ImmutableSortedSet.of(),
+            ImmutableSortedSet.of()));
+
+    // should not invalidate
+    WatchmanPathEvent event = WatchmanPathEvent.of(tmp.getRoot(), kind, Paths.get("dir1/file"));
+    cache.getInvalidator().onFileSystemChange(event);
+    Optional<DirectoryList> dlist = cache.get(ImmutableDirectoryListKey.of(Paths.get("dir")));
+    assertTrue(dlist.isPresent());
+
+    // should invalidate
+    event = WatchmanPathEvent.of(tmp.getRoot(), kind, Paths.get("dir/file1"));
+    cache.getInvalidator().onFileSystemChange(event);
+
+    dlist = cache.get(ImmutableDirectoryListKey.of(Paths.get("dir")));
+    assertFalse(dlist.isPresent());
+  }
+
+  @Test
+  public void whenFileListNotChangeThenNotInvalidate() {
+    DirectoryListCache cache = DirectoryListCache.of(tmp.getRoot());
+    cache.put(
+        ImmutableDirectoryListKey.of(Paths.get("dir")),
+        ImmutableDirectoryList.of(
+            ImmutableSortedSet.of(Paths.get("file")),
+            ImmutableSortedSet.of(),
+            ImmutableSortedSet.of()));
+
+    // should not invalidate
+    WatchmanPathEvent event =
+        WatchmanPathEvent.of(tmp.getRoot(), WatchmanPathEvent.Kind.MODIFY, Paths.get("dir/file"));
+    cache.getInvalidator().onFileSystemChange(event);
+    Optional<DirectoryList> dlist = cache.get(ImmutableDirectoryListKey.of(Paths.get("dir")));
+    assertTrue(dlist.isPresent());
+  }
+
+  @Test
+  public void whenOverflowThenInvalidateAll() {
+    DirectoryListCache cache = DirectoryListCache.of(tmp.getRoot());
+    cache.put(
+        ImmutableDirectoryListKey.of(Paths.get("dir")),
+        ImmutableDirectoryList.of(
+            ImmutableSortedSet.of(Paths.get("file")),
+            ImmutableSortedSet.of(),
+            ImmutableSortedSet.of()));
+
+    cache.put(
+        ImmutableDirectoryListKey.of(Paths.get("dir1")),
+        ImmutableDirectoryList.of(
+            ImmutableSortedSet.of(),
+            ImmutableSortedSet.of(Paths.get("dir1/subdir1")),
+            ImmutableSortedSet.of()));
+
+    // should not invalidate
+    WatchmanOverflowEvent event = WatchmanOverflowEvent.of(tmp.getRoot(), "Test");
+    cache.getInvalidator().onFileSystemChange(event);
+    Optional<DirectoryList> dlist = cache.get(ImmutableDirectoryListKey.of(Paths.get("dir")));
+    assertFalse(dlist.isPresent());
+
+    dlist = cache.get(ImmutableDirectoryListKey.of(Paths.get("dir1")));
+    assertFalse(dlist.isPresent());
+  }
+}
