bb888eddd6f125b6b2b189cf5162e515800ba28d
Bob Yang <bobyf@fb.com>
2019-03-18 18:27:51 -0700

    Add new TriFunction interface
    Summary: .
    
    Reviewed By: sbalabanov
    
    fbshipit-source-id: 0aa7ea9bbc

---
+++ src/com/facebook/buck/util/function/TriFunction.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.util.function;
+
+/**
+ * Represents a function that accepts four arguments and produces a result. This is the four-arity
+ * specialization of {@link java.util.function.Function}.
+ *
+ * <p>This is a functional interface whose functional method is {@link #apply(Object, Object, Object
+ * )}.
+ *
+ * @param <T> the type of the first argument to the function
+ * @param <U> the type of the second argument to the function
+ * @param <V> the type of the third argument to the function
+ * @param <R> the type of the result of the function
+ * @see java.util.function.Function
+ */
+@FunctionalInterface
+public interface TriFunction<T, U, V, R> {
+  /**
+   * Applies this function to the given arguments.
+   *
+   * @param t the first function argument
+   * @param u the second function argument
+   * @param v the third function argument
+   * @return the function result
+   */
+  R apply(T t, U u, V v);
+}
