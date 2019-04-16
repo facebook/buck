56c5b94dfab1bd4ac82ed1ab4869dfd40d7c5016
Bob Yang <bobyf@fb.com>
2019-04-16 00:52:39 -0700

    Decrease visibility of BgTask methods
    Reviewed By: sbalabanov
    
    fbshipit-source-id: 6832946bb1

--- src/com/facebook/buck/support/bgtasks/BackgroundTask.java
+++ src/com/facebook/buck/support/bgtasks/BackgroundTask.java
@@ -31,15 +31,15 @@ import org.immutables.value.Value;
 public abstract class BackgroundTask<T> {
 
   @Value.Parameter
-  public abstract String getName();
+  abstract String getName();
 
   @Value.Parameter
-  public abstract TaskAction<T> getAction();
+  abstract TaskAction<T> getAction();
 
   @Value.Parameter
-  protected abstract T getActionArgs();
+  abstract T getActionArgs();
 
-  public abstract Optional<Timeout> getTimeout();
+  abstract Optional<Timeout> getTimeout();
 
   @Value.Default
   boolean getShouldCancelOnRepeat() {
