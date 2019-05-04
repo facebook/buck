b4ae01e9d18a5812d337772b9b34c44230b1ea76
Bob Yang <bobyf@fb.com>
2019-05-03 17:15:51 -0700

    Implement Composition of graph computations
    Summary:
    Implements composition of two computations.
    The composition is currently implemented such that given a base composed computation,
    and a composer that returns a set of new keys given the key and result of the base computation,
    returns a computation that takes in a composed key of the first computation and the final result class.
    
    This lets you easily chain together several computations and request specific results given the very base key
    
    Reviewed By: sbalabanov
    
    fbshipit-source-id: 736824af6b

--- src/com/facebook/buck/core/graph/transformation/composition/Composition.java
+++ src/com/facebook/buck/core/graph/transformation/composition/Composition.java
@@ -16,10 +16,9 @@
 package com.facebook.buck.core.graph.transformation.composition;
 
 import com.facebook.buck.core.graph.transformation.GraphComputation;
-import com.facebook.buck.core.graph.transformation.model.ComposedKey;
-import com.facebook.buck.core.graph.transformation.model.ComposedResult;
 import com.facebook.buck.core.graph.transformation.model.ComputeKey;
 import com.facebook.buck.core.graph.transformation.model.ComputeResult;
+import com.google.common.collect.Iterables;
 
 /**
  * Performs a composition over two {@link GraphComputation}s.
@@ -30,16 +29,42 @@ import com.facebook.buck.core.graph.transformation.model.ComputeResult;
  * ComposedResult(R1) with C2 that transforms K2 to R2 results in a computation C' that transforms
  * ComposedKey(K1, R2) to ComposedResult(R2). This is under a {@link Composer} that takes K1 and R1
  * and returns a set of K2's to be computed.
+ *
+ * <p>TODO(bobyf): currently composition only builds properly left to right. It should be
+ * associative.
  */
-public interface Composition {
+public class Composition {
 
-  <
+  /**
+   * Creates a composed computation from a base composed computation used for chaining multiple
+   * {@link GraphComputation} together.
+   *
+   * @param resultClass the result class of this composition
+   * @param baseComputation the base computation
+   * @param composer a {@link KeyComposer} that takes results of the base computation and determines
+   *     how to trigger the computation for the result class desired.
+   * @param <Key1> the type of base computation key
+   * @param <Result1> the type of base computation result
+   * @param <Key2> the type of the second composing computation
+   * @param <Result2> the result type of the second composing computation
+   * @return a {@link ComposedComputation} that takes in a composed key from {@link Key1} to {@link
+   *     com.facebook.buck.core.graph.transformation.model.ComposedResult} of {@link Result2}
+   */
+  @SuppressWarnings("unchecked")
+  public static <
           Key1 extends ComputeKey<Result1>,
           Result1 extends ComputeResult,
           Key2 extends ComputeKey<Result2>,
           Result2 extends ComputeResult>
-      GraphComputation<ComposedKey<Key1, Result2>, ComposedResult<Result2>> of(
-          GraphComputation<ComposedKey<Key1, Result1>, ComposedResult<Result1>> computation1,
-          GraphComputation<Key2, Result2> computation2,
-          Composer<Key1, Result1> composer);
+      ComposedComputation<Key1, Result1, Result2> of(
+          Class<Result2> resultClass,
+          ComposedComputation<Key1, ?, Result1> baseComputation,
+          KeyComposer<Key1, Result1, Key2> composer) {
+
+    return new ComposedComputation<>(
+        baseComputation.getIdentifier(),
+        resultClass,
+        composer,
+        identity -> (Result2) Iterables.getOnlyElement(identity.values()));
+  }
 }
---
+++ test/com/facebook/buck/core/graph/transformation/composition/CompositionTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.core.graph.transformation.composition;
+
+import static org.junit.Assert.assertEquals;
+
+import com.facebook.buck.core.graph.transformation.impl.ChildrenAdder.LongNode;
+import com.facebook.buck.core.graph.transformation.impl.ChildrenSumMultiplier.LongMultNode;
+import com.facebook.buck.core.graph.transformation.impl.FakeComputationEnvironment;
+import com.facebook.buck.core.graph.transformation.impl.ImmutableLongMultNode;
+import com.facebook.buck.core.graph.transformation.impl.ImmutableLongNode;
+import com.facebook.buck.core.graph.transformation.model.ComposedComputationIdentifier;
+import com.facebook.buck.core.graph.transformation.model.ComposedKey;
+import com.facebook.buck.core.graph.transformation.model.ImmutableComposedKey;
+import com.facebook.buck.core.graph.transformation.model.ImmutableComposedResult;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import org.junit.Test;
+
+public class CompositionTest {
+
+  @Test
+  public void composedComputationReturnsCorrectPreliminaryDeps() {
+    ComposedComputation<LongNode, LongNode, LongNode> baseComputation =
+        new ComposedComputation<>(
+            ComposedComputationIdentifier.of(LongNode.IDENTIFIER, LongNode.class),
+            LongNode.class,
+            (ignored1, ignored2) -> ImmutableSet.of(),
+            identify -> (LongNode) Iterables.getOnlyElement(identify.values()));
+
+    ComposedComputation<LongNode, LongNode, LongMultNode> composedComputation =
+        Composition.of(LongMultNode.class, baseComputation, (ignored1, ignored2) -> null);
+
+    assertEquals(
+        ImmutableSet.of(ImmutableComposedKey.of(ImmutableLongNode.of(1), LongNode.class)),
+        composedComputation.discoverPreliminaryDeps(
+            ImmutableComposedKey.of(ImmutableLongNode.of(1), LongMultNode.class)));
+  }
+
+  @Test
+  public void composedComputationReturnsCorrectDeps() throws Exception {
+
+    LongNode originKey = ImmutableLongNode.of(1);
+    LongNode originResult = ImmutableLongNode.of(2);
+    ComposedKey<LongNode, LongNode> originComposedKey =
+        ImmutableComposedKey.of(originKey, LongNode.class);
+
+    ImmutableSet<LongMultNode> expectedDeps =
+        ImmutableSet.of(ImmutableLongMultNode.of(1), ImmutableLongMultNode.of(2));
+
+    ComposedComputation<LongNode, LongNode, LongNode> baseComputation =
+        new ComposedComputation<>(
+            ComposedComputationIdentifier.of(LongNode.IDENTIFIER, LongNode.class),
+            LongNode.class,
+            (ignored1, ignored2) -> ImmutableSet.of(),
+            identify -> (LongNode) Iterables.getOnlyElement(identify.values()));
+
+    KeyComposer<LongNode, LongNode, LongMultNode> composer =
+        (key, result) -> {
+          assertEquals(originKey, key);
+          assertEquals(originResult, result);
+          return expectedDeps;
+        };
+
+    ComposedComputation<LongNode, LongNode, LongMultNode> composedComputation =
+        Composition.of(LongMultNode.class, baseComputation, composer);
+
+    FakeComputationEnvironment environment =
+        new FakeComputationEnvironment(
+            ImmutableMap.of(
+                originComposedKey, ImmutableComposedResult.of(ImmutableList.of(originResult))));
+
+    assertEquals(
+        expectedDeps,
+        composedComputation.discoverDeps(
+            ImmutableComposedKey.of(ImmutableLongNode.of(1), LongMultNode.class), environment));
+  }
+
+  @Test
+  public void composedComputationTransformsProperly() throws Exception {
+    FakeComputationEnvironment environment =
+        new FakeComputationEnvironment(
+            ImmutableMap.of(
+                ImmutableComposedKey.of(ImmutableLongNode.of(1), LongNode.class),
+                ImmutableComposedResult.of(
+                    ImmutableList.of(ImmutableLongNode.of(1), ImmutableLongNode.of(2))),
+                ImmutableLongMultNode.of(1),
+                ImmutableLongMultNode.of(1),
+                ImmutableLongMultNode.of(2),
+                ImmutableLongMultNode.of(2)));
+
+    KeyComposer<LongNode, LongNode, LongMultNode> composer =
+        (key, result) -> ImmutableSet.of(ImmutableLongMultNode.of(result.get()));
+
+    ComposedComputation<LongNode, LongNode, LongNode> baseComputation =
+        new ComposedComputation<>(
+            ComposedComputationIdentifier.of(LongNode.IDENTIFIER, LongNode.class),
+            LongNode.class,
+            (ignored1, ignored2) -> ImmutableSet.of(),
+            identify -> (LongNode) Iterables.getOnlyElement(identify.values()));
+
+    ComposedComputation<LongNode, LongNode, LongMultNode> composedComputation =
+        Composition.of(LongMultNode.class, baseComputation, composer);
+
+    assertEquals(
+        ImmutableComposedResult.of(
+            ImmutableList.of(ImmutableLongMultNode.of(1), ImmutableLongMultNode.of(2))),
+        composedComputation.transform(
+            ImmutableComposedKey.of(ImmutableLongNode.of(1), LongMultNode.class), environment));
+  }
+}
