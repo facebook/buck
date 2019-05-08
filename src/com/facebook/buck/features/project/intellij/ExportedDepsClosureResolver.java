a5ad5a4ee5481da560ed2fdd16a1a9e2bbc9e55d
Naveen Narayanan <naveen.narayanan@uber.com>
2019-05-08 10:13:17 -0700

    Adding Android prebuilt aar to exported dep resolver
    Summary: * Dependencies from Android prebuilt aar should be resolved as exported deps
    
    Test Plan: * Ran `buck project libraries/foundation/rib/rib-views/rib-view-pager/...` and validated that `androidx` aar deps are present in the generated `.iml` files
    
    Reviewers: ravi, gkorlam
    
    Revert Plan: Revert
    
    Differential Revision: https://code.uberinternal.com/D2809309

--- src/com/facebook/buck/features/project/intellij/ExportedDepsClosureResolver.java
+++ src/com/facebook/buck/features/project/intellij/ExportedDepsClosureResolver.java
@@ -16,6 +16,7 @@
 
 package com.facebook.buck.features.project.intellij;
 
+import com.facebook.buck.android.AndroidPrebuiltAarDescriptionArg;
 import com.facebook.buck.core.description.arg.BuildRuleArg;
 import com.facebook.buck.core.model.BuildTarget;
 import com.facebook.buck.core.model.targetgraph.TargetGraph;
@@ -63,6 +64,9 @@ public class ExportedDepsClosureResolver {
               .build();
     } else if (targetNode.getConstructorArg() instanceof PrebuiltJarDescriptionArg) {
       PrebuiltJarDescriptionArg arg = (PrebuiltJarDescriptionArg) targetNode.getConstructorArg();
+      exportedDeps = arg.getDeps();
+    } else if (targetNode.getConstructorArg() instanceof AndroidPrebuiltAarDescriptionArg) {
+      AndroidPrebuiltAarDescriptionArg arg = (AndroidPrebuiltAarDescriptionArg) targetNode.getConstructorArg();
       exportedDeps = arg.getDeps();
     }
 
