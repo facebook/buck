b51dff0de60b8ae2cd79db31c3258432171644a3
Thales Machado <thales@uber.com>
2019-05-15 10:23:46 +0200

    Added test

--- src/com/facebook/buck/jvm/java/PluginLoaderJavaFileManager.java
+++ src/com/facebook/buck/jvm/java/PluginLoaderJavaFileManager.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.facebook.buck.jvm.java;
 
 import com.google.common.collect.ImmutableList;
@@ -27,8 +42,7 @@ public class PluginLoaderJavaFileManager extends ForwardingStandardJavaFileManag
   @Override
   public ClassLoader getClassLoader(Location location) {
     // We only provide the shared classloader if there are plugins defined.
-    if (StandardLocation.ANNOTATION_PROCESSOR_PATH.equals(location) &&
-        javacPlugins.size() > 0) {
+    if (StandardLocation.ANNOTATION_PROCESSOR_PATH.equals(location) && javacPlugins.size() > 0) {
       return pluginFactory.getClassLoaderForProcessorGroups(javacPlugins);
     }
     return super.getClassLoader(location);
