0c15a057fe5e4daf6db2f2dd4e280a7fe47cc0f3
Sergey Balabanov <sergeyb@fb.com>
2019-05-23 14:04:54 -0700

    Multitenant index to stream loading from JSON
    Summary:
    Currently loading a big JSON file is slow and very memory hungry as it materialize the whole value tree into memory first. This change makes it partially streamed - it loads packages and deleted paths as trees but each package is processed sequentially.
    
    Granularity can be improved by streaming each target individually, but I stop at the package level with the hope it is good enough.
    
    Reviewed By: bolinfest
    
    fbshipit-source-id: 65c7c2f2d6

--- src/com/facebook/buck/multitenant/importer/IndexBuilder.kt
+++ src/com/facebook/buck/multitenant/importer/IndexBuilder.kt
@@ -28,65 +28,114 @@ import com.facebook.buck.multitenant.service.RawBuildRule
 import com.facebook.buck.rules.visibility.VisibilityPattern
 import com.facebook.buck.util.json.ObjectMappers
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.NullNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.google.common.collect.ImmutableMap
 import com.google.common.collect.ImmutableSet
 import java.io.InputStream
 
 /**
- * Ultimately, we would like to use kotlinx.serialization for this, but we are currently blocked by
- * https://youtrack.jetbrains.com/issue/KT-30998.
+ * Read commit state from JSON and populate index with data
+ * @return List of hashes of all commits processed
  */
 fun populateIndexFromStream(
         indexAppender: IndexAppender,
-        stream: InputStream): List<String> = ObjectMappers.createParser(stream)
-        .enable(JsonParser.Feature.ALLOW_COMMENTS)
-        .enable(JsonParser.Feature.ALLOW_TRAILING_COMMA)
-        .readValueAsTree<JsonNode>()
-        .asSequence().map { commit ->
-            val hash = commit.get("commit").asText()
-            val added = toBuildPackages(commit.get("added"))
-            val modified = toBuildPackages(commit.get("modified"))
-            val removed = toRemovedPackages(commit.get("removed"))
-            val changes = BuildPackageChanges(added, modified, removed)
-            indexAppender.addCommitData(hash, changes)
-            hash
-        }.toList()
+        stream: InputStream): List<String> {
+    val parser = ObjectMappers.createParser(stream)
+            .enable(JsonParser.Feature.ALLOW_COMMENTS)
+            .enable(JsonParser.Feature.ALLOW_TRAILING_COMMA)
 
-private fun toBuildPackages(node: JsonNode?): List<BuildPackage> {
-    if (node == null) {
-        return listOf()
+    val result = mutableListOf<String>()
+
+    // Loading a big JSON file with `readValueAsTree` is slow and very memory hungry. We use
+    // a mixed approach - stream load JSON by reading token sequentially up to the package
+    // definition and use `readValueAsTree` to load the package itself and transform it into
+    // `BuildPackage`, allowing garbage collector to pick up JsonNode afterwards as we progress
+    // with other packages.
+    // Granularity can be improved by streaming each target individually if packages are too
+    // big, at this moment it seems to be good enough.
+
+    // top level data structure is an array of commits
+    check(parser.nextToken() == JsonToken.START_ARRAY)
+    while (parser.nextToken() != JsonToken.END_ARRAY) {
+        check(parser.currentToken == JsonToken.START_OBJECT)
+        var commit: String? = null
+        val added = mutableListOf<BuildPackage>()
+        val modified = mutableListOf<BuildPackage>()
+        val removed = mutableListOf<FsAgnosticPath>()
+        while (parser.nextToken() != JsonToken.END_OBJECT) {
+            // commit data is defined with 4 possible fields: commit, added, modified, removed
+            // 'added' and 'modified' contain a list of packages
+            // 'removed' contain a list of paths denoting removed packages
+            check(parser.currentToken == JsonToken.FIELD_NAME)
+            val fieldName = parser.currentName();
+            when (fieldName) {
+                "commit" -> {
+                    check(parser.nextToken() == JsonToken.VALUE_STRING)
+                    commit = parser.valueAsString
+                }
+                "added" -> parsePackages(parser, added)
+                "modified" -> parsePackages(parser, modified)
+                "removed" -> parsePaths(parser, removed)
+                else -> throw IllegalStateException("Unrecognized field $fieldName")
+            }
+        }
+        val commitRequired = requireNotNull(commit)
+        indexAppender.addCommitData(commitRequired, BuildPackageChanges(added, modified, removed))
+        result.add(commitRequired)
     }
+    return result
+}
 
-    return node.map { buildPackageItem ->
-        val path = FsAgnosticPath.of(buildPackageItem.get("path").asText())
-        val nodesAttr = buildPackageItem.get("nodes")
-        val rules = nodesAttr.fields().asSequence().map { (name, rule) ->
-            var ruleType: String? = null
-            val deps = mutableSetOf<String>()
-            val attrs = ImmutableMap.builder<String, Any>()
-            for (field in rule.fields()) {
-                when (field.key) {
-                    "attributes" -> {
-                        for (attr in field.value.fields()) {
-                            attrs.put(attr.key, normalizeJsonValue(attr.value))
-                            if (attr.key == "buck.type") {
-                                ruleType = attr.value.asText()
-                            }
+private fun parsePaths(parser: JsonParser, list: MutableList<FsAgnosticPath>) {
+    check(parser.nextToken() == JsonToken.START_ARRAY)
+    val removeNode = parser.readValueAsTree<JsonNode>()
+    // 'removeNode' is an Array node, iterating through which gives paths of removed packages
+    if (removeNode !is NullNode) {
+        list.addAll(removeNode.map { p -> FsAgnosticPath.of(p.asText()) })
+    }
+}
+
+private fun parsePackages(parser: JsonParser, list: MutableList<BuildPackage>) {
+    check(parser.nextToken() == JsonToken.START_ARRAY)
+    while (parser.nextToken() != JsonToken.END_ARRAY) {
+        val packageNode = parser.readValueAsTree<JsonNode>()
+        // 'removeNode' is an Object which is a fully parsed package node
+        // with the same structure as RawTargetNodeWithDepsPackage
+        if (packageNode !is NullNode) {
+            list.add(toBuildPackage(packageNode))
+        }
+    }
+}
+
+private fun toBuildPackage(nodes: JsonNode): BuildPackage {
+    val path = FsAgnosticPath.of(nodes.get("path").asText())
+    val rules = nodes.get("nodes").fields().asSequence().map { (name, rule) ->
+        var ruleType: String? = null
+        val deps = mutableSetOf<String>()
+        val attrs = ImmutableMap.builder<String, Any>()
+        for (field in rule.fields()) {
+            when (field.key) {
+                "attributes" -> {
+                    for (attr in field.value.fields()) {
+                        attrs.put(attr.key, normalizeJsonValue(attr.value))
+                        if (attr.key == "buck.type") {
+                            ruleType = attr.value.asText()
                         }
                     }
-                    "deps" -> deps.addAll(field.value.asSequence().map { it.asText() })
                 }
+                "deps" -> deps.addAll(field.value.asSequence().map { it.asText() })
             }
-            requireNotNull(ruleType)
-            val buildTarget = BuildTargets.createBuildTargetFromParts(path, name)
-            val depsAsTargets = deps.map { BuildTargets.parseOrThrow(it) }.toSet()
-            createRawRule(buildTarget, ruleType, depsAsTargets, attrs.build())
-        }.toSet()
-        BuildPackage(path, rules)
-    }
+        }
+        requireNotNull(ruleType)
+        val buildTarget = BuildTargets.createBuildTargetFromParts(path, name)
+        val depsAsTargets = deps.map { BuildTargets.parseOrThrow(it) }.toSet()
+        createRawRule(buildTarget, ruleType, depsAsTargets, attrs.build())
+    }.toSet()
+    return BuildPackage(path, rules)
 }
 
 private fun normalizeJsonValue(value: JsonNode): Any {
