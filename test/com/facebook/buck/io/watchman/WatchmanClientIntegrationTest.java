4712f531dda7edc1a02837434e8d6f2bde6d69ff
Matt Glazar <strager@fb.com>
2019-06-05 14:05:24 -0700

    Re-enable Watchman test on Linux
    Summary:
    WatchmanClientIntegrationTest seems to be working on Linux now. Re-enable it.
    
    Watchman recently started requiring .watchmanconfig to be valid JSON. Fix the mock .watchmanconfig so Watchman no longer complains.
    
    Reviewed By: bobyangyf
    
    shipit-source-id: 0ddf4d5811

--- test/com/facebook/buck/io/watchman/WatchmanClientIntegrationTest.java
+++ test/com/facebook/buck/io/watchman/WatchmanClientIntegrationTest.java
@@ -41,7 +41,6 @@ import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Assume;
@@ -136,8 +135,6 @@ public class WatchmanClientIntegrationTest {
 
   @Before
   public void setUp() throws IOException, InterruptedException {
-    // watchman tests are currently very flaky on Linux
-    Assume.assumeThat(Platform.detect(), Matchers.not(Matchers.is(Platform.LINUX)));
     startWatchman();
     Assume.assumeTrue(watchmanProcess != null);
     workspace = TestDataHelper.createProjectWorkspaceForScenario(this, "watchman", tmp);
