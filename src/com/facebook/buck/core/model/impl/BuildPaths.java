de080ea79bd3bf34528001d1b9357f33fffe9d34
Bob Yang <bobyf@fb.com>
2019-06-11 16:49:16 -0700

    Add BuildPaths to systematically generate the output paths from buildtarget
    Reviewed By: philipjameson
    
    shipit-source-id: 8c996da3ed

---
+++ src/com/facebook/buck/core/model/impl/BuildPaths.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.core.model.impl;
+
+import com.facebook.buck.core.model.BuildTarget;
+import com.facebook.buck.io.filesystem.ProjectFilesystem;
+import java.nio.file.Path;
+
+/**
+ * Utility class that generates the build output paths for {@link BuildTarget}s in a systematic
+ * manner.
+ */
+public class BuildPaths {
+
+  private BuildPaths() {}
+
+  /**
+   * Return a path to a file in the buck-out/bin/ directory, formatted with the target short name.
+   *
+   * @param target The {@link BuildTarget} to scope this path to.
+   * @return A {@link java.nio.file.Path} under buck-out/bin, scoped to the base path of {@code
+   *     target}.
+   */
+  public static Path getScratchDir(ProjectFilesystem filesystem, BuildTarget target) {
+    return BuildTargetPaths.getScratchPath(filesystem, target, getFormat(target));
+  }
+
+  /**
+   * Return a path to a file in the buck-out/annotation/ directory, formatted with the target short
+   * name.
+   *
+   * @param target The {@link BuildTarget} to scope this path to.
+   * @return A {@link java.nio.file.Path} under buck-out/annotation, scoped to the base path of
+   *     {@code target}.
+   */
+  public static Path getAnnotationDir(ProjectFilesystem filesystem, BuildTarget target) {
+    return BuildTargetPaths.getAnnotationPath(filesystem, target, getFormat(target));
+  }
+
+  /**
+   * Return a relative path to a file in the buck-out/gen/ directory, formatted with the target
+   * short name.
+   *
+   * @param target The {@link BuildTarget} to scope this path to.
+   * @return A {@link java.nio.file.Path} under buck-out/gen, scoped to the base path of {@code
+   *     target}.
+   */
+  public static Path getGenDir(ProjectFilesystem filesystem, BuildTarget target) {
+    return BuildTargetPaths.getGenPath(filesystem, target, getFormat(target));
+  }
+
+  private static String getFormat(BuildTarget target) {
+    return target.isFlavored() ? "%s" : "%s__";
+  }
+}
---
+++ test/com/facebook/buck/core/model/impl/BuildPathsTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.facebook.buck.core.model.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import com.facebook.buck.core.model.BuildTarget;
+import com.facebook.buck.core.model.BuildTargetFactory;
+import com.facebook.buck.io.filesystem.ProjectFilesystem;
+import com.facebook.buck.io.filesystem.impl.FakeProjectFilesystem;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
+public class BuildPathsTest {
+
+  private final ProjectFilesystem filesystem = new FakeProjectFilesystem();
+
+  @SuppressWarnings("unused")
+  private Object getTargetsForTest() {
+    return new Object[] {
+      new Object[] {
+        BuildTargetFactory.newInstance("//my/folder:foo"), Paths.get("my/folder/foo__")
+      },
+      new Object[] {
+        BuildTargetFactory.newInstance("//my/folder:foo#flavour"),
+        Paths.get("my/folder/foo#flavour")
+      }
+    };
+  }
+
+  @Test
+  @Parameters(method = "getTargetsForTest")
+  public void genPathFormat(BuildTarget target, Path path) {
+    assertEquals(Paths.get("buck-out/gen").resolve(path), BuildPaths.getGenDir(filesystem, target));
+  }
+
+  @Test
+  @Parameters(method = "getTargetsForTest")
+  public void annotationPathFormat(BuildTarget target, Path path) {
+    assertEquals(
+        Paths.get("buck-out/annotation/").resolve(path),
+        BuildPaths.getAnnotationDir(filesystem, target));
+  }
+
+  @Test
+  @Parameters(method = "getTargetsForTest")
+  public void scratchPathFormat(BuildTarget target, Path path) {
+    assertEquals(
+        Paths.get("buck-out/bin").resolve(path), BuildPaths.getScratchDir(filesystem, target));
+  }
+}
