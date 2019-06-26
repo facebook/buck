58be3050829327604b0ca7f32990505811877292
Alisdair Johnstone <alisdair@fb.com>
2019-06-26 09:00:23 -0700

    Property used to enable/disable RE experiment is now configurable
    Summary:
    The property used to enable RE experiment is now configurable. For example
    
    ```
    [remote_execution]
    auto_re_experiment_property=experiment_one
    [experiments]
    experiment_one=true
    ```
    
    In this example RE would be enabled (assuming auto_re_strategy is configured to enable upon experiment passing)
    
    Defaults to use the existing property 'remote_execution_beta_test' if a custom property has not been specified.
    
    Reviewed By: michsien
    
    shipit-source-id: 59af97fd3b

--- src/com/facebook/buck/remoteexecution/config/AbstractRemoteExecutionConfig.java
+++ src/com/facebook/buck/remoteexecution/config/AbstractRemoteExecutionConfig.java
@@ -130,10 +130,25 @@ abstract class AbstractRemoteExecutionConfig implements ConfigView<BuckConfig> {
   // Auxiliary flag used for setting custom build tags.
   public static final String BUILD_TAGS_KEY = "build_tags";
 
+  // Property inside [experiments] section that will be used to enable Remote Execution
+  // automatically or not
+  public static final String AUTO_RE_EXPERIMENT_PROPERTY_KEY = "auto_re_experiment_property";
+
+  public static final String DEFAULT_AUTO_RE_EXPERIMENT_PROPERTY = "remote_execution_beta_test";
+
+  private String getAutoReExperimentPropertyKey() {
+    return getValue(AUTO_RE_EXPERIMENT_PROPERTY_KEY).orElse(DEFAULT_AUTO_RE_EXPERIMENT_PROPERTY);
+  }
+
+  @VisibleForTesting
+  boolean isExperimentEnabled() {
+    return getDelegate().getBooleanValue("experiments", getAutoReExperimentPropertyKey(), false);
+  }
+
   public boolean isRemoteExecutionAutoEnabled(String username, List<String> commandArguments) {
     return isRemoteExecutionAutoEnabled(
         isBuildWhitelistedForRemoteExecution(username, commandArguments),
-        getDelegate().getBooleanValue("experiments", "remote_execution_beta_test", false),
+        isExperimentEnabled(),
         getAutoRemoteExecutionStrategy());
   }
 
