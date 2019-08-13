facebook/buck

commit bb4732644d232fe99d42ebeb8ed5a8a03461c555

Allow to specify bundle destination for apple resource rules
Summary: Extend `apple_resource` rule so it's possible to specify the destination in resulting bundle where files should be copied. Adding 5 options for now which are "frameworks", "executables", "plugins", "xpcservices" and default "resources". Non-standard destination options are equivalent to Xcode "Copy Files" build phase with corresponding destination.

Reviewed By: milend

shipit-source-id: 06f5e0ef8d

src/com/facebook/buck/apple/AbstractAppleBundleResources.java
@@ -20,7 +20,9 @@ import com.facebook.buck.core.rulekey.AddsToRuleKey;
 import com.facebook.buck.core.sourcepath.SourcePath;
 import com.facebook.buck.core.util.immutables.BuckStyleImmutable;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.immutables.value.Value;
 
 /** Resources to be bundled into a bundle. */
@@ -31,7 +33,7 @@ abstract class AbstractAppleBundleResources implements AddsToRuleKey {
    * Directories that should be copied into the bundle as directories of files with the same name.
    */
   @AddToRuleKey
-  public abstract ImmutableSet<SourcePath> getResourceDirs();
+  public abstract ImmutableSet<SourcePathWithAppleBundleDestination> getResourceDirs();
 
   /**
    * Directories whose contents should be copied into the root of the resources subdirectory.
@@ -40,22 +42,57 @@ abstract class AbstractAppleBundleResources implements AddsToRuleKey {
    * generates a directory of files.
    */
   @AddToRuleKey
-  public abstract ImmutableSet<SourcePath> getDirsContainingResourceDirs();
+  public abstract ImmutableSet<SourcePathWithAppleBundleDestination>
+      getDirsContainingResourceDirs();
 
   /** Files that are copied to the root of the resources subdirectory. */
   @AddToRuleKey
-  public abstract ImmutableSet<SourcePath> getResourceFiles();
+  public abstract ImmutableSet<SourcePathWithAppleBundleDestination> getResourceFiles();
 
   /** Resource files with localization variants. */
   @AddToRuleKey
   public abstract ImmutableSet<SourcePath> getResourceVariantFiles();
 
+  public Set<SourcePath> getResourceDirsForDestination(AppleBundleDestination destination) {
+    return extractSourcePathsForDestination(getResourceDirs(), destination);
+  }
+
+  public Set<SourcePath> getResourceFilesForDestination(AppleBundleDestination destination) {
+    return extractSourcePathsForDestination(getResourceFiles(), destination);
+  }
+
+  private static Set<SourcePath> extractSourcePathsForDestination(
+      ImmutableSet<SourcePathWithAppleBundleDestination> sourcePathWithAppleBundleDestinations,
+      AppleBundleDestination destination) {
+    return sourcePathWithAppleBundleDestinations.stream()
+        .filter(pathWithDestination -> destination == pathWithDestination.getDestination())
+        .map(SourcePathWithAppleBundleDestination::getSourcePath)
+        .collect(Collectors.toSet());
+  }
+
+  /** All kinds of destinations that are used by paths in this object. */
+  public Set<AppleBundleDestination> getAllDestinations() {
+    Stream<AppleBundleDestination> result =
+        getResourcesWithDestinationStream()
+            .map(SourcePathWithAppleBundleDestination::getDestination);
+    if (!getResourceVariantFiles().isEmpty()) {
+      result = Stream.concat(result, Stream.of(AppleBundleDestination.RESOURCES));
+    }
+    return result.collect(Collectors.toSet());
+  }
+
   /** Returns all the SourcePaths from the different types of resources. */
   public Iterable<SourcePath> getAll() {
-    return Iterables.concat(
-        getResourceDirs(),
-        getDirsContainingResourceDirs(),
-        getResourceFiles(),
-        getResourceVariantFiles());
+    return Stream.concat(
+            getResourcesWithDestinationStream()
+                .map(SourcePathWithAppleBundleDestination::getSourcePath),
+            getResourceVariantFiles().stream())
+        .collect(Collectors.toSet());
+  }
+
+  private Stream<SourcePathWithAppleBundleDestination> getResourcesWithDestinationStream() {
+    return Stream.concat(
+        getResourceDirs().stream(),
+        Stream.concat(getDirsContainingResourceDirs().stream(), getResourceFiles().stream()));
   }
 }

src/com/facebook/buck/apple/AbstractSourcePathWithAppleBundleDestination.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple;
+
+import com.facebook.buck.core.rulekey.AddToRuleKey;
+import com.facebook.buck.core.rulekey.AddsToRuleKey;
+import com.facebook.buck.core.sourcepath.SourcePath;
+import com.facebook.buck.core.util.immutables.BuckStyleImmutable;
+import org.immutables.value.Value;
+
+/**
+ * Simple type representing a {@link com.facebook.buck.core.sourcepath.SourcePath} and a destination
+ * {@link com.facebook.buck.apple.AppleBundleDestination} in a resulting bundle where a file or a
+ * directory with this path should be copied.
+ */
+@Value.Immutable()
+@BuckStyleImmutable
+public abstract class AbstractSourcePathWithAppleBundleDestination implements AddsToRuleKey {
+  @Value.Parameter
+  @AddToRuleKey
+  public abstract SourcePath getSourcePath();
+
+  @Value.Parameter
+  @AddToRuleKey
+  public abstract AppleBundleDestination getDestination();
+
+  /**
+   * Construct a new immutable {@code SourcePathWithAppleBundleDestination} instance with default
+   * value of apple bundle destination.
+   *
+   * @param sourcePath The value for the {@code sourcePath} attribute
+   * @return An immutable SourcePathWithAppleBundleDestination instance
+   */
+  public static SourcePathWithAppleBundleDestination of(SourcePath sourcePath) {
+    return SourcePathWithAppleBundleDestination.builder()
+        .setSourcePath(sourcePath)
+        .setDestination(AppleBundleDestination.defaultValue())
+        .build();
+  }
+}

src/com/facebook/buck/apple/AppleResourceDescription.java
@@ -17,6 +17,7 @@
 package com.facebook.buck.apple;
 
 import com.facebook.buck.core.description.arg.CommonDescriptionArg;
+import com.facebook.buck.core.exceptions.HumanReadableException;
 import com.facebook.buck.core.model.BuildTarget;
 import com.facebook.buck.core.model.Flavor;
 import com.facebook.buck.core.model.Flavored;
@@ -31,6 +32,8 @@ import com.facebook.buck.core.sourcepath.SourcePath;
 import com.facebook.buck.core.util.immutables.BuckStyleImmutable;
 import com.facebook.buck.io.filesystem.ProjectFilesystem;
 import com.google.common.collect.ImmutableSet;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.immutables.value.Value;
 
 /** Description for an apple_resource rule which copies resource files to the built bundle. */
@@ -66,9 +69,26 @@ public class AppleResourceDescription
       ProjectFilesystem filesystem,
       BuildRuleResolver resolver) {
     AppleResourceDescriptionArg appleResource = targetNode.getConstructorArg();
-    builder.addAllResourceDirs(appleResource.getDirs());
-    builder.addAllResourceFiles(appleResource.getFiles());
-    builder.addAllResourceVariantFiles(appleResource.getVariants());
+    AppleBundleDestination destination =
+        appleResource.getDestination().orElse(AppleBundleDestination.defaultValue());
+    builder.addAllResourceDirs(
+        appleResource.getDirs().stream()
+            .map(sourcePath -> SourcePathWithAppleBundleDestination.of(sourcePath, destination))
+            .collect(Collectors.toSet()));
+    builder.addAllResourceFiles(
+        appleResource.getFiles().stream()
+            .map(sourcePath -> SourcePathWithAppleBundleDestination.of(sourcePath, destination))
+            .collect(Collectors.toSet()));
+    ImmutableSet<SourcePath> variants = appleResource.getVariants();
+    if (!variants.isEmpty() && destination != AppleBundleDestination.RESOURCES) {
+      throw new HumanReadableException(
+          String.format(
+              ("Resource \"%s\" contains localization variants, but destination \"%s\" is "
+                  + "non-standard. It should have standard \\\"resource\\\" destination\""),
+              appleResource.getName(),
+              destination));
+    }
+    builder.addAllResourceVariantFiles(variants);
   }
 
   @BuckStyleImmutable
@@ -81,5 +101,7 @@ public class AppleResourceDescription
     ImmutableSet<SourcePath> getVariants();
 
     ImmutableSet<BuildTarget> getResourcesFromDeps();
+
+    Optional<AppleBundleDestination> getDestination();
   }
 }

docs/rule/apple_resource.soy
@@ -83,6 +83,15 @@ an <a href="{ROOT}rule/apple_bundle.html"><code>apple_bundle()</code> rule</a>.
   {/param}
 {/call}
 
+{call buck.arg}
+  {param name: 'destination' /}
+  {param default : 'resources' /}
+  {param desc}
+  Specifies the destination in the final application bundle where resource will be copied. Possible
+  values: "resources", "frameworks", "executables", "plugins", "xpcservices".
+  {/param}
+{/call}
+
 
 {/param} // args
 

src/com/facebook/buck/apple/AppleBundle.java
@@ -465,8 +465,8 @@ public class AppleBundle extends AbstractBuildRuleWithDeclaredAndExtraDeps
 
     addStepsToCopyExtensionBundlesDependencies(context, stepsBuilder, codeSignOnCopyPathsBuilder);
 
-    for (SourcePath variantSourcePath : resources.getResourceVariantFiles()) {
-      Path variantFilePath = context.getSourcePathResolver().getAbsolutePath(variantSourcePath);
+    for (SourcePath path : resources.getResourceVariantFiles()) {
+      Path variantFilePath = context.getSourcePathResolver().getAbsolutePath(path);
 
       Path variantDirectory = variantFilePath.getParent();
       if (variantDirectory == null || !variantDirectory.toString().endsWith(".lproj")) {
@@ -476,8 +476,9 @@ public class AppleBundle extends AbstractBuildRuleWithDeclaredAndExtraDeps
             variantFilePath);
       }
 
+      Path bundleDestinationPath = bundleRoot.resolve(destinations.getResourcesPath());
       Path bundleVariantDestinationPath =
-          resourcesDestinationPath.resolve(variantDirectory.getFileName());
+          bundleDestinationPath.resolve(variantDirectory.getFileName());
       stepsBuilder.add(
           MkdirStep.of(
               BuildCellRelativePath.fromCellRelativePath(
@@ -688,15 +689,19 @@ public class AppleBundle extends AbstractBuildRuleWithDeclaredAndExtraDeps
       AppleBundleResources resources, SourcePathResolver resolver) {
     // Ensure there are no resources that will overwrite each other
     // TODO: handle ResourceDirsContainingResourceDirs
-    Set<Path> resourcePaths = new HashSet<>();
-    for (SourcePath path :
-        Iterables.concat(resources.getResourceDirs(), resources.getResourceFiles())) {
-      Path pathInBundle = resolver.getRelativePath(path).getFileName();
-      if (resourcePaths.contains(pathInBundle)) {
-        throw new HumanReadableException(
-            "Bundle contains multiple resources with path %s", pathInBundle);
-      } else {
-        resourcePaths.add(pathInBundle);
+    for (AppleBundleDestination destination : resources.getAllDestinations()) {
+      Set<Path> resourcePaths = new HashSet<>();
+      for (SourcePath path :
+          Iterables.concat(
+              resources.getResourceDirsForDestination(destination),
+              resources.getResourceFilesForDestination(destination))) {
+        Path pathInBundle = resolver.getRelativePath(path).getFileName();
+        if (resourcePaths.contains(pathInBundle)) {
+          throw new HumanReadableException(
+              "Bundle contains multiple resources with path %s", pathInBundle);
+        } else {
+          resourcePaths.add(pathInBundle);
+        }
       }
     }
   }
@@ -839,30 +844,44 @@ public class AppleBundle extends AbstractBuildRuleWithDeclaredAndExtraDeps
     if (verifyResources) {
       verifyResourceConflicts(resources, context.getSourcePathResolver());
     }
-    Path resourcesDestinationPath = bundleRoot.resolve(this.destinations.getResourcesPath());
-    stepsBuilder.add(
-        MkdirStep.of(
-            BuildCellRelativePath.fromCellRelativePath(
-                context.getBuildCellRootPath(), getProjectFilesystem(), resourcesDestinationPath)));
-    for (SourcePath dir : resources.getResourceDirs()) {
+
+    for (AppleBundleDestination bundleDestination : resources.getAllDestinations()) {
+      Path bundleDestinationPath = bundleRoot.resolve(bundleDestination.getPath(destinations));
+      stepsBuilder.add(
+          MkdirStep.of(
+              BuildCellRelativePath.fromCellRelativePath(
+                  context.getBuildCellRootPath(), getProjectFilesystem(), bundleDestinationPath)));
+    }
+
+    for (SourcePathWithAppleBundleDestination dirWithDestination : resources.getResourceDirs()) {
+      Path bundleDestinationPath =
+          bundleRoot.resolve(dirWithDestination.getDestination().getPath(destinations));
       stepsBuilder.add(
           CopyStep.forDirectory(
               getProjectFilesystem(),
-              context.getSourcePathResolver().getAbsolutePath(dir),
-              resourcesDestinationPath,
+              context.getSourcePathResolver().getAbsolutePath(dirWithDestination.getSourcePath()),
+              bundleDestinationPath,
               CopyStep.DirectoryMode.DIRECTORY_AND_CONTENTS));
     }
-    for (SourcePath dir : resources.getDirsContainingResourceDirs()) {
+
+    for (SourcePathWithAppleBundleDestination dirWithDestination :
+        resources.getDirsContainingResourceDirs()) {
+      Path bundleDestinationPath =
+          bundleRoot.resolve(dirWithDestination.getDestination().getPath(destinations));
       stepsBuilder.add(
           CopyStep.forDirectory(
               getProjectFilesystem(),
-              context.getSourcePathResolver().getAbsolutePath(dir),
-              resourcesDestinationPath,
+              context.getSourcePathResolver().getAbsolutePath(dirWithDestination.getSourcePath()),
+              bundleDestinationPath,
               CopyStep.DirectoryMode.CONTENTS_ONLY));
     }
-    for (SourcePath file : resources.getResourceFiles()) {
-      Path resolvedFilePath = context.getSourcePathResolver().getAbsolutePath(file);
-      Path destinationPath = resourcesDestinationPath.resolve(resolvedFilePath.getFileName());
+
+    for (SourcePathWithAppleBundleDestination fileWithDestination : resources.getResourceFiles()) {
+      Path resolvedFilePath =
+          context.getSourcePathResolver().getAbsolutePath(fileWithDestination.getSourcePath());
+      Path bundleDestinationPath =
+          bundleRoot.resolve(fileWithDestination.getDestination().getPath(destinations));
+      Path destinationPath = bundleDestinationPath.resolve(resolvedFilePath.getFileName());
       addResourceProcessingSteps(
           context.getSourcePathResolver(), resolvedFilePath, destinationPath, stepsBuilder);
     }

src/com/facebook/buck/apple/AppleBundleDestination.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.apple;
+
+import java.nio.file.Path;
+
+/**
+ * Abstraction of a place in a resulting bundle where resource will be copied. Actual value of path
+ * relative to bundle root depends on a platform.
+ */
+public enum AppleBundleDestination {
+  RESOURCES,
+  FRAMEWORKS,
+  EXECUTABLES,
+  PLUGINS,
+  XPCSERVICES;
+
+  public static AppleBundleDestination defaultValue() {
+    return RESOURCES;
+  }
+
+  /**
+   * @param destinations Platform-specific set of concrete path values in a bundle.
+   * @return Value of path relative to bundle root where resource will be copied.
+   */
+  public Path getPath(AbstractAppleBundleDestinations destinations) {
+    switch (this) {
+      case RESOURCES:
+        return destinations.getResourcesPath();
+      case EXECUTABLES:
+        return destinations.getExecutablesPath();
+      case FRAMEWORKS:
+        return destinations.getFrameworksPath();
+      case PLUGINS:
+        return destinations.getPlugInsPath();
+      case XPCSERVICES:
+        return destinations.getXPCServicesPath();
+      default:
+        throw new IllegalStateException("Unhandled AppleBundleDestination " + this);
+    }
+  }
+}

src/com/facebook/buck/features/apple/project/NewNativeTargetProjectMutator.java
@@ -20,6 +20,7 @@ import com.dd.plist.NSArray;
 import com.dd.plist.NSDictionary;
 import com.dd.plist.NSString;
 import com.facebook.buck.apple.AppleAssetCatalogDescriptionArg;
+import com.facebook.buck.apple.AppleBundleDestination;
 import com.facebook.buck.apple.AppleHeaderVisibilities;
 import com.facebook.buck.apple.AppleResourceDescriptionArg;
 import com.facebook.buck.apple.AppleWrapperResourceArg;
@@ -28,8 +29,10 @@ import com.facebook.buck.apple.RuleUtils;
 import com.facebook.buck.apple.XcodePostbuildScriptDescription;
 import com.facebook.buck.apple.XcodePrebuildScriptDescription;
 import com.facebook.buck.apple.XcodeScriptDescriptionArg;
+import com.facebook.buck.apple.xcode.xcodeproj.CopyFilePhaseDestinationSpec;
 import com.facebook.buck.apple.xcode.xcodeproj.PBXBuildFile;
 import com.facebook.buck.apple.xcode.xcodeproj.PBXBuildPhase;
+import com.facebook.buck.apple.xcode.xcodeproj.PBXCopyFilesBuildPhase;
 import com.facebook.buck.apple.xcode.xcodeproj.PBXFileReference;
 import com.facebook.buck.apple.xcode.xcodeproj.PBXFrameworksBuildPhase;
 import com.facebook.buck.apple.xcode.xcodeproj.PBXGroup;
@@ -70,7 +73,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -300,6 +305,7 @@ class NewNativeTargetProjectMutator {
       addPhasesAndGroupsForSources(target, targetGroup);
       addFrameworksBuildPhase(project, target);
       addResourcesFileReference(targetGroup);
+      addCopyResourcesToNonStdDestinationPhases(target, targetGroup);
       addResourcesBuildPhase(target, targetGroup);
       target.getBuildPhases().addAll((Collection<? extends PBXBuildPhase>) copyFilesPhases);
       addRunScriptBuildPhases(target, postBuildRunScriptPhases);
@@ -559,20 +565,100 @@ class NewNativeTargetProjectMutator {
         ignored -> {});
   }
 
-  private PBXBuildPhase addResourcesBuildPhase(PBXNativeTarget target, PBXGroup targetGroup) {
+  private void addCopyResourcesToNonStdDestinationPhases(
+      PBXNativeTarget target, PBXGroup targetGroup) {
+    List<AppleBundleDestination> allNonStandardDestinations =
+        recursiveResources.stream()
+            .map(AppleResourceDescriptionArg::getDestination)
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .distinct()
+            .filter(e -> e != AppleBundleDestination.RESOURCES)
+            .sorted(Comparator.naturalOrder())
+            .collect(Collectors.toList());
+    for (AppleBundleDestination destination : allNonStandardDestinations) {
+      addCopyResourcesToNonStdDestinationPhase(target, targetGroup, destination);
+    }
+  }
+
+  private void addCopyResourcesToNonStdDestinationPhase(
+      PBXNativeTarget target, PBXGroup targetGroup, AppleBundleDestination destination) {
+    CopyFilePhaseDestinationSpec destinationSpec =
+        CopyFilePhaseDestinationSpec.of(pbxCopyPhaseDestination(destination));
+    PBXCopyFilesBuildPhase phase = new PBXCopyFilesBuildPhase(destinationSpec);
+    Set<AppleResourceDescriptionArg> resourceDescriptionArgsForDestination =
+        recursiveResources.stream()
+            .filter(
+                e ->
+                    e.getDestination().orElse(AppleBundleDestination.defaultValue()) == destination)
+            .collect(Collectors.toSet());
+    addResourcePathsToBuildPhase(
+        phase,
+        targetGroup,
+        resourceDescriptionArgsForDestination,
+        new HashSet<>(),
+        new HashSet<>());
+    if (!phase.getFiles().isEmpty()) {
+      target.getBuildPhases().add(phase);
+      LOG.debug(
+          "Added copy resources to non standard destination %s as build phase %s",
+          destination, phase);
+    }
+  }
+
+  private PBXCopyFilesBuildPhase.Destination pbxCopyPhaseDestination(
+      AppleBundleDestination destination) {
+    switch (destination) {
+      case FRAMEWORKS:
+        return PBXCopyFilesBuildPhase.Destination.FRAMEWORKS;
+      case EXECUTABLES:
+        return PBXCopyFilesBuildPhase.Destination.EXECUTABLES;
+      case RESOURCES:
+        return PBXCopyFilesBuildPhase.Destination.RESOURCES;
+      case PLUGINS:
+        return PBXCopyFilesBuildPhase.Destination.PLUGINS;
+      case XPCSERVICES:
+        return PBXCopyFilesBuildPhase.Destination.XPC;
+      default:
+        throw new IllegalStateException("Unhandled AppleBundleDestination " + destination);
+    }
+  }
+
+  private void addResourcesBuildPhase(PBXNativeTarget target, PBXGroup targetGroup) {
+    PBXBuildPhase phase = new PBXResourcesBuildPhase();
+    Set<AppleResourceDescriptionArg> standardDestinationResources =
+        recursiveResources.stream()
+            .filter(
+                e ->
+                    e.getDestination().orElse(AppleBundleDestination.defaultValue())
+                        == AppleBundleDestination.RESOURCES)
+            .collect(Collectors.toSet());
+    addResourcePathsToBuildPhase(
+        phase, targetGroup, standardDestinationResources, recursiveAssetCatalogs, wrapperResources);
+    if (!phase.getFiles().isEmpty()) {
+      target.getBuildPhases().add(phase);
+      LOG.debug("Added resources build phase %s", phase);
+    }
+  }
+
+  private void addResourcePathsToBuildPhase(
+      PBXBuildPhase phase,
+      PBXGroup targetGroup,
+      Set<AppleResourceDescriptionArg> resourceArgs,
+      Set<AppleAssetCatalogDescriptionArg> assetCatalogArgs,
+      Set<AppleWrapperResourceArg> resourcePathArgs) {
     ImmutableSet.Builder<Path> resourceFiles = ImmutableSet.builder();
     ImmutableSet.Builder<Path> resourceDirs = ImmutableSet.builder();
     ImmutableSet.Builder<Path> variantResourceFiles = ImmutableSet.builder();
 
     collectResourcePathsFromConstructorArgs(
-        recursiveResources,
-        recursiveAssetCatalogs,
-        wrapperResources,
+        resourceArgs,
+        assetCatalogArgs,
+        resourcePathArgs,
         resourceFiles,
         resourceDirs,
         variantResourceFiles);
 
-    PBXBuildPhase phase = new PBXResourcesBuildPhase();
     addResourcesFileReference(
         targetGroup,
         resourceFiles.build(),
@@ -586,11 +672,6 @@ class NewNativeTargetProjectMutator {
           PBXBuildFile buildFile = new PBXBuildFile(input);
           phase.getFiles().add(buildFile);
         });
-    if (!phase.getFiles().isEmpty()) {
-      target.getBuildPhases().add(phase);
-      LOG.debug("Added resources build phase %s", phase);
-    }
-    return phase;
   }
 
   private void collectResourcePathsFromConstructorArgs(

commit 8ee0a77c03a406220f1647b9409f78cc59b165d9

Fix indeterminism for apple bundle with resources rules
Summary: Issue introduced in https://github.com/facebook/buck/commit/bb4732644d232fe99d42ebeb8ed5a8a03461c555 as non-sorted sets were used to build an `AbstractAppleBundleResources`.

Reviewed By: miaoyipu

shipit-source-id: afad4cac3a

src/com/facebook/buck/apple/AbstractAppleBundleResources.java
@@ -21,6 +21,9 @@ import com.facebook.buck.core.sourcepath.SourcePath;
 import com.facebook.buck.core.util.immutables.BuckStyleImmutable;
 import com.google.common.collect.ImmutableSet;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.immutables.value.Value;
@@ -71,14 +74,15 @@ abstract class AbstractAppleBundleResources implements AddsToRuleKey {
   }
 
   /** All kinds of destinations that are used by paths in this object. */
-  public Set<AppleBundleDestination> getAllDestinations() {
+  public SortedSet<AppleBundleDestination> getAllDestinations() {
     Stream<AppleBundleDestination> result =
         getResourcesWithDestinationStream()
             .map(SourcePathWithAppleBundleDestination::getDestination);
     if (!getResourceVariantFiles().isEmpty()) {
       result = Stream.concat(result, Stream.of(AppleBundleDestination.RESOURCES));
     }
-    return result.collect(Collectors.toSet());
+    Supplier<SortedSet<AppleBundleDestination>> supplier = TreeSet::new;
+    return result.collect(Collectors.toCollection(supplier));
   }
 
   /** Returns all the SourcePaths from the different types of resources. */

src/com/facebook/buck/apple/AbstractSourcePathWithAppleBundleDestination.java
@@ -29,7 +29,9 @@ import org.immutables.value.Value;
  */
 @Value.Immutable()
 @BuckStyleImmutable
-public abstract class AbstractSourcePathWithAppleBundleDestination implements AddsToRuleKey {
+public abstract class AbstractSourcePathWithAppleBundleDestination
+    implements AddsToRuleKey, Comparable<AbstractSourcePathWithAppleBundleDestination> {
+
   @Value.Parameter
   @AddToRuleKey
   public abstract SourcePath getSourcePath();
@@ -50,5 +52,16 @@ public abstract class AbstractSourcePathWithAppleBundleDestination implements Ad
         .setSourcePath(sourcePath)
         .setDestination(AppleBundleDestination.defaultValue())
         .build();
+  }
+
+  @Override
+  public int compareTo(AbstractSourcePathWithAppleBundleDestination o) {
+    if (getSourcePath() != o.getSourcePath()) {
+      return getSourcePath().compareTo(o.getSourcePath());
+    }
+    if (getDestination() != o.getDestination()) {
+      return getDestination().compareTo(o.getDestination());
+    }
+    return 0;
   }
 }

src/com/facebook/buck/apple/AppleResourceDescription.java
@@ -33,6 +33,9 @@ import com.facebook.buck.core.util.immutables.BuckStyleImmutable;
 import com.facebook.buck.io.filesystem.ProjectFilesystem;
 import com.google.common.collect.ImmutableSet;
 import java.util.Optional;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.immutables.value.Value;
 
@@ -71,14 +74,15 @@ public class AppleResourceDescription
     AppleResourceDescriptionArg appleResource = targetNode.getConstructorArg();
     AppleBundleDestination destination =
         appleResource.getDestination().orElse(AppleBundleDestination.defaultValue());
+    Supplier<SortedSet<SourcePathWithAppleBundleDestination>> supplier = TreeSet::new;
     builder.addAllResourceDirs(
         appleResource.getDirs().stream()
             .map(sourcePath -> SourcePathWithAppleBundleDestination.of(sourcePath, destination))
-            .collect(Collectors.toSet()));
+            .collect(Collectors.toCollection(supplier)));
     builder.addAllResourceFiles(
         appleResource.getFiles().stream()
             .map(sourcePath -> SourcePathWithAppleBundleDestination.of(sourcePath, destination))
-            .collect(Collectors.toSet()));
+            .collect(Collectors.toCollection(supplier)));
     ImmutableSet<SourcePath> variants = appleResource.getVariants();
     if (!variants.isEmpty() && destination != AppleBundleDestination.RESOURCES) {
       throw new HumanReadableException(