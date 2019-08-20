8341172c63a6a292c1a8efb803e2ccbd3fe59ce5
Bob Yang <bobyf@fb.com>
2019-08-20 12:06:49 -0700

    Extract CompiledClassFileFinder
    Summary: extract so new test protocol java test rule can use it
    
    Reviewed By: philipjameson
    
    shipit-source-id: 1fa96ccb0e

--- src/com/facebook/buck/jvm/java/JavaTest.java
+++ src/com/facebook/buck/jvm/java/JavaTest.java
@@ -20,7 +20,6 @@ import com.facebook.buck.android.device.TargetDevice;
 import com.facebook.buck.core.build.buildable.context.BuildableContext;
 import com.facebook.buck.core.build.context.BuildContext;
 import com.facebook.buck.core.build.execution.context.ExecutionContext;
-import com.facebook.buck.core.exceptions.BuckUncheckedExecutionException;
 import com.facebook.buck.core.model.BuildId;
 import com.facebook.buck.core.model.BuildTarget;
 import com.facebook.buck.core.model.Flavor;
@@ -44,7 +43,6 @@ import com.facebook.buck.core.toolchain.tool.Tool;
 import com.facebook.buck.core.util.log.Logger;
 import com.facebook.buck.event.BuckEventBus;
 import com.facebook.buck.io.BuildCellRelativePath;
-import com.facebook.buck.io.file.MorePaths;
 import com.facebook.buck.io.filesystem.ProjectFilesystem;
 import com.facebook.buck.jvm.core.HasClasspathEntries;
 import com.facebook.buck.jvm.core.JavaLibrary;
@@ -63,8 +61,6 @@ import com.facebook.buck.test.TestRunningOptions;
 import com.facebook.buck.test.XmlTestResultParser;
 import com.facebook.buck.test.result.type.ResultType;
 import com.facebook.buck.test.selectors.TestSelectorList;
-import com.facebook.buck.util.ZipFileTraversal;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -72,7 +68,6 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -89,8 +84,6 @@ import java.util.SortedSet;
 import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.stream.Stream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import javax.annotation.Nullable;
 
 @SuppressWarnings("PMD.TestClassWithoutTestCases")
@@ -511,113 +504,6 @@ public class JavaTest extends AbstractBuildRuleWithDeclaredAndExtraDeps
   @Override
   public SortedSet<BuildRule> getExportedProvidedDeps() {
     return ImmutableSortedSet.of();
-  }
-
-  @VisibleForTesting
-  static class CompiledClassFileFinder {
-
-    private final Set<String> classNamesForSources;
-
-    CompiledClassFileFinder(JavaLibrary rule, SourcePathResolver pathResolver) {
-      Path outputPath;
-      SourcePath outputSourcePath = rule.getSourcePathToOutput();
-      if (outputSourcePath != null) {
-        outputPath = pathResolver.getAbsolutePath(outputSourcePath);
-      } else {
-        outputPath = null;
-      }
-      classNamesForSources =
-          getClassNamesForSources(
-              rule.getJavaSrcs(), outputPath, rule.getProjectFilesystem(), pathResolver);
-    }
-
-    public Set<String> getClassNamesForSources() {
-      return classNamesForSources;
-    }
-
-    /**
-     * When a collection of .java files is compiled into a directory, that directory will have a
-     * subfolder structure that matches the package structure of the input .java files. In general,
-     * the .java files will be 1:1 with the .class files with two notable exceptions: (1) There will
-     * be an additional .class file for each inner/anonymous class generated. These types of classes
-     * are easy to identify because they will contain a '$' in the name. (2) A .java file that
-     * defines multiple top-level classes (yes, this can exist:
-     * http://stackoverflow.com/questions/2336692/java-multiple-class-declarations-in-one-file) will
-     * generate multiple .class files that do not have '$' in the name. In this method, we perform a
-     * strict check for (1) and use a heuristic for (2). It is possible to filter out the type (2)
-     * situation with a stricter check that aligns the package directories of the .java files and
-     * the .class files, but it is a pain to implement. If this heuristic turns out to be
-     * insufficient in practice, then we can fix it.
-     *
-     * @param sources paths to .java source files that were passed to javac
-     * @param jarFilePath jar where the generated .class files were written
-     */
-    @VisibleForTesting
-    static ImmutableSet<String> getClassNamesForSources(
-        Set<SourcePath> sources,
-        @Nullable Path jarFilePath,
-        ProjectFilesystem projectFilesystem,
-        SourcePathResolver resolver) {
-      if (jarFilePath == null) {
-        return ImmutableSet.of();
-      }
-
-      Set<String> sourceClassNames = Sets.newHashSetWithExpectedSize(sources.size());
-      try {
-        JavaPaths.getExpandedSourcePaths(
-                sources.stream()
-                    .map(resolver::getAbsolutePath)
-                    .collect(ImmutableList.toImmutableList()))
-            .stream()
-            .map(MorePaths::getNameWithoutExtension)
-            .forEach(sourceClassNames::add);
-      } catch (IOException e) {
-        throw new BuckUncheckedExecutionException(
-            e, "When determining possible java test class names.");
-      }
-
-      ImmutableSet.Builder<String> testClassNames = ImmutableSet.builder();
-      Path jarFile = projectFilesystem.getPathForRelativePath(jarFilePath);
-      ZipFileTraversal traversal =
-          new ZipFileTraversal(jarFile) {
-
-            @Override
-            public void visit(ZipFile zipFile, ZipEntry zipEntry) {
-              String name = new File(zipEntry.getName()).getName();
-
-              // Ignore non-.class files.
-              if (!name.endsWith(".class")) {
-                return;
-              }
-
-              // As a heuristic for case (2) as described in the Javadoc, make sure the name of the
-              // .class file matches the name of a .java/.scala/.xxx file.
-              String nameWithoutDotClass = name.substring(0, name.length() - ".class".length());
-              if (!sourceClassNames.contains(nameWithoutDotClass)) {
-                return;
-              }
-
-              // Make sure it is a .class file that corresponds to a top-level .class file and not
-              // an
-              // inner class.
-              if (!name.contains("$")) {
-                String fullyQualifiedNameWithDotClassSuffix = zipEntry.getName().replace('/', '.');
-                String className =
-                    fullyQualifiedNameWithDotClassSuffix.substring(
-                        0, fullyQualifiedNameWithDotClassSuffix.length() - ".class".length());
-                testClassNames.add(className);
-              }
-            }
-          };
-      try {
-        traversal.traverse();
-      } catch (IOException e) {
-        // There's nothing sane to do here. The jar file really should exist.
-        throw new RuntimeException(e);
-      }
-
-      return testClassNames.build();
-    }
   }
 
   @Override
