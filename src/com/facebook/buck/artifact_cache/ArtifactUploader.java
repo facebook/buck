facebook/buck

commit 35c93b88c04ea88d1f65f149a73b7c549f38389a

Use zstandard instead of zip for cache files. (#2 attempt)
Summary:
This reverts commit fe7391f044815871f790a05f66633a91d515b002
and adds zstd-jni as a dependency to the Ant configuration used to build Buck.

Reviewed By: sbalabanov

fbshipit-source-id: 1291dbe9ad

src/com/facebook/buck/artifact_cache/ArtifactUploader.java
@@ -29,18 +29,27 @@ import com.facebook.buck.util.CloseableHolder;
 import com.facebook.buck.util.ErrorLogger;
 import com.facebook.buck.util.NamedTemporaryFile;
 import com.facebook.buck.util.exceptions.BuckUncheckedExecutionException;
-import com.facebook.buck.util.zip.Zip;
+import com.facebook.buck.util.zip.ZipConstants;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.io.ByteStreams;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.SortedSet;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.zstandard.ZstdCompressorOutputStream;
 
 /**
  * ArtifactUploader contains the logic of how to take a list of artifact paths and metadata and
@@ -54,18 +63,18 @@ public class ArtifactUploader {
       ArtifactCache artifactCache,
       BuckEventBus eventBus,
       ImmutableMap<String, String> buildMetadata,
-      SortedSet<Path> pathsToIncludeInZip,
+      SortedSet<Path> pathsToIncludeInArchive,
       BuildTarget buildTarget,
       ProjectFilesystem projectFilesystem) {
-    NamedTemporaryFile zip =
-        getTemporaryArtifactZip(
-            buildTarget, projectFilesystem, ruleKeys, eventBus, pathsToIncludeInZip);
+    NamedTemporaryFile archive =
+        getTemporaryArtifactArchive(
+            buildTarget, projectFilesystem, ruleKeys, eventBus, pathsToIncludeInArchive);
 
     // Store the artifact, including any additional metadata.
     ListenableFuture<Void> storeFuture =
         artifactCache.store(
             ArtifactInfo.builder().setRuleKeys(ruleKeys).setMetadata(buildMetadata).build(),
-            BorrowablePath.borrowablePath(zip.get()));
+            BorrowablePath.borrowablePath(archive.get()));
     Futures.addCallback(
         storeFuture,
         new FutureCallback<Void>() {
@@ -100,10 +109,11 @@ public class ArtifactUploader {
 
           private void onCompletion() {
             try {
-              // The zip file may have been borrowed when storing to the cache so only close it if
+              // The archive file may have been borrowed when storing to the cache so only close it
+              // if
               // it still exists.
-              if (Files.exists(zip.get())) {
-                zip.close();
+              if (Files.exists(archive.get())) {
+                archive.close();
               }
             } catch (IOException e) {
               new ErrorLogger(
@@ -118,8 +128,8 @@ public class ArtifactUploader {
                         public void logVerbose(Throwable e) {
                           LOG.debug(
                               e,
-                              "When deleting temporary zip %s for upload of %s.",
-                              zip.get(),
+                              "When deleting temporary archive %s for upload of %s.",
+                              archive.get(),
                               buildTarget);
                         }
                       },
@@ -132,29 +142,61 @@ public class ArtifactUploader {
     return storeFuture;
   }
 
-  private static NamedTemporaryFile getTemporaryArtifactZip(
+  private static NamedTemporaryFile getTemporaryArtifactArchive(
       BuildTarget buildTarget,
       ProjectFilesystem projectFilesystem,
       ImmutableSet<RuleKey> ruleKeys,
       BuckEventBus eventBus,
-      SortedSet<Path> pathsToIncludeInZip) {
+      SortedSet<Path> pathsToIncludeInArchive) {
     ArtifactCompressionEvent.Started started =
         ArtifactCompressionEvent.started(ArtifactCompressionEvent.Operation.COMPRESS, ruleKeys);
     eventBus.post(started);
-    try (CloseableHolder<NamedTemporaryFile> zip =
+    try (CloseableHolder<NamedTemporaryFile> archive =
         new CloseableHolder<>(
             new NamedTemporaryFile(
-                "buck_artifact_" + MostFiles.sanitize(buildTarget.getShortName()), ".zip"))) {
-      Zip.create(projectFilesystem, pathsToIncludeInZip, zip.get().get());
-      return zip.release();
+                "buck_artifact_" + MostFiles.sanitize(buildTarget.getShortName()), ".tar.zst"))) {
+      compress(projectFilesystem, pathsToIncludeInArchive, archive.get().get());
+      return archive.release();
     } catch (IOException e) {
       throw new BuckUncheckedExecutionException(
           e,
-          "When creating artifact zip for %s containing: \n %s.",
+          "When creating artifact archive for %s containing: \n %s.",
           buildTarget,
-          Joiner.on('\n').join(ImmutableSortedSet.copyOf(pathsToIncludeInZip)));
+          Joiner.on('\n').join(ImmutableSortedSet.copyOf(pathsToIncludeInArchive)));
     } finally {
       eventBus.post(ArtifactCompressionEvent.finished(started));
+    }
+  }
+
+  /** Archive and compress 'pathsToIncludeInArchive' into 'out', using tar+zstandard. */
+  @VisibleForTesting
+  static void compress(
+      ProjectFilesystem projectFilesystem, Collection<Path> pathsToIncludeInArchive, Path out)
+      throws IOException {
+    try (OutputStream o = new BufferedOutputStream(Files.newOutputStream(out));
+        OutputStream z = new ZstdCompressorOutputStream(o);
+        TarArchiveOutputStream archive = new TarArchiveOutputStream(z)) {
+      archive.setLongFileMode(TarArchiveOutputStream.LONGFILE_POSIX);
+      for (Path path : pathsToIncludeInArchive) {
+        boolean isRegularFile = !projectFilesystem.isDirectory(path);
+
+        // Add a file entry.
+        TarArchiveEntry e = new TarArchiveEntry(path.toString() + (isRegularFile ? "" : "/"));
+        e.setMode((int) projectFilesystem.getPosixFileMode(path));
+        e.setModTime(ZipConstants.getFakeTime());
+
+        if (isRegularFile) {
+          e.setSize(projectFilesystem.getFileSize(path));
+          archive.putArchiveEntry(e);
+          try (InputStream input = projectFilesystem.newFileInputStream(path)) {
+            ByteStreams.copy(input, archive);
+          }
+        } else {
+          archive.putArchiveEntry(e);
+        }
+        archive.closeArchiveEntry();
+      }
+      archive.finish();
     }
   }
 }

src/com/facebook/buck/artifact_cache/BUCK
@@ -54,8 +54,11 @@ java_immutables_library(
         "//src/com/facebook/buck/util:io",
         "//src/com/facebook/buck/util:util",
         "//src/com/facebook/buck/util/zip:zip",
+        "//third-party/java/commons-compress:commons-compress",
+        "//third-party/java/guava:guava",
         "//third-party/java/jackson:jackson-annotations",
         "//third-party/java/jackson:jackson-databind",
+        "//third-party/java/zstd-jni:zstd-jni",
     ],
 )
 

src/com/facebook/buck/util/unarchive/BUCK
@@ -14,5 +14,6 @@ java_library(
         "//third-party/java/commons-compress:commons-compress",
         "//third-party/java/guava:guava",
         "//third-party/java/jsr:jsr305",
+        "//third-party/java/zstd-jni:zstd-jni",
     ],
 )

test/com/facebook/buck/artifact_cache/BUCK
@@ -172,6 +172,7 @@ standard_java_test(
         "//third-party/java/aosp:aosp",
         "//third-party/java/args4j:args4j",
         "//third-party/java/asm:asm",
+        "//third-party/java/commons-compress:commons-compress",
         "//third-party/java/dd-plist:dd-plist",
         "//third-party/java/guava:guava",
         "//third-party/java/hamcrest:hamcrest-junit",

test/com/facebook/buck/core/build/engine/impl/BUCK
@@ -41,8 +41,10 @@ java_test(
         "//test/com/facebook/buck/testutil/integration:util",
         "//test/com/facebook/buck/util/config:testutil",
         "//test/com/facebook/buck/util/timing:testutil",
+        "//third-party/java/commons-compress:commons-compress",
         "//third-party/java/easymock:easymock",
         "//third-party/java/junit:junit",
+        "//third-party/java/zstd-jni:zstd-jni",
     ],
 )
 

test/com/facebook/buck/jvm/java/BUCK
@@ -1,4 +1,7 @@
-load("//tools/build_rules:java_rules.bzl", "java_test")
+load(
+    "//tools/build_rules:java_rules.bzl",
+    "java_test",
+)
 
 java_library(
     name = "testutil",
@@ -180,7 +183,9 @@ java_library(
 
 java_library(
     name = "javac-env",
-    srcs = ["JavaCompilationConstants.java"],
+    srcs = [
+        "JavaCompilationConstants.java",
+    ],
     visibility = [
         "//test/...",
     ],
@@ -381,7 +386,9 @@ java_test(
         exclude = SLOW_INTEGRATION_TESTS,
     ),
     has_immutable_types = True,
-    labels = ["long_running"],
+    labels = [
+        "long_running",
+    ],
     resources = glob(
         ["testdata/**"],
     ),
@@ -546,5 +553,6 @@ java_test(
         "//third-party/java/okio:okio",
         "//third-party/java/stringtemplate:stringtemplate",
         "//third-party/java/thrift:libthrift",
+        "//third-party/java/zstd-jni:zstd-jni",
     ],
 )

test/com/facebook/buck/testutil/integration/BUCK
@@ -250,6 +250,7 @@ java_library(
         "//third-party/java/aosp:aosp",
         "//third-party/java/args4j:args4j",
         "//third-party/java/asm:asm",
+        "//third-party/java/commons-compress:commons-compress",
         "//third-party/java/dd-plist:dd-plist",
         "//third-party/java/dx:dx",
         "//third-party/java/guava:guava",
@@ -270,6 +271,7 @@ java_library(
         "//third-party/java/okio:okio",
         "//third-party/java/stringtemplate:stringtemplate",
         "//third-party/java/thrift:libthrift",
+        "//third-party/java/zstd-jni:zstd-jni",
     ],
 )
 

third-party/java/commons-compress/BUCK
@@ -1,7 +1,7 @@
 prebuilt_jar(
     name = "commons-compress",
-    binary_jar = "commons-compress-1.8.1.jar",
-    javadoc_url = "http://commons.apache.org/proper/commons-compress/javadocs/api-1.8.1/",
+    binary_jar = "commons-compress-1.17.jar",
+    javadoc_url = "http://commons.apache.org/proper/commons-compress/javadocs/api-1.17/",
     licenses = [
         "LICENSE",
     ],

commit 9c45e976578c3cee17bc9114abd12fc08edcd06e

Fix ArtifactUploader.java TarArchiveEntry timezone issue (#2317)
Summary:
This fixed the issue at https://github.com/facebook/buck/issues/2316
This issue introduced from https://github.com/facebook/buck/commit/35c93b88c04ea88d1f65f149a73b7c549f38389a#diff-b12d580cc80ce3c8f0f5d2549fa61f67R4702

unlike `ZipEntry `, `TarArchiveEntry` would not include timezone convert.
https://docs.oracle.com/javase/8/docs/api/java/util/zip/ZipEntry.html#setTime-long-
https://commons.apache.org/proper/commons-compress/apidocs/org/apache/commons/compress/archivers/tar/TarArchiveEntry.html#setModTime-java.util.Date-

Since `Calendar` class is timezone sensitive, we should not depend on Calendar generated unix timestamp in `TarArchiveEntry`,  but use
https://github.com/facebook/buck/blob/a6c7d7391a7a8e575c6372cac6690a240b5907ea/src/com/facebook/buck/cxx/toolchain/objectfile/ObjectFileCommonModificationDate.java#L25 directly as same as
https://github.com/facebook/buck/blob/a6c7d7391a7a8e575c6372cac6690a240b5907ea/src/com/facebook/buck/cxx/FileLastModifiedDateContentsScrubber.java#L28

since ObjectFileCommonModificationDate is more generic I moved it from `cxx.toolchain` to `util`.

`com.facebook.buck.cxx.toolchain.objectfile.ObjectFileCommonModificationDate` --> `com.facebook.buck.util.ObjectFileCommonModificationDate`
Pull Request resolved: https://github.com/facebook/buck/pull/2317

Reviewed By: rajyengi, gnuthor

Pulled By: gnuthor

shipit-source-id: 367cfa2b30

src/com/facebook/buck/artifact_cache/ArtifactUploader.java
@@ -29,8 +29,8 @@ import com.facebook.buck.io.filesystem.ProjectFilesystem;
 import com.facebook.buck.util.CloseableHolder;
 import com.facebook.buck.util.ErrorLogger;
 import com.facebook.buck.util.NamedTemporaryFile;
+import com.facebook.buck.util.ObjectFileCommonModificationDate;
 import com.facebook.buck.util.types.Unit;
-import com.facebook.buck.util.zip.ZipConstants;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Joiner;
 import com.google.common.base.Throwables;
@@ -205,7 +205,7 @@ public class ArtifactUploader {
         int mode = (int) projectFilesystem.getPosixFileMode(path);
         // If permissions don't allow for owner to r or w, update to u+=rw and g+=r
         e.setMode((mode & 384) == 0 ? (mode | 416) : mode);
-        e.setModTime(ZipConstants.getFakeTime());
+        e.setModTime((long) ObjectFileCommonModificationDate.COMMON_MODIFICATION_TIME_STAMP * 1000);
 
         if (isRegularFile) {
           long pathSize = projectFilesystem.getFileSize(path);

src/com/facebook/buck/cxx/FileLastModifiedDateContentsScrubber.java
@@ -15,8 +15,8 @@
  */
 package com.facebook.buck.cxx;
 
-import com.facebook.buck.cxx.toolchain.objectfile.ObjectFileCommonModificationDate;
 import com.facebook.buck.io.file.FileAttributesScrubber;
+import com.facebook.buck.util.ObjectFileCommonModificationDate;
 import java.nio.file.Path;
 
 /** Resets the file's last modification date. */

src/com/facebook/buck/cxx/toolchain/objectfile/BUCK
@@ -9,5 +9,6 @@ java_immutables_library(
     deps = [
         "//src/com/facebook/buck/io:io",
         "//src/com/facebook/buck/io/file:file",
+        "//src/com/facebook/buck/util:util",
     ],
 )

src/com/facebook/buck/cxx/toolchain/objectfile/Machos.java
@@ -16,6 +16,7 @@
 
 package com.facebook.buck.cxx.toolchain.objectfile;
 
+import com.facebook.buck.util.ObjectFileCommonModificationDate;
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;

src/com/facebook/buck/cxx/toolchain/objectfile/ObjectFileScrubbers.java
@@ -17,6 +17,7 @@
 package com.facebook.buck.cxx.toolchain.objectfile;
 
 import com.facebook.buck.io.file.FileContentsScrubber;
+import com.facebook.buck.util.ObjectFileCommonModificationDate;
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;

src/com/facebook/buck/util/ObjectFileCommonModificationDate.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package com.facebook.buck.cxx.toolchain.objectfile;
+package com.facebook.buck.util;
 
 public class ObjectFileCommonModificationDate {
   private ObjectFileCommonModificationDate() {}

test/com/facebook/buck/cxx/ArchiveStepIntegrationTest.java
@@ -29,7 +29,6 @@ import com.facebook.buck.cxx.config.CxxBuckConfig;
 import com.facebook.buck.cxx.toolchain.Archiver;
 import com.facebook.buck.cxx.toolchain.CxxPlatform;
 import com.facebook.buck.cxx.toolchain.CxxPlatformUtils;
-import com.facebook.buck.cxx.toolchain.objectfile.ObjectFileCommonModificationDate;
 import com.facebook.buck.cxx.toolchain.objectfile.ObjectFileScrubbers;
 import com.facebook.buck.io.filesystem.ProjectFilesystem;
 import com.facebook.buck.io.filesystem.TestProjectFilesystems;
@@ -37,6 +36,7 @@ import com.facebook.buck.step.TestExecutionContext;
 import com.facebook.buck.step.fs.FileScrubberStep;
 import com.facebook.buck.testutil.TemporaryPaths;
 import com.facebook.buck.testutil.TestConsole;
+import com.facebook.buck.util.ObjectFileCommonModificationDate;
 import com.facebook.buck.util.environment.Platform;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;