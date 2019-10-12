3f7f2136c35f19d3b513c856ecbc01da8dd836a3
Stiopa Koltsov <nga@fb.com>
2019-10-11 17:53:20 -0700

    AcyclicDepthFirstPostOrderTraversalWithPayload
    Summary:
    `AcyclicDepthFirstPostOrderTraversal` copy-paste variant which associates payload with node and returns it from `traverse` function.
    
    `AcyclicDepthFirstPostOrderTraversal` is implemented as a thin wrapper of `AcyclicDepthFirstPostOrderTraversalWithPayload`.
    
    Used in the following diff; split here for easier review.
    
    Reviewed By: jtorkkola
    
    shipit-source-id: 561537ac33

--- src/com/facebook/buck/core/util/graph/AcyclicDepthFirstPostOrderTraversal.java
+++ src/com/facebook/buck/core/util/graph/AcyclicDepthFirstPostOrderTraversal.java
@@ -16,14 +16,9 @@
 
 package com.facebook.buck.core.util.graph;
 
-import com.google.common.base.Preconditions;
+import com.facebook.buck.util.types.Pair;
+import com.facebook.buck.util.types.Unit;
 import com.google.common.collect.Iterables;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.Set;
 import java.util.function.Predicate;
 
 /**
@@ -35,10 +30,14 @@ import java.util.function.Predicate;
  */
 public class AcyclicDepthFirstPostOrderTraversal<T> {
 
-  private final GraphTraversable<T> traversable;
+  private final AcyclicDepthFirstPostOrderTraversalWithPayload<T, Unit> traversal;
 
   public AcyclicDepthFirstPostOrderTraversal(GraphTraversable<T> traversable) {
-    this.traversable = traversable;
+    this.traversal =
+        new AcyclicDepthFirstPostOrderTraversalWithPayload<>(
+            n -> {
+              return new Pair<>(Unit.UNIT, traversable.findChildren(n));
+            });
   }
 
   public Iterable<T> traverse(Iterable<? extends T> initialNodes) throws CycleException {
@@ -58,96 +57,7 @@ public class AcyclicDepthFirstPostOrderTraversal<T> {
   public Iterable<T> traverse(
       Iterable<? extends T> initialNodes, Predicate<T> shouldExploreChildren)
       throws CycleException {
-    // This corresponds to the current chain of nodes being explored. Enforcing this invariant makes
-    // this data structure useful for debugging.
-    Deque<Explorable> toExplore = new LinkedList<>();
-    for (T node : initialNodes) {
-      toExplore.add(new Explorable(node));
-    }
-
-    Set<T> inProgress = new HashSet<>();
-    LinkedHashSet<T> explored = new LinkedHashSet<>();
-
-    while (!toExplore.isEmpty()) {
-      Explorable explorable = toExplore.peek();
-      T node = explorable.node;
-
-      // This could happen if one of the initial nodes is a dependency of the other, for example.
-      if (explored.contains(node)) {
-        toExplore.removeFirst();
-        continue;
-      }
-
-      inProgress.add(node);
-
-      // Find children that need to be explored to add to the stack.
-      int stackSize = toExplore.size();
-      if (shouldExploreChildren.test(node)) {
-        for (Iterator<? extends T> iter = explorable.children; iter.hasNext(); ) {
-          T child = iter.next();
-          if (inProgress.contains(child)) {
-            throw createCycleException(child, toExplore);
-          } else if (!explored.contains(child)) {
-            toExplore.addFirst(new Explorable(child));
-
-            // Without this break statement:
-            // (1) Children will be explored in reverse order instead of the specified order.
-            // (2) CycleException may contain extra nodes.
-            // Comment out the break statement and run the unit test to verify this for yourself.
-            break;
-          }
-        }
-      }
-
-      if (stackSize == toExplore.size()) {
-        // Nothing was added to toExplore, so the current node can be popped off the stack and
-        // marked as explored.
-        toExplore.removeFirst();
-        inProgress.remove(node);
-        explored.add(node);
-      }
-    }
-
-    Preconditions.checkState(inProgress.isEmpty(), "No more nodes should be in progress.");
-
-    return Iterables.unmodifiableIterable(explored);
-  }
-
-  /**
-   * A node that needs to be explored, paired with a (possibly paused) iteration of its children.
-   */
-  private class Explorable {
-    private final T node;
-    private final Iterator<? extends T> children;
-
-    Explorable(T node) {
-      this.node = node;
-      this.children = traversable.findChildren(node);
-    }
-  }
-
-  private CycleException createCycleException(
-      T collisionNode, Iterable<Explorable> currentExploration) {
-    Deque<T> chain = new LinkedList<>();
-    chain.add(collisionNode);
-
-    boolean foundStartOfCycle = false;
-    for (Explorable explorable : currentExploration) {
-      T node = explorable.node;
-      chain.addFirst(node);
-      if (collisionNode.equals(node)) {
-        // The start of the cycle has been reached!
-        foundStartOfCycle = true;
-        break;
-      }
-    }
-
-    Preconditions.checkState(
-        foundStartOfCycle,
-        "Start of cycle %s should appear in traversal history %s.",
-        collisionNode,
-        chain);
-
-    return new CycleException(chain);
+    return Iterables.unmodifiableIterable(
+        traversal.traverse(initialNodes, shouldExploreChildren).keySet());
   }
 }
