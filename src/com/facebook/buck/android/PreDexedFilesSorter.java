88dfe9d8b61e0311c412180635398941644eab68
Thomas Jacob <tja@fb.com>
2019-11-07 16:06:00 -0800

    Revert "Extend canary index id from 2 bytes to 7"
    Summary:
    This reverts commit 5ef9f662302bac46cccd89fa0c976df09341a303.
    
    makes some instrumentation tests fail
    
    https://our.intern.facebook.com/intern/testinfra/diagnostics/1407375032507542.844424944887167.1573160454/
    
    Reviewed By: christolliday
    
    shipit-source-id: efbef6202d

--- src/com/facebook/buck/android/PreDexedFilesSorter.java
+++ src/com/facebook/buck/android/PreDexedFilesSorter.java
@@ -257,13 +257,13 @@ public class PreDexedFilesSorter {
       throw new HumanReadableException(message.toString());
     }
 
-    /** @see CanaryFactory#create(String, String) */
+    /** @see CanaryFactory#create(String, int) */
     private DexWithClasses createCanary(
         ProjectFilesystem filesystem,
         String storeName,
         int index,
         ImmutableList.Builder<Step> steps) {
-      FileLike fileLike = CanaryFactory.create(storeName, String.format("%07d", index));
+      FileLike fileLike = CanaryFactory.create(storeName, index);
       String canaryDirName = "canary_" + storeName + "_" + String.valueOf(index);
       Path scratchDirectoryForCanaryClass = scratchDirectory.resolve(canaryDirName);
 
