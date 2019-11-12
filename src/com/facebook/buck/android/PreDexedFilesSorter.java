2205b8ae7b31ae87671b643440cca1bc97a4fc25
Chris Tolliday <ctolliday@fb.com>
2019-11-12 10:28:43 -0800

    Revert "Revert "[dex] Extend canary index id from 2 bytes to 7""
    Reviewed By: asp2insp
    
    shipit-source-id: 0964ba85a4

--- src/com/facebook/buck/android/PreDexedFilesSorter.java
+++ src/com/facebook/buck/android/PreDexedFilesSorter.java
@@ -257,13 +257,13 @@ public class PreDexedFilesSorter {
       throw new HumanReadableException(message.toString());
     }
 
-    /** @see CanaryFactory#create(String, int) */
+    /** @see CanaryFactory#create(String, String) */
     private DexWithClasses createCanary(
         ProjectFilesystem filesystem,
         String storeName,
         int index,
         ImmutableList.Builder<Step> steps) {
-      FileLike fileLike = CanaryFactory.create(storeName, index);
+      FileLike fileLike = CanaryFactory.create(storeName, String.format("%07d", index));
       String canaryDirName = "canary_" + storeName + "_" + String.valueOf(index);
       Path scratchDirectoryForCanaryClass = scratchDirectory.resolve(canaryDirName);
 
