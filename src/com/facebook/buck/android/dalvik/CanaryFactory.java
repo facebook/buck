c6d10cf77a5dbbe3d255a478281145d3fc8d0b37
Chris Tolliday <ctolliday@fb.com>
2019-11-13 20:03:50 -0800

    Revert "Revert "Revert "[dex] Extend canary index id from 2 bytes to 7"""
    shipit-source-id: ea051b2694

--- src/com/facebook/buck/android/dalvik/CanaryFactory.java
+++ src/com/facebook/buck/android/dalvik/CanaryFactory.java
@@ -28,70 +28,37 @@ import java.util.Arrays;
 
 /** Helper to create a "canary" class for the secondary DEX. See {@link #create}. */
 public class CanaryFactory {
-  static final String CANARY_PATH_FORMAT = "%s/dex%s/Canary.class";
+
+  static final String CANARY_PATH_FORMAT = "%s/dex%02d/Canary.class";
 
   /**
    * Produced by compiling the following Java file with JDK 7 with "-target 6 -source 6".
    *
    * <pre>
-   * package secondary.dex000_000;
+   * package secondary.dex01;
    * public interface Canary {}
-   * </pre>
-   *
-   * To generate:
-   *
-   * <pre>
-   * $ echo -e "package secondary.dex000_000;\npublic interface Canary {}" > Canary.java
-   * $ javac -target 6 -source 6 Canary.java
-   * $ xxd -c 4 -g 1 Canary.class | cut -d' ' -f2-5 | sed -E 's/(..) ?/(byte) 0x\1, /g'
    * </pre>
    */
   private static final byte[] CANARY_TEMPLATE = {
-    (byte) 0xca, (byte) 0xfe, (byte) 0xba, (byte) 0xbe,
-    (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x32,
-    (byte) 0x00, (byte) 0x07, (byte) 0x07, (byte) 0x00,
-    (byte) 0x05, (byte) 0x07, (byte) 0x00, (byte) 0x06,
-    // 16
-    (byte) 0x01, (byte) 0x00, (byte) 0x0a, (byte) 0x53,
-    (byte) 0x6f, (byte) 0x75, (byte) 0x72, (byte) 0x63,
-    (byte) 0x65, (byte) 0x46, (byte) 0x69, (byte) 0x6c,
-    (byte) 0x65, (byte) 0x01, (byte) 0x00, (byte) 0x0b,
-    // 32
-    (byte) 0x43, (byte) 0x61, (byte) 0x6e, (byte) 0x61,
-    (byte) 0x72, (byte) 0x79, (byte) 0x2e, (byte) 0x6a,
-    (byte) 0x61, (byte) 0x76, (byte) 0x61, (byte) 0x01,
-    (byte) 0x00, (byte) 0x1b,
-    // secondary/dex (Offset = 46)
-    (byte) 0x73, (byte) 0x65, (byte) 0x63, (byte) 0x6f,
-    (byte) 0x6e, (byte) 0x64, (byte) 0x61, (byte) 0x72,
-    (byte) 0x79, (byte) 0x2f, (byte) 0x64, (byte) 0x65,
-    (byte) 0x78,
-    // 000_000
-    (byte) 0x30, (byte) 0x30, (byte) 0x30, (byte) 0x5f,
-    (byte) 0x30, (byte) 0x30, (byte) 0x30,
-    // ...
-    (byte) 0x2f, (byte) 0x43, (byte) 0x61, (byte) 0x6e,
-    (byte) 0x61, (byte) 0x72, (byte) 0x79, (byte) 0x01,
-    (byte) 0x00, (byte) 0x10, (byte) 0x6a, (byte) 0x61,
-    (byte) 0x76, (byte) 0x61, (byte) 0x2f, (byte) 0x6c,
-    (byte) 0x61, (byte) 0x6e, (byte) 0x67, (byte) 0x2f,
-    (byte) 0x4f, (byte) 0x62, (byte) 0x6a, (byte) 0x65,
-    (byte) 0x63, (byte) 0x74, (byte) 0x06, (byte) 0x01,
-    (byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x02,
-    (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-    (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01,
-    (byte) 0x00, (byte) 0x03, (byte) 0x00, (byte) 0x00,
-    (byte) 0x00, (byte) 0x02, (byte) 0x00, (byte) 0x04,
+    -54, -2, -70, -66, 0x00, 0x00, 0x00, 0x32,
+    0x00, 0x05, 0x07, 0x00, 0x03, 0x07, 0x00, 0x04,
+    0x01, 0x00, 0x16, 0x73, 0x65, 0x63, 0x6f, 0x6e,
+    0x64, 0x61, 0x72, 0x79, 0x2f, 0x64, 0x65, 0x78,
+    0x30, 0x31, 0x2f, 0x43, 0x61, 0x6e, 0x61, 0x72,
+    0x79, 0x01, 0x00, 0x10, 0x6a, 0x61, 0x76, 0x61,
+    0x2f, 0x6c, 0x61, 0x6e, 0x67, 0x2f, 0x4f, 0x62,
+    0x6a, 0x65, 0x63, 0x74, 0x06, 0x01, 0x00, 0x01,
+    0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00
   };
 
-  private static final int CANARY_STORE_OFFSET = 46;
-
   /**
-   * Offset into {@link #CANARY_TEMPLATE} where we find the 7-byte UTF-8 index that must be updated
+   * Offset into {@link #CANARY_TEMPLATE} where we find the 2-byte UTF-8 index that must be updated
    * for each canary class to change the package.
    */
-  private static final int CANARY_INDEX_OFFSET =
-      CANARY_STORE_OFFSET + "secondary/dex".getBytes(Charsets.UTF_8).length;
+  private static final int CANARY_INDEX_OFFSET = 32;
+
+  private static final int CANARY_STORE_OFFSET = 19;
 
   /** Utility class: do not instantiate */
   private CanaryFactory() {}
@@ -103,15 +70,16 @@ public class CanaryFactory {
    * on Google Maps, but we are on AOSP).
    *
    * @param store dex store name of the current zip (to ensure unique names).
-   * @param index Index of the current zip (to ensure unique names), must be 7 UTF-8 bytes long
+   * @param index Index of the current zip (to ensure unique names).
    */
-  public static FileLike create(String store, String index) {
+  public static FileLike create(String store, int index) {
     byte[] canaryClass = Arrays.copyOf(CANARY_TEMPLATE, CANARY_TEMPLATE.length);
-    byte[] canaryIndexBytes = index.getBytes(Charsets.UTF_8);
+    String canaryIndexStr = String.format("%02d", index);
+    byte[] canaryIndexBytes = canaryIndexStr.getBytes(Charsets.UTF_8);
     Preconditions.checkState(
-        canaryIndexBytes.length == 7,
-        "Formatted index string \"" + index + "\" is not exactly 7 bytes.");
-    System.arraycopy(canaryIndexBytes, 0, canaryClass, CANARY_INDEX_OFFSET, 7);
+        canaryIndexBytes.length == 2,
+        "Formatted index string \"" + canaryIndexStr + "\" is not exactly 2 bytes.");
+    System.arraycopy(canaryIndexBytes, 0, canaryClass, CANARY_INDEX_OFFSET, 2);
     byte[] canaryStoreBytes = store.getBytes(Charsets.UTF_8);
     Preconditions.checkState(
         canaryStoreBytes.length == 9,
