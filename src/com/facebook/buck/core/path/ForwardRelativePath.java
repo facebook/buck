eaf86795e29384b0989c8c0ecef32480c53078cf
Stiopa Koltsov <nga@fb.com>
2019-11-16 16:14:44 -0800

    Fix ForwardRelativePath.resolve
    Summary: Also implement a test
    
    Reviewed By: bobyangyf
    
    shipit-source-id: c42d7092fd

--- src/com/facebook/buck/core/path/ForwardRelativePath.java
+++ src/com/facebook/buck/core/path/ForwardRelativePath.java
@@ -141,6 +141,7 @@ public class ForwardRelativePath implements Comparable<ForwardRelativePath> {
       // skip validation
       String[] segments = new String[this.segments.length + other.segments.length];
       System.arraycopy(this.segments, 0, segments, 0, this.segments.length);
+      System.arraycopy(other.segments, 0, segments, this.segments.length, other.segments.length);
       return new ForwardRelativePath(segments);
     }
   }
--- test/com/facebook/buck/core/path/ForwardRelativePathTest.java
+++ test/com/facebook/buck/core/path/ForwardRelativePathTest.java
@@ -192,4 +192,17 @@ public class ForwardRelativePathTest {
 
     assertFalse(ForwardRelativePath.of("ab").startsWith(ForwardRelativePath.of("a")));
   }
+
+  @Test
+  public void resolve() {
+    String[] paths = new String[] {"", "foo", "bar/baz"};
+    for (String p1 : paths) {
+      for (String p2 : paths) {
+        ForwardRelativePath naive =
+            ForwardRelativePath.of(p1 + (!p1.isEmpty() && !p2.isEmpty() ? "/" : "") + p2);
+        ForwardRelativePath fast = ForwardRelativePath.of(p1).resolve(ForwardRelativePath.of(p2));
+        assertEquals(naive, fast);
+      }
+    }
+  }
 }
