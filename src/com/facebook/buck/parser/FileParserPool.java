2150e6f252693274a19bc175f292538293c05b4d
Chatura Atapattu <chatatap@fb.com>
2019-12-01 11:14:30 -0800

    Make ProjectBuildFileParserPool generic and extract out FileParserFactory
    Summary:
    Previously, `ProjectBuildFileParserPool` would vend `ProjectBuildFileParser`s using a shared resource pool, creating parsers from `ProjectBuildFileParserFactory`.
    
    We can elevate `FileParserPool` to an abstract class generic, allowing us to reuse this implementation for vending any type of `FileParser`, including the new `PackageFileParser` for parsing `PACKAGE` files.
    
    This diff also adds a concrete implementation for `PackageFileParserFactory` for creating PACKAGE parsers.
    
    Reviewed By: stepancheg
    
    shipit-source-id: e40c80154c

--- src/com/facebook/buck/parser/FileParserPool.java
+++ src/com/facebook/buck/parser/FileParserPool.java
@@ -18,9 +18,19 @@ package com.facebook.buck.parser;
 import com.facebook.buck.core.cell.Cell;
 import com.facebook.buck.event.BuckEventBus;
 import com.facebook.buck.io.watchman.Watchman;
+import com.facebook.buck.parser.api.FileManifest;
+import com.facebook.buck.parser.api.FileParser;
+import com.facebook.buck.util.concurrent.ResourcePool;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import javax.annotation.concurrent.GuardedBy;
 
 /**
  * Allows multiple concurrently executing futures to share a constrained number of parsers.
@@ -29,12 +39,94 @@ import java.nio.file.Path;
  * requested the associated 'requests' are queued up. As soon as a parser is returned it will be
  * used to satisfy the first pending request, otherwise it is "parked".
  */
-public interface FileParserPool<T> extends AutoCloseable {
+abstract class FileParserPool<T extends FileManifest> implements AutoCloseable {
+  private final int maxParsersPerCell;
 
-  ListenableFuture<T> getManifest(
+  @GuardedBy("this")
+  final Map<Cell, ResourcePool<FileParser<T>>> parserResourcePools;
+
+  @GuardedBy("this")
+  private final Map<Cell, FileParser<T>> nonPooledCells;
+
+  private final FileParserFactory<T> fileParserFactory;
+  private final AtomicBoolean closing;
+
+  /** @param maxParsersPerCell maximum number of parsers to create for a single cell. */
+  public FileParserPool(int maxParsersPerCell, FileParserFactory<T> fileParserFactory) {
+    Preconditions.checkArgument(maxParsersPerCell > 0);
+
+    this.maxParsersPerCell = maxParsersPerCell;
+    this.parserResourcePools = new HashMap<>();
+    this.nonPooledCells = new HashMap<>();
+    this.fileParserFactory = fileParserFactory;
+    this.closing = new AtomicBoolean(false);
+  }
+
+  /**
+   * @param cell the cell in which we're parsing
+   * @param parseFile the file to parse
+   * @param executorService where to perform the parsing.
+   * @return a {@link ListenableFuture} containing the result of the parsing. The future will be
+   *     cancelled if the {@link ProjectBuildFileParserPool#close()} method is called.
+   */
+  public ListenableFuture<T> getManifest(
       BuckEventBus buckEventBus,
       Cell cell,
       Watchman watchman,
       Path parseFile,
-      ListeningExecutorService executorService);
+      ListeningExecutorService executorService) {
+    Preconditions.checkState(!closing.get());
+
+    if (shouldUsePoolForCell(cell)) {
+      return getResourcePoolForCell(buckEventBus, cell, watchman)
+          .scheduleOperationWithResource(parser -> parser.getManifest(parseFile), executorService);
+    }
+    FileParser<T> parser = getParserForCell(buckEventBus, cell, watchman);
+    return executorService.submit(() -> parser.getManifest(parseFile));
+  }
+
+  private synchronized ResourcePool<FileParser<T>> getResourcePoolForCell(
+      BuckEventBus buckEventBus, Cell cell, Watchman watchman) {
+    return parserResourcePools.computeIfAbsent(
+        cell,
+        c ->
+            new ResourcePool<>(
+                maxParsersPerCell,
+                // If the Python process garbles the output stream then the bser codec doesn't
+                // always
+                // recover and subsequent attempts at invoking the parser will fail.
+                ResourcePool.ResourceUsageErrorPolicy.RETIRE,
+                () -> fileParserFactory.createFileParser(buckEventBus, c, watchman, false)));
+  }
+
+  private synchronized FileParser<T> getParserForCell(
+      BuckEventBus buckEventBus, Cell cell, Watchman watchman) {
+    return nonPooledCells.computeIfAbsent(
+        cell, c -> fileParserFactory.createFileParser(buckEventBus, c, watchman, false));
+  }
+
+  abstract void reportProfile();
+
+  @Override
+  public void close() {
+    reportProfile();
+    ImmutableSet<ResourcePool<FileParser<T>>> resourcePools;
+    ImmutableSet<FileParser<T>> parsers;
+    synchronized (this) {
+      Preconditions.checkState(!closing.get());
+      closing.set(true);
+      resourcePools = ImmutableSet.copyOf(parserResourcePools.values());
+      parsers = ImmutableSet.copyOf(nonPooledCells.values());
+    }
+    for (FileParser<T> parser : parsers) {
+      try {
+        parser.close();
+      } catch (InterruptedException | IOException e) {
+        throw new RuntimeException("Could not properly close a parser.", e);
+      }
+    }
+    resourcePools.forEach(ResourcePool::close);
+  }
+
+  abstract boolean shouldUsePoolForCell(Cell cell);
 }
