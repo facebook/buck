7fd695933e77af788c6b4f737f5a48f4bbb15503
Thomas Jacob <tja@fb.com>
2019-12-11 19:44:47 -0800

    Apply license lint to all of buck, step 2, fixup
    Summary:
    allow-large-files
    
    The automatic change in D18723773 breaks
    some tests and also overrides some copyrights that
    it shouldn't, lets fix those
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: rajyengi
    
    shipit-source-id: 343e7e33a1250de48b46c6022ab9198f78ba57e4

--- test/com/facebook/buck/util/LicenseCheckTest.java
+++ test/com/facebook/buck/util/LicenseCheckTest.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2013-present Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.facebook.buck.util;
 
@@ -62,8 +62,8 @@ public class LicenseCheckTest {
             // The newline character doesn't match "\w", "\\n" so do a non-greedy match until the
             // next part of the copyright.
             "^/\\\\*.*?"
-                + "\\\\* Copyright 20\\d\\d-present Facebook, Inc\\..*?"
-                + "\\\\* Licensed under the Apache License, Version 2.0 \\(the \"License\"\\); you may.*",
+                + "\\\\* Copyright (\\(c\\)|20\\d\\d-present) Facebook, Inc\\..*?"
+                + "\\\\* Licensed under the Apache License, Version 2.0 \\(the \"License\"\\);.*?",
             Pattern.MULTILINE | Pattern.DOTALL);
 
     private static final Path TEST_DATA = Paths.get("testdata");
