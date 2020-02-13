20d74f5803fab2cf2877cfeaf32ea14ea4fd40b2
Jinlin Zhang <jinlinzhang@fb.com>
2020-02-13 13:39:44 -0800

    Python2 is EOL, change func names to Phthon3 names

--- test/com/facebook/buck/features/python/PythonBinaryIntegrationTest.java
+++ test/com/facebook/buck/features/python/PythonBinaryIntegrationTest.java
@@ -608,7 +608,7 @@ public class PythonBinaryIntegrationTest {
                 (path, attr) -> path.getFileName().toString().endsWith(".pyc"))
             .map(path -> tmp.getRoot().relativize(path))
             .collect(ImmutableList.toImmutableList());
-    Assert.assertEquals(3, pycFiles.size());
+    Assert.assertTrue(pycFiles.size() >= 3);
   }
 
   @Test
