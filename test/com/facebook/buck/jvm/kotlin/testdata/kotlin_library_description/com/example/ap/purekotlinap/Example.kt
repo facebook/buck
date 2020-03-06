f4076b56091f1c5a9411cc20b25823739da3a393
Ian Childs <ianc@fb.com>
2020-03-06 10:20:51 -0800

    Back out "Explicitly specify annotation processors to KAPT - second attempt"
    Summary:
    I'm an idiot and this still breaks the buck bump. Reverting to unblock. Will add it behind a config option next time :(
    
    Original commit changeset: d27699b105be
    
    Reviewed By: jiawei-lyu
    
    shipit-source-id: 1c92cbfa01901bc79b518e07228638e2ef2b2302

--- test/com/facebook/buck/jvm/kotlin/testdata/kotlin_library_description/com/example/ap/purekotlinap/Example.kt
+++ test/com/facebook/buck/jvm/kotlin/testdata/kotlin_library_description/com/example/ap/purekotlinap/Example.kt
@@ -1,8 +1,6 @@
 package com.example.ap.purekotlinap
 
 import com.example.ap.kotlinannotation.KotlinAnnotation
-import com.example.ap.kotlinannotation.ThrowingKotlinAnnotation
 
 @KotlinAnnotation
-@ThrowingKotlinAnnotation
 class Example
