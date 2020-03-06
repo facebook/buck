a58860069b6de53371588268a38c6f0b4f554f76
Ian Childs <ianc@fb.com>
2020-03-05 17:45:52 -0800

    Explicitly specify annotation processors to KAPT - second attempt
    Summary:
    Original commit changeset: c8e1d769addc
    
    First landed in D20100001. That caused a problem when build aloha_messenger_release, which was fixed in D20151785, so trying to land this one again.
    
    shipit-source-id: d27699b105be0246090702fd41028111c9a6154a

--- src/com/facebook/buck/jvm/kotlin/KotlincToJarStepFactory.java
+++ src/com/facebook/buck/jvm/kotlin/KotlincToJarStepFactory.java
@@ -41,6 +41,7 @@ import com.facebook.buck.jvm.java.JavacOptions;
 import com.facebook.buck.jvm.java.JavacPluginJsr199Fields;
 import com.facebook.buck.jvm.java.JavacPluginParams;
 import com.facebook.buck.jvm.java.JavacToJarStepFactory;
+import com.facebook.buck.jvm.java.ResolvedJavacPluginProperties;
 import com.facebook.buck.jvm.kotlin.KotlinLibraryDescription.AnnotationProcessingTool;
 import com.facebook.buck.step.Step;
 import com.facebook.buck.step.fs.CopyStep;
@@ -69,6 +70,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
@@ -91,6 +93,7 @@ public class KotlincToJarStepFactory extends CompileToJarStepFactory implements 
   private static final String X_PLUGIN_ARG = "-Xplugin=";
   private static final String KAPT3_PLUGIN = "plugin:org.jetbrains.kotlin.kapt3:";
   private static final String AP_CLASSPATH_ARG = KAPT3_PLUGIN + "apclasspath=";
+  private static final String AP_PROCESSORS_ARG = KAPT3_PLUGIN + "processors=";
   // output path for generated sources;
   private static final String SOURCES_ARG = KAPT3_PLUGIN + "sources=";
   private static final String CLASSES_ARG = KAPT3_PLUGIN + "classes=";
@@ -203,6 +206,14 @@ public class KotlincToJarStepFactory extends CompileToJarStepFactory implements 
         ImmutableList<String> annotationProcessors =
             ImmutableList.copyOf(
                 javacOptions.getJavaAnnotationProcessorParams().getPluginProperties().stream()
+                    .map(ResolvedJavacPluginProperties::getProcessorNames)
+                    .flatMap(Set::stream)
+                    .map(name -> AP_PROCESSORS_ARG + name)
+                    .collect(Collectors.toList()));
+
+        ImmutableList<String> apClassPaths =
+            ImmutableList.copyOf(
+                javacOptions.getJavaAnnotationProcessorParams().getPluginProperties().stream()
                     .map(
                         resolvedJavacPluginProperties ->
                             resolvedJavacPluginProperties.getJavacPluginJsr199Fields(
@@ -218,6 +229,7 @@ public class KotlincToJarStepFactory extends CompileToJarStepFactory implements 
                     AP_CLASSPATH_ARG
                         + kotlinc.getAnnotationProcessorPath(buildContext.getSourcePathResolver()))
                 .add(AP_CLASSPATH_ARG + kotlinc.getStdlibPath(buildContext.getSourcePathResolver()))
+                .addAll(apClassPaths)
                 .addAll(annotationProcessors)
                 .add(SOURCES_ARG + projectFilesystem.resolve(sourcesOutput))
                 .add(CLASSES_ARG + projectFilesystem.resolve(classesOutput))
