f7be635f41e2dd0788daf3cd6972defa1d6eeab2
Mykola Semko <msemko@fb.com>
2020-03-24 13:20:29 -0700

    Simplification of NamedPipesTest
    Summary:
    Test simplification:
    Instead of writing and parsing a plain text use write/read json objects.
    
    Reviewed By: stepancheg
    
    shipit-source-id: 493e2ef328ad72d7a44905cc7823186e3533f507

--- test/com/facebook/buck/io/namedpipes/NamedPipesTest.java
+++ test/com/facebook/buck/io/namedpipes/NamedPipesTest.java
@@ -20,24 +20,21 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 import com.facebook.buck.core.util.log.Logger;
 import com.facebook.buck.util.environment.Platform;
-import com.google.common.base.Throwables;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import com.facebook.buck.util.json.ObjectMappers;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -48,46 +45,37 @@ public class NamedPipesTest {
 
   private static Logger LOG = Logger.get(NamedPipesTest.class);
 
-  /** used to signal end of the file for named pipe communication */
-  private static final String EOF = "EOF_MAGIC_TOKEN";
-
-  private static final String READ_MESSAGE_PREFIX = "receive time:";
-  private static final String WRITE_MESSAGE_PREFIX = "send time:";
+  private static final String DELIMITER = "EndOfObject";
 
   /**
    * Test creates a named pipe. Then writes to it 3 messages with 1 second delay. Reader executes as
    * a separate thread.
    *
-   * <p>End of file send as a separate magic token. In the end test verifies that message that were
-   * sent was received in about 100ms.
+   * <p>In the end test verifies that message that were sent was received in about 500ms.
    */
   @Test
-  public void testNamedPipes() throws IOException {
+  public void testNamedPipes() {
 
-    // TODO msemko@:
     Assume.assumeThat(Platform.detect(), is(not(Platform.WINDOWS)));
-
-    StringWriter stringWriter = new StringWriter();
 
     NamedPipeFactory namedPipeFactory = NamedPipeFactory.getFactory();
     ExecutorService executorService = Executors.newSingleThreadExecutor();
+    List<ReceivedNamedPipeJsonMessage> receivedMessages = new ArrayList<>();
     String namedPipePath = null;
     try (NamedPipe namedPipe = namedPipeFactory.create()) {
+      LOG.info("Named pipe created: " + namedPipe);
       namedPipePath = namedPipe.getName();
-      println(stringWriter, "Named pipe: " + namedPipePath);
-      executorService.execute(readFromNamedPipeRunnable(namedPipePath, stringWriter));
+      executorService.execute(readFromNamedPipeRunnable(namedPipePath, receivedMessages));
 
-      try (OutputStream outputStream = namedPipe.getOutputStream();
-          BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream))) {
-        println(stringWriter, "Starting write messages into a named pipe!");
-        writeToNamedPipe(writer, "Hello pipe reader!");
+      try (DataOutputStream outputStream = new DataOutputStream(namedPipe.getOutputStream())) {
+        LOG.info("Starting write messages into a named pipe!");
+        writeToNamedPipe(outputStream, "Hello pipe reader!");
         TimeUnit.SECONDS.sleep(1);
-        writeToNamedPipe(writer, "Hello again!");
+        writeToNamedPipe(outputStream, "Hello again!");
         TimeUnit.SECONDS.sleep(1);
-        writeToNamedPipe(writer, "Bye!");
-        writeToNamedPipe(writer, EOF, /* withPrefix */ false);
+        writeToNamedPipe(outputStream, "Bye!");
       } catch (IOException e) {
-        println(stringWriter, "Can't write into a named pipe: " + namedPipePath, e);
+        LOG.error(e, "Can't write into a named pipe: " + namedPipePath);
       } finally {
         executorService.shutdown();
         if (!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
@@ -95,113 +83,94 @@ public class NamedPipesTest {
         }
       }
     } catch (Exception e) {
-      println(stringWriter, "Can't create a named pipe.", e);
+      LOG.error(e, "Can't create a named pipe.");
     }
 
+    assertNotNull("Named pipe has not been created!", namedPipePath);
     assertFalse("Named pipe file has to be deleted!", Files.exists(Paths.get(namedPipePath)));
 
-    String output = stringWriter.toString();
-    LOG.info("Output: " + System.lineSeparator() + "---" + System.lineSeparator() + output);
+    assertEquals(
+        "Expected that reader was able to read 3 messages over named pipe!",
+        3,
+        receivedMessages.size());
+    for (ReceivedNamedPipeJsonMessage message : receivedMessages) {
+      Instant receivedInstant = message.getReceivedTime();
+      Instant sentInstant = Instant.parse(message.getMessage().getTimeSent());
 
-    int messages = 0;
-    try (Scanner scanner = new Scanner(output)) {
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        if (line.startsWith(READ_MESSAGE_PREFIX)) {
-          messages++;
-
-          line = line.substring(READ_MESSAGE_PREFIX.length());
-          String receiveTimestampString = line.substring(line.indexOf("[") + 1, line.indexOf("]"));
-          line = line.substring(line.indexOf(WRITE_MESSAGE_PREFIX) + WRITE_MESSAGE_PREFIX.length());
-          String sendTimestampString = line.substring(line.indexOf("[") + 1, line.indexOf("]"));
-
-          Instant receivedInstant = Instant.parse(receiveTimestampString);
-          Instant sentInstant = Instant.parse(sendTimestampString);
-
-          assertFalse(
-              "Receive timestamp: "
-                  + receiveTimestampString
-                  + "must be after or equals send timestamp: "
-                  + sendTimestampString,
-              receivedInstant.isBefore(sentInstant));
-          assertFalse(
-              "Receive timestamp: "
-                  + receiveTimestampString
-                  + "must be not more than 100ms different from the send timestamp: "
-                  + sendTimestampString,
-              receivedInstant.minus(Duration.ofMillis(100)).isAfter(sentInstant));
-        }
-      }
+      assertFalse(
+          "Receive timestamp: "
+              + receivedInstant
+              + " must be after or equals send timestamp: "
+              + sentInstant,
+          receivedInstant.isBefore(sentInstant));
+      assertFalse(
+          "Receive timestamp: "
+              + receivedInstant
+              + " must be not more than 500ms different from the send timestamp: "
+              + sentInstant,
+          receivedInstant.minus(Duration.ofMillis(500)).isAfter(sentInstant));
     }
-
-    assertEquals("Expected that reader was able to read 3 messages over named pipe!", 3, messages);
   }
 
-  private static Runnable readFromNamedPipeRunnable(String namedPipePath, Writer writer) {
-    return new Runnable() {
+  private static Runnable readFromNamedPipeRunnable(
+      String namedPipePath, List<ReceivedNamedPipeJsonMessage> receivedMessages) {
 
+    return new Runnable() {
       @Override
       public void run() {
-        try {
-          readFromNamedPath(namedPipePath);
+        NamedPipeFactory namedPipeFactory = NamedPipeFactory.getFactory();
+        try (NamedPipe namedPipe = namedPipeFactory.connect(Paths.get(namedPipePath));
+            InputStream inputStream = namedPipe.getInputStream()) {
+
+          LOG.info("Read named pipe: " + namedPipe);
+
+          String line;
+          int read;
+          StringBuilder sb = new StringBuilder();
+          while ((read = inputStream.read()) != -1) {
+            sb.append((char) read);
+            if (isEndOfTheObject(sb)) {
+              line = sb.toString();
+              sb.setLength(0);
+              handleReadLine(line, receivedMessages);
+            }
+          }
+
+          LOG.info("Finishing reader thread!");
         } catch (IOException e) {
-          LOG.error(e);
+          LOG.error(e, "Can't read from a named pipe: " + namedPipePath);
         }
       }
 
-      private void readFromNamedPath(String namedPipePath) throws IOException {
-        println(writer, "Read named pipe path: " + namedPipePath);
-
-        NamedPipeFactory namedPipeFactory = NamedPipeFactory.getFactory();
-        try (NamedPipe namedPipe = namedPipeFactory.connect(Paths.get(namedPipePath))) {
-          try (InputStream inputStream = namedPipe.getInputStream();
-              BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-              if (line.equals(EOF)) {
-                println(writer, "Pipe is closed!");
-                break;
-              }
-              handleReadLine(line);
-            }
-            println(writer, "Finishing reader thread!");
-          } catch (IOException e) {
-            println(writer, "Can't read from a named pipe: " + namedPipePath, e);
+      private boolean isEndOfTheObject(StringBuilder sb) {
+        int length = sb.length();
+        if (length < DELIMITER.length()) {
+          return false;
+        }
+        for (int i = 0; i < DELIMITER.length(); i++) {
+          if (DELIMITER.charAt(i) != sb.charAt(length - DELIMITER.length() + i)) {
+            return false;
           }
         }
+        return true;
       }
 
-      private void handleReadLine(String line) throws IOException {
-        println(writer, READ_MESSAGE_PREFIX + " [" + Instant.now() + "]\t" + line);
+      private void handleReadLine(String line, List<ReceivedNamedPipeJsonMessage> receivedMessages)
+          throws IOException {
+        NamedPipeJsonMessage jsonMessage =
+            ObjectMappers.readValue(line, NamedPipeJsonMessage.class);
+        LOG.info("json message received : " + jsonMessage);
+        receivedMessages.add(
+            ImmutableReceivedNamedPipeJsonMessage.ofImpl(Instant.now(), jsonMessage));
       }
     };
   }
 
-  private static void writeToNamedPipe(BufferedWriter writer, String message) throws IOException {
-    writeToNamedPipe(writer, message, true);
-  }
-
-  private static void writeToNamedPipe(BufferedWriter writer, String message, boolean withPrefix)
+  private static void writeToNamedPipe(DataOutputStream outputStream, String message)
       throws IOException {
-    if (withPrefix) {
-      writer.write(WRITE_MESSAGE_PREFIX + " [" + Instant.now() + "]\t");
-    }
-    writer.write(message);
-    writer.newLine();
-    writer.flush();
-  }
-
-  private static void println(Writer writer, String line) throws IOException {
-    println(writer, line, null);
-  }
-
-  private static void println(Writer writer, String line, Exception e) throws IOException {
-    writer.write(line);
-    writer.write(System.lineSeparator());
-    if (e != null) {
-      writer.write("cause:" + Throwables.getStackTraceAsString(e));
-      writer.write(System.lineSeparator());
-    }
+    NamedPipeJsonMessage namedPipeJsonMessage =
+        ImmutableNamedPipeJsonMessage.ofImpl(Instant.now().toString(), message);
+    ObjectMappers.WRITER.writeValue((DataOutput) outputStream, namedPipeJsonMessage);
+    outputStream.writeUTF(DELIMITER);
   }
 }
