fb88483c81c7c2e2b276c3c6ffa6580161b7d233
Milen Dzhumerov <mgd@fb.com>
2020-04-01 02:45:14 -0700

    Extract DefaultOnDiskInfo test utility
    Summary: We want to re-use the code that creates a DefaultOnDiskInfo in another test, so let's extract it into a `testutil` library.
    
    Reviewed By: cjhopman
    
    shipit-source-id: d4bcea5c891fdb9bbe21c21b3edd1b4427a56ca0

--- test/com/facebook/buck/core/build/engine/buildinfo/DefaultOnDiskBuildInfoIntegrationTest.java
+++ test/com/facebook/buck/core/build/engine/buildinfo/DefaultOnDiskBuildInfoIntegrationTest.java
@@ -19,22 +19,10 @@ package com.facebook.buck.core.build.engine.buildinfo;
 import static org.junit.Assert.assertEquals;
 
 import com.facebook.buck.core.build.engine.buildinfo.BuildInfo.MetadataKey;
-import com.facebook.buck.core.model.BuildId;
-import com.facebook.buck.core.model.BuildTarget;
-import com.facebook.buck.core.model.BuildTargetFactory;
-import com.facebook.buck.core.model.impl.BuildTargetPaths;
-import com.facebook.buck.io.filesystem.ProjectFilesystem;
-import com.facebook.buck.io.filesystem.TestProjectFilesystems;
 import com.facebook.buck.testutil.TemporaryPaths;
-import com.facebook.buck.util.json.ObjectMappers;
-import com.facebook.buck.util.timing.Clock;
-import com.facebook.buck.util.timing.FakeClock;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.ImmutableSortedSet;
 import java.io.IOException;
-import java.nio.file.Path;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -46,108 +34,28 @@ public class DefaultOnDiskBuildInfoIntegrationTest {
 
   public DefaultOnDiskBuildInfoIntegrationTest() {}
 
-  private ProjectFilesystem projectFilesystem;
-
-  private Path metadataDirectory;
-  private Path filePath;
-  private Path dirPath;
-  private Path subDir;
-  private Path emptySubDir;
-  private Path fileWithinDirPath;
-  private Path otherPathWithinDir;
-  private Path symlinkPath;
-  private Path fileViaSymlinkPath;
-
-  private DefaultOnDiskBuildInfo onDiskBuildInfo;
+  private DefaultOnDiskInfoPreparer preparer;
 
   @Before
   public void setUp() throws IOException {
-    BuildTarget buildTarget = BuildTargetFactory.newInstance("//foo/bar:baz");
-
-    projectFilesystem = TestProjectFilesystems.createProjectFilesystem(tmp.getRoot());
-
-    Clock clock = FakeClock.doNotCare();
-    BuildId buildId = new BuildId("cat");
-    BuildInfoStore buildInfoStore = new SQLiteBuildInfoStore(projectFilesystem);
-
-    BuildInfoRecorder recorder =
-        new BuildInfoRecorder(
-            buildTarget, projectFilesystem, buildInfoStore, clock, buildId, ImmutableMap.of());
-
-    recorder.addBuildMetadata("build_key0", "value0");
-    recorder.addBuildMetadata("build_key1", "value1");
-
-    recorder.addMetadata("artifact_key0", "value0");
-    recorder.addMetadata("artifact_key1", "value1");
-
-    filePath = BuildTargetPaths.getGenPath(projectFilesystem, buildTarget, "%s/some.file");
-    dirPath = BuildTargetPaths.getGenPath(projectFilesystem, buildTarget, "%s/some_dir");
-    subDir = dirPath.resolve("sub_dir");
-    emptySubDir = dirPath.resolve("empty_sub_dir");
-    fileWithinDirPath = subDir.resolve("some_inner.path");
-    otherPathWithinDir = subDir.resolve("other.file");
-    Path symlinkedDirPath =
-        BuildTargetPaths.getScratchPath(projectFilesystem, buildTarget, "%s/symlinked_dir");
-    symlinkPath = BuildTargetPaths.getGenPath(projectFilesystem, buildTarget, "%s/symlink");
-    Path fileInSymlinkedDirPath = symlinkedDirPath.resolve("file_in_symlink");
-    fileViaSymlinkPath = symlinkPath.resolve("file_in_symlink");
-
-    recorder.recordArtifact(filePath);
-    recorder.recordArtifact(dirPath);
-    recorder.recordArtifact(fileWithinDirPath);
-    recorder.recordArtifact(symlinkPath);
-
-    projectFilesystem.mkdirs(subDir);
-    projectFilesystem.mkdirs(emptySubDir);
-    projectFilesystem.createParentDirs(filePath);
-    projectFilesystem.writeContentsToPath("data0", filePath);
-    projectFilesystem.writeContentsToPath("data1", fileWithinDirPath);
-    projectFilesystem.writeContentsToPath("data2", otherPathWithinDir);
-    projectFilesystem.writeContentsToPath("other0", filePath.getParent().resolve("non.recorded"));
-
-    projectFilesystem.mkdirs(symlinkedDirPath);
-    projectFilesystem.createSymLink(
-        symlinkPath, projectFilesystem.resolve(symlinkedDirPath), false);
-    projectFilesystem.writeContentsToPath("data3", fileInSymlinkedDirPath);
-
-    recorder.addMetadata(
-        BuildInfo.MetadataKey.RECORDED_PATHS,
-        recorder.getRecordedPaths().stream()
-            .map(Object::toString)
-            .collect(ImmutableList.toImmutableList()));
-    recorder.addMetadata(
-        MetadataKey.RECORDED_PATH_HASHES,
-        ObjectMappers.WRITER.writeValueAsString(
-            ImmutableMap.of(otherPathWithinDir.toString(), "random-hash")));
-    ImmutableSortedSet<Path> recursivePaths =
-        DefaultOnDiskBuildInfo.getRecursivePaths(recorder.getRecordedPaths(), projectFilesystem);
-    long outputSize = DefaultOnDiskBuildInfo.getOutputSize(recursivePaths, projectFilesystem);
-    recorder.addMetadata(MetadataKey.OUTPUT_SIZE, Long.toString(outputSize));
-    recorder.addBuildMetadata(MetadataKey.ORIGIN_BUILD_ID, "build-id");
-
-    recorder.writeMetadataToDisk(true);
-
-    onDiskBuildInfo = new DefaultOnDiskBuildInfo(buildTarget, projectFilesystem, buildInfoStore);
-
-    metadataDirectory =
-        BuildInfo.getPathToArtifactMetadataDirectory(buildTarget, projectFilesystem);
+    preparer = new DefaultOnDiskInfoPreparer(tmp);
   }
 
   @Test
   public void testPathsAndMetadataForArtifactAreCorrect() throws IOException {
     assertEquals(
         ImmutableSortedSet.of(
-            metadataDirectory,
-            metadataDirectory.resolve(MetadataKey.ARTIFACT_METADATA),
-            filePath,
-            dirPath,
-            subDir,
-            emptySubDir,
-            fileWithinDirPath,
-            otherPathWithinDir,
-            symlinkPath,
-            fileViaSymlinkPath),
-        onDiskBuildInfo.getPathsForArtifact());
+            preparer.metadataDirectory,
+            preparer.metadataDirectory.resolve(MetadataKey.ARTIFACT_METADATA),
+            preparer.filePath,
+            preparer.dirPath,
+            preparer.subDir,
+            preparer.emptySubDir,
+            preparer.fileWithinDirPath,
+            preparer.otherPathWithinDir,
+            preparer.symlinkPath,
+            preparer.fileViaSymlinkPath),
+        preparer.onDiskBuildInfo.getPathsForArtifact());
     assertEquals(
         ImmutableSortedMap.of(
             BuildInfo.MetadataKey.ADDITIONAL_INFO,
@@ -158,12 +66,12 @@ public class DefaultOnDiskBuildInfoIntegrationTest {
             "value0",
             "build_key1",
             "value1"),
-        ImmutableSortedMap.copyOf(onDiskBuildInfo.getMetadataForArtifact()));
+        ImmutableSortedMap.copyOf(preparer.onDiskBuildInfo.getMetadataForArtifact()));
   }
 
   @Test
   public void testSuccessfulValidation() throws IOException {
-    onDiskBuildInfo.validateArtifact();
+    preparer.onDiskBuildInfo.validateArtifact();
   }
 
   @Test
@@ -172,9 +80,9 @@ public class DefaultOnDiskBuildInfoIntegrationTest {
     // We're specifically not deleting a path from RECORDED_PATHS because that will throw
     // an exception much earlier when trying to compute output size, using a path from
     // RECORDED_PATH_HASHES means we're testing for additional failures.
-    projectFilesystem.deleteFileAtPathIfExists(otherPathWithinDir);
+    preparer.projectFilesystem.deleteFileAtPathIfExists(preparer.otherPathWithinDir);
 
     exceptionRule.expect(IllegalStateException.class);
-    onDiskBuildInfo.validateArtifact();
+    preparer.onDiskBuildInfo.validateArtifact();
   }
 }
---
+++ test/com/facebook/buck/core/build/engine/buildinfo/DefaultOnDiskInfoPreparer.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.buck.core.build.engine.buildinfo;
+
+import com.facebook.buck.core.model.BuildId;
+import com.facebook.buck.core.model.BuildTarget;
+import com.facebook.buck.core.model.BuildTargetFactory;
+import com.facebook.buck.core.model.impl.BuildTargetPaths;
+import com.facebook.buck.io.filesystem.ProjectFilesystem;
+import com.facebook.buck.io.filesystem.TestProjectFilesystems;
+import com.facebook.buck.testutil.TemporaryPaths;
+import com.facebook.buck.util.json.ObjectMappers;
+import com.facebook.buck.util.timing.Clock;
+import com.facebook.buck.util.timing.FakeClock;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedSet;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class DefaultOnDiskInfoPreparer {
+  public ProjectFilesystem projectFilesystem;
+
+  public Path metadataDirectory;
+  public Path filePath;
+  public Path dirPath;
+  public Path subDir;
+  public Path emptySubDir;
+  public Path fileWithinDirPath;
+  public Path otherPathWithinDir;
+  public Path symlinkPath;
+  public Path fileViaSymlinkPath;
+
+  public DefaultOnDiskBuildInfo onDiskBuildInfo;
+
+  public DefaultOnDiskInfoPreparer(TemporaryPaths tmp) throws IOException {
+    BuildTarget buildTarget = BuildTargetFactory.newInstance("//foo/bar:baz");
+
+    projectFilesystem = TestProjectFilesystems.createProjectFilesystem(tmp.getRoot());
+
+    Clock clock = FakeClock.doNotCare();
+    BuildId buildId = new BuildId("cat");
+    BuildInfoStore buildInfoStore = new SQLiteBuildInfoStore(projectFilesystem);
+
+    BuildInfoRecorder recorder =
+        new BuildInfoRecorder(
+            buildTarget, projectFilesystem, buildInfoStore, clock, buildId, ImmutableMap.of());
+
+    recorder.addBuildMetadata("build_key0", "value0");
+    recorder.addBuildMetadata("build_key1", "value1");
+
+    recorder.addMetadata("artifact_key0", "value0");
+    recorder.addMetadata("artifact_key1", "value1");
+
+    filePath = BuildTargetPaths.getGenPath(projectFilesystem, buildTarget, "%s/some.file");
+    dirPath = BuildTargetPaths.getGenPath(projectFilesystem, buildTarget, "%s/some_dir");
+    subDir = dirPath.resolve("sub_dir");
+    emptySubDir = dirPath.resolve("empty_sub_dir");
+    fileWithinDirPath = subDir.resolve("some_inner.path");
+    otherPathWithinDir = subDir.resolve("other.file");
+    Path symlinkedDirPath =
+        BuildTargetPaths.getScratchPath(projectFilesystem, buildTarget, "%s/symlinked_dir");
+    symlinkPath = BuildTargetPaths.getGenPath(projectFilesystem, buildTarget, "%s/symlink");
+    Path fileInSymlinkedDirPath = symlinkedDirPath.resolve("file_in_symlink");
+    fileViaSymlinkPath = symlinkPath.resolve("file_in_symlink");
+
+    recorder.recordArtifact(filePath);
+    recorder.recordArtifact(dirPath);
+    recorder.recordArtifact(fileWithinDirPath);
+    recorder.recordArtifact(symlinkPath);
+
+    projectFilesystem.mkdirs(subDir);
+    projectFilesystem.mkdirs(emptySubDir);
+    projectFilesystem.createParentDirs(filePath);
+    projectFilesystem.writeContentsToPath("data0", filePath);
+    projectFilesystem.writeContentsToPath("data1", fileWithinDirPath);
+    projectFilesystem.writeContentsToPath("data2", otherPathWithinDir);
+    projectFilesystem.writeContentsToPath("other0", filePath.getParent().resolve("non.recorded"));
+
+    projectFilesystem.mkdirs(symlinkedDirPath);
+    projectFilesystem.createSymLink(
+        symlinkPath, projectFilesystem.resolve(symlinkedDirPath), false);
+    projectFilesystem.writeContentsToPath("data3", fileInSymlinkedDirPath);
+
+    recorder.addMetadata(
+        BuildInfo.MetadataKey.RECORDED_PATHS,
+        recorder.getRecordedPaths().stream()
+            .map(Object::toString)
+            .collect(ImmutableList.toImmutableList()));
+    recorder.addMetadata(
+        BuildInfo.MetadataKey.RECORDED_PATH_HASHES,
+        ObjectMappers.WRITER.writeValueAsString(
+            ImmutableMap.of(otherPathWithinDir.toString(), "random-hash")));
+    ImmutableSortedSet<Path> recursivePaths =
+        DefaultOnDiskBuildInfo.getRecursivePaths(recorder.getRecordedPaths(), projectFilesystem);
+    long outputSize = DefaultOnDiskBuildInfo.getOutputSize(recursivePaths, projectFilesystem);
+    recorder.addMetadata(BuildInfo.MetadataKey.OUTPUT_SIZE, Long.toString(outputSize));
+    recorder.addBuildMetadata(BuildInfo.MetadataKey.ORIGIN_BUILD_ID, "build-id");
+
+    recorder.writeMetadataToDisk(true);
+
+    onDiskBuildInfo = new DefaultOnDiskBuildInfo(buildTarget, projectFilesystem, buildInfoStore);
+
+    metadataDirectory =
+        BuildInfo.getPathToArtifactMetadataDirectory(buildTarget, projectFilesystem);
+  }
+}
