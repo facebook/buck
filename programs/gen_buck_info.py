86fe37e7e35dfa8ecc664f8235fd9b88fc16ce76
Jinlin Zhang <jinlinzhang@fb.com>
2020-05-13 16:34:51 -0700

    Pass release info to ant build

--- programs/gen_buck_info.py
+++ programs/gen_buck_info.py
@@ -52,7 +52,7 @@ def main(argv):
     candidate_paths = []
     vcs_module = None
     while vcs_module is None and os.path.dirname(path) != path:
-        while not os.path.exists(os.path.join(path, ".buckconfig")):
+        while not os.path.exists(os.path.join(path, ".buckconfig")) and os.path.dirname(path) != path:
             path = os.path.dirname(path)
         for vcs_dir, module in SUPPORTED_VCS.items():
             if os.path.exists(os.path.join(path, vcs_dir)):
