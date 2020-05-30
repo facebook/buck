b58ca57686e3e30ca370bd2ba3e7bb0e49f73258
Mykola Semko <msemko@fb.com>
2020-05-29 21:40:30 -0700

    AbstractIsolatedExecutionStep
    Summary: ^
    
    Reviewed By: irenewchen
    
    shipit-source-id: ee15fbd0e0960d7cbcf754be9501e309d6fe9c9f

---
+++ src/com/facebook/buck/step/isolatedsteps/AbstractIsolatedExecutionStep.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.buck.step.isolatedsteps;
+
+import com.facebook.buck.core.build.execution.context.IsolatedExecutionContext;
+import com.facebook.buck.step.StepExecutionResult;
+import java.io.IOException;
+
+/**
+ * Abstract implementation of {@link IsolatedStep} that takes the description as a constructor
+ * parameter and requires only the implementation of {@link
+ * #executeIsolatedStep(IsolatedExecutionContext)}. This facilitates the creation of an anonymous
+ * implementation of {@link IsolatedStep}.
+ */
+public abstract class AbstractIsolatedExecutionStep extends IsolatedStep {
+
+  private final String description;
+
+  public AbstractIsolatedExecutionStep(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public abstract StepExecutionResult executeIsolatedStep(IsolatedExecutionContext context)
+      throws IOException, InterruptedException;
+
+  @Override
+  public String getShortName() {
+    return description;
+  }
+
+  @Override
+  public String getIsolatedStepDescription(IsolatedExecutionContext context) {
+    return description;
+  }
+}
