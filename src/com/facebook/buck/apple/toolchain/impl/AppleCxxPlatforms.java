296818e65364350f87798b881ef9c5e67c84b86b
Jinlin Zhang <jinlinzhang@fb.com>
2020-06-02 15:47:01 -0700

    Do not set the sdk_version for ld version > 520 (#2468)
    Summary:
    With the 11.4.1 Xcode toolchain, when warnings are treated as fatal (which some apps do), `ld` errors out on both the platform_version and sdk_version being set. https://reviews.llvm.org/rG25ce33a6e4f3b13732c0f851e68390dc2acb9123 shows that the platform is derived, and set after version 520.
    
    We can always expect the Xcode toolchain to contain `ld`, so if we get the version, for versions above 520, we can stop setting the `sdk_version`. I haven't been able to find when it became an error.
    
    ```
    ld: warning: -sdk_version and -platform_version are not compatible, ignoring -sdk_version
    ld: fatal warning(s) induced error (-fatal_warnings)
    ```
    
    LD versions:
    
    ```
    chatatap@chatatap-mac ~/fbsource> /Applications/Xcode_11.4.1_fb.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ld -v
    @(#)PROGRAM:ld  PROJECT:ld64-556.6
    BUILD 13:10:29 Apr  7 2020
    configured to support archs: armv6 armv7 armv7s arm64 arm64e arm64_32 i386 x86_64 x86_64h armv6m armv7k armv7m armv7em
    LTO support using: LLVM version 11.0.3, (clang-1103.0.32.59) (static support for 26, runtime is 26)
    TAPI support using: Apple TAPI version 11.0.0 (tapi-1100.0.11)
    
    chatatap@chatatap-mac ~/fbsource> /Applications/Xcode_11.3.1_fb.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ld -v
    @(#)PROGRAM:ld  PROJECT:ld64-530
    BUILD 18:57:17 Dec 13 2019
    configured to support archs: armv6 armv7 armv7s arm64 arm64e arm64_32 i386 x86_64 x86_64h armv6m armv7k armv7m armv7em
    LTO support using: LLVM version 11.0.0, (clang-1100.0.33.17) (static support for 23, runtime is 23)
    TAPI support using: Apple TAPI version 11.0.0 (tapi-1100.0.11)
    ```
    
    If we cannot get the `ld` version for whatever reason, we fall back to the default behavior of setting the `sdk_version`.
    
    Reviewed By: milend
    
    shipit-source-id: 6d3181c705c14c141ae902e7f414c870a9a26324
    
    Co-authored-by: Chatura Atapattu <chatatap@fb.com>

--- src/com/facebook/buck/apple/toolchain/impl/AppleCxxPlatforms.java
+++ src/com/facebook/buck/apple/toolchain/impl/AppleCxxPlatforms.java
@@ -61,6 +61,11 @@ import com.facebook.buck.rules.args.StringArg;
 import com.facebook.buck.swift.toolchain.SwiftPlatform;
 import com.facebook.buck.swift.toolchain.SwiftTargetTriple;
 import com.facebook.buck.swift.toolchain.impl.SwiftPlatformFactory;
+import com.facebook.buck.util.Console;
+import com.facebook.buck.util.DefaultProcessExecutor;
+import com.facebook.buck.util.MoreSuppliers;
+import com.facebook.buck.util.ProcessExecutor;
+import com.facebook.buck.util.ProcessExecutorParams;
 import com.facebook.buck.util.environment.Platform;
 import com.facebook.buck.util.stream.RichStream;
 import com.google.common.annotations.VisibleForTesting;
@@ -76,9 +81,12 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.text.ParseException;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
 import java.util.regex.Pattern;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
@@ -133,7 +141,7 @@ public class AppleCxxPlatforms {
     return appleCxxPlatformsBuilder.build();
   }
 
-  private static Tool getXcodeTool(
+  private static VersionedTool getXcodeTool(
       ProjectFilesystem filesystem,
       ImmutableList<Path> toolSearchPaths,
       XcodeToolFinder xcodeToolFinder,
@@ -183,15 +191,6 @@ public class AppleCxxPlatforms {
     }
 
     AppleConfig appleConfig = buckConfig.getView(AppleConfig.class);
-
-    ImmutableList.Builder<String> ldflagsBuilder = ImmutableList.builder();
-    ldflagsBuilder.addAll(Linkers.iXlinker("-sdk_version", targetSdk.getVersion()));
-    if (appleConfig.linkAllObjC()) {
-      ldflagsBuilder.addAll(Linkers.iXlinker("-ObjC"));
-    }
-    if (targetSdk.getApplePlatform().equals(ApplePlatform.WATCHOS)) {
-      ldflagsBuilder.addAll(Linkers.iXlinker("-bitcode_verify"));
-    }
 
     // Populate Xcode version keys from Xcode's own Info.plist if available.
     Optional<String> xcodeBuildVersion = Optional.empty();
@@ -423,7 +422,9 @@ public class AppleCxxPlatforms {
     LOG.debug(
         "Headers verification platform whitelist: %s", headerVerification.getPlatformWhitelist());
     ImmutableList<String> ldFlags =
-        ImmutableList.<String>builder().addAll(cflags).addAll(ldflagsBuilder.build()).build();
+        getLdFlags(targetSdk, filesystem, xcodeToolFinder, toolSearchPaths, appleConfig, version);
+    ImmutableList<String> combinedLdFlags =
+        ImmutableList.<String>builder().addAll(cflags).addAll(ldFlags).build();
     ImmutableList<Arg> cflagsArgs = ImmutableList.copyOf(StringArg.from(cflags));
 
     CxxPlatform cxxPlatform =
@@ -442,7 +443,7 @@ public class AppleCxxPlatforms {
                 new ConstantToolProvider(clangXxPath),
                 config.shouldCacheLinks(),
                 appleConfig.shouldLinkScrubConcurrently()),
-            StringArg.from(ldFlags),
+            StringArg.from(combinedLdFlags),
             ImmutableMultimap.of(),
             strip,
             ArchiverProvider.from(new BsdArchiver(ar)),
@@ -511,6 +512,30 @@ public class AppleCxxPlatforms {
         .setCodesignProvider(appleConfig.getCodesignProvider());
 
     return platformBuilder.build();
+  }
+
+  private static ImmutableList<String> getLdFlags(
+      AppleSdk targetSdk,
+      ProjectFilesystem filesystem,
+      XcodeToolFinder xcodeToolFinder,
+      ImmutableList<Path> toolSearchPaths,
+      AppleConfig appleConfig,
+      String version) {
+    ImmutableList.Builder<String> builder = ImmutableList.builder();
+
+    if (shouldSetSDKVersion(filesystem, xcodeToolFinder, toolSearchPaths, appleConfig, version)
+        .get()) {
+      builder.addAll(Linkers.iXlinker("-sdk_version", targetSdk.getVersion()));
+    }
+
+    if (appleConfig.linkAllObjC()) {
+      builder.addAll(Linkers.iXlinker("-ObjC"));
+    }
+    if (targetSdk.getApplePlatform().equals(ApplePlatform.WATCHOS)) {
+      builder.addAll(Linkers.iXlinker("-bitcode_verify"));
+    }
+
+    return builder.build();
   }
 
   private static Optional<SwiftPlatform> getSwiftPlatform(
@@ -603,6 +628,77 @@ public class AppleCxxPlatforms {
       throw new HumanReadableException("Cannot find tool %s in paths %s", tool, toolSearchPaths);
     }
     return result.get();
+  }
+
+  private static Supplier<Boolean> shouldSetSDKVersion(
+      ProjectFilesystem filesystem,
+      XcodeToolFinder xcodeToolFinder,
+      ImmutableList<Path> toolSearchPaths,
+      AppleConfig appleConfig,
+      String version) {
+    return MoreSuppliers.memoize(
+        () -> {
+          // If the Clang driver detects ld version at 520 or above, it will pass -platform_version,
+          // otherwise it will pass -<platform>_version_min. As -platform_version is incompatible
+          // with -sdk_version (which Buck passes), we should only be passing -sdk_version if we
+          // believe the driver will not pass it.
+          // https://reviews.llvm.org/rG25ce33a6e4f3b13732c0f851e68390dc2acb9123
+          Optional<Double> ldVersion =
+              getLdVersion(filesystem, xcodeToolFinder, toolSearchPaths, appleConfig, version);
+          if (ldVersion.isPresent()) {
+            return ldVersion.get() < 520;
+          }
+          return true;
+        });
+  }
+
+  private static Optional<Double> getLdVersion(
+      ProjectFilesystem filesystem,
+      XcodeToolFinder xcodeToolFinder,
+      ImmutableList<Path> toolSearchPaths,
+      AppleConfig appleConfig,
+      String version) {
+
+    VersionedTool ld =
+        getXcodeTool(filesystem, toolSearchPaths, xcodeToolFinder, appleConfig, "ld", version);
+
+    ProcessExecutor executor = new DefaultProcessExecutor(Console.createNullConsole());
+    ProcessExecutorParams processExecutorParams =
+        ProcessExecutorParams.builder()
+            .setCommand(ImmutableList.of(ld.getPath().toString(), "-v"))
+            .build();
+    Set<ProcessExecutor.Option> options = EnumSet.of(ProcessExecutor.Option.EXPECTING_STD_OUT);
+    ProcessExecutor.Result result;
+    try {
+      result =
+          executor.launchAndExecute(
+              processExecutorParams,
+              options,
+              /* stdin */ Optional.empty(),
+              /* timeOutMs */ Optional.empty(),
+              /* timeOutHandler */ Optional.empty());
+    } catch (InterruptedException | IOException e) {
+      LOG.debug("Could not execute ld -v, continuing with setting the sdk_version.");
+      return Optional.empty();
+    }
+
+    if (result.getExitCode() != 0) {
+      throw new RuntimeException(
+          result.getMessageForUnexpectedResult(ld.getPath().toString() + " -v"));
+    }
+
+    Double parsedVersion = 0.0;
+    try {
+      // We expect the version string to be of the form "@(#)PROGRAM:ld  PROJECT:ld64-556.6"
+      String versionStr = result.getStderr().get().split("\n")[0];
+      parsedVersion = Double.parseDouble(versionStr.split("ld64-")[1]);
+    } catch (Exception e) {
+      LOG.debug(
+          "Unable to parse the ld version from %s, continuing with setting the sdk_version.",
+          result.getStderr().get());
+    }
+
+    return Optional.of(parsedVersion);
   }
 
   @VisibleForTesting
