28184afad63b5f29ed1aa06e87fbdef1bf860a07
Lucas Marçal <lucas.fernandes@easynvest.com.br>
2020-08-08 18:41:37 -0300

    Add region and language option to launch and test actions in xcode_workspace_config

--- src/com/facebook/buck/features/apple/common/XcodeWorkspaceConfigDescription.java
+++ src/com/facebook/buck/features/apple/common/XcodeWorkspaceConfigDescription.java
@@ -100,6 +100,10 @@ public class XcodeWorkspaceConfigDescription
     Optional<ImmutableMap<SchemeActionType, ImmutableMap<String, String>>>
         getEnvironmentVariables();
 
+    Optional<ImmutableMap<String, String>> getApplicationLanguage();
+
+    Optional<ImmutableMap<String, String>> getApplicationRegion();
+
     Optional<ImmutableMap<SchemeActionType, BuildTarget>> getExpandVariablesBasedOn();
 
     /**
