1a2544f8f3f8a07a7dc644d0a531c4a6dbe54712
Bill Ye Zheng <billzheng@fb.com>
2020-10-24 09:25:51 -0700

    Run Configuration for Buck Install
    Reviewed By: cute-jumper
    
    fbshipit-source-id: 918dfbb887f22a9ca8b8417252fd331549f826d9

---
+++ tools/ideabuck/src/com/facebook/buck/intellij/ideabuck/configurations/BuckInstallConfigurationType.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.buck.intellij.ideabuck.configurations;
+
+import com.facebook.buck.intellij.ideabuck.icons.BuckIcons;
+import com.intellij.execution.configuration.ConfigurationFactoryEx;
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.ConfigurationType;
+import com.intellij.execution.configurations.ConfigurationTypeUtil;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.project.Project;
+import javax.swing.Icon;
+import org.jetbrains.annotations.NotNull;
+
+public class BuckInstallConfigurationType implements ConfigurationType {
+  public static BuckInstallConfigurationType getInstance() {
+    return ConfigurationTypeUtil.findConfigurationType(BuckInstallConfigurationType.class);
+  }
+
+  private static final String ID = "Buck install";
+
+  private final ConfigurationFactoryEx myFactory;
+
+  public BuckInstallConfigurationType() {
+    myFactory =
+        new ConfigurationFactoryEx(this) {
+          @Override
+          @NotNull
+          public RunConfiguration createTemplateConfiguration(@NotNull Project project) {
+            return new BuckInstallConfiguration(project, this, "");
+          }
+        };
+  }
+
+  @Override
+  public String getDisplayName() {
+    return ID;
+  }
+
+  @Override
+  public String getConfigurationTypeDescription() {
+    return "Runs Buck install on the target, with an option to specify which activity to start.";
+  }
+
+  @Override
+  public Icon getIcon() {
+    return BuckIcons.DEFAULT_BUCK_ICON;
+  }
+
+  @NotNull
+  @Override
+  public String getId() {
+    return ID;
+  }
+
+  @Override
+  public ConfigurationFactory[] getConfigurationFactories() {
+    return new ConfigurationFactory[] {myFactory};
+  }
+}
