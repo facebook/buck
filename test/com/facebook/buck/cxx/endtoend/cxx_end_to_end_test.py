8f3d5f91bcfe39534dcd875ba673317d6376ec54
Scott Cao <scottcao@fb.com>
2020-10-29 16:50:58 -0700

    Add pytest conftest.py
    Summary:
    Currently importing a fixture in a test case requires adding a linter comment because the linter doesn't think the import is used.
    However, pytest always checks conftest.py for fixtures and automatically imports those in a test case.
    Import pytest fixtures in conftest.py so that they no longer require imports.
    Expose conftest.py as a resource in the associated test target.
    
    Reviewed By: bobyangyf
    
    fbshipit-source-id: 84aaffb39a0aad5b0e4893a19b5243f9a60249c2

--- test/com/facebook/buck/cxx/endtoend/cxx_end_to_end_test.py
+++ test/com/facebook/buck/cxx/endtoend/cxx_end_to_end_test.py
@@ -14,11 +14,7 @@
 
 from xplat.build_infra.buck_api.buck_repo import BuckRepo
 from xplat.build_infra.buck_e2e import asserts
-from xplat.build_infra.buck_e2e.repo_workspace import (  # noqa F401
-    buck_test,
-    exec_path,
-    repo,
-)
+from xplat.build_infra.buck_e2e.repo_workspace import buck_test, exec_path
 
 
 # TODO: Add decorator to run this with and without buckd
