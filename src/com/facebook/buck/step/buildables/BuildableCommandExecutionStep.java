5f7c2dfb93a6ecdee76afb1bb78bbbbb703dc808
Mykola Semko <msemko@fb.com>
2021-03-03 21:37:23 -0800

    Log to error when external actions executed unsuccessfully.
    Summary:
    We are blind when external actions execution failed.
    
    The error message is just:
    
    ```
    Command failed with exit code 139.
    
    command: [<some_path>/third-party/build-tools/java/java11.sh, -cp, <some_path>/buck-out/tmp/resources/f2b20f4d88b28ef12422d24fdc1914f7/external_actions, com.facebook.buck.external.main.ExternalActionsExecutable, buck-out/tmp/buildable_command_5779931574159280370]
    ```
    
    This diff would change that. We would print into logs with error level if external action execution failed. Standard out and standard error outputs from the external process would be added into a log message.
    
    Reviewed By: stepancheg
    
    fbshipit-source-id: b045d9df5aced059815829eacf9299e414653da8

--- src/com/facebook/buck/step/buildables/BuildableCommandExecutionStep.java
+++ src/com/facebook/buck/step/buildables/BuildableCommandExecutionStep.java
@@ -18,6 +18,7 @@ package com.facebook.buck.step.buildables;
 
 import com.facebook.buck.core.build.execution.context.IsolatedExecutionContext;
 import com.facebook.buck.core.filesystems.AbsPath;
+import com.facebook.buck.core.util.log.Logger;
 import com.facebook.buck.downwardapi.processexecutor.DownwardApiProcessExecutor;
 import com.facebook.buck.external.constants.ExternalBinaryBuckConstants;
 import com.facebook.buck.io.filesystem.ProjectFilesystem;
@@ -25,16 +26,20 @@ import com.facebook.buck.rules.modern.model.BuildableCommand;
 import com.facebook.buck.step.Step;
 import com.facebook.buck.step.StepExecutionResult;
 import com.facebook.buck.step.isolatedsteps.IsolatedStep;
+import com.facebook.buck.util.ProcessExecutor;
+import com.facebook.buck.util.ProcessExecutor.Result;
 import com.facebook.buck.util.ProcessExecutorParams;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 import javax.annotation.Nullable;
 
 /**
@@ -45,6 +50,8 @@ import javax.annotation.Nullable;
  * com.facebook.buck.rules.modern.BuildableWithExternalAction}.
  */
 public class BuildableCommandExecutionStep extends IsolatedStep {
+
+  private static final Logger LOG = Logger.get(BuildableCommandExecutionStep.class);
 
   public static final String EXTERNAL_ACTIONS_PROPERTY_NAME = "buck.external_actions";
   private static final String EXTERNAL_ACTIONS_MAIN =
@@ -85,12 +92,33 @@ public class BuildableCommandExecutionStep extends IsolatedStep {
       throws IOException, InterruptedException {
     Path buildableCommandPath = writeBuildableCommandAndGetPath();
     DownwardApiProcessExecutor downwardApiProcessExecutor = context.getDownwardApiProcessExecutor();
-    return StepExecutionResult.of(
+    ProcessExecutorParams processExecutorParams =
+        ProcessExecutorParams.builder()
+            .addAllCommand(getCommand(buildableCommandPath))
+            .setEnvironment(getEnvs())
+            .build();
+
+    Result executionResult =
         downwardApiProcessExecutor.launchAndExecute(
-            ProcessExecutorParams.builder()
-                .addAllCommand(getCommand(buildableCommandPath))
-                .setEnvironment(getEnvs())
-                .build()));
+            processExecutorParams,
+            ImmutableSet.<ProcessExecutor.Option>builder()
+                .add(ProcessExecutor.Option.EXPECTING_STD_OUT)
+                .add(ProcessExecutor.Option.EXPECTING_STD_ERR)
+                .build(),
+            Optional.empty(),
+            Optional.empty(),
+            Optional.empty());
+
+    StepExecutionResult stepExecutionResult = StepExecutionResult.of(executionResult);
+    if (!stepExecutionResult.isSuccess()) {
+      LOG.error(
+          "External action: %s has been finished with exit code: %s%n Std out: %s%n Std err: %s%n",
+          executionResult.getCommand(),
+          executionResult.getExitCode(),
+          executionResult.getStdout(),
+          executionResult.getStderr());
+    }
+    return stepExecutionResult;
   }
 
   @Override
