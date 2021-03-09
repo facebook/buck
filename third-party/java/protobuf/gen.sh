3cd8de36fcd232027e9baccc12d85e3ddcfd1ea3
Mykola Semko <msemko@fb.com>
2021-03-09 12:31:01 -0800

    Update protobuf compilation gen.sh script
    Summary:
    Buck is no longer under git so removed git commands from the script.
    Instead of just compiling the modified files, let's remove and generate new files.
    This script invokes rarely, so we could afford that :)
    
    Added a verification step in the end of the scrips `
    buck build buck//src-gen/...` that would verify buck could build generated files.
    Usually when protoc version is different than what buck use it would verify the issues. (I've also added commented section on how to download and insteall required protoc version)
    
    Reviewed By: gmambro
    
    fbshipit-source-id: 764c9ddc3b43246ef3e4b9709c081a5d9565928c

--- third-party/java/protobuf/gen.sh
+++ third-party/java/protobuf/gen.sh
@@ -21,53 +21,72 @@
 set -e
 current_dir=$(pwd)
 
-IS_GIT=
-if git rev-parse --git-dir > /dev/null 2>&1; then
-    # This is a valid git repository
-
-    cd "$(git rev-parse --show-toplevel)"
-    IS_GIT=1
-else
-    # this is not a git repository
-
-    script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    # assume this script is in third-party/java/protobuf
-    cd "${script_dir}/../../.."
-fi
-
-# In a git repo we can use git ls-files which is faster and more
-# accurate than a find
 function ls_files_cmd {
     pattern="$1"
-    if [ -n "$IS_GIT" ]; then
-        git ls-files -c -o -- ${pattern}
-    else
-        find . -path "./${pattern}"
-    fi
+    find . -path "./${pattern}"
 }
-
-PROTOC="protoc"
-
-# Compile all proto files visible to git (cached and others).
-for f in $(ls_files_cmd src/**.proto); do
-   $PROTOC --java_out=src-gen/ --java_opt=annotate_code $f
-done
-
-# Remove metadata files.
-for f in $(ls_files_cmd src-gen/**/proto/*.meta); do
-   rm $f
-done
 
 # Add generated tag to make tooling correctly recognize them as generated.
 # Insert tag name into the command so that tooling does not mistaken this file with generated code.
-tag_name="generated"
-for f in $(ls_files_cmd src-gen/**/proto/*.java); do
-  # Do not use sed - the only portable version of prepending a line is awful.
-  temp_file=$(mktemp)
-  echo "// @$tag_name" > $temp_file
-  cat $f >> $temp_file
-  mv $temp_file $f
+function set_generated_tag {
+    tag_name="generated"
+    pattern="$1"
+    for f in $(ls_files_cmd "${pattern}"); do
+      # Do not use sed - the only portable version of prepending a line is awful.
+      temp_file=$(mktemp)
+      echo "// @$tag_name" > "$temp_file"
+      cat "$f" >> "$temp_file"
+      mv "$temp_file" "$f"
+    done
+}
+
+function remove_files {
+    pattern="$1"
+    for f in $(ls_files_cmd "${pattern}"); do
+       rm "$f"
+    done
+}
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# assume this script is in third-party/java/protobuf
+cd "${script_dir}/../../.."
+
+#----------------------------------------------
+#  Sync protoc version with library version used.
+#----------------------------------------------
+#   PROTOC_VERSION=3.7.0
+#   PROTOC_ZIP=protoc-${PROTOC_VERSION}-osx-x86_64.zip
+#   curl -OL "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/$PROTOC_ZIP"
+#   sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
+#   sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
+#   rm -f $PROTOC_ZIP
+#   sudo chown -R $(whoami) /usr/local/bin/protoc
+
+# default for macos
+OS="osx"
+PROTOC="protoc"
+
+
+# Compile all proto files.
+remove_files src-gen/**/proto/*.java
+remove_files src-gen/**/model/*.java
+for f in $(ls_files_cmd src/**/*.proto); do
+   $PROTOC \
+   --java_out=src-gen/ \
+   --java_opt=annotate_code \
+   --plugin=protoc-gen-grpc-java=third-party/java/grpc/protoc-gen-grpc-java-1.10.1-${OS}-x86_64.exe \
+   --grpc-java_out=src-gen/ \
+   "$f"
 done
+
+# Remove metadata files.
+remove_files src-gen/**/*.pb.meta
+
+set_generated_tag src-gen/**/proto/*.java
+set_generated_tag src-gen/**/model/*.java
+
+#Verify generated srcs are valid
+buck build buck//src-gen/...
 
 cd "$current_dir"
 set +e
