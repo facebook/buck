3b339f6c9c50825a769ca556b6454f94d1707762
Stiopa Koltsov <nga@fb.com>
2021-05-12 12:01:24 -0700

    Slightly faster dot call
    Summary:
    When evaluating expression like:
    
    ```
    x.append(1)
    ```
    
    `x.append` creates a new function object (`append` descriptor bound to `x` object), so caching of linking at call site only creates overhead.
    
    This diff skips caching (and linking) at call site if a function is a builtin.
    
    Reviewed By: aniketmathur
    
    fbshipit-source-id: 7993e285f35f010c05a401143351f785576e82f8

--- third-party/java/bazel/src/main/java/net/starlark/java/eval/BcDynCallSite.java
+++ third-party/java/bazel/src/main/java/net/starlark/java/eval/BcDynCallSite.java
@@ -2,6 +2,7 @@ package net.starlark.java.eval;
 
 import javax.annotation.Nullable;
 
+/** Call site for {@link BcInstr.Opcode#CALL} instruction. */
 class BcDynCallSite {
   private final StarlarkCallableLinkSig linkSig;
 
@@ -9,11 +10,25 @@ class BcDynCallSite {
     this.linkSig = linkSig;
   }
 
-  @Nullable
-  private StarlarkCallableLinked linkedCache;
+  @Nullable private StarlarkCallableLinked linkedCache;
 
-  Object call(StarlarkCallable callable, StarlarkThread thread, Object[] args, @Nullable Sequence<?> starArgs, @Nullable Dict<?, ?> kwargs)
+  Object call(
+      StarlarkCallable callable,
+      StarlarkThread thread,
+      Object[] args,
+      @Nullable Sequence<?> starArgs,
+      @Nullable Dict<?, ?> kwargs)
       throws InterruptedException, EvalException {
+    if (callable instanceof BuiltinFunction) {
+      // Skip caching for builtin functions for two reasons:
+      // * linking does not really link anything for builtin functions
+      // * when `CALL` instruction is used for builtin (as opposed to `CALL_LINKED`)
+      //     it is likely instance call like `x.append`, so a function is a fresh object,
+      //     and it will be cache miss anyway
+      BuiltinFunction builtinFunction = (BuiltinFunction) callable;
+      return builtinFunction.linkAndCall(linkSig, thread, args, starArgs, kwargs);
+    }
+
     StarlarkCallableLinked linked = this.linkedCache;
     if (linked == null || linked.orig != callable) {
       this.linkedCache = linked = callable.linkCall(linkSig);
