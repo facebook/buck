dc0b8836e5ab8f2381edd245cc100fe862392a80
Stiopa Koltsov <nga@fb.com>
2021-05-14 00:57:34 -0700

    Increase expression length in stack overflow test
    Summary:
    Starlark parser detects too large recursion and reports it as parser error instead of java stack overflow.
    
    `testParseFileStackOverflow` tests it.
    
    ovrsource linux jobs started to fail because this test fails with unexpected end of input error instead of expected stack overflow.
    
    Perhaps something changed in our servers configurations like limits or java version.
    
    Increase the expression size to make stack overflow again.
    
    Reviewed By: ndmitchell
    
    fbshipit-source-id: 658d5e10aa2faa6e84110ba62d92fc157d34e2cc

--- third-party/java/bazel/src/test/java/net/starlark/java/syntax/ParserTest.java
+++ third-party/java/bazel/src/test/java/net/starlark/java/syntax/ParserTest.java
@@ -1348,7 +1348,7 @@ public final class ParserTest {
 
   private static ParserInput veryDeepExpression() {
     StringBuilder s = new StringBuilder();
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 10000; i++) {
       s.append("{");
     }
     return ParserInput.fromString(s.toString(), "foo.star");
