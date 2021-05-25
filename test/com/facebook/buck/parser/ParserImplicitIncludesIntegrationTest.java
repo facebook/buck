d939398a41c0d8c380b8333f053d4208737a60a2
Stiopa Koltsov <nga@fb.com>
2021-05-24 20:10:26 -0700

    Implement a test for implicit includes
    Summary: Implement a test, but disable it for Starlark (enabled in the following diff D28654050).
    
    Reviewed By: bobyangyf
    
    fbshipit-source-id: 33aa1f7ab76b7575cd59d61ef7421dbed428f485

---
+++ test/com/facebook/buck/parser/ParserImplicitIncludesIntegrationTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.buck.parser;
+
+import com.facebook.buck.parser.api.Syntax;
+import com.facebook.buck.testutil.TemporaryPaths;
+import com.facebook.buck.testutil.integration.ProjectWorkspace;
+import com.facebook.buck.testutil.integration.TestDataHelper;
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import org.junit.Assume;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ParserImplicitIncludesIntegrationTest {
+  @Rule public TemporaryPaths temporaryFolder = new TemporaryPaths();
+
+  @Parameterized.Parameters(name = "{0}")
+  public static Collection<Object[]> getParsers() {
+    return ImmutableList.of(new Object[] {Syntax.PYTHON_DSL}, new Object[] {Syntax.SKYLARK});
+  }
+
+  @Parameterized.Parameter(value = 0)
+  public Syntax syntax;
+
+  @Test
+  public void smoke() throws Exception {
+    // TODO(nga): implicit includes are not implemented in Starlark
+    Assume.assumeTrue(syntax != Syntax.SKYLARK);
+
+    ProjectWorkspace workspace =
+        TestDataHelper.createProjectWorkspaceForScenario(
+            this, "parser_implicit_includes", temporaryFolder);
+    workspace.addBuckConfigLocalOption("parser", "default_build_file_syntax", syntax.name());
+    workspace.setUp();
+
+    workspace.runBuckBuild("//:foo").assertSuccess();
+  }
+}
