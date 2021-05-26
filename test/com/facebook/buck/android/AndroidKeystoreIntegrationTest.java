890a5fb3732edb8fe5bb0ff83d3ef84677da328b
Chris Tolliday <ctolliday@fb.com>
2021-05-26 15:59:36 -0700

    Expose keystore file and keystore properties file
    Summary: This enables using these files in genrules and not just native rules. Currently post processing steps that use these files create extra `export_files` for them, however there are a large number of targets that do not have matching `export_files` configured or they are out of sync with `keystore` rules.
    
    Reviewed By: longinoa
    
    fbshipit-source-id: e983ca8de60b82ff9644d26595fb61241903e9da

---
+++ test/com/facebook/buck/android/AndroidKeystoreIntegrationTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.buck.android;
+
+import static org.junit.Assert.assertEquals;
+
+import com.facebook.buck.testutil.TemporaryPaths;
+import com.facebook.buck.testutil.integration.ProjectWorkspace;
+import com.facebook.buck.testutil.integration.TestDataHelper;
+import java.io.IOException;
+import java.nio.file.Path;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AndroidKeystoreIntegrationTest {
+
+  @Rule public TemporaryPaths tmpFolder = new TemporaryPaths();
+  private ProjectWorkspace workspace;
+
+  @Before
+  public void setUp() throws IOException {
+    workspace =
+        TestDataHelper.createProjectWorkspaceForScenario(
+            new AndroidKeystoreIntegrationTest(), "android_project", tmpFolder);
+    workspace.setUp();
+  }
+
+  @Test
+  public void testKeystoreOutput() throws IOException {
+    Path output = workspace.buildAndReturnOutput("//keystores:copy_keystore");
+    String copyOutput = workspace.getFileContents(output);
+    String source = workspace.getFileContents("keystores/debug.keystore");
+    assertEquals(source, copyOutput);
+  }
+
+  @Test
+  public void testKeystorePropertiesOutput() throws IOException {
+    Path output = workspace.buildAndReturnOutput("//keystores:copy_keystore_properties");
+    String copyOutput = workspace.getFileContents(output);
+    String source = workspace.getFileContents("keystores/debug.keystore.properties");
+    assertEquals(source, copyOutput);
+  }
+}
