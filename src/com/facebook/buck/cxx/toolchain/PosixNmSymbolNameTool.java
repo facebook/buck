eda09941445fa47b3610272c5523e1163a6301dd
Andrew Gallagher <andrewjcg@fb.com>
2021-05-28 10:06:14 -0700

    Fix global symbols rule in cross-cell builds
    Summary:
    Form the output path to the global symbols file correctly when building
    in the non-root cell.
    
    Reviewed By: igorsugak
    
    fbshipit-source-id: 3027b56e35e3d3c63ad1334167e74cee2a1d8078

--- src/com/facebook/buck/cxx/toolchain/PosixNmSymbolNameTool.java
+++ src/com/facebook/buck/cxx/toolchain/PosixNmSymbolNameTool.java
@@ -20,6 +20,7 @@ import com.facebook.buck.core.build.buildable.context.BuildableContext;
 import com.facebook.buck.core.build.context.BuildContext;
 import com.facebook.buck.core.build.execution.context.IsolatedExecutionContext;
 import com.facebook.buck.core.filesystems.AbsPath;
+import com.facebook.buck.core.filesystems.ForwardRelPath;
 import com.facebook.buck.core.filesystems.RelPath;
 import com.facebook.buck.core.model.BuildTarget;
 import com.facebook.buck.core.model.TargetConfiguration;
@@ -181,7 +182,7 @@ public class PosixNmSymbolNameTool implements SymbolNameTool {
           new IsolatedStep() {
 
             private final AbsPath symsFile =
-                context.getBuildCellRootPath().resolve(output.getPath());
+                getProjectFilesystem().resolve(ForwardRelPath.ofPath(output.getPath()));
             private final ImmutableMap<String, String> env =
                 nm.getEnvironment(context.getSourcePathResolver());
             private final ImmutableList<String> cmd =
