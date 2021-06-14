edf046998fe3ad0e7ffc989bf4a4a0a1c34e7306
Stiopa Koltsov <nga@fb.com>
2021-06-14 14:07:12 -0700

    Remove windows-specific path relativization from HybridGlobberFactory
    Summary:
    This is some strange code.
    
    The code was added in D12944598, see the discussion in the [linked thread](https://fb.workplace.com/groups/askbuck/permalink/2168362386545698/). Function was added by someone who is no longer works with us.
    
    This code is not used now after previous refactorings (relativization is not needed anymore).
    
    I'm not sure what was the original problem, perhaps absolute either path passed to the function or hybrid globber root was not normalized properly.
    
    CC people who participated in the discussion of the original issue.
    
    Reviewed By: akrieger
    
    fbshipit-source-id: 6c6e1b9843aaa9c3c72b5f29093d7b08b8d2529d

--- src/com/facebook/buck/skylark/io/impl/HybridGlobberFactory.java
+++ src/com/facebook/buck/skylark/io/impl/HybridGlobberFactory.java
@@ -21,22 +21,14 @@ import com.facebook.buck.core.filesystems.ForwardRelPath;
 import com.facebook.buck.io.watchman.ProjectWatch;
 import com.facebook.buck.io.watchman.Watchman;
 import com.facebook.buck.io.watchman.WatchmanClient;
-import com.facebook.buck.io.watchman.WatchmanQuery;
-import com.facebook.buck.io.watchman.WatchmanQueryFailedException;
 import com.facebook.buck.skylark.io.Globber;
 import com.facebook.buck.skylark.io.GlobberFactory;
-import com.facebook.buck.util.environment.Platform;
-import com.facebook.buck.util.types.Either;
 import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 import javax.annotation.Nullable;
 
 /** Provides instances of {@link com.facebook.buck.skylark.io.impl.HybridGlobber}. */
 public class HybridGlobberFactory implements GlobberFactory {
-  private static final long TIMEOUT_NANOS = TimeUnit.SECONDS.toNanos(10);
-  private static final long WARN_TIMEOUT_NANOS = TimeUnit.SECONDS.toNanos(1);
 
   private final WatchmanClient watchmanClient;
   private final AbsPath projectRoot;
@@ -51,40 +43,6 @@ public class HybridGlobberFactory implements GlobberFactory {
     this.projectWatches = projectWatches;
   }
 
-  /** Keeps relevant results of watch-project operation */
-  private static class WatchProjectResult {
-    private final String watchRoot;
-    private final String relativePath;
-
-    WatchProjectResult(String watchRoot, String relativePath) {
-      this.watchRoot = watchRoot;
-      this.relativePath = relativePath;
-    }
-  }
-
-  /**
-   * Ask Watchman to get the final path for a file, following links (and ReparsePoints on Windows).
-   *
-   * @param filePath the path for which we want to get the final path.
-   * @return the inal path from Watchman for {@code filePath}, if Watchman is possible to calculate
-   *     and {@code Optional.empty()} otherwise.
-   * @throws IOException
-   * @throws InterruptedException
-   */
-  public Either<WatchProjectResult, WatchmanClient.Timeout> getWatchmanRelativizedFinalPath(
-      AbsPath filePath) throws IOException, InterruptedException, WatchmanQueryFailedException {
-    return watchmanClient
-        .queryWithTimeout(
-            TIMEOUT_NANOS, WARN_TIMEOUT_NANOS, WatchmanQuery.watchProject(filePath.toString()))
-        .mapLeft(
-            result -> {
-              String watchRoot = result.getWatch();
-              String relativePath = result.getRelativePath();
-              return new WatchProjectResult(
-                  Objects.requireNonNull(watchRoot), Objects.requireNonNull(relativePath));
-            });
-  }
-
   @Override
   public AbsPath getRoot() {
     return projectRoot;
@@ -92,38 +50,14 @@ public class HybridGlobberFactory implements GlobberFactory {
 
   @Override
   public Globber create(ForwardRelPath basePathRel) {
-    AbsPath basePath = projectRoot.resolve(basePathRel);
-    AbsPath cellPath = projectRoot;
+    final AbsPath basePath = projectRoot.resolve(basePathRel);
+    final AbsPath cellPath = projectRoot;
     String watchRoot = cellPath.toString();
     @Nullable ProjectWatch projectWatch = projectWatches.get(cellPath);
     if (projectWatch != null) {
       watchRoot = projectWatch.getWatchRoot();
     }
-    String relativeRoot = null;
-    try {
-      relativeRoot = cellPath.relativize(basePath).toString();
-    } catch (IllegalArgumentException e) {
-      if (Platform.detect() == Platform.WINDOWS) {
-        // It is possible that on Windows we have a base root that is going through a different
-        // drive (ReparsePoint-ed path).
-        // In such case the basePath and watchRoot could point to different drives. Relativization
-        // on different drives results in an exception.
-        // In such case, ask Watchman to relativize the ReparsePoint path to the project root.
-        // If exception is thrown while getting the final path from Watchman or relativizing the
-        // path, report the original exception.
-        try {
-          Either<WatchProjectResult, WatchmanClient.Timeout> watchmanRelativizedPaths =
-              getWatchmanRelativizedFinalPath(basePath);
-          if (watchmanRelativizedPaths.isLeft()) {
-            WatchProjectResult result = watchmanRelativizedPaths.getLeft();
-            watchRoot = result.watchRoot;
-            relativeRoot = result.relativePath;
-          }
-        } catch (IOException | InterruptedException | WatchmanQueryFailedException e1) {
-          throw e;
-        }
-      }
-    }
+    String relativeRoot = basePathRel.toString();
     return new HybridGlobber(
         NativeGlobber.create(basePath),
         WatchmanGlobber.create(watchmanClient, relativeRoot, watchRoot));
