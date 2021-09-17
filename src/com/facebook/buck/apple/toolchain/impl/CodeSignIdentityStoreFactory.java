512a8118698f827b75e5d977237766f8ef9e389c
Alexey Kozhevnikov <akozhevnikov@fb.com>
2021-09-17 04:23:37 -0700

    allow any code sign identities names
    Summary: there should be no constraint on identity name, remove complicated regex logic and make it more flexible
    
    Reviewed By: vitalii-topoliuk
    
    fbshipit-source-id: 643225252ef755e28755f24aad2b0e5736e17013

--- src/com/facebook/buck/apple/toolchain/impl/CodeSignIdentityStoreFactory.java
+++ src/com/facebook/buck/apple/toolchain/impl/CodeSignIdentityStoreFactory.java
@@ -44,8 +44,7 @@ public class CodeSignIdentityStoreFactory implements ToolchainFactory<CodeSignId
 
   // Parse the fingerprint and name, but don't match invalid certificates (revoked, expired, etc).
   private static final Pattern CODE_SIGN_IDENTITY_PATTERN =
-      Pattern.compile(
-          "([A-F0-9]{40}) \"((iPhone.*|Apple (Development|Distribution)|Developer ID Application:).*)\"(?!.*\\(CSSMERR_.*\\))");
+      Pattern.compile("([A-F0-9]{40}) \"(.+)\"(?!.*\\(CSSMERR_.*\\))");
 
   /**
    * Construct a store by asking the system keychain for all stored code sign identities.
