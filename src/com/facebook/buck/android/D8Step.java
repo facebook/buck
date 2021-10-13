9af214b83f247cc98159900a89d2fccee48a44ba
Ian Childs <ianc@fb.com>
2021-10-13 05:52:35 -0700

    Move D8Command construction to generic class
    Summary: Extracting out so that we can use it in a separate executable in the next diff.
    
    Reviewed By: jiawei-lyu
    
    fbshipit-source-id: 2fd4587b506761665344a8fa50a621d92c9e7dc1

--- src/com/facebook/buck/android/D8Step.java
+++ src/com/facebook/buck/android/D8Step.java
@@ -17,14 +17,10 @@
 package com.facebook.buck.android;
 
 import com.android.tools.r8.CompilationFailedException;
-import com.android.tools.r8.CompilationMode;
-import com.android.tools.r8.D8Command;
 import com.android.tools.r8.Diagnostic;
-import com.android.tools.r8.DiagnosticsHandler;
-import com.android.tools.r8.OutputMode;
 import com.android.tools.r8.utils.AbortException;
-import com.android.tools.r8.utils.InternalOptions;
 import com.facebook.buck.android.dex.D8Options;
+import com.facebook.buck.android.dex.D8Utils;
 import com.facebook.buck.android.toolchain.AndroidPlatformTarget;
 import com.facebook.buck.core.build.execution.context.IsolatedExecutionContext;
 import com.facebook.buck.event.ConsoleEvent;
@@ -36,19 +32,13 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
 /** Runs d8. */
@@ -137,65 +127,24 @@ public class D8Step extends IsolatedStep {
   }
 
   private int executeInProcess(IsolatedExecutionContext context) throws IOException {
-    D8DiagnosticsHandler diagnosticsHandler = new D8DiagnosticsHandler();
+    D8Utils.D8DiagnosticsHandler diagnosticsHandler = new D8Utils.D8DiagnosticsHandler();
     try {
-      Set<Path> inputs = new HashSet<>();
-      for (Path rawFile : filesToDex) {
-        Path toDex = filesystem.resolve(rawFile);
-        if (Files.isRegularFile(toDex)) {
-          inputs.add(toDex);
-        } else {
-          try (Stream<Path> paths = Files.walk(toDex)) {
-            paths.filter(path -> path.toFile().isFile()).forEach(inputs::add);
-          }
-        }
-      }
+      resourcesReferencedInCode =
+          D8Utils.runD8Command(
+              diagnosticsHandler,
+              outputDexFile,
+              filesToDex.stream().map(filesystem::resolve).collect(Collectors.toList()),
+              options,
+              primaryDexClassNamesPath,
+              androidPlatformTarget.getAndroidJar(),
+              classpathFiles == null
+                  ? null
+                  : classpathFiles.stream()
+                      .map(filesystem::getPathForRelativeExistingPath)
+                      .collect(Collectors.toList()),
+              bucketId,
+              minSdkVersion);
 
-      // D8 only outputs to dex if the output path is a directory. So we output to a temporary dir
-      // and move it over to the final location
-      boolean outputToDex = outputDexFile.getFileName().toString().endsWith(".dex");
-      Path output = outputToDex ? Files.createTempDirectory("buck-d8") : outputDexFile;
-
-      D8Command.Builder builder =
-          D8Command.builder(diagnosticsHandler)
-              .addProgramFiles(inputs)
-              .setIntermediate(options.contains(D8Options.INTERMEDIATE))
-              .addLibraryFiles(androidPlatformTarget.getAndroidJar())
-              .setMode(
-                  options.contains(D8Options.NO_OPTIMIZE)
-                      ? CompilationMode.DEBUG
-                      : CompilationMode.RELEASE)
-              .setOutput(output, OutputMode.DexIndexed)
-              .setDisableDesugaring(options.contains(D8Options.NO_DESUGAR))
-              .setInternalOptionsModifier(
-                  (InternalOptions opt) -> {
-                    opt.testing.forceJumboStringProcessing =
-                        options.contains(D8Options.FORCE_JUMBO);
-                  });
-
-      bucketId.ifPresent(builder::setBucketId);
-      minSdkVersion.ifPresent(builder::setMinApiLevel);
-      primaryDexClassNamesPath.ifPresent(builder::addMainDexListFiles);
-
-      if (classpathFiles != null && !classpathFiles.isEmpty()) {
-        // classpathFiles is needed only for D8 java 8 desugar
-        ImmutableSet.Builder<Path> absolutePaths = ImmutableSet.builder();
-        for (Path classpathFile : classpathFiles) {
-          absolutePaths.add(filesystem.getPathForRelativeExistingPath(classpathFile));
-        }
-        builder.addClasspathFiles(absolutePaths.build());
-      }
-      D8Command d8Command = builder.build();
-      com.android.tools.r8.D8.run(d8Command);
-
-      if (outputToDex) {
-        File[] outputs = output.toFile().listFiles();
-        if (outputs != null && (outputs.length > 0)) {
-          Files.move(outputs[0].toPath(), outputDexFile, StandardCopyOption.REPLACE_EXISTING);
-        }
-      }
-
-      resourcesReferencedInCode = d8Command.getDexItemFactory().computeReferencedResources();
       return SUCCESS_EXIT_CODE;
     } catch (CompilationFailedException e) {
       if (isOverloadedDexException(e)) {
@@ -222,7 +171,7 @@ public class D8Step extends IsolatedStep {
   }
 
   private void postCompilationFailureToConsole(
-      IsolatedExecutionContext context, D8DiagnosticsHandler diagnosticsHandler) {
+      IsolatedExecutionContext context, D8Utils.D8DiagnosticsHandler diagnosticsHandler) {
     context.postEvent(
         ConsoleEvent.severe(
             String.join(
@@ -290,19 +239,6 @@ public class D8Step extends IsolatedStep {
   @Nullable
   Collection<String> getResourcesReferencedInCode() {
     return resourcesReferencedInCode;
-  }
-
-  private static class D8DiagnosticsHandler implements DiagnosticsHandler {
-
-    private final List<Diagnostic> diagnostics = new ArrayList<>();
-
-    @Override
-    public void warning(Diagnostic warning) {
-      diagnostics.add(warning);
-    }
-
-    @Override
-    public void info(Diagnostic info) {}
   }
 
   public Path getOutputDexFile() {
---
+++ src/com/facebook/buck/android/dex/D8Utils.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.buck.android.dex;
+
+import com.android.tools.r8.CompilationFailedException;
+import com.android.tools.r8.CompilationMode;
+import com.android.tools.r8.D8Command;
+import com.android.tools.r8.Diagnostic;
+import com.android.tools.r8.DiagnosticsHandler;
+import com.android.tools.r8.OutputMode;
+import com.android.tools.r8.utils.InternalOptions;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/** Runs d8. */
+public class D8Utils {
+
+  public static Collection<String> runD8Command(
+      D8DiagnosticsHandler diagnosticsHandler,
+      Path outputDexFile,
+      Iterable<Path> filesToDex,
+      Set<D8Options> options,
+      Optional<Path> primaryDexClassNamesPath,
+      Path androidJarPath,
+      Collection<Path> classpathFiles,
+      Optional<String> bucketId,
+      Optional<Integer> minSdkVersion)
+      throws CompilationFailedException, IOException {
+    Set<Path> inputs = new HashSet<>();
+    for (Path toDex : filesToDex) {
+      if (Files.isRegularFile(toDex)) {
+        inputs.add(toDex);
+      } else {
+        try (Stream<Path> paths = Files.walk(toDex)) {
+          paths.filter(path -> path.toFile().isFile()).forEach(inputs::add);
+        }
+      }
+    }
+
+    // D8 only outputs to dex if the output path is a directory. So we output to a temporary dir
+    // and move it over to the final location
+    boolean outputToDex = outputDexFile.getFileName().toString().endsWith(".dex");
+    Path output = outputToDex ? Files.createTempDirectory("buck-d8") : outputDexFile;
+
+    D8Command.Builder builder =
+        D8Command.builder(diagnosticsHandler)
+            .addProgramFiles(inputs)
+            .setIntermediate(options.contains(D8Options.INTERMEDIATE))
+            .addLibraryFiles(androidJarPath)
+            .setMode(
+                options.contains(D8Options.NO_OPTIMIZE)
+                    ? CompilationMode.DEBUG
+                    : CompilationMode.RELEASE)
+            .setOutput(output, OutputMode.DexIndexed)
+            .setDisableDesugaring(options.contains(D8Options.NO_DESUGAR))
+            .setInternalOptionsModifier(
+                (InternalOptions opt) -> {
+                  opt.testing.forceJumboStringProcessing = options.contains(D8Options.FORCE_JUMBO);
+                });
+
+    bucketId.ifPresent(builder::setBucketId);
+    minSdkVersion.ifPresent(builder::setMinApiLevel);
+    primaryDexClassNamesPath.ifPresent(builder::addMainDexListFiles);
+
+    if (classpathFiles != null) {
+      // classpathFiles is needed only for D8 Java 8 desugar
+      builder.addClasspathFiles(classpathFiles);
+    }
+
+    D8Command d8Command = builder.build();
+    com.android.tools.r8.D8.run(d8Command);
+
+    if (outputToDex) {
+      File[] outputs = output.toFile().listFiles();
+      if (outputs != null && (outputs.length > 0)) {
+        Files.move(outputs[0].toPath(), outputDexFile, StandardCopyOption.REPLACE_EXISTING);
+      }
+    }
+
+    return d8Command.getDexItemFactory().computeReferencedResources();
+  }
+
+  public static class D8DiagnosticsHandler implements DiagnosticsHandler {
+
+    public final List<Diagnostic> diagnostics = new ArrayList<>();
+
+    @Override
+    public void warning(Diagnostic warning) {
+      diagnostics.add(warning);
+    }
+
+    @Override
+    public void info(Diagnostic info) {}
+  }
+}
