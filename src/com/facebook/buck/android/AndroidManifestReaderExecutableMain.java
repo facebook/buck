b1bb06b19edfbde3fd9b9ab4891a3b807a38ebfc
Ian Childs <ianc@fb.com>
2021-10-26 02:41:21 -0700

    Move AndroidManifestReaderExecutableMain to use args4j
    Summary: Much nicer interface.
    
    Reviewed By: cjhopman, mykola-semko
    
    fbshipit-source-id: 8d4f54c54765447ab14d769c1f75defd91092c7c

--- src/com/facebook/buck/android/AndroidManifestReaderExecutableMain.java
+++ src/com/facebook/buck/android/AndroidManifestReaderExecutableMain.java
@@ -19,31 +19,42 @@ package com.facebook.buck.android;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.logging.Logger; // NOPMD
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
-/**
- * Main entry point for executing {@link AndroidManifestReader} calls.
- *
- * <p>Expected usage: {@code this_binary <command> <android_manifest_path> <output_path}.
- */
+/** Main entry point for executing {@link AndroidManifestReader} calls. */
 public class AndroidManifestReaderExecutableMain {
 
-  private static final Logger LOG =
-      Logger.getLogger(AndroidManifestReaderExecutableMain.class.getName());
+  @Option(name = "--manifest-path", required = true)
+  private String manifestPath;
+
+  @Option(name = "--command", required = true)
+  private String command;
+
+  @Option(name = "--output-path", required = true)
+  private String outputPath;
 
   public static void main(String[] args) throws IOException {
-    if (args.length != 3) {
-      LOG.severe("Must specify a command, a path to the Android Manifest, and an output path");
+    AndroidManifestReaderExecutableMain main = new AndroidManifestReaderExecutableMain();
+    CmdLineParser parser = new CmdLineParser(main);
+    try {
+      parser.parseArgument(args);
+      main.run();
+      System.exit(0);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
       System.exit(1);
     }
+  }
 
-    String manifestPath = args[1];
+  private void run() throws IOException {
     AndroidManifestReader androidManifestReader =
         DefaultAndroidManifestReader.forPath(Paths.get(manifestPath));
 
-    String command = args[0];
     if (command.equals("get_package")) {
-      Files.write(Paths.get(args[2]), androidManifestReader.getPackage().getBytes());
+      Files.write(Paths.get(outputPath), androidManifestReader.getPackage().getBytes());
     } else {
       throw new IllegalArgumentException("Unknown command: " + command);
     }
