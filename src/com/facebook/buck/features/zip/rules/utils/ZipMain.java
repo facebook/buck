88e981fb708a6a2d27f4ad599277388eca96a806
Mykola Semko <msemko@fb.com>
2021-11-02 01:24:37 -0700

    ZipMain to arg4j
    Summary: ^
    
    Reviewed By: IanChilds
    
    fbshipit-source-id: 862d1ba9624520d43fa597716f38378d45f1fe2c

--- src/com/facebook/buck/features/zip/rules/utils/ZipMain.java
+++ src/com/facebook/buck/features/zip/rules/utils/ZipMain.java
@@ -23,6 +23,7 @@ import com.facebook.buck.util.zip.collect.OnDuplicateEntry;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -31,99 +32,99 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.StringArrayOptionHandler;
 
-/**
- * Main entry point for generating Zip files
- *
- * <p>Expected usage: {@code this_binary --output_path <output_path> --on_duplicate_entry
- * <on_duplicate_entry_value> --entries_file <file with (artifact + isSource flag) pairs>
- * --zip_sources <zip sources paths> --entries_to_exclude <entries_to_exclude patterns> } .
- */
+/** Main entry point for generating Zip files */
 public class ZipMain {
 
   private static final AbsPath CURRENT_DIRECTORY =
       AbsPath.of(Paths.get(".").normalize().toAbsolutePath());
 
-  /** Main entry point into ZipFile binary */
+  @Option(name = "--output_path", required = true)
+  private String outputPath;
+
+  @Option(name = "--on_duplicate_entry", required = true)
+  private OnDuplicateEntry onDuplicateEntry;
+
+  @Option(name = "--zip_sources", handler = StringArrayOptionHandler.class)
+  private List<String> zipSources;
+
+  @Option(name = "--entries_to_exclude", handler = StringArrayOptionHandler.class)
+  private List<String> entriesToExclude;
+
+  @Option(name = "--entries_file")
+  private File entriesFile;
+
   public static void main(String[] args) throws IOException {
-    RelPath outputPath = null;
-    OnDuplicateEntry onDuplicateEntry = null;
-    ImmutableMap<RelPath, RelPath> entryMap = ImmutableMap.of();
-    ImmutableList.Builder<RelPath> zipSources = ImmutableList.builder();
-    ImmutableSet.Builder<Pattern> entriesToExclude = ImmutableSet.builder();
+    ZipMain main = new ZipMain();
+    CmdLineParser parser = new CmdLineParser(main);
+    try {
+      parser.parseArgument(args);
+      main.run();
+      System.exit(0);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.exit(1);
+    }
+  }
 
-    for (int i = 0; i < args.length; ) {
-      String argName = args[i];
-      switch (argName) {
-        case "--output_path":
-          outputPath = RelPath.get(args[i + 1]);
-          i = i + 2;
-          break;
-
-        case "--on_duplicate_entry":
-          onDuplicateEntry = OnDuplicateEntry.valueOf(args[i + 1].toUpperCase());
-          i = i + 2;
-          break;
-
-        case "--zip_sources":
-          int zipSourcesIndex = i + 1;
-          while (zipSourcesIndex < args.length && !args[zipSourcesIndex].startsWith("--")) {
-            zipSources.add(RelPath.get(args[zipSourcesIndex++]));
-          }
-          i = zipSourcesIndex;
-          break;
-
-        case "--entries_to_exclude":
-          int entriesToExcludeIndex = i + 1;
-          while (entriesToExcludeIndex < args.length
-              && !args[entriesToExcludeIndex].startsWith("--")) {
-            entriesToExclude.add(Pattern.compile(args[entriesToExcludeIndex++]));
-          }
-          i = entriesToExcludeIndex;
-          break;
-
-        case "--entries_file":
-          AbsPath entriesFilePath = CURRENT_DIRECTORY.resolve(args[i + 1]);
-          List<String> entriesLines = Files.readAllLines(entriesFilePath.getPath());
-
-          Map<Path, AbsPath> map = new LinkedHashMap<>();
-          for (int lineIndex = 0; lineIndex < entriesLines.size(); lineIndex += 3) {
-            String entry = entriesLines.get(lineIndex);
-            AbsPath zipEntryAbsPath = CURRENT_DIRECTORY.resolve(entry);
-            RelPath shortPath = RelPath.get(entriesLines.get(lineIndex + 1));
-            boolean isSourceFlag = Boolean.parseBoolean(entriesLines.get(lineIndex + 2));
-
-            if (isSourceFlag) {
-              RelativePathMapUtils.addPathToRelativePathMap(
-                  CURRENT_DIRECTORY,
-                  map,
-                  CURRENT_DIRECTORY.getPath(),
-                  zipEntryAbsPath,
-                  shortPath.getPath());
-            } else {
-              RelativePathMapUtils.addPathToRelativePathMap(
-                  CURRENT_DIRECTORY,
-                  map,
-                  zipEntryAbsPath.getPath().getParent(),
-                  zipEntryAbsPath,
-                  zipEntryAbsPath.getFileName());
-            }
-          }
-          entryMap = ZipUtils.toRelPathEntryMap(ImmutableMap.copyOf(map), CURRENT_DIRECTORY);
-
-          i = i + 2;
-          break;
-
-        default:
-          throw new IllegalStateException("Unrecognized argument: " + argName);
+  /** Main entry point into ZipFile binary */
+  private void run() throws IOException {
+    RelPath outputPath = RelPath.get(this.outputPath);
+    ImmutableList.Builder<RelPath> zipSourcesBuilder = ImmutableList.builder();
+    if (zipSources != null) {
+      for (String zipSource : zipSources) {
+        zipSourcesBuilder.add(RelPath.get(zipSource));
       }
     }
+
+    ImmutableSet.Builder<Pattern> entriesToExcludeBuilder = ImmutableSet.builder();
+    if (entriesToExclude != null) {
+      for (String entryToExclude : entriesToExclude) {
+        entriesToExcludeBuilder.add(Pattern.compile(entryToExclude));
+      }
+    }
+
+    Map<Path, AbsPath> map = new LinkedHashMap<>();
+    if (entriesFile != null) {
+      AbsPath entriesFilePath = CURRENT_DIRECTORY.resolve(entriesFile.toPath());
+      List<String> entriesLines = Files.readAllLines(entriesFilePath.getPath());
+
+      for (int lineIndex = 0; lineIndex < entriesLines.size(); lineIndex += 3) {
+        String entry = entriesLines.get(lineIndex);
+        AbsPath zipEntryAbsPath = CURRENT_DIRECTORY.resolve(entry);
+        RelPath shortPath = RelPath.get(entriesLines.get(lineIndex + 1));
+        boolean isSourceFlag = Boolean.parseBoolean(entriesLines.get(lineIndex + 2));
+
+        if (isSourceFlag) {
+          RelativePathMapUtils.addPathToRelativePathMap(
+              CURRENT_DIRECTORY,
+              map,
+              CURRENT_DIRECTORY.getPath(),
+              zipEntryAbsPath,
+              shortPath.getPath());
+        } else {
+          RelativePathMapUtils.addPathToRelativePathMap(
+              CURRENT_DIRECTORY,
+              map,
+              zipEntryAbsPath.getPath().getParent(),
+              zipEntryAbsPath,
+              zipEntryAbsPath.getFileName());
+        }
+      }
+    }
+    ImmutableMap<RelPath, RelPath> entryMap =
+        ZipUtils.toRelPathEntryMap(ImmutableMap.copyOf(map), CURRENT_DIRECTORY);
 
     ZipUtils.createZipFile(
         CURRENT_DIRECTORY,
         entryMap,
-        zipSources.build(),
-        entriesToExclude.build(),
+        zipSourcesBuilder.build(),
+        entriesToExcludeBuilder.build(),
         onDuplicateEntry,
         outputPath);
 
