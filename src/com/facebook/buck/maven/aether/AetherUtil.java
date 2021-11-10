97f662346edf9f7055b2a6b5aeb5336ec3a90bd9
Mykola Semko <msemko@fb.com>
2021-11-10 02:35:23 -0800

    remove unused methods from AetherUtil
    Summary: ^
    
    Reviewed By: IanChilds
    
    fbshipit-source-id: a61afd344717996a1d5403be0899dff15204a69b

--- src/com/facebook/buck/maven/aether/AetherUtil.java
+++ src/com/facebook/buck/maven/aether/AetherUtil.java
@@ -16,23 +16,15 @@
 
 package com.facebook.buck.maven.aether;
 
-import static org.eclipse.aether.repository.RepositoryPolicy.CHECKSUM_POLICY_FAIL;
-
-import java.net.URL;
-import java.util.Optional;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.impl.DefaultServiceLocator;
-import org.eclipse.aether.repository.Authentication;
-import org.eclipse.aether.repository.RemoteRepository;
-import org.eclipse.aether.repository.RepositoryPolicy;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.spi.locator.ServiceLocator;
 import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
-import org.eclipse.aether.util.repository.AuthenticationBuilder;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.helpers.NOPLoggerFactory;
 
@@ -41,42 +33,6 @@ public class AetherUtil {
   public static final String CLASSIFIER_SOURCES = "sources";
 
   private AetherUtil() {}
-
-  public static RemoteRepository toRemoteRepository(
-      URL repoUrl, Optional<String> username, Optional<String> password) {
-    return toRemoteRepository(repoUrl.toString(), username, password);
-  }
-
-  public static RemoteRepository toRemoteRepository(
-      String repoUrl, Optional<String> username, Optional<String> password) {
-    RemoteRepository.Builder repo =
-        new RemoteRepository.Builder(null, "default", repoUrl)
-            .setPolicy(new RepositoryPolicy(true, null, CHECKSUM_POLICY_FAIL));
-
-    if (username.isPresent() && password.isPresent()) {
-      Authentication authentication =
-          new AuthenticationBuilder()
-              .addUsername(username.get())
-              .addPassword(password.get())
-              .build();
-      repo.setAuthentication(authentication);
-    }
-
-    return repo.build();
-  }
-
-  public static RemoteRepository toRemoteRepository(Repository repo) {
-    RemoteRepository.Builder builder =
-        new RemoteRepository.Builder(repo.getUrl(), "default", repo.getUrl())
-            .setPolicy(new RepositoryPolicy(true, null, CHECKSUM_POLICY_FAIL));
-
-    if (repo.user != null && repo.password != null) {
-      builder.setAuthentication(
-          new AuthenticationBuilder().addUsername(repo.user).addPassword(repo.password).build());
-    }
-
-    return builder.build();
-  }
 
   public static ServiceLocator initServiceLocator() {
     DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
