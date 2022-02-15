93daae42be469953c6f35867278d48c881449515
Saleh Dindar <salh@fb.com>
2022-02-14 18:33:34 -0800

    Update SQLite JDBC to 3.36.0.3 to get Apple Silicon support
    Summary: The latest version has support for Apple Silicon.
    
    Reviewed By: stepancheg
    
    fbshipit-source-id: 10b8c8afd77ce890904f9713511358dede20eda6

--- third-party/java/sqlite/fetch.sh
+++ third-party/java/sqlite/fetch.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-BIN_FILENAME="sqlite-jdbc-3.20.0.jar"
-BIN_URL="http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.20.0/sqlite-jdbc-3.20.0.jar"
-BIN_HASH="143b1c0a453c9a8f77be14209ea15391d1e0eb93348fcfabf03cc227b0edae73"
-SOURCE_FILENAME="sqlite-jdbc-3.20.0-sources.jar"
-SOURCE_URL="http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.20.0/sqlite-jdbc-3.20.0-sources.jar"
-SOURCE_HASH="db6a12de3990dac9eb5af4f97d01e160f0c875bdad88a5e28aee39467d915f4b"
+BIN_FILENAME="sqlite-jdbc-3.36.0.3.jar"
+BIN_URL="https://repo1.maven.org/maven2/org/xerial/sqlite-jdbc/3.36.0.3/sqlite-jdbc-3.36.0.3.jar"
+BIN_HASH="af3a3376391e186a0fed63ecd414b72a882bf452667b490a0be3abf85b637d3f"
+SOURCE_FILENAME="sqlite-jdbc-3.36.0.3-sources.jar"
+SOURCE_URL="https://repo1.maven.org/maven2/org/xerial/sqlite-jdbc/3.36.0.3/sqlite-jdbc-3.36.0.3-sources.jar"
+SOURCE_HASH="abeee9e28cdadbc5e4749f49745205a2295402bbcd43fe1ee9678db016dfcbf7"
 
-wget "$BIN_URL" -O "$BIN_FILENAME"
+curl "$BIN_URL" -o "$BIN_FILENAME"
 REAL_BIN_HASH=$(shasum -a 256 "$BIN_FILENAME" | awk {'print $1'})
 if [ "$REAL_BIN_HASH" != "$BIN_HASH" ]; then
   echo "File from ${BIN_URL} has hash ${REAL_BIN_HASH}, expected ${BIN_HASH}"
   exit 1
 fi
 
-wget "$SOURCE_URL" -O "$SOURCE_FILENAME"
+curl "$SOURCE_URL" -o "$SOURCE_FILENAME"
 REAL_SOURCE_HASH=$(shasum -a 256 "$SOURCE_FILENAME" | awk {'print $1'})
 if [ "$REAL_SOURCE_HASH" != "$SOURCE_HASH" ]; then
   echo "File from ${SOURCE_URL} has hash ${REAL_SOURCE_HASH}, expected ${SOURCE_HASH}"
