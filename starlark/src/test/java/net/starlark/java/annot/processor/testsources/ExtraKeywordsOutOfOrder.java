f1ffab8998152af25c8c8ff41fd36718f8d48b73
Neil Mitchell <ndmitchell@fb.com>
2022-03-15 12:37:10 -0700

    Update partial copyright headers
    Summary: Updated with a replace, because these are partial copyright headers which don't automatically update.
    
    Reviewed By: chatura-atapattu
    
    fbshipit-source-id: 3d8e6b270f373653d5a0a36bc63ce8df7521fe93

--- starlark/src/test/java/net/starlark/java/annot/processor/testsources/ExtraKeywordsOutOfOrder.java
+++ starlark/src/test/java/net/starlark/java/annot/processor/testsources/ExtraKeywordsOutOfOrder.java
@@ -1,5 +1,5 @@
 /*
- * Portions Copyright (c) Facebook, Inc. and its affiliates.
+ * Portions Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
