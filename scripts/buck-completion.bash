e8129a6a36c236391a5ace9e3688b3f0865922ca
Neil Mitchell <ndmitchell@fb.com>
2022-03-17 02:15:04 -0700

    Update copyright in the completions script
    Summary: The auto formatter was breaking the script, so separate the header into sections.
    
    Reviewed By: stepancheg
    
    fbshipit-source-id: 4ef5dc8d774d9f537f369015fa21c50e266b260e

--- scripts/buck-completion.bash
+++ scripts/buck-completion.bash
@@ -1,5 +1,17 @@
-# Copyright 2004-present Facebook. All Rights Reserved.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Usage:
 #
 # To enable tab-completion for buck commands in bash, run the following
@@ -283,7 +295,7 @@ function _buck_completion_try_file_or_folder() {
 }
 
 function _buck_completion_try_arg_file() {
-  path="$1"  
+  path="$1"
   local -a raw_files; raw_files=( $(compgen -A file -- "$path") )
   for p in "${raw_files[@]}"; do
     if [[ -d "$p" ]]; then
