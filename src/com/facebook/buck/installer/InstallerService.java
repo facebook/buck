b8ca86c4a3f57bc35ad89a86b0b36dd3341ddceb
Mykola Semko <msemko@fb.com>
2022-08-17 13:21:33 -0700

    buck install: call installer only after all files are received
    Summary: Wait till the last file ready request has been received before interact with the installer part (device or emulator)
    
    Reviewed By: IanChilds
    
    fbshipit-source-id: 8fb7c5eae545404e34456df1a62fa4a1e301310f

--- src/com/facebook/buck/installer/InstallerService.java
+++ src/com/facebook/buck/installer/InstallerService.java
@@ -26,22 +26,44 @@ import com.facebook.buck.install.model.ShutdownRequest;
 import com.facebook.buck.install.model.ShutdownResponse;
 import com.facebook.buck.util.types.Unit;
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.util.concurrent.SettableFuture;
 import io.grpc.stub.StreamObserver;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger; // NOPMD
+import java.util.stream.Collectors;
 
-/** Installer Service that implements {@code install.proto} */
+/**
+ * Installer Service that implements {@code install.proto}
+ *
+ * <p>The workflow:
+ *
+ * <ol>
+ *   <li>client (buck2) sends `install` request with install id and file map. It could be multiple
+ *       targets that would have different install id associated with them.
+ *   <li>client sends multiple `fileReady` requests (file data + install id)
+ *   <li>{@link InstallerService} tracks all received `fileReady` requests associated with the same
+ *       install id. When all files received then {@link InstallerService} calls {@link
+ *       InstallCommand} to actually install all received files for the specific install id into the
+ *       device/emulator.
+ *   <li>client sends `shutdownServer` when wants to stop interactions and asks {@link
+ *       InstallerService} to terminate.
+ * </ol>
+ */
 public class InstallerService extends InstallerGrpc.InstallerImplBase {
 
   private final InstallCommand installer;
   private final SettableFuture<Unit> installFinished;
   private final Logger logger;
-  private final Map<InstallId, Set<String>> installIdToFilesMap = new HashMap<>();
+  private final Map<InstallId, Map<String, Optional<Path>>> installIdToFilesMap = new HashMap<>();
 
   public InstallerService(
       InstallCommand installer, SettableFuture<Unit> installFinished, Logger logger) {
@@ -69,7 +91,12 @@ public class InstallerService extends InstallerGrpc.InstallerImplBase {
         String.format(
             "Received install id: %s to files map request info: %s",
             installId.getValue(), filesMap));
-    installIdToFilesMap.put(installId, filesMap.keySet());
+    synchronized (installIdToFilesMap) {
+      installIdToFilesMap.put(
+          installId,
+          filesMap.keySet().stream()
+              .collect(Collectors.toMap(Function.identity(), ignore -> Optional.empty())));
+    }
     return InstallResponse.newBuilder().setInstallId(installId.getValue()).build();
   }
 
@@ -93,16 +120,65 @@ public class InstallerService extends InstallerGrpc.InstallerImplBase {
             "Received artifact %s located at %s for install id: %s",
             name, path, installId.getValue()));
 
-    logger.info("Starting install for file name: " + name);
-    InstallResult installResult = installer.install(name, Paths.get(path));
-
     FileResponse.Builder fileResponseBuilder =
         FileResponse.newBuilder().setName(name).setPath(path).setInstallId(installId.getValue());
-    if (installResult.isError()) {
-      fileResponseBuilder.setErrorDetail(
-          ErrorDetail.newBuilder().setMessage(installResult.getErrorMessage()).build());
+
+    ImmutableMap<String, Path> installFilesMap;
+    synchronized (installIdToFilesMap) {
+      Map<String, Optional<Path>> filesMap = installIdToFilesMap.get(installId);
+      filesMap.put(name, Optional.of(Paths.get(path)));
+      installFilesMap = getFilesMapToInstall(filesMap);
     }
+
+    if (!installFilesMap.isEmpty()) {
+      InstallResult installResult = install(installId, installFilesMap);
+      if (installResult.isError()) {
+        fileResponseBuilder.setErrorDetail(
+            ErrorDetail.newBuilder().setMessage(installResult.getErrorMessage()).build());
+      }
+    }
+
     return fileResponseBuilder.build();
+  }
+
+  private ImmutableMap<String, Path> getFilesMapToInstall(Map<String, Optional<Path>> filesMap) {
+    ImmutableMap.Builder<String, Path> installFilesMapBuilder = ImmutableMap.builder();
+    boolean allFilesReceived = true;
+    for (Map.Entry<String, Optional<Path>> fileEntry : filesMap.entrySet()) {
+      String fileName = fileEntry.getKey();
+      Optional<Path> pathOptional = fileEntry.getValue();
+
+      if (pathOptional.isEmpty()) {
+        allFilesReceived = false;
+        break;
+      }
+      installFilesMapBuilder.put(fileName, pathOptional.get());
+    }
+
+    if (allFilesReceived) {
+      return installFilesMapBuilder.build();
+    }
+    return ImmutableMap.of();
+  }
+
+  private InstallResult install(InstallId installId, Map<String, Path> filesMap) {
+    logger.info(String.format("Starting install for install id: %s", installId.getValue()));
+
+    Set<String> errorMessages = new HashSet<>();
+    for (Map.Entry<String, Path> fileEntry : filesMap.entrySet()) {
+      String name = fileEntry.getKey();
+      Path path = fileEntry.getValue();
+      logger.info(String.format("Starting install for file name: %s and path: %s", name, path));
+      InstallResult installResult = installer.install(name, path);
+      if (installResult.isError()) {
+        errorMessages.add(installResult.getErrorMessage());
+      }
+    }
+
+    if (errorMessages.isEmpty()) {
+      return InstallResult.success();
+    }
+    return InstallResult.error(errorMessages.toString());
   }
 
   @Override
