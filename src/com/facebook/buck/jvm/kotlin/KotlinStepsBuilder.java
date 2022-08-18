140246c9f3cb80a0de2b184dcadeafd28c12ba65
Ashok Menon <ashokmenon@fb.com>
2022-08-18 08:21:10 -0700

    Introducing KotlinStepsBuilder
    Summary:
    This class is responsible for deserialising a build command into a list of
    steps (like `JavaStepsBuilder`).  This diff sets up the basics:
    
    - The start of a protocol buffer schema for KotlinCD.
    - The generated classes for serialisation/deserialisation.
    - The ability to create a CompileToJarStepsFactory from the command (currently
      unused).
    
    The diff doesn't add new functionality, only the stubs of classes/modules which
    will be filled by future diffs.
    
    Reviewed By: mykola-semko
    
    fbshipit-source-id: 06a6110171ea11a2448355f18375088b5c3d2729

---
+++ src/com/facebook/buck/jvm/kotlin/KotlinStepsBuilder.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.buck.jvm.kotlin;
+
+import com.facebook.buck.cd.model.kotlin.BaseCommandParams;
+import com.facebook.buck.cd.model.kotlin.BuildKotlinCommand;
+import com.facebook.buck.core.filesystems.AbsPath;
+import com.facebook.buck.jvm.cd.DefaultCompileStepsBuilderFactory;
+import com.facebook.buck.step.isolatedsteps.IsolatedStep;
+import com.facebook.buck.util.types.Pair;
+import com.google.common.collect.ImmutableList;
+
+/** Converts serialized protobuf commands into steps to compile Kotlin targets. */
+public class KotlinStepsBuilder {
+  private final ImmutableList<IsolatedStep> steps;
+  private final AbsPath ruleCellRoot;
+
+  public KotlinStepsBuilder(BuildKotlinCommand buildKotlinCommand) {
+    Pair<AbsPath, ImmutableList<IsolatedStep>> pair = buildSteps(buildKotlinCommand);
+    ruleCellRoot = pair.getFirst();
+    steps = pair.getSecond();
+  }
+
+  /** @return the steps corresponding to the protobuf command */
+  public ImmutableList<IsolatedStep> getSteps() {
+    return steps;
+  }
+
+  /** @return the rule cell root. */
+  public AbsPath getRuleCellRoot() {
+    return ruleCellRoot;
+  }
+
+  private Pair<AbsPath, ImmutableList<IsolatedStep>> buildSteps(
+      BuildKotlinCommand buildKotlinCommand) {
+    BaseCommandParams baseCommandParams = buildKotlinCommand.getBaseCommandParams();
+    DaemonKotlincToJarStepFactory kotlincToJarStepFactory =
+        new DaemonKotlincToJarStepFactory(
+            baseCommandParams.getHasAnnotationProcessing(), baseCommandParams.getWithDownwardApi());
+
+    /* DefaultCompileStepsBuilderFactory<KotlinExtraParams> stepsBuilderFactory = */
+    new DefaultCompileStepsBuilderFactory<>(kotlincToJarStepFactory);
+
+    return new Pair<>(null, null);
+  }
+}
