18f8a6552f24a9d1cdc867ff054791c96ba8586c
Mykola Semko <msemko@fb.com>
2022-08-18 15:01:07 -0700

    buck2 install: install files concurrently
    Summary:
    In my previous change I changed a way files are installed. We wait till all files received and only after send them into installer, but that caused issues as installer could be blocked and wait till all files received.
    
    The solution is to call installer concurrently with all received files.
    
    Reviewed By: lebentle
    
    fbshipit-source-id: e6a4955cb7be2510ab8500eb135a6a1fb445ddf3

--- src/com/facebook/buck/installer/InstallerService.java
+++ src/com/facebook/buck/installer/InstallerService.java
@@ -16,6 +16,8 @@
 
 package com.facebook.buck.installer;
 
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+
 import com.facebook.buck.install.model.ErrorDetail;
 import com.facebook.buck.install.model.FileReadyRequest;
 import com.facebook.buck.install.model.FileResponse;
@@ -24,9 +26,15 @@ import com.facebook.buck.install.model.InstallResponse;
 import com.facebook.buck.install.model.InstallerGrpc;
 import com.facebook.buck.install.model.ShutdownRequest;
 import com.facebook.buck.install.model.ShutdownResponse;
+import com.facebook.buck.util.concurrent.MostExecutors;
 import com.facebook.buck.util.types.Unit;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.SettableFuture;
 import io.grpc.stub.StreamObserver;
 import java.nio.file.Path;
@@ -36,6 +44,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger; // NOPMD
@@ -59,6 +71,18 @@ import java.util.stream.Collectors;
  * </ol>
  */
 public class InstallerService extends InstallerGrpc.InstallerImplBase {
+
+  private static final ThreadPoolExecutor THREAD_POOL =
+      new ThreadPoolExecutor(
+          0,
+          Integer.MAX_VALUE,
+          1,
+          TimeUnit.SECONDS,
+          new SynchronousQueue<>(),
+          new MostExecutors.NamedThreadFactory("Installer"));
+
+  private static final ListeningExecutorService LISTENING_EXECUTOR_SERVICE =
+      MoreExecutors.listeningDecorator(THREAD_POOL);
 
   private final InstallCommand installer;
   private final SettableFuture<Unit> installFinished;
@@ -111,7 +135,8 @@ public class InstallerService extends InstallerGrpc.InstallerImplBase {
     }
   }
 
-  private FileResponse handleFileReadyRequest(FileReadyRequest request) {
+  private FileResponse handleFileReadyRequest(FileReadyRequest request)
+      throws InterruptedException {
     InstallId installId = InstallId.of(request.getInstallId());
     String name = request.getName();
     String path = request.getPath();
@@ -161,24 +186,71 @@ public class InstallerService extends InstallerGrpc.InstallerImplBase {
     return ImmutableMap.of();
   }
 
-  private InstallResult install(InstallId installId, Map<String, Path> filesMap) {
+  private InstallResult install(InstallId installId, Map<String, Path> filesMap)
+      throws InterruptedException {
     logger.info(String.format("Starting install for install id: %s", installId.getValue()));
 
     Set<String> errorMessages = new HashSet<>();
+    CountDownLatch latch = new CountDownLatch(filesMap.size());
+
     for (Map.Entry<String, Path> fileEntry : filesMap.entrySet()) {
       String name = fileEntry.getKey();
       Path path = fileEntry.getValue();
-      logger.info(String.format("Starting install for file name: %s and path: %s", name, path));
-      InstallResult installResult = installer.install(name, path);
-      if (installResult.isError()) {
-        errorMessages.add(installResult.getErrorMessage());
-      }
+
+      ListenableFuture<InstallResult> future =
+          LISTENING_EXECUTOR_SERVICE.submit(() -> install(name, path));
+      Futures.addCallback(
+          future, getInstallResultCallback(latch, errorMessages, name, path), directExecutor());
     }
+
+    // wait for all install futures
+    latch.await();
 
     if (errorMessages.isEmpty()) {
       return InstallResult.success();
     }
     return InstallResult.error(errorMessages.toString());
+  }
+
+  private FutureCallback<InstallResult> getInstallResultCallback(
+      CountDownLatch latch, Set<String> errorMessages, String name, Path path) {
+    return new FutureCallback<>() {
+      @Override
+      public void onSuccess(InstallResult installResult) {
+        if (installResult.isError()) {
+          String errorMessage = installResult.getErrorMessage();
+          logger.info(
+              String.format(
+                  "Installation of file name: %s and path: %s failed with error: %s",
+                  name, path, errorMessage));
+          synchronized (errorMessages) {
+            errorMessages.add(errorMessage);
+          }
+        } else {
+          logger.info(
+              String.format("Installation of file name: %s and path: %s finished.", name, path));
+        }
+        latch.countDown();
+      }
+
+      @Override
+      public void onFailure(Throwable thrown) {
+        String stackTraceAsString = Throwables.getStackTraceAsString(thrown);
+        logger.info(
+            String.format(
+                "Installation of file name: %s and path: %s failed with error: %s",
+                name, path, stackTraceAsString));
+        synchronized (errorMessages) {
+          errorMessages.add(stackTraceAsString);
+        }
+        latch.countDown();
+      }
+    };
+  }
+
+  private InstallResult install(String name, Path path) {
+    logger.info(String.format("Starting install for file name: %s and path: %s", name, path));
+    return installer.install(name, path);
   }
 
   @Override
