b0bf7bb047bbc6c993343ebe71ec6d42721e1b37
Thomas Orozco <torozco@fb.com>
2022-09-07 02:49:15 -0700

    buck: fix a broken test
    Summary:
    Like it says in the title. Looks like `__loader__.get_data` returns bytes
    on Windows. Just open the file normally instead.
    
    Reviewed By: ndmitchell
    
    fbshipit-source-id: d47c1b1fe8ebd6929db004f0ccd4af013c130845

--- test/com/facebook/buck/features/python/testdata/python_binary/cxx_resources/main.py
+++ test/com/facebook/buck/features/python/testdata/python_binary/cxx_resources/main.py
@@ -1,9 +1,13 @@
 import os
+import sys
 
 want = "hello world!\n"
 
 got = __loader__.get_data(
     os.path.join(os.path.dirname(__file__), "__cxx_resources__", "blah", "foo.dat")
 )
+
+if sys.version_info >= (3,):
+    got = got.decode("utf-8")
 
 assert want == got, "expected {}, got {}".format(repr(want), repr(got))
